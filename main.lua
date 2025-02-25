7eecc31451c3524913a9b39be23e629886996ac5
Matthew Hopkins <matthop88@gmail.com>
2025-02-25 12:09:58 -0800

    Enclosed within love.load() function, complete with a section header and function header

--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
-require "tools/colorInspector"
+--------------------------------------------------------------
+--                     LOVE2D Functions                     --
+--------------------------------------------------------------
+
+-- Function Name: love.load()
+-- Called By:     LOVE2D application, once during start
+--------------------------------------------------------------
+function love.load()
+	require "tools/colorInspector"
+end
 
 --require "game/main"
