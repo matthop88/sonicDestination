e9a050cf7ffdce03b6e0913b9ee84fc6895da94f
Matthew Hopkins <matthop88@gmail.com>
2025-02-25 12:19:21 -0800

    Added more specific feature list including scrolling and zooming.

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -10,6 +10,16 @@
           an RGB description of the color both to
           the screen and to the text console.
 
+                           Features
+                           --------
+        * Image can be scrolled using the up, down,
+          left and right arrow keys.
+        * Image can be zoomed in using the 'z' key
+          and zoomed out using the 'a' key
+        * Clicking on any pixel of the image prints
+          an RGB description of the color both to
+          the screen and to the text console.
+
 --]]
 
 --------------------------------------------------------------
