8bb571a1fdf7b0fb0e887a516b21980f5be6aba4
Matthew Hopkins <matthop88@gmail.com>
2025-02-28 15:24:58 -0800

    Now multiplying by scale inside of draw() function

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -57,7 +57,7 @@ end
 -- Called By:     LOVE2D application, every single frame
 --------------------------------------------------------------
 function love.draw()
-    love.graphics.draw(IMAGE, x, y, 0, scale, scale)
+    love.graphics.draw(IMAGE, x * scale, y * scale, 0, scale, scale)
 end
 
 -- Function Name: love.update()
