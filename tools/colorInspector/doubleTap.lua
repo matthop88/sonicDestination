96d023e16205bf9eb1db66e8787aa9b6830cb504
Matthew Hopkins <matthop88@gmail.com>
2025-02-28 14:54:38 -0800

    Created doubleTap.lua with contents of DOUBLE-TAP section

---
+++ tools/colorInspector/doubleTap.lua
@@ -0,0 +1,21 @@
+DOUBLE_TAP_THRESHOLD = 0.2
+
+lastKeypressed       = nil
+lastKeypressedTime   = 0
+
+function isDoubleTap(key)
+    return lastKeypressed == key and getTimeElapsedSinceLastKeypress() < DOUBLE_TAP_THRESHOLD
+end
+
+function getTimeElapsedSinceLastKeypress()
+    return love.timer.getTime() - lastKeypressedTime
+end
+
+function isWithinDoubleTapThreshold()
+    return getTimeElapsedSinceLastKeypress() <= DOUBLE_TAP_THRESHOLD
+end
+
+function setLastKeypressed(key)
+    lastKeypressed     = key 
+    lastKeypressedTime = love.timer.getTime()
+end
