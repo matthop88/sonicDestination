d6c31b508302f357cb5a2c596b3f327948e2fc48
Matthew Hopkins <matthop88@gmail.com>
2025-02-28 16:06:10 -0800

    Deleted mousepressed and mousereleased functions

--- tools/colorInspector/zooming.lua
+++ tools/colorInspector/zooming.lua
@@ -17,30 +17,6 @@ function zoomFromCoordinates(dt, mx, my)
     y = y - (my / oldScale) + (my / scale)
 end   
 
-function love.mousepressed(mx, my)
-    -- Translate x, y so that point at (mx, my) is at upper left corner of screen
-    local leftX = 0
-    local topY  = 0
-
-    local dx = (mx - leftX) / scale
-    local dy = (my -  topY) / scale
-
-    x = x - dx
-    y = y - dy
-end
-
-function love.mousereleased(mx, my)
-    -- Translate x, y so that point at upper left corner of screen moves to (mx, my)
-    local leftX = 0
-    local topY  = 0
-
-    local dx = (mx - leftX) / scale
-    local dy = (my -  topY) / scale
-
-    x = x + dx
-    y = y + dy
-end
-
 function handleZoomKeypressed(key)
     if     key == "z" then zoomIn()
     elseif key == "a" then zoomOut()
