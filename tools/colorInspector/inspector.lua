613e267a4ffdbaf63ceffc7078d811ed58be9376
Matthew Hopkins <matthop88@gmail.com>
2025-03-04 13:52:01 -0800

    Removed cleanup code at end of drawReadout()

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -136,7 +136,4 @@ function drawReadout()
     local rAlign  = "center"
 
     love.graphics.printf(rMsg, rX, rY, rWidth, rAlign)
-
-    love.graphics.setColor(1, 1, 1)
-    -- XXX: This assumes that original color was white
 end
