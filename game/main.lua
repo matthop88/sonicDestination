1e9d92aa7ddab8c977ef93c6bcc83514ba11ec25
Matthew Hopkins <matthop88@gmail.com>
2025-03-05 14:08:22 -0800

    Made green color darker, because it's LOUD

--- game/main.lua
+++ game/main.lua
@@ -38,7 +38,7 @@ function love.draw()
     -- All drawing code goes in here
     -- Draw Workspace
     -- 1. Draw dark green background (a filled rectangle the size of the window)
-    love.graphics.setColor(0, 1, 0)
+    love.graphics.setColor(0, 0.45, 0)
     love.graphics.rectangle("fill", 0, 0, 1024, 768)
 end
 
