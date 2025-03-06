8d496ee2e0d3e0ce4fd04bb612e8b0fe1fda3f3c
Matthew Hopkins <matthop88@gmail.com>
2025-03-05 16:13:40 -0800

    Experimented with x at 300 and -300 to simulate viewpoint change

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -29,7 +29,7 @@
 IMAGE = love.graphics.newImage("resources/images/spriteSheets/sonic1.png")
         -- https://www.spriters-resource.com/sega_genesis_32x/sonicth1/sheet/21628/
 
-x = 0
+x = -300
 y = 0
 
 --------------------------------------------------------------
