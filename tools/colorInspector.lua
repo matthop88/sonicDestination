587517f8d6a4aa390c219dab863d9e105758ed18
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 10:35:44 -0800

    Added another level of abstraction; have handleLeft/Right/Up/DownKeypressed() functions

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -67,12 +67,10 @@ end
 -- Parameters:    key - text value of key pressed by the user
 --------------------------------------------------------------
 function love.keypressed(key)
-    if     key == "left"   then scrollLeft()
-    elseif key == "right"  then scrollRight()
-    elseif key == "up"     then scrollUp()
-    elseif key == "down"   then scrollDown()
-    elseif key == "return" then x = 0
-    elseif key == "space"  then x = WINDOW_WIDTH - IMAGE:getWidth()
+    if     key == "left"   then handleLeftKeypressed()
+    elseif key == "right"  then handleRightKeypressed()
+    elseif key == "up"     then handleUpKeypressed()
+    elseif key == "down"   then handleDownKeypressed()
     end
 end
 
@@ -94,15 +92,20 @@ end
 --                  Specialized Functions                   --
 --------------------------------------------------------------
 
-function scrollLeft()         xSpeed =  SCROLL_SPEED       end
-function scrollRight()        xSpeed = -SCROLL_SPEED       end
-function scrollUp()           ySpeed =  SCROLL_SPEED       end
-function scrollDown()         ySpeed = -SCROLL_SPEED       end
+function handleLeftKeypressed()  scrollLeft()                 end
+function handleRightKeypressed() scrollRight()                end
+function handleUpKeypressed()    scrollUp()                   end
+function handleDownKeypressed()  scrollDown()                 end
 
-function stopScrollingLeft()  xSpeed = math.min(0, xSpeed) end
-function stopScrollingRight() xSpeed = math.max(0, xSpeed) end
-function stopScrollingUp()    ySpeed = math.min(0, ySpeed) end
-function stopScrollingDown()  ySpeed = math.max(0, ySpeed) end
+function scrollLeft()            xSpeed =  SCROLL_SPEED       end
+function scrollRight()           xSpeed = -SCROLL_SPEED       end
+function scrollUp()              ySpeed =  SCROLL_SPEED       end
+function scrollDown()            ySpeed = -SCROLL_SPEED       end
+
+function stopScrollingLeft()     xSpeed = math.min(0, xSpeed) end
+function stopScrollingRight()    xSpeed = math.max(0, xSpeed) end
+function stopScrollingUp()       ySpeed = math.min(0, ySpeed) end
+function stopScrollingDown()     ySpeed = math.max(0, ySpeed) end
 
 function keepImageInBounds()
     x = math.min(0, math.max(x, WINDOW_WIDTH  - IMAGE:getWidth()))
