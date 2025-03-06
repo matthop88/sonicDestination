b5a07e4c6972723d2792c2ec67eccfe61c7728cd
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 13:36:50 -0800

    Reverted gag in code

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -12,9 +12,9 @@
 
                            Features
                            --------
-       [.] Image can be scrolled using the up, down,
+       [X] Image can be scrolled using the up, down,
            left and right arrow keys.
-       [ ] Image can be zoomed in using the 'z' key
+       [.] Image can be zoomed in using the 'z' key
            and zoomed out using the 'a' key
        [ ] Clicking on any pixel of the image prints
            an RGB description of the color both to
@@ -32,9 +32,6 @@ else
     IMAGE = love.graphics.newImage("resources/images/sadNoFileImage.png")
 end
 
-TIME_OVER_IMAGE             = love.graphics.newImage("resources/images/spriteSheets/timeOverSheet.png")
-TIME_OVER_QUAD              = love.graphics.newQuad(474, 431, 138, 16, TIME_OVER_IMAGE:getWidth(), TIME_OVER_IMAGE:getHeight())
-
 SCROLL_SPEED                = 400
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 DOUBLE_TAP_THRESHOLD        = 0.2
@@ -45,11 +42,6 @@ xSpeed, ySpeed              = 0, 0
 lastKeypressed              = nil
 lastKeypressedTime          = 0
 dashing                     = false
-
-showTimeOver                = false
-
-backgroundAlpha             = nil
-foregroundAlpha             = nil
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -67,19 +59,6 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 --------------------------------------------------------------
 function love.draw()
     love.graphics.draw(IMAGE, x, y)
-    if backgroundAlpha ~= nil then
-        love.graphics.setColor(0, 0, 0, backgroundAlpha)
-        love.graphics.rectangle("fill", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-        love.graphics.setColor(1, 1, 1)
-    end
-    if showTimeOver then
-        love.graphics.draw(TIME_OVER_IMAGE, TIME_OVER_QUAD, 55, 260, 0, 5, 5)
-    end
-    if foregroundAlpha ~= nil then
-        love.graphics.setColor(0, 0, 0, foregroundAlpha)
-        love.graphics.rectangle("fill", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-        love.graphics.setColor(1, 1, 1)
-    end
 end
 
 -- Function Name: love.update()
@@ -91,12 +70,6 @@ function love.update(dt)
     x = x + (xSpeed * dt)
     y = y + (ySpeed * dt)
 
-    if backgroundAlpha ~= nil then
-        backgroundAlpha = backgroundAlpha + (0.2 * dt)
-    end
-    if foregroundAlpha ~= nil then
-        foregroundAlpha = foregroundAlpha + (0.6 * dt)
-    end
     normalizeImage()
 end
 
@@ -106,17 +79,6 @@ end
 --------------------------------------------------------------
 function love.keypressed(key)
     handleKeypressed(key)
-    if key == "space" then
-        showTimeOver = not showTimeOver
-        love.mouse.setVisible(false)
-        if showTimeOver then
-            local gameOverSound = love.audio.newSource("resources/game-over.mp3", "static")
-            backgroundAlpha = -0.5
-            gameOverSound:play()
-        end
-    elseif key == "return" then
-        foregroundAlpha = 0
-    end
 end
 
 -- Function Name: love.keyreleased()
