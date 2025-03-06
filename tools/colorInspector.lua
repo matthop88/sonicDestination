84cb40c0230e6109653c283e919607af6331a31e
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 10:14:33 -0800

    Removed comment block from keepInBounds()

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -105,14 +105,6 @@ function stopScrollingUp()    ySpeed = math.min(0, ySpeed) end
 function stopScrollingDown()  ySpeed = math.max(0, ySpeed) end
 
 function keepInBounds()
-    --[[
-        If we have scrolled too far to the left
-                         or too far to the right
-                         or too far up
-                         or too far down
-        then correct
-    --]]
-
     -- Don't scroll too far to right or too far to left
     x = math.min(0, math.max(x, WINDOW_WIDTH - IMAGE:getWidth()))
      
