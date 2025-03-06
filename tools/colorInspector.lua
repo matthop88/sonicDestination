1bd5c7b9a0118f9ae0d68937ed121d3a7d1e713e
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 11:51:51 -0800

    Checked off scrolling checkbox

--- tools/colorInspector.lua
+++ tools/colorInspector.lua
@@ -12,7 +12,7 @@
 
                            Features
                            --------
-       [.] Image can be scrolled using the up, down,
+       [X] Image can be scrolled using the up, down,
            left and right arrow keys.
        [ ] Image can be zoomed in using the 'z' key
            and zoomed out using the 'a' key
