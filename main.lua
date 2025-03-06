a0d60e8e71ad5b82267de55b23ede6d0639954d5
Matthew Hopkins <matthop88@gmail.com>
2025-03-05 16:02:43 -0800

    Added capability to take command-line argument. Print out first argument to console

--- main.lua
+++ main.lua
@@ -4,9 +4,13 @@
 
 -- Function Name: love.load()
 -- Called By:     LOVE2D application, once during start
+-- Parameters:    args - list of command-line arguments
+--                       passed by user in console
 --------------------------------------------------------------
-function love.load()
+function love.load(args)
+	print(args[1])
 	require "tools/colorInspector"
 end
 
 --require "game/main"
+
