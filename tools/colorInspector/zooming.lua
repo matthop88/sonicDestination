8a62484f27b9e81960c2cdea77f228e3cefcd94b
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 17:10:11 -0800

    Added love.mousereleased() which reverses translation

--- tools/colorInspector/zooming.lua
+++ tools/colorInspector/zooming.lua
@@ -46,6 +46,18 @@ function love.mousepressed(mx, my)
     y = y - dy
 end
 
+function love.mousereleased(mx, my)
+    -- Translate x, y so that point at upper left corner of screen moves to (mx, my)
+    local leftX = 0
+    local topY  = 0
+
+    local dx = (mx - leftX) / scale
+    local dy = (my -  topY) / scale
+
+    x = x + dx
+    y = y + dy
+end
+
 function zoomIn()             scaleDelta =  ZOOM_SPEED           end
 function zoomOut()            scaleDelta = -ZOOM_SPEED           end
 
