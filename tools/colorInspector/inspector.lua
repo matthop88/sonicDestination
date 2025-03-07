41eccdae840194373e37f08d3d43f1ba5e7a53e8
Matthew Hopkins <matthop88@gmail.com>
2025-03-06 16:13:06 -0800

    Replaced zooming code with requiring file inside of inspector.lua

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -135,37 +135,7 @@ require "tools/colorInspector/scrolling"
 
 -------------------------- ZOOMING ---------------------------
 
-ZOOM_SPEED = 2
-scaleDelta = 0
-
-function handleZoomKeypressed(key)
-    if     key == "z" then zoomIn()
-    elseif key == "a" then zoomOut()
-    end
-end
-
-function handleZoomKeyreleased(key)
-    if     key == "z" then stopZoomingIn()
-	elseif key == "a" then stopZoomingOut()
-    end
-end
-    
-function updateZooming(dt)
-	if scaleDelta ~= 0 then
-        --[[
-            Adjust x and y of image so that we are zooming in at point
-            the mouse is at
-        --]]
-
-        scale = scale + scaleDelta * dt * scale
-    end
-end
-
-function zoomIn()             scaleDelta =  ZOOM_SPEED           end
-function zoomOut()            scaleDelta = -ZOOM_SPEED           end
-
-function stopZoomingIn()      scaleDelta =  0                    end
-function stopZoomingOut()     scaleDelta =  0                    end
+require "tools/colorInspector/zooming"
 
 
 
