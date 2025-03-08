349d6e613a9d10c70fd4468d323d48dd3e778996
Matthew Hopkins <matthop88@gmail.com>
2025-03-07 17:03:06 -0800

    Added simple printToReadout() function, which prints a string to console

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -105,6 +105,10 @@ function keepImageInBounds()
     y = math.min(0, math.max(y, (WINDOW_HEIGHT / scale) - IMAGE:getHeight()))
 end
 
+function printToReadout(msg)
+    print("Printing to readout: ", msg)
+end
+
 
 
 
