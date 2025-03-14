61aa8d47a3a35d32706d879c31b26adc292f0c4c
Matthew Hopkins <matthop88@gmail.com>
2025-03-14 15:19:34 -0700

    Now importing image.lua into inspector.lua

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -34,6 +34,7 @@ require "tools/colorInspector/scrolling"
 require "tools/colorInspector/zooming"
 require "tools/colorInspector/readout"
 require "tools/colorInspector/color"
+require "tools/colorInspector/image"
 
 --------------------------------------------------------------
 --                      Global Variables                    --
@@ -111,30 +112,6 @@ end
 --------------------------------------------------------------
 --                  Specialized Functions                   --
 --------------------------------------------------------------
-
----------------------------- IMAGE -------------------------------
-
-if __INSPECTOR_FILE ~= nil then
-    IMAGE_DATA = love.image.newImageData("resources/images/spriteSheets/" .. __INSPECTOR_FILE .. ".png")
-else
-    IMAGE_DATA = love.image.newImageData("resources/images/sadNoFileImage.png")
-end
-
-IMAGE = love.graphics.newImage(IMAGE_DATA)
-
-function drawImage()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(IMAGE, x * scale, y * scale, 0, scale, scale)
-end
-
-function updateImage()
-    if isMotionless() then keepImageInBounds() end
-end
-
-function keepImageInBounds()
-    x = math.min(0, math.max(x, (WINDOW_WIDTH  / scale) - IMAGE:getWidth()))
-    y = math.min(0, math.max(y, (WINDOW_HEIGHT / scale) - IMAGE:getHeight()))
-end
 
 ---------------------------- COLOR -------------------------------
 
