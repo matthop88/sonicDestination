340dc4f918113929932e9b60b8e7e4cba2eae636
Matthew Hopkins <matthop88@gmail.com>
2025-03-14 14:47:10 -0700

    Added MEDIUM_GREY to color.lua

--- tools/colorInspector/color.lua
+++ tools/colorInspector/color.lua
@@ -1,9 +1,10 @@
 COLOR = {
-    JET_BLACK         = { 0, 0, 0      },
-    TRANSPARENT_BLACK = { 0, 0, 0, 0.5 },
-    TRANSPARENT_BLUE  = { 0, 0, 1, 0.5 },
-    PURE_WHITE        = { 1, 1, 1      },
-    TRANSPARENT_WHITE = { 1, 1, 1, 0.5 },
-    RED               = { 1, 0, 0      },
-    YELLOW            = { 1, 1, 0      },
+    JET_BLACK         = { 0,   0,   0      },
+    TRANSPARENT_BLACK = { 0,   0,   0, 0.5 },
+    TRANSPARENT_BLUE  = { 0,   0,   1, 0.5 },
+    PURE_WHITE        = { 1,   1,   1      },
+    TRANSPARENT_WHITE = { 1,   1,   1, 0.5 },
+    RED               = { 1,   0,   0      },
+    YELLOW            = { 1,   1,   0      },
+    MEDIUM_GREY       = { 0.5, 0.5, 0.5    },
 }
