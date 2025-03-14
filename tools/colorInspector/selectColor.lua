e39dd5ba8d77e5ed60178de4ad8435962f5e8eb9
Matthew Hopkins <matthop88@gmail.com>
2025-03-14 15:32:40 -0700

    Added selectColor.lua with contents of COLOR section

---
+++ tools/colorInspector/selectColor.lua
@@ -0,0 +1,24 @@
+selectedColor = nil
+
+function identifyColor(mx, my)
+    local imageX = math.min(getImageWidth(),  math.floor((mx / scale) - x))
+    local imageY = math.min(getImageHeight(), math.floor((my / scale) - y))
+    
+    local r, g, b = getImagePixelAt(imageX, imageY)
+    
+    print(string.format("{ %.2f, %.2f, %.2f }", r, g, b))
+    printToReadout(string.format("R = %s, G = %s, B = %s", love.math.colorToBytes(r, g, b)))
+    
+    return { r, g, b }
+end
+
+function drawSelectedColor()
+    if selectedColor ~= nil then
+        local mx, my = love.mouse.getPosition()
+        love.graphics.setColor(selectedColor)
+        love.graphics.rectangle("fill", mx - 50, my - 50, 100, 100)
+        love.graphics.setLineWidth(3)
+        love.graphics.setColor(COLOR.JET_BLACK)
+        love.graphics.rectangle("line", mx - 50, my - 50, 100, 100)
+    end
+end
