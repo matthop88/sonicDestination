4d300d4dc948e25700002f6dd599cf3e0601761e
Matthew Hopkins <matthop88@gmail.com>
2025-03-15 07:04:02 -0700

    First stab at generating selectedIndex, printing to console

--- tools/colorInspector/inspector.lua
+++ tools/colorInspector/inspector.lua
@@ -205,8 +205,10 @@ function selectPaletteColorAt(mx, my)
     -- Temporary hack to prevent program from crashing
     selectImageColorAt(0, 0)
     local hIndex = math.floor((mx - PALETTE_LEFT) / 68) + 1
-		local vIndex = math.floor((my - 3           ) / 66) + 1
-		print("H Index = " .. hIndex .. ", V Index = " .. vIndex)
+    local vIndex = math.floor((my - 3           ) / 66) + 1
+    print("H Index = " .. hIndex .. ", V Index = " .. vIndex)
+    local selectedIndex = vIndex * 2 + hIndex
+    print("Selected Index: " .. selectedIndex)
 end
 
 -- ...
