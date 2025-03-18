2680804122cfa568fcb50d5b75f7e51b8c0c7d52
Matthew Hopkins <matthop88@gmail.com>
2025-03-18 14:07:18 -0700

    Simplified method names in scrolling.lua. Also fixed bugs changing colons to dots

--- tools/colorInspector/scrolling.lua
+++ tools/colorInspector/scrolling.lua
@@ -7,48 +7,48 @@ SCROLLING = {
     ySpeed  = 0,
     dashing = false,
 
-    handleScrollKeypressed = function(self, key)
+    handleKeypressed = function(self, key)
         self.dashing = isDoubleTap(key)
         
-        if     key == "left"  then self:scrollLeft()
-        elseif key == "right" then self:scrollRight()
-        elseif key == "up"    then self:scrollUp()
-        elseif key == "down"  then self:scrollDown()
+        if     key == "left"  then self:startLeft()
+        elseif key == "right" then self:startRight()
+        elseif key == "up"    then self:startUp()
+        elseif key == "down"  then self:startDown()
         end
     
         setLastKeypressed(key)
     end,
 
-    handleScrollKeyreleased = function(self, key)
-        if     key == "left"  then self:stopScrollingLeft()
-        elseif key == "right" then self:stopScrollingRight()
-        elseif key == "up"    then self:stopScrollingUp()
-        elseif key == "down"  then self:stopScrollingDown()
+    handleKeyreleased = function(self, key)
+        if     key == "left"  then self:stopLeft()
+        elseif key == "right" then self:stopRight()
+        elseif key == "up"    then self:stopUp()
+        elseif key == "down"  then self:stopDown()
         end
     end,
     
-    updateScrolling = function(self, dt)
+    update = function(self, dt)
         IMAGE_VIEWER:moveImage(self.xSpeed * dt, self.ySpeed * dt)
     end,
     
-    scrollLeft         = function(self) self.xSpeed =   self:calculateScrollSpeed()  end,
-    scrollRight        = function(self) self.xSpeed = -(self:calculateScrollSpeed()) end,
-    scrollUp           = function(self) self.ySpeed =   self:calculateScrollSpeed()  end,
-    scrollDown         = function(self) self.ySpeed = -(self:calculateScrollSpeed()) end,
+    startLeft  = function(self) self.xSpeed =   self:calculateSpeed()  end,
+    startRight = function(self) self.xSpeed = -(self:calculateSpeed()) end,
+    startUp    = function(self) self.ySpeed =   self:calculateSpeed()  end,
+    startDown  = function(self) self.ySpeed = -(self:calculateSpeed()) end,
       
-    stopScrollingLeft  = function(self) self.xSpeed = math.min(0, self.xSpeed)       end,
-    stopScrollingRight = function(self) self.xSpeed = math.max(0, self.xSpeed)       end,
-    stopScrollingUp    = function(self) self.ySpeed = math.min(0, self.ySpeed)       end,
-    stopScrollingDown  = function(self) self.ySpeed = math.max(0, self.ySpeed)       end,
+    stopLeft   = function(self) self.xSpeed = math.min(0, self.xSpeed) end,
+    stopRight  = function(self) self.xSpeed = math.max(0, self.xSpeed) end,
+    stopUp     = function(self) self.ySpeed = math.min(0, self.ySpeed) end,
+    stopDown   = function(self) self.ySpeed = math.max(0, self.ySpeed) end,
     
-    calculateScrollSpeed = function(self)
-        if self:dashing then return SCROLL_SPEED * 2
+    calculateSpeed = function(self)
+        if self.dashing then return SCROLL_SPEED * 2
         else                 return SCROLL_SPEED
         end
     end,
     
     isMotionless = function(self)
-        return not isWithinDoubleTapThreshold() and self:xSpeed == 0 and self:ySpeed == 0
+        return not isWithinDoubleTapThreshold() and self.xSpeed == 0 and self.ySpeed == 0
     end,
 }
 
