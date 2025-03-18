69e463a3d71485b7a1dd6897cc03a2ff7e05349c
Matthew Hopkins <matthop88@gmail.com>
2025-03-18 13:13:18 -0700

    Changed names in palette.lua to not include the world 'palette'

--- tools/colorInspector/palette.lua
+++ tools/colorInspector/palette.lua
@@ -1,72 +1,72 @@
 require "tools/colorInspector/color"
 
 PALETTE = {
-    paletteTable   = { },
-    PALETTE_LEFT   = WINDOW_WIDTH - 134,
-    paletteInFocus = false,
+    table       = { },
+    LEFT_BORDER = WINDOW_WIDTH - 134,
+    inFocus     = false,
 
-    isPaletteInFocus = function(self)
-        return self.paletteInFocus
+    isInFocus = function(self)
+        return self.inFocus
     end,
 
-    drawPalette = function(self)
-        local mx, my   = love.mouse.getPosition()
-        self.paletteInFocus = mx >= self.PALETTE_LEFT
+    draw = function(self)
+        local mx, my = love.mouse.getPosition()
+        self.inFocus = mx >= self.LEFT_BORDER
     
-        self:drawPaletteGrid()
-        self:drawPaletteColors()
+        self:drawGrid()
+        self:drawColors()
         self:highlightSelectedColor()
     end,
 
-    drawPaletteGrid = function(self)
-        love.graphics.setColor(self:calculatePaletteGridColor())
+    drawGrid = function(self)
+        love.graphics.setColor(self:calculateGridColor())
         love.graphics.setLineWidth(3)
-        love.graphics.rectangle("line", self.PALETTE_LEFT, 3, 132, WINDOW_HEIGHT - 6)
-        self:drawPaletteGridLines()
+        love.graphics.rectangle("line", self.LEFT_BORDER, 3, 132, WINDOW_HEIGHT - 6)
+        self:drawGridLines()
     end,
 
-    calculatePaletteGridColor = function(self)
-        if self:isPaletteInFocus() then return COLOR.MEDIUM_GREY
-        else                            return COLOR.TRANSPARENT_WHITE
+    calculateGridColor = function(self)
+        if self:isInFocus() then return COLOR.MEDIUM_GREY
+        else                     return COLOR.TRANSPARENT_WHITE
         end
     end,
 
-    drawPaletteGridLines = function(self)
+    drawGridLines = function(self)
         for i = 0, 8 do
-            love.graphics.rectangle("line", self.PALETTE_LEFT,      (i * 66) + 3, 66, 66)
-            love.graphics.rectangle("line", WINDOW_WIDTH - 68,      (i * 66) + 3, 66, 66)
+            love.graphics.rectangle("line", self.LEFT_BORDER,         (i * 66) + 3, 66, 66)
+            love.graphics.rectangle("line", WINDOW_WIDTH - 68,        (i * 66) + 3, 66, 66)
         end
     end,
 
-    drawPaletteColors = function(self)
-        for colorIndex, color in ipairs(self.paletteTable) do
-            love.graphics.setColor(self:calculatePaletteColorToDraw(color))
-            local x, y = self:calculateCoordinatesOfPaletteColor(colorIndex)
+    drawColors = function(self)
+        for colorIndex, color in ipairs(self.table) do
+            love.graphics.setColor(self:calculateColorToDraw(color))
+            local x, y = self:calculateCoordinatesOfColor(colorIndex)
             love.graphics.rectangle("fill", x, y, 60, 60)
         end
     end,
 
-    calculatePaletteColorToDraw = function(self, baseColor)
-        if self:isPaletteInFocus() or compareColors(baseColor, getSelectedColor()) then
+    calculateColorToDraw = function(self, baseColor)
+        if self:isInFocus() or compareColors(baseColor, getSelectedColor()) then
             return baseColor
         else
             return { baseColor[1], baseColor[2], baseColor[3], 0.2 }
         end
     end,
 
-    calculateCoordinatesOfPaletteColor = function(self, colorIndex)
+    calculateCoordinatesOfColor = function(self, colorIndex)
         local column = (colorIndex - 1) % 2
         local row    = math.floor((colorIndex - 1) / 2)
     
-        local x = self.PALETTE_LEFT + (column * 66) + 3
-        local y =                     (row    * 66) + 6
+        local x = self.LEFT_BORDER + (column * 66) + 3
+        local y =                    (row    * 66) + 6
     
         return x, y
     end,
 
     highlightSelectedColor = function(self)
-        for i, color in ipairs(self.paletteTable) do
-            local x, y = self:calculateCoordinatesOfPaletteColor(i)
+        for i, color in ipairs(self.table) do
+            local x, y = self:calculateCoordinatesOfColor(i)
             if compareColors(color, getSelectedColor()) then
                 love.graphics.setColor(COLOR.YELLOW)
                 love.graphics.setLineWidth(6)
@@ -75,25 +75,25 @@ PALETTE = {
         end
     end,
 
-    insertColorIntoPalette = function(self, color)
-        if not self:colorBelongsToPalette(color) then
-            table.insert(self.paletteTable, color)
+    insertColor = function(self, color)
+        if not self:colorBelongs(color) then
+            table.insert(self.table, color)
         end
     end,
 
-    colorBelongsToPalette = function(self, color)
-        for _, c in ipairs(self.paletteTable) do
+    colorBelongs = function(self, color)
+        for _, c in ipairs(self.table) do
             if compareColors(c, color) then return true end
         end
     end,
 
-    selectPaletteColorAt = function(self, mx, my)
-        local hIndex = math.floor((mx - self.PALETTE_LEFT) / 68)
-        local vIndex = math.floor((my - 3                ) / 66)
+    selectColorAt = function(self, mx, my)
+        local hIndex = math.floor((mx - self.LEFT_BORDER) / 68)
+        local vIndex = math.floor((my - 3               ) / 66)
         local selectedIndex = vIndex * 2 + hIndex + 1
     
-        if self.paletteTable[selectedIndex] then
-            setSelectedColor(self.paletteTable[selectedIndex])
+        if self.table[selectedIndex] then
+            setSelectedColor(self.table[selectedIndex])
         end
     end
 }
