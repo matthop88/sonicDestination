c3b848587b0c9e7cf8ee4868ae846710e6fee7cf
Matthew Hopkins <matthop88@gmail.com>
2025-03-25 09:33:09 -0700

    Inside engine.lua, extracted to smaller init() functions

--- plugins/engine.lua
+++ plugins/engine.lua
@@ -1,56 +1,66 @@
 return ({
     oldDraw          = function()       end,
-	oldUpdate        = function(dt)     end,
-	oldKeypressed    = function(key)    end,
-	oldKeyreleased   = function(key)    end,
-	oldMousepressed  = function(mx, my) end,
-	oldMousereleased = function(mx, my) end,
+    oldUpdate        = function(dt)     end,
+    oldKeypressed    = function(key)    end,
+    oldKeyreleased   = function(key)    end,
+    oldMousepressed  = function(mx, my) end,
+    oldMousereleased = function(mx, my) end,
 
     init = function(self)
-        
-        self.oldDraw = love.draw or self.oldDraw
-        love.draw          = function() 
-            self.oldDraw()
-            self:draw()                      
-        end
+        self:initDraw()
+        self:initUpdate()
+        self:initKeypressed()
+        self:initKeyreleased()
+        self:initMousepressed()
+        self:initMousereleased()
 
+        return self
+    end,
+
+    initDraw = function(self)
+        self.oldDraw = love.draw or self.oldDraw
+        love.draw = function() 
+            self.oldDraw()
+            self:draw()
+        end
+    end,
+
+    initUpdate = function(self)
         self.oldUpdate = love.update or self.oldUpdate
         love.update        = function(dt) 
             self.oldUpdate(dt)
-            self:update(dt)                
+            self:update(dt)
         end
-
-        self.oldKeypressed = love.keypressed or self.oldKeypressed
-        love.keypressed    = function(key) 
-            if not self.oldKeypressed(key) then
-                self:keypressed(key)          
-            end
-        end
-
-        self.oldKeyreleased = love.keyreleased or self.oldKeyreleased
-        love.keyreleased   = function(key) 
-            if not self.oldKeyreleased(key) then
-                self:keyreleased(key)         
-            end
-        end
-
-        self.oldMousepressed = love.mousepressed or self.oldMousepressed
-        love.mousepressed  = function(mx, my) 
-            if not self.oldMousepressed(mx, my) then
-                self:mousepressed(mx, my)  
-            end
-        end
-
-        self.oldMousereleased = love.mousereleased or self.oldMousereleased
-        love.mousereleased = function(mx, my) 
-            if not self.oldMousereleased(mx, my) then
-                self:mousereleased(mx, my) 
-            end
-        end
-            
-        return self
     end,
-            
+
+    initKeypressed = function(self)
+        self.oldKeypressed = love.keypressed or self.oldKeypressed
+        love.keypressed = function(key) 
+            if not self.oldKeypressed(key) then self:keypressed(key) end
+        end
+    end,
+
+    initKeyreleased = function(self)
+        self.oldKeyreleased = love.keyreleased or self.oldKeyreleased
+        love.keyreleased = function(key) 
+            if not self.oldKeyreleased(key) then self:keyreleased(key) end
+        end
+    end,
+
+    initMousepressed = function(self)
+        self.oldMousepressed = love.mousepressed or self.oldMousepressed
+        love.mousepressed  = function(mx, my)
+            if not self.oldMousepressed(mx, my) then self:mousepressed(mx, my) end
+        end
+    end,
+
+    initMousereleased = function(self)
+        self.oldMousereleased = love.mousereleased or self.oldMousereleased
+        love.mousereleased = function(mx, my)
+            if not self.oldMousereleased(mx, my) then self:mousereleased(mx, my) end
+        end
+    end,
+             
     draw = function(self)
         for _, plugin in ipairs(self) do
             if plugin.draw ~= nil then
@@ -105,4 +115,5 @@ return ({
         table.insert(self, plugin)
         return self
     end,
+        
 }):init()
