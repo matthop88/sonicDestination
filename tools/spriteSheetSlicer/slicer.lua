239ae90657e7a53c515d76996f5e9cf377d48218
Matthew Hopkins <matthop88@gmail.com>
2025-03-26 14:31:23 -0700

    Checked off 1st checkbox

--- tools/spriteSheetSlicer/slicer.lua
+++ tools/spriteSheetSlicer/slicer.lua
@@ -4,7 +4,7 @@
 --                  Functional Specifications               --
 --------------------------------------------------------------
 
-[ ] 1. Program "automagically" finds borders of all sprites in image
+[X] 1. Program "automagically" finds borders of all sprites in image
 [ ] 2. Border is drawn when mouse moves over a sprite
 [ ] 3. When a sprite is clicked on, x, y, width and height are
        displayed on screen.
