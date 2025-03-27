cbbd5c00bc2e89f5e632cb3a967aec5bf0e61eaa
Matthew Hopkins <matthop88@gmail.com>
2025-03-27 11:31:05 -0700

    Oops! Reverted to findEnclosingRect function

--- tools/spriteSheetSlicer/slicer.lua
+++ tools/spriteSheetSlicer/slicer.lua
@@ -93,8 +93,12 @@ end
 drawCurrentRect = function()
     local imageX, imageY = getImageViewer():screenToImageCoordinates(love.mouse:getPosition())
     
-    currentRect:calculateUsing(imageX, imageY, SPRITE_RECTS.findEnclosingRect)
+    currentRect:calculateUsing(imageX, imageY, findEnclosingRect)
     currentRect:draw()
+end
+
+findEnclosingRect = function(imageX, imageY))
+    return SPRITE_RECTS:findEnclosingRect(imageX, imageY)
 end
 
 --------------------------------------------------------------
