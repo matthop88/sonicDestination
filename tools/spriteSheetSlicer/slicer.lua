feb710a0dd65b0081a9e568ccbb436e59999238f
Matthew Hopkins <matthop88@gmail.com>
2025-03-27 11:27:43 -0700

    Extracted slicing engine stuff, now utilizing external library

--- tools/spriteSheetSlicer/slicer.lua
+++ tools/spriteSheetSlicer/slicer.lua
@@ -97,78 +97,6 @@ drawCurrentRect = function()
     currentRect:draw()
 end
 
-slice = function()
-    local widthInPixels, heightInPixels = getImageViewer():getImageSize()    
-        
-    doSlicing(widthInPixels, heightInPixels)
-
-    print(ASCII_ART)
-end
-
-doSlicing = function(pixWidth, pixHeight)
-    for y = 0, pixHeight - 1 do
-        for x = 0, pixWidth - 1 do
-            doSlicingAt(x, y)
-        end
-    end
-end
-
-doSlicingAt = function(x, y)
-    local pixelColor = getImageViewer():getPixelColorAt(x, y)
-    processPixelAt(x, y, pixelColor)
-end
-
-createPixelProcessor = function()
-    local prevColor, leftX = nil, nil
-
-    local processEnteringLeftOfSprite = function(x, y, thisColor)
-        if enteringLeftOfSprite(prevColor, thisColor) then
-            local hasValidLeftBorder = colorsMatch(thisColor, SPRITE_BACKGROUND_COLOR)
-            SPRITE_RECTS:addSlice(x, y, hasValidLeftBorder)
-            leftX = x
-        end
-    end
-
-    local processExitingRightOfSprite = function(x, y, thisColor)
-        if exitingRightOfSprite(prevColor, thisColor) then
-            SPRITE_RECTS:updateSpriteWidth(leftX, y, x)
-            leftX = nil
-        end
-    end
-
-    local doPixelProcessing = function(x, y, thisColor)
-        processEnteringLeftOfSprite(x, y, thisColor)
-        processExitingRightOfSprite(x, y, thisColor)
-    end
-
-    return function(x, y, thisColor)
-        if x == 0 then 
-            prevColor, leftX = nil, nil 
-        end
-        doPixelProcessing(x, y, thisColor)
-        prevColor = thisColor
-    end
-end
-
-enteringLeftOfSprite = function(prevColor, thisColor)
-    return  colorsMatch(prevColor, MARGIN_BACKGROUND_COLOR)
-    and not colorsMatch(thisColor, MARGIN_BACKGROUND_COLOR)
-end
-
-exitingRightOfSprite = function(prevColor, thisColor)
-    return not colorsMatch(prevColor, MARGIN_BACKGROUND_COLOR)
-           and colorsMatch(thisColor, MARGIN_BACKGROUND_COLOR)
-end
-
-processPixelAt = createPixelProcessor()
-
-colorsMatch = function(c1, c2)
-    return c1 ~= nil and c2 ~= nil
-       and math.abs(c1.r - c2.r) < 0.005
-       and math.abs(c1.g - c2.g) < 0.005 
-       and math.abs(c1.b - c2.b) < 0.005
-end
-
 --------------------------------------------------------------
 --                          Plugins                         --
 --------------------------------------------------------------
@@ -188,6 +116,8 @@ PLUGINS = require("plugins/engine")
 --------------------------------------------------------------
 --                Static code - is executed last            --
 --------------------------------------------------------------
+
+require("tools/spriteSheetSlicer/slicingEngine")
 
 slice()
 
