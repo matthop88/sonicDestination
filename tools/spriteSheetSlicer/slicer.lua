b89b9b680316f5f72804f92c977dc2c9c7bee512
Matthew Hopkins <matthop88@gmail.com>
2025-03-27 11:00:05 -0700

    Extracted smartRect to smartRect.lua part 2: deletion from slicer.lua

--- tools/spriteSheetSlicer/slicer.lua
+++ tools/spriteSheetSlicer/slicer.lua
@@ -69,48 +69,7 @@ SPRITE_BACKGROUND_COLOR = { r = 0.26, g = 0.60, b = 0.19 }
 
 SPRITE_RECTS = require("tools/spriteSheetSlicer/spriteRects")
 
-smartRect = {
-    rect = nil,
-    
-    initFrom = function(self, rect)        self.rect  = rect end,
-    isActive = function(self)       return self.rect ~= nil  end,
-    
-    getX     = function(self)       return self.rect.x       end,
-    getY     = function(self)       return self.rect.y       end,
-    getW     = function(self)       return self.rect.w       end,
-    getH     = function(self)       return self.rect.h       end,
-    
-    containsPt = function(self, x, y)
-        return  self:isActive()
-            and x >= self:getX()
-            and x <= self:getX() + self:getW() - 1
-            and y >= self:getY()
-            and y <= self:getY() + self:getH() - 1
-    end,
-
-    draw = function(self)
-        if self:isActive() then
-            if love.mouse.isDown(1) then self:drawFilled()
-            else                         self:drawOutline()
-            end
-        end
-    end,
-
-    drawFilled = function(self)
-        love.graphics.setColor(1, 1, 0.8, 0.5)
-        love.graphics.rectangle("fill", self:toScreenRect())
-    end,
-
-    toScreenRect = function(self)
-        return getImageViewer():imageToScreenRect(self:getX() - 2, self:getY() - 2, self:getW() + 4, self:getH() + 4)
-    end,
-
-    drawOutline = function(self)
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.setLineWidth(3 * getImageViewer():getScale())
-        love.graphics.rectangle("line", self:toScreenRect())
-    end,
-}
+smartRect    = require("tools/spriteSheetSlicer/smartRect")
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
