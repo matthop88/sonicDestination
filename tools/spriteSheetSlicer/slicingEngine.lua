22663d1f1ee490ab46608c6d32c7fdb8db59b946
Matthew Hopkins <matthop88@gmail.com>
2025-03-27 16:21:41 -0700

    Changed slicingEngine.lua to handle new algorithm

--- tools/spriteSheetSlicer/slicingEngine.lua
+++ tools/spriteSheetSlicer/slicingEngine.lua
@@ -1,80 +1,62 @@
-local MARGIN_BACKGROUND_COLOR = { r = 0.05, g = 0.28, b = 0.03 }
-local SPRITE_BACKGROUND_COLOR = { r = 0.26, g = 0.60, b = 0.19 }
-
-local SPRITE_RECTS = require("tools/spriteSheetSlicer/spriteRects")
-
-local colorsMatch = function(c1, c2)
-    return c1 ~= nil and c2 ~= nil
-       and math.abs(c1.r - c2.r) < 0.005
-       and math.abs(c1.g - c2.g) < 0.005 
-       and math.abs(c1.b - c2.b) < 0.005
-end
-
-local enteringLeftOfSprite = function(prevColor, thisColor)
-    return  colorsMatch(prevColor, MARGIN_BACKGROUND_COLOR)
-    and not colorsMatch(thisColor, MARGIN_BACKGROUND_COLOR)
-end
-
-local exitingRightOfSprite = function(prevColor, thisColor)
-    return not colorsMatch(prevColor, MARGIN_BACKGROUND_COLOR)
-           and colorsMatch(thisColor, MARGIN_BACKGROUND_COLOR)
-end
-
-local createPixelProcessor = function()
-    local prevColor, leftX = nil, nil
-
-    local processEnteringLeftOfSprite = function(x, y, thisColor)
-        if enteringLeftOfSprite(prevColor, thisColor) then
-            local hasValidLeftBorder = colorsMatch(thisColor, SPRITE_BACKGROUND_COLOR)
-            SPRITE_RECTS:addSlice(x, y, hasValidLeftBorder)
-            leftX = x
-        end
-    end
-
-    local processExitingRightOfSprite = function(x, y, thisColor)
-        if exitingRightOfSprite(prevColor, thisColor) then
-            SPRITE_RECTS:updateSpriteWidth(leftX, y, x)
-            leftX = nil
-        end
-    end
-
-    local doPixelProcessing = function(x, y, thisColor)
-        processEnteringLeftOfSprite(x, y, thisColor)
-        processExitingRightOfSprite(x, y, thisColor)
-    end
-
-    return function(x, y, thisColor)
-        if x == 0 then 
-            prevColor, leftX = nil, nil 
-        end
-        doPixelProcessing(x, y, thisColor)
-        prevColor = thisColor
-    end
-end
-
-local processPixelAt = createPixelProcessor()
+local sliceTable   = require("tools/spriteSheetSlicer/sliceTable")
+local sliceFactory = require("tools/spriteSheetSlicer/sliceFactory")
 
 return {
-    slice = function(self)
-        local widthInPixels, heightInPixels = getImageViewer():getImageSize()    
-        
-        self:doSlicing(widthInPixels, heightInPixels)
+    MARGIN_BACKGROUND_COLOR = nil,
+    SPRITE_BACKGROUND_COLOR = nil,
+    IMAGE_WIDTH             = nil,
+    IMAGE_HEIGHT            = nil,
+    getImageViewer          = nil,
+    pixelAnalyzer           = nil,
+    prevColor               = nil,
+    thisColor               = nil,
+    
+    slice = function(self, marginBgColor, spriteBgColor, imageViewerFn)
+        self.getImageViewer = imageViewerFn
+        self.IMAGE_WIDTH, self.IMAGE_HEIGHT = self.getImageViewer():getImageSize()
+        self.pixelAnalyzer  = require("tools/spriteSheetSlicer/pixelAnalyzer")
+                                      :init(marginBgColor, spriteBgColor) 
 
-        print(ASCII_ART)
-
-        return SPRITE_RECTS:getWalkableRectList()
+        self:doSlicing()
+        return self:finalizeSpriteRects()
     end,
 
-    doSlicing = function(self, pixWidth, pixHeight)
-        for y = 0, pixHeight - 1 do
-            for x = 0, pixWidth - 1 do
-                self:doSlicingAt(x, y)
-            end
+    doSlicing = function(self)
+        for y = 0, self.IMAGE_HEIGHT - 1 do
+            local newSlices = self:createHorizontalSlicesFromLine(y)
+            self:addHorizontalSlices(newSlices, y)
         end
     end,
 
-    doSlicingAt = function(self, x, y)
-        local pixelColor = getImageViewer():getPixelColorAt(x, y)
-        processPixelAt(x, y, pixelColor)
+    createHorizontalSlicesFromLine = function(self, y)
+        self.prevColor = nil
+        return self:appendHorizontalSlicesTo({ }, y)
+    end,
+
+    appendHorizontalSlicesTo = function(self, sliceList, y)
+        for x = 0, self.IMAGE_WIDTH - 1 do
+            self.thisColor = self:getImageViewer():getPixelColorAt(x, y)
+            local newSlice = self:produceSliceFrom(x, y)
+            if newSlice ~= nil then table.insert(sliceList, newSlice) end
+        end
+        return sliceList
+    end,
+
+    produceSliceFrom = function(self, x, y)
+        local pixelTransitionData = self.pixelAnalyzer:analyzePixelTransition(self.prevColor, self.thisColor)
+        self.prevColor = self.thisColor
+        return sliceFactory:produceSliceUsing(pixelTransitionData, x, y)
+    end,
+
+    addHorizontalSlices = function(self, horizSlices, y)
+        sliceTable:markCompletedSlicesAbove(y)
+        sliceTable:update()
+        if #horizSlices > 0 then
+            sliceTable:addHorizontalSlices(horizSlices)
+        end
+    end,
+
+    finalizeSpriteRects = function(self)
+        return sliceTable:getFinishedSpriteRects()
     end,
 }
