cef8c1c60a5d3818f5dbc84154f995af9dd10601
Matthew Hopkins <matthop88@gmail.com>
2025-04-01 17:17:48 -0700

    Extracted findLeftEdge()

--- tools/spriteSheetSlicer/slicingEngine.lua
+++ tools/spriteSheetSlicer/slicingEngine.lua
@@ -53,16 +53,19 @@ return {
     end,
 
     processPixelAt = function(self, x, y)
-        if x == 0 then
-            self.prevColor = nil
-        end
+        if x == 0 then self.prevColor = nil end
 
-        if self:isLeftEdge(self.imageViewer:getPixelColorAt(x, y)) then
+        self:findLeftEdge(x, y)
+        
+        self.prevColor = thisColor
+    end,
+
+    findLeftEdge = function(self, x, y)
+        local pixelColor = self.imageViewer:getPixelColorAt(x, y)
+        if self:isLeftEdge(pixelColor) then
             print("Found Left Edge at x = " .. x .. ", y = " .. y)
             self.matchesFound = self.matchesFound + 1
         end
-
-        self.prevColor = thisColor
     end,
 
     isLeftEdge  = function(self, thisColor)
