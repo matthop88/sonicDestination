761685b476cc4d17fa570ece5688fd18a5776092
Matthew Hopkins <matthop88@gmail.com>
2025-04-08 13:33:10 -0700

    Created pixelAnalyzer.lua, which has the color matching and edge determining functions

---
+++ tools/spriteSheetSlicer/pixelAnalyzer.lua
@@ -0,0 +1,24 @@
+return {
+    isProbablyLeftEdge  = function(self, thisColor)
+        return      self:colorsMatch(self.prevColor, self.marginBGColor)
+            and not self:colorsMatch(thisColor,      self.marginBGColor)
+    end,
+
+    isDefinitelyLeftEdge  = function(self, thisColor)
+        return  self:colorsMatch(self.prevColor, self.marginBGColor)
+            and self:colorsMatch(thisColor,      self.spriteBGColor)
+    end,
+
+    isLikelyRightEdge = function(self, thisColor)
+        return not self:colorsMatch(self.prevColor, self.marginBGColor)
+               and self:colorsMatch(thisColor,      self.marginBGColor)
+    end,
+    
+    colorsMatch = function(self, c1, c2)
+        return c1 ~= nil 
+           and c2 ~= nil
+           and math.abs(c1.r - c2.r) < 0.005
+           and math.abs(c1.g - c2.g) < 0.005 
+           and math.abs(c1.b - c2.b) < 0.005
+    end,
+}
