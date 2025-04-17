0923dd1c392079ce795c08049464e4cff0679ef9
Matthew Hopkins <matthop88@gmail.com>
2025-04-17 14:13:44 -0700

    game/main now requires sonic.lua

--- game/main.lua
+++ game/main.lua
@@ -31,25 +31,7 @@ WINDOW_WIDTH        = 1024
 WINDOW_HEIGHT       =  768
 
 WORKSPACE           = require("game/workspace")
-
-SONIC               = {
-    x = 464,   y = 454,
-    image        = love.graphics.newImage("game/resources/images/spriteSheets/sonic1.png"),
-    standingRect = { x = 43, y = 257, w = 32, h = 40 },
-    standingQuad = nil,
-
-    init = function(self)
-        self.standingQuad = love.graphics.newQuad(self.standingRect.x,   self.standingRect.y,
-                                                  self.standingRect.w,   self.standingRect.h,
-                                                  self.image:getWidth(), self.image:getHeight())
-        self.image:setFilter("nearest", "nearest")
-    end,
-
-    draw = function(self)
-        love.graphics.setColor(COLOR_PURE_WHITE)
-        love.graphics.draw(self.image, self.standingQuad, self.x, self.y, 0, 3, 3)
-    end,
-}
+SONIC               = require("game/sonic")
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
