02bd374624a1289c0400a91a90ce100962559c8d
Matthew Hopkins <matthop88@gmail.com>
2025-04-17 14:29:10 -0700

    Created shorter aliases to love.graphics.getWidth and love.graphics.getHeight to make them more readable

--- game/workspace.lua
+++ game/workspace.lua
@@ -3,6 +3,9 @@ local COLOR_GREEN         = { 0, 0.45, 0 }
 local COLOR_PURE_WHITE    = { 1, 1,    1 }
 
 return {
+    getWidth  = love.graphics.getWidth,
+    getHeight = love.graphics.getHeight,
+    
     draw = function(self)
         self:drawBackground()
         self:drawHorizontalLine()
@@ -10,12 +13,12 @@ return {
 
     drawBackground = function(self)
         love.graphics.setColor(COLOR_GREEN)
-        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+        love.graphics.rectangle("fill", 0, 0, self:getWidth(), self:getHeight())
     end,
 
     drawHorizontalLine = function(self)
         love.graphics.setColor(COLOR_PURE_WHITE)
         love.graphics.setLineWidth(3)
-        love.graphics.line(0, love.graphics.getHeight() * 3 / 4, love.graphics.getWidth(), love.graphics.getHeight() * 3 / 4)
+        love.graphics.line(0, self:getHeight() * 3/4, self:getWidth(), self:getHeight() * 3/4)
     end,
 }
