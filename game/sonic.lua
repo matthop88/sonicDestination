4646c54ab2aa875a2e9bce8baade528a2cde577f
Matthew Hopkins <matthop88@gmail.com>
2025-04-17 14:13:17 -0700

    Copied SONIC object to sonic.lua

---
+++ game/sonic.lua
@@ -0,0 +1,18 @@
+return {
+    x = 464,   y = 454,
+    image        = love.graphics.newImage("game/resources/images/spriteSheets/sonic1.png"),
+    standingRect = { x = 43, y = 257, w = 32, h = 40 },
+    standingQuad = nil,
+
+    init = function(self)
+        self.standingQuad = love.graphics.newQuad(self.standingRect.x,   self.standingRect.y,
+                                                  self.standingRect.w,   self.standingRect.h,
+                                                  self.image:getWidth(), self.image:getHeight())
+        self.image:setFilter("nearest", "nearest")
+    end,
+
+    draw = function(self)
+        love.graphics.setColor(COLOR_PURE_WHITE)
+        love.graphics.draw(self.image, self.standingQuad, self.x, self.y, 0, 3, 3)
+    end,
+}
