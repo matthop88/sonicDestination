0ee11735e3ee3263a3d1424a6ea87a20224cdcad
Matthew Hopkins <matthop88@gmail.com>
2025-04-17 17:53:23 -0700

    sonic.lua now utilizes sprite.lua

--- game/sonic.lua
+++ game/sonic.lua
@@ -1,39 +1,26 @@
-local COLOR_PURE_WHITE    = { 1, 1,    1 }
-
 return ({
-    x = 464,   y = 454,
-    image        = love.graphics.newImage("game/resources/images/spriteSheets/sonic1.png"),
-    offset       = { x = 16, y = 20 },
-    standingRect = { x = 43, y = 257, w = 32, h = 40 },
-    standingQuad = nil,
-
-    scale        = 3,
-
+    x      = 464,   
+    y      = 454,
+    sprite = require("game/sprite"),
+    
     init = function(self)
-        self.standingQuad = love.graphics.newQuad(self.standingRect.x,   self.standingRect.y,
-                                                  self.standingRect.w,   self.standingRect.h,
-                                                  self.image:getWidth(), self.image:getHeight())
-        self.image:setFilter("nearest", "nearest")
-
+        -- Initialization code goes here
+        -- ...
+            
         return self
     end,
 
     draw = function(self)
-        love.graphics.setColor(COLOR_PURE_WHITE)
-        love.graphics.draw(self.image,       self.standingQuad,
-                           self:getImageX(), self:getImageY(),
-                        0, self.scale,       self.scale)
+        self.sprite:draw(self:getX(), self:getY())
     end,
 
     --------------------------------------------------------------
     --                  Specialized Functions                   --
     --------------------------------------------------------------
 
-    getX      = function(self) return self.x                                     end,
-    getY      = function(self) return self.y                                     end,
-    getImageX = function(self) return self:getX() - (self.offset.x * self.scale) end,
-    getImageY = function(self) return self:getY() - (self.offset.y * self.scale) end,
-        
+    getX      = function(self) return self.x end,
+    getY      = function(self) return self.y end,
+         
     moveTo    = function(self, x, y)
         self.x, self.y = x, y
     end,
