0774958d4867c5aa603675b1067a311ab8383e5b
Matthew Hopkins <matthop88@gmail.com>
2025-04-18 12:57:08 -0700

    Added back vertical lines to workspace for horizontally centering Sonic

--- game/workspace.lua
+++ game/workspace.lua
@@ -9,6 +9,7 @@ return {
     draw = function(self)
         self:drawBackground()
         self:drawHorizontalLine()
+        self:drawVerticalLines()
     end,
 
     drawBackground = function(self)
@@ -20,5 +21,12 @@ return {
         love.graphics.setColor(COLOR_PURE_WHITE)
         love.graphics.setLineWidth(3)
         love.graphics.line(0, self:getHeight() * 3/4, self:getWidth(), self:getHeight() * 3/4)
+    end,
+
+    drawVerticalLines = function(self)
+        love.graphics.setLineWidth(1)
+        love.graphics.line(self:getWidth() / 2,      0, self:getWidth() / 2,      self:getHeight())
+        love.graphics.line(self:getWidth() / 2 - 48, 0, self:getWidth() / 2 - 48, self:getHeight())
+        love.graphics.line(self:getWidth() / 2 + 48, 0, self:getWidth() / 2 + 48, self:getHeight())
     end,
 }
