ccb27925375a1535a0f349b4612cfd34ca580d37
Matthew Hopkins <matthop88@gmail.com>
2025-04-18 12:41:38 -0700

    Added variables and init() function to mouseTracking plugin

--- plugins/modules/mouseTracking.lua
+++ plugins/modules/mouseTracking.lua
@@ -1,3 +1,13 @@
 return {
+    origX  = 0,
+    origY  = 0,
+    object = nil,
 
+    init   = function(self, params)
+        self.object = params.object
+        self.origX  = params.originX
+        self.origY  = params.originY
+
+        return self
+    }
 }
