e9521fc1efd7e1629e41a7e7c7084acdd2353cdf
Matthew Hopkins <matthop88@gmail.com>
2025-04-22 11:21:42 -0700

    Added pseudo code in saveChanges() for actually saving file

--- tools/transparencyEditor/editorApp.lua
+++ tools/transparencyEditor/editorApp.lua
@@ -101,10 +101,7 @@ saveChanges = function()
         if imgName == nil then
             printToReadout("Cannot save changes without valid file.")
         else
-            -- ...
-            -- ...
-            -- ...
-
+            -- imageData:encode("png", imgName .. ".png")
             changesMade = false
             printToReadout("Changes have been saved.")
         end
