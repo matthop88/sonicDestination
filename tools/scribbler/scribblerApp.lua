e1bdc58359315c253cc98d7ab7896e980f34488b
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 10:00:36 -0700

    Implemented draw() of rect jot

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -75,18 +75,9 @@ local rectJot = {
         love.graphics.setColor(1, 1, 1)
         love.graphics.setLineWidth(5)
         
-        local prevX, prevY = mx, my
-
-        for n, pt in ipairs(self.data) do
-            if n == 1 then
-                love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
-            else
-                love.graphics.line(prevX, prevY, pt.x, pt.y)
-            end
-            prevX, prevY = pt.x, pt.y
+        if self.data.x ~= nil and self.data.y ~= nil then
+            love.graphics.rectangle("line", self.data.x, self.data.y, mx - self.data.x, my - self.data.y)
         end
-
-        love.graphics.line(prevX, prevY, mx, my)
 
         love.mouse.setVisible(false)
         love.graphics.rectangle("fill", mx - 2, my - 2, 5, 5)
