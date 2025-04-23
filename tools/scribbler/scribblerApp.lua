8b86f816a4099167f3ac83fedd0304a5ae8f53ca
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 12:37:33 -0700

    Working jot is transparent

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -81,6 +81,22 @@ local picture = {
     end,
 }
 
+local scribbleJotDraw = function(self)
+    love.graphics.setColor(self.data.color)
+    love.graphics.setLineWidth(5)
+    
+    local prevX, prevY = nil, nil
+    
+    for n, pt in ipairs(self.data) do
+        if n == 1 then
+            love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
+        else
+            love.graphics.line(prevX, prevY, pt.x, pt.y)
+        end
+        prevX, prevY = pt.x, pt.y
+    end
+end
+
 local scribbleTool = ({ 
 
     init = function(self, picture)
@@ -88,47 +104,34 @@ local scribbleTool = ({
         return self
     end,
         
-    jot = {
-        data = { },
-        
-        draw = function(self)
-            love.graphics.setColor(1, 1, 1)
-            love.graphics.setLineWidth(5)
-            
-            local prevX, prevY = nil, nil
-            
-            for n, pt in ipairs(self.data) do
-                if n == 1 then
-                    love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
-                else
-                    love.graphics.line(prevX, prevY, pt.x, pt.y)
-                end
-                prevX, prevY = pt.x, pt.y
-            end
-        end,
-    },
+    jot = nil,
 
     draw = function(self, mx, my)
-        self.jot:draw()
+        if self.jot then self.jot:draw() end
 
         love.mouse.setVisible(false)
         love.graphics.rectangle("fill", mx - 2, my - 2, 5, 5)
     end,
     
     penUp = function(self, mx, my)
-        self.picture:addJot(self.jot)
-        self.jot = {
-            data = { },
-            draw = self.jot.draw,
-        }
+        if self.jot then 
+            self.jot.data.color = { 1, 1, 1 }
+            self.picture:addJot(self.jot) 
+        end
+        self.jot = nil
 
         printToReadout("Pen up at x = " .. mx .. ", y = " .. my)
     end,
 
     penDown = function(self, mx, my)
         printToReadout("Pen down at x = " .. mx .. ", y = " .. my)
-        self.jot.data = {
-            { x = mx, y = my }
+        self.jot = {
+            data = { 
+                color = { 1, 1, 1, 0.5 },
+                { x = mx, y = my },
+            },
+                
+            draw = scribbleJotDraw,
         }
     end,
 
