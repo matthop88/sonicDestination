bf2f173dd661039cb8b60f7f63fcf90d73f4e2cb
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 12:49:57 -0700

    Extracted scribbleTool from scribblerApp

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -81,70 +81,7 @@ local picture = {
     end,
 }
 
-local scribbleJotDraw = function(self)
-    love.graphics.setColor(self.data.color)
-    love.graphics.setLineWidth(5)
-    
-    local prevX, prevY = nil, nil
-    
-    for n, pt in ipairs(self.data) do
-        if n == 1 then
-            love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
-        else
-            love.graphics.line(prevX, prevY, pt.x, pt.y)
-        end
-        prevX, prevY = pt.x, pt.y
-    end
-end
-
-local scribbleTool = ({ 
-
-    init = function(self, picture)
-        self.picture = picture
-        return self
-    end,
-        
-    jot = nil,
-
-    draw = function(self, mx, my)
-        if self.jot then self.jot:draw() end
-
-        love.mouse.setVisible(false)
-        love.graphics.rectangle("fill", mx - 2, my - 2, 5, 5)
-    end,
-    
-    penUp = function(self, mx, my)
-        if self.jot then 
-            self.jot.data.color = { 1, 1, 1 }
-            self.picture:addJot(self.jot) 
-        end
-        self.jot = nil
-
-        printToReadout("Pen up at x = " .. mx .. ", y = " .. my)
-    end,
-
-    penDown = function(self, mx, my)
-        printToReadout("Pen down at x = " .. mx .. ", y = " .. my)
-        self.jot = {
-            data = { 
-                color = { 1, 1, 1, 0.5 },
-                { x = mx, y = my },
-            },
-                
-            draw = scribbleJotDraw,
-        }
-    end,
-
-    penMoved = function(self, mx, my)
-        printToReadout("Pen moved to x = " .. mx .. ", y = " .. my)
-    end,
-
-    penDragged = function(self, mx, my)
-        printToReadout("Pen dragged to x = " .. mx .. ", y = " .. my)
-        table.insert(self.jot.data, { x = mx, y = my })
-    end,
-
-}):init(picture)
+local scribbleTool = require("tools/scribbler/jotTools/scribble")
 
 local lineTool = { 
 
