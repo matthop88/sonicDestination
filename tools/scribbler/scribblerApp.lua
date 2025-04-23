2fffe932d16d67773376b4bf95ba0e2ac3c26e29
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 14:52:32 -0700

    Extracted picture code from scribblerApp

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -67,38 +67,7 @@ local mousePosition = ({
         
 }):init()
 
-local picture = {
-    jots = {
-        tailIndex = 0,
-
-        draw = function(self)
-            for n, jot in ipairs(self) do
-                if n <= self.tailIndex then
-                    jot:draw()
-                end
-            end
-        end,
-
-        add = function(self, jot)
-            self.tailIndex = self.tailIndex + 1
-            self[self.tailIndex] = jot
-        end,
-
-        undo = function(self)
-            self.tailIndex = math.max(0, self.tailIndex - 1)
-        end,
-
-        redo = function(self)
-            self.tailIndex = math.min(#self, self.tailIndex + 1)
-        end,
-    },
-
-    draw   = function(self)      self.jots:draw()   end,
-    addJot = function(self, jot) self.jots:add(jot) end,
-    undo   = function(self)      self.jots:undo()   end,
-    redo   = function(self)      self.jots:redo()   end,    
-}
-
+local picture      = require("tools/scribbler/picture")
 local scribbleTool = require("tools/scribbler/jotTools/scribble"):init(picture)
 local lineTool     = require("tools/scribbler/jotTools/line"    ):init(picture)
 local rectTool     = require("tools/scribbler/jotTools/rect"    ):init(picture)
