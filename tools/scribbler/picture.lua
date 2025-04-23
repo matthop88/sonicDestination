5544acbb865158b5076c13bfe51eefe7069438f5
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 14:50:55 -0700

    Copied picture code to tools/scribbler/picture.lua

---
+++ tools/scribbler/picture.lua
@@ -0,0 +1,31 @@
+return {
+    jots = {
+        tailIndex = 0,
+
+        draw = function(self)
+            for n, jot in ipairs(self) do
+                if n <= self.tailIndex then
+                    jot:draw()
+                end
+            end
+        end,
+
+        add = function(self, jot)
+            self.tailIndex = self.tailIndex + 1
+            self[self.tailIndex] = jot
+        end,
+
+        undo = function(self)
+            self.tailIndex = math.max(0, self.tailIndex - 1)
+        end,
+
+        redo = function(self)
+            self.tailIndex = math.min(#self, self.tailIndex + 1)
+        end,
+    },
+
+    draw   = function(self)      self.jots:draw()   end,
+    addJot = function(self, jot) self.jots:add(jot) end,
+    undo   = function(self)      self.jots:undo()   end,
+    redo   = function(self)      self.jots:redo()   end,    
+}
