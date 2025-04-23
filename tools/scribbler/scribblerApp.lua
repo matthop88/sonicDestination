f90cbd385fd5ba214d995de75cb3b45a90b0a844
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 14:48:58 -0700

    Moved draw, add, undo and redo inside jots object itself

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -70,28 +70,33 @@ local mousePosition = ({
 local picture = {
     jots = {
         tailIndex = 0,
+
+        draw = function(self)
+            for n, jot in ipairs(self) do
+                if n <= self.tailIndex then
+                    jot:draw()
+                end
+            end
+        end,
+
+        add = function(self, jot)
+            self.tailIndex = self.tailIndex + 1
+            self[self.tailIndex] = jot
+        end,
+
+        undo = function(self)
+            self.tailIndex = math.max(0, self.tailIndex - 1)
+        end,
+
+        redo = function(self)
+            self.tailIndex = math.min(#self, self.tailIndex + 1)
+        end,
     },
 
-    draw = function(self)
-        for n, jot in ipairs(self.jots) do
-            if n <= self.jots.tailIndex then
-                jot:draw()
-            end
-        end
-    end,
-
-    addJot = function(self, jot)
-        self.jots.tailIndex = self.jots.tailIndex + 1
-        self.jots[self.jots.tailIndex] = jot
-    end,
-
-    undo = function(self)
-        self.jots.tailIndex = math.max(0, self.jots.tailIndex - 1)
-    end,
-
-    redo = function(self)
-        self.jots.tailIndex = math.min(#self.jots, self.jots.tailIndex + 1)
-    end,    
+    draw   = function(self)      self.jots:draw()   end,
+    addJot = function(self, jot) self.jots:add(jot) end,
+    undo   = function(self)      self.jots:undo()   end,
+    redo   = function(self)      self.jots:redo()   end,    
 }
 
 local scribbleTool = require("tools/scribbler/jotTools/scribble"):init(picture)
