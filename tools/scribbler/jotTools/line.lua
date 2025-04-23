47923a61ae078f0bb050c1ca3871396546a30a09
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 14:58:17 -0700

    Line tool has jot inside of it, alpha when drawing

--- tools/scribbler/jotTools/line.lua
+++ tools/scribbler/jotTools/line.lua
@@ -4,26 +4,38 @@
         self.picture = picture
         return self
     end,
-    
-    data = { },
-    
-    draw = function(self, mx, my)
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.setLineWidth(5)
-        
-        local prevX, prevY = mx, my
-        
-        for n, pt in ipairs(self.data) do
-            if n == 1 then
-                love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
-            else
-                love.graphics.line(prevX, prevY, pt.x, pt.y)
-            end
-            prevX, prevY = pt.x, pt.y
-        end
 
-        love.graphics.line(prevX, prevY, mx, my)
+    jot = {
+        data = { },
+    
+        draw = function(self, mx, my)
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.setLineWidth(5)
+            
+            local prevX, prevY = mx, my
+            
+            for n, pt in ipairs(self.data) do
+                if n == 1 then
+                    love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
+                else
+                    love.graphics.line(prevX, prevY, pt.x, pt.y)
+                end
+                prevX, prevY = pt.x, pt.y
+            end
+        end,
+    },
+
+    draw = function(self, mx, my)
+        self.jot:draw()
+
+        if #self.jot.data > 0 then
+            love.graphics.setColor(1, 1, 1, 0.5)
+            local prevX = self.jot.data[#self.jot.data].x
+            local prevY = self.jot.data[#self.jot.data].y
+            love.graphics.line(prevX, prevY, mx, my)
+        end
         
+        love.graphics.setColor(1, 1, 1)
         love.mouse.setVisible(false)
         love.graphics.rectangle("fill", mx - 2, my - 2, 5, 5)
     end,
@@ -33,7 +45,7 @@
     end,
 
     penDown = function(self, mx, my)
-        table.insert(self.data, { x = mx, y = my })
+        table.insert(self.jot.data, { x = mx, y = my })
     end,
 
     penMoved = function(self, mx, my)
