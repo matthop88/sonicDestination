9496f7dd9a675567b8523ffb7b53a8dc0457b11f
Matthew Hopkins <matthop88@gmail.com>
2025-04-23 09:58:48 -0700

    Created rectJot by copying lineJot

--- tools/scribbler/scribblerApp.lua
+++ tools/scribbler/scribblerApp.lua
@@ -67,6 +67,49 @@ local mousePosition = ({
         
 }):init()
 
+local rectJot = { 
+    
+    data = { },
+
+    draw = function(self, mx, my)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.setLineWidth(5)
+        
+        local prevX, prevY = mx, my
+
+        for n, pt in ipairs(self.data) do
+            if n == 1 then
+                love.graphics.rectangle("fill", pt.x - 2, pt.y - 2, 5, 5)
+            else
+                love.graphics.line(prevX, prevY, pt.x, pt.y)
+            end
+            prevX, prevY = pt.x, pt.y
+        end
+
+        love.graphics.line(prevX, prevY, mx, my)
+
+        love.mouse.setVisible(false)
+        love.graphics.rectangle("fill", mx - 2, my - 2, 5, 5)
+    end,
+
+    penUp = function(self, mx, my)
+        -- Do nothing
+    end,
+
+    penDown = function(self, mx, my)
+        table.insert(self.data, { x = mx, y = my })
+    end,
+
+    penMoved = function(self, mx, my)
+        -- Do nothing
+    end,
+
+    penDragged = function(self, mx, my)
+        -- Do nothing
+    end,
+
+}
+
 local lineJot = { 
 
     data = { },
