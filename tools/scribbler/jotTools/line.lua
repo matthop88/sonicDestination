f90e033982c8c8b921523dcd3b158c16ad31a70e
Matthew Hopkins <matthop88@gmail.com>
2025-04-25 18:00:53 -0700

    Extracted mutableColor from line.lua

--- tools/scribbler/jotTools/line.lua
+++ tools/scribbler/jotTools/line.lua
@@ -1,26 +1,4 @@
-local mutableColor = {
-    index = 1,
-
-    { 1, 1, 1 },
-    { 1, 1, 0 },
-    { 1, 0, 1 },
-    { 1, 0, 0 },
-    { 0, 1, 1 },
-    { 0, 1, 0 },
-    { 0, 0, 1 },
-    { 0, 0, 0 },
-
-    next = function(self)
-        self.index = self.index + 1
-        if self.index > #self then
-            self.index = 1
-        end
-    end,
-
-    get = function(self)
-        return self[self.index]
-    end,
-}
+local mutableColor = require("tools/scribbler/mutableColor")
 
 local doLineJotDrawing = function(self, mx, my)
     local prevX, prevY = mx, my
