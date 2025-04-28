25dcae9e863666913f45b49dffc3375746e7ff19
Matthew Hopkins <matthop88@gmail.com>
2025-04-28 10:30:23 -0700

    Add files via upload

---
+++ tools/scribbler/data/standingSM.lua
@@ -0,0 +1,70 @@
+return { version = "0.2", 
+  {
+    name = "rect",
+    color = { 0, 0, 0 },
+    data = { x = 120, y = 630, w = 75, h = 34, filled = false },
+  },
+  {
+    name = "rect",
+    color = { 1, 1, 1 },
+    data = { x = 640, y = 192, w = 288, h = 224, filled = false },
+  },
+  {
+    name = "rect",
+    color = { 1, 1, 0 },
+    data = { x = 96, y = 192, w = 288, h = 224, filled = true },
+  },
+  {
+    name = "rect",
+    color = { 1, 1, 0 },
+    data = { x = 640, y = 192, w = 288, h = 224, filled = true },
+  },
+  {
+    name = "rect",
+    color = { 1, 1, 1 },
+    data = { x = 96, y = 192, w = 288, h = 224, filled = false },
+  },
+  {
+    name = "line",
+    color = { 1, 1, 1 },
+    data = {
+      { x = 384, y = 256 },
+      { x = 640, y = 256 },
+      { x = 608, y = 224 },
+      { x = 640, y = 256 },
+      { x = 608, y = 288 },
+    },
+  },
+  {
+    name = "line",
+    color = { 1, 1, 1 },
+    data = {
+      { x = 640, y = 352 },
+      { x = 384, y = 352 },
+      { x = 416, y = 320 },
+      { x = 384, y = 352 },
+      { x = 416, y = 384 },
+    },
+  },
+  {
+    name = "text",
+    color = { 0, 0, 0 },
+    data = { x = 136, y = 284, fontSize = 32, message = "Standing Left", },
+  },
+  {
+    name = "text",
+    color = { 0, 0, 0 },
+    data = { x = 669, y = 286, fontSize = 32, message = "Standing Right", },
+  },
+  {
+    name = "text",
+    color = { 1, 1, 1 },
+    data = { x = 460, y = 200, fontSize = 40, message = "R On", },
+  },
+  {
+    name = "text",
+    color = { 1, 1, 1 },
+    data = { x = 462, y = 297, fontSize = 40, message = "L On", },
+  },
+
+}
