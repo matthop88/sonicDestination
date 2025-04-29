c917f0d4042c9cc3a8b0002943345a462023832b
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 10:58:03 -0700

    Extracted PEGBOARD from viewer.lua

--- tools/stateMachine/viewer.lua
+++ tools/stateMachine/viewer.lua
@@ -39,30 +39,7 @@ local COLORS                      = require("tools/lib/colors")
 local LABEL_FONT                  = love.graphics.newFont(32)
 local GRID_SIZE                   = 32
 
-local PEGBOARD = {
-    draw = function(self)
-        self:drawBackground()
-        self:drawHoles()
-    end,
-
-    drawBackground = function(self)
-        love.graphics.setColor(COLORS.MOSS_GREEN)
-        love.graphics.rectangle("fill", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-    end,
-
-    drawHoles = function(self)
-        for y = 0, WINDOW_HEIGHT, 32 do
-            for x = 0, WINDOW_WIDTH, 32 do
-                self:drawHole(x, y)
-            end
-        end
-    end,
-
-    drawHole = function(self, x, y)
-        love.graphics.setColor(COLORS.TRANSLUCENT_WHITE)
-        love.graphics.rectangle("fill", x - 1, y - 1, 3, 3)
-    end,
-}
+local PEGBOARD                    = require("tools/stateMachine/pegboard")
 
 local BOX = {
     draw = function(self, label, x, y, w, h)
