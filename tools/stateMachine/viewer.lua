8d95ba1437851ee0d294fb15d495ce04f82bdda5
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 11:15:04 -0700

    Extracted ARROW from viewer.lua

--- tools/stateMachine/viewer.lua
+++ tools/stateMachine/viewer.lua
@@ -41,49 +41,7 @@ local GRID_SIZE                   = 32
 
 local PEGBOARD                    = require("tools/stateMachine/pegboard"):init(GRID_SIZE)
 local BOX                         = require("tools/stateMachine/box"):init(GRID_SIZE, LABEL_FONT)
-
-local ARROW = {
-    draw = function(self, label, x1, y1, x2, y2)
-        love.graphics.setColor(COLORS.MEDIUM_YELLOW)
-        love.graphics.setLineWidth(4)
-        self:drawComponents(label, x1 * GRID_SIZE, y1 * GRID_SIZE, x2 * GRID_SIZE, y2 * GRID_SIZE)
-    end,
-
-    drawComponents = function(self, label, x1, y1, x2, y2)
-        self:drawBody(x1, y1, x2, y2)
-        self:drawHead(x1, y1, x2, y2)
-        self:drawLabel(label, x1, y1, x2, y2)
-    end,
-   
-    drawBody = function(self, x1, y1, x2, y2)
-        love.graphics.line(x1, y1, x2, y2)
-    end,
-
-    drawHead = function(self, x1, y1, x2, y2)
-        if x2 > x1 then
-            self:drawHeadRight(x1, y1, x2, y2)
-        else
-            self:drawHeadLeft( x1, y1, x2, y2)
-        end
-    end,
-
-    drawHeadRight = function(self, x1, y1, x2, y2)
-        love.graphics.line(x2, y2, x2 - 24, y2 - 16)
-        love.graphics.line(x2, y2, x2 - 24, y2 + 16)
-    end,
-
-    drawHeadLeft = function(self, x1, y1, x2, y2)
-        love.graphics.line(x2, y2, x2 + 24, y2 - 16)
-        love.graphics.line(x2, y2, x2 + 24, y2 + 16)
-    end,
-
-    drawLabel = function(self, label, x1, y1, x2, y2)
-        love.graphics.setColor(COLORS.PURE_WHITE)
-        love.graphics.printf(label, math.min(x1,  x2), y1 - LABEL_FONT:getHeight(), 
-                                    math.abs(x2 - x1), "center")
-    end,
-
-}
+local ARROW                       = require("tools/stateMachine/arrow"):init(GRID_SIZE, LABEL_FONT)
 
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
