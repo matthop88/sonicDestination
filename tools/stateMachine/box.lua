cbadccacb8858d363ab76976804ab800911ef021
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 11:12:15 -0700

    Created box.lua

---
+++ tools/stateMachine/box.lua
@@ -0,0 +1,34 @@
+local COLORS = require "tools/lib/colors"
+local GRID_SIZE, LABEL_FONT
+
+return {
+    init = function(self, gridSize, labelFont)
+        GRID_SIZE = gridSize
+        LABEL_FONT = labelFont
+        return self
+    end,
+
+    draw = function(self, label, x, y, w, h)
+        self:drawComponents(label, x * GRID_SIZE, y * GRID_SIZE, 
+                                   w * GRID_SIZE, h * GRID_SIZE)
+    end,
+
+    drawComponents = function(self, label, x, y, w, h)
+        self:drawShape(       x, y, w, h)
+        self:drawLabel(label, x, y, w, h)
+    end,
+
+    drawShape = function(self, x, y, w, h)
+        love.graphics.setColor(COLORS.LIGHT_YELLOW)
+        love.graphics.rectangle("fill", x, y, w, h)
+        love.graphics.setColor(COLORS.JET_BLACK)
+        love.graphics.setLineWidth(5)
+        love.graphics.rectangle("line", x, y, w, h)
+    end,
+
+    drawLabel = function(self, label, x, y, w, h)
+        love.graphics.setFont(LABEL_FONT) 
+        love.graphics.setColor(COLORS.JET_BLACK)
+        love.graphics.printf(label, x, y + (h - LABEL_FONT:getHeight()) / 2, w, "center")
+    end,
+}
