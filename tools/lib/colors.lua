cd19f69e1b0d3a75a80633c256be08037ef4d664
Matthew Hopkins <matthop88@gmail.com>
2025-04-28 17:49:26 -0700

    Changed PEGBOARD_GREEN to MOSS_GREEN and PEG_HOLE to TRANSLUCENT_WHITE

--- tools/lib/colors.lua
+++ tools/lib/colors.lua
@@ -1,11 +1,11 @@
 return {
-    PURE_WHITE        = { 1,    1,    1         },
+	PURE_WHITE        = { 1,    1,    1         },
 	TRANSPARENT_WHITE = { 1,    1,    1,    0.1 },
+	TRANSLUCENT_WHITE = { 1,    1,    1,    0.5 },
 	JET_BLACK         = { 0,    0,    0         },
 	TRANSPARENT_BLACK = { 0,    0,    0,    0.5 },
 	LIGHT_GREY        = { 0.75, 0.75, 0.75      },
 	LIGHT_YELLOW      = { 1,    1,    0.75      },
 	MEDIUM_YELLOW     = { 1,    0.8,  0.5       },
-	PEGBOARD_GREEN    = { 0,    0.3,  0.3       },
-	PEGHOLES          = { 1,    1,    1,    0.5 },
+	MOSS_GREEN        = { 0,    0.3,  0.3       },
 }
