662cb2f2c57af45a6b4f4d4a909f13b1ed5969cd
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 17:29:58 -0700

    Graphics.lua now allows x and y positional shifting

--- tools/lib/graphics.lua
+++ tools/lib/graphics.lua
@@ -1,12 +1,20 @@
 return {
-	x = 0,
-	y = 0,
+    x = 0,
+    y = 0,
+    
+    setColor = function(self, color)
+        love.graphics.setColor(color)
+    end,
 
-	setColor = function(self, color)
-		love.graphics.setColor(color)
-	end,
+    rectangle = function(self, mode, x, y, w, h)
+        love.graphics.rectangle(mode, x + self.x, y + self.y, w, h)
+    end,
 
-	rectangle = function(self, mode, x, y, w, h)
-		love.graphics.rectangle(mode, x, y, w, h)
-	end, 
+    setX = function(self, x)
+        self.x = x
+    end,
+    
+    setY = function(self, y)
+        self.y = y
+    end,
 }
