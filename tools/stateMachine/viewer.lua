855c431ccdc1a3885c026a7ba7e235e303ba097a
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 18:09:00 -0700

    Updated viewer.lua to pass GRAFX into arrow

--- tools/stateMachine/viewer.lua
+++ tools/stateMachine/viewer.lua
@@ -41,7 +41,7 @@ local GRAFX                       = require "tools/lib/graphics"
 
 local PEGBOARD                    = require("tools/stateMachine/pegboard"):init(GRID_SIZE, GRAFX)
 local BOX                         = require("tools/stateMachine/box"):init(GRID_SIZE, LABEL_FONT, GRAFX)
-local ARROW                       = require("tools/stateMachine/arrow"):init(GRID_SIZE, LABEL_FONT)
+local ARROW                       = require("tools/stateMachine/arrow"):init(GRID_SIZE, LABEL_FONT, GRAFX)
 
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
