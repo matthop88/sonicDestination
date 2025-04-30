1ad6cf3e5b681a383b148bfc8388fa85d717edb6
Matthew Hopkins <matthop88@gmail.com>
2025-04-29 18:00:35 -0700

    Divided graphics.lua into sections

--- tools/lib/graphics.lua
+++ tools/lib/graphics.lua
@@ -1,15 +1,21 @@
 return {
     x = 0,
     y = 0,
+
+    ---------------------- Property Setter Functions -------------------
     
     setColor = function(self, color)
         love.graphics.setColor(color)
     end,
 
+    ----------------------- Shape Drawing Functions --------------------
+    
     rectangle = function(self, mode, x, y, w, h)
         love.graphics.rectangle(mode, x + self.x, y + self.y, w, h)
     end,
 
+    ------------------------- Scrolling Functions ----------------------
+    
     setX = function(self, x)
         self.x = x
     end,
@@ -18,11 +24,7 @@ return {
         self.y = y
     end,
 
-    keepImageInBounds = function(self)
-        -- do nothing
-    end,
-
-    moveImage = function(self, deltaX, deltaY)
+     moveImage = function(self, deltaX, deltaY)
         self.x = self.x + deltaX
         self.y = self.y + deltaY
     end,
