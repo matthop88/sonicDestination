7f881222e51bd4314106b614f8a49fb71d11528b
Matthew Hopkins <matthop88@gmail.com>
2025-05-02 15:19:37 -0700

    Added highlighted methods to arrow. Some refactoring of mouseInsideRect(), etc

--- tools/stateMachine/arrow.lua
+++ tools/stateMachine/arrow.lua
@@ -32,11 +32,15 @@ local function lineToRect(x1, y1, x2, y2)
     return x, y, w, h
 end
 
-local function isPtInside(sx, sy, x1, y1, x2, y2)
-    local x, y, w, h = lineToRect(x1, y1, x2, y2)
-
+local function isPtInRect(sx, sy, x, y, w, h))
     local px, py = GRAFX:screenToImageCoordinates(sx, sy)
     return px >= x and px <= x + w and py >= y and py <= y + h
+end
+
+local function isMouseInsideRect(x, y, w, h)
+    local mx, my = love.mouse.getPosition()
+    
+    return isPtInRect(mx, my, x, y, w, h)
 end
 
 return {
@@ -53,18 +57,27 @@ return {
     end,
 
     drawComponents = function(self, label, x1, y1, x2, y2)
-        self:drawBody(x1, y1, x2, y2)
-        self:drawHead(x1, y1, x2, y2)
-        self:drawLabel(label, x1, y1, x2, y2)
+        if not isMouseInsideRect(lineToRect(x1, y1, x2, y2)) then
+            self:drawBody(        x1, y1, x2, y2)
+            self:drawHead(        x1, y1, x2, y2)
+            self:drawLabel(label, x1, y1, x2, y2)
+        else
+            self:drawHighlightedBody(        x1, y1, x2, y2)
+            self:drawHighlightedHead(        x1, y1, x2, y2)
+            self:drawHighlightedLabel(label, x1, y1, x2, y2)
+        end
     end,
 
     drawBody = function(self, x1, y1, x2, y2)
-        local mx, my = love.mouse.getPosition()
-        if not isPtInside(mx, my, x1, y1, x2, y2) then
-            GRAFX:setColor(COLORS.MEDIUM_YELLOW)
-            GRAFX:setLineWidth(4)
-            GRAFX:line(x1, y1, x2, y2)
-        end
+        GRAFX:setColor(COLORS.MEDIUM_YELLOW)
+        GRAFX:setLineWidth(4)
+        GRAFX:line(x1, y1, x2, y2)
+    end,
+
+    drawHighlightedBody = function(self, x1, y1, x2, y2)
+        GRAFX:setColor(COLORS.RED)
+        GRAFX:setLineWidth(4)
+        GRAFX:line(x1, y1, x2, y2)
     end,
 
     drawHead = function(self, x1, y1, x2, y2)
@@ -75,8 +88,23 @@ return {
         end
     end,
 
+    drawHighlightedHead = function(self, x1, y1, x2, y2)
+        if x2 > x1 then
+            self:drawHighlightedHeadRight(x1, y1, x2, y2)
+        else
+            self:drawHighlightedHeadLeft( x1, y1, x2, y2)
+        end
+    end,
+
     drawHeadRight = function(self, x1, y1, x2, y2)
         GRAFX:setColor(COLORS.MEDIUM_YELLOW)
+        GRAFX:setLineWidth(4)
+        GRAFX:line(x2, y2, x2 - 24, y2 - 16)
+        GRAFX:line(x2, y2, x2 - 24, y2 + 16)
+    end,
+
+    drawHighlightedHeadRight = function(self, x1, y1, x2, y2)
+        GRAFX:setColor(COLORS.RED)
         GRAFX:setLineWidth(4)
         GRAFX:line(x2, y2, x2 - 24, y2 - 16)
         GRAFX:line(x2, y2, x2 - 24, y2 + 16)
@@ -89,8 +117,22 @@ return {
         GRAFX:line(x2, y2, x2 + 24, y2 + 16)
     end,
 
+    drawHighlightedHeadLeft = function(self, x1, y1, x2, y2)
+        GRAFX:setColor(COLORS.RED)
+        GRAFX:setLineWidth(4)
+        GRAFX:line(x2, y2, x2 + 24, y2 - 16)
+        GRAFX:line(x2, y2, x2 + 24, y2 + 16)
+    end,
+
     drawLabel = function(self, label, x1, y1, x2, y2)
         GRAFX:setFontSize(LABEL_FONT_SIZE) 
+        GRAFX:setColor(COLORS.PURE_WHITE)
+        GRAFX:printf(label, math.min(x1,  x2), y1 - GRAFX:getFontHeight(), 
+                            math.abs(x2 - x1), "center")
+    end,
+
+    drawHighlightedLabel = function(self, label, x1, y1, x2, y2)
+        GRAFX:setFontSize(LABEL_FONT_SIZE + 2) 
         GRAFX:setColor(COLORS.PURE_WHITE)
         GRAFX:printf(label, math.min(x1,  x2), y1 - GRAFX:getFontHeight(), 
                             math.abs(x2 - x1), "center")
