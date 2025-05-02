161ee2c9cb1daa85558d33ca2a3dda132f776124
Matthew Hopkins <matthop88@gmail.com>
2025-05-02 14:46:45 -0700

    Added RED to colors.lua

--- tools/lib/colors.lua
+++ tools/lib/colors.lua
@@ -5,6 +5,7 @@ return {
 	JET_BLACK         = { 0,    0,    0         },
 	TRANSPARENT_BLACK = { 0,    0,    0,    0.5 },
 	LIGHT_GREY        = { 0.75, 0.75, 0.75      },
+	RED               = { 1,    0,    0         },
 	LIGHT_YELLOW      = { 1,    1,    0.75      },
 	MEDIUM_YELLOW     = { 1,    0.8,  0.5       },
 	MOSS_GREEN        = { 0,    0.3,  0.3       },
