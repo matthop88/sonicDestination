2d14ff29d6f3d46fd828616aa0b409defb599296
Matthew Hopkins <matthop88@gmail.com>
2025-05-13 13:39:43 -0700

    Changed standing structure to match running structure. Code is a bit ugly

--- game/sprite.lua
+++ game/sprite.lua
@@ -3,8 +3,10 @@ local COLOR_PURE_WHITE = { 1, 1, 1 }
 return ({
     image    = love.graphics.newImage(relativePath("resources/images/spriteSheets/sonic1Transparent.png")),
     standing = {
-        rect = { x = 43, y = 257, w = 32, h = 40 },
-        quad = nil,
+        rects = {
+            { x =  43, y = 257, w = 32, h = 40 },
+        },
+        quads = { },
     },
     running  = {
         rects = {
@@ -15,25 +17,25 @@ return ({
             { x = 319, y = 347, w = 40, h = 40 },
             { x = 390, y = 348, w = 40, h = 40 },
         },
-        quads = {
-
-        },
+        quads = { },
     },
     
     offset   = { x = 16, y = 20 },
     scale    = { x =  3, y =  3 },
 
     init = function(self)
-        self.standing.quad = love.graphics.newQuad(self.standing.rect.x,  self.standing.rect.y,
-                                                   self.standing.rect.w,  self.standing.rect.h,
-                                                   self.image:getWidth(), self.image:getHeight())
+        table.insert(self.standing.quads, love.graphics.newQuad(
+            self.standing.rects[1].x,     self.standing.rects[1].y,
+            self.standing.rects[1].w,     self.standing.rects[1].h,
+            self.image:getWidth(),        self.image:getHeight()))
+        
         self.image:setFilter("nearest", "nearest")
         return self
     end,
 
     draw = function(self, x, y)
         love.graphics.setColor(COLOR_PURE_WHITE)
-        love.graphics.draw(self.image,        self.standing.quad,
+        love.graphics.draw(self.image,        self.standing.quads[1],
                            self:getImageX(x), self:getImageY(y),
                         0, self.scale.x,      self.scale.y)
     end,
