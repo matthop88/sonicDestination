7d780969d3e8a56f04c47c3adc5dd60c8edfc9f9
Matthew Hopkins <matthop88@gmail.com>
2025-05-14 12:47:13 -0700

    Modified workspace to utilize tools/lib/graphics

--- game/workspace.lua
+++ game/workspace.lua
@@ -1,3 +1,5 @@
+local graphics = require("tools/lib/graphics")
+
 local COLOR_GREEN         = { 0, 0.45, 0 }
                             -- https://htmlcolorcodes.com/colors/shades-of-green/
 local COLOR_PURE_WHITE    = { 1, 1,    1 }
@@ -9,24 +11,16 @@ return {
     draw = function(self)
         self:drawBackground()
         self:drawHorizontalLine()
-        --self:drawVerticalLines()
     end,
 
     drawBackground = function(self)
-        love.graphics.setColor(COLOR_GREEN)
-        love.graphics.rectangle("fill", 0, 0, self:getWidth(), self:getHeight())
+        graphics:setColor(COLOR_GREEN)
+        graphics:rectangle("fill", 0, 0, self:getWidth(), self:getHeight())
     end,
 
     drawHorizontalLine = function(self)
-        love.graphics.setColor(COLOR_PURE_WHITE)
-        love.graphics.setLineWidth(3)
-        love.graphics.line(0, self:getHeight() * 3/4, self:getWidth(), self:getHeight() * 3/4)
-    end,
-
-    drawVerticalLines = function(self)
-        love.graphics.setLineWidth(1)
-        love.graphics.line(self:getWidth() / 2,      0, self:getWidth() / 2,      self:getHeight())
-        love.graphics.line(self:getWidth() / 2 - 48, 0, self:getWidth() / 2 - 48, self:getHeight())
-        love.graphics.line(self:getWidth() / 2 + 48, 0, self:getWidth() / 2 + 48, self:getHeight())
+        graphics:setColor(COLOR_PURE_WHITE)
+        graphics:setLineWidth(3)
+        graphics:line(0, self:getHeight() * 3/4, self:getWidth(), self:getHeight() * 3/4)
     end,
 }
