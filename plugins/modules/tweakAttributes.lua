c3089315ac2423a30c6e51a9337f592b75b7dcbe
Matthew Hopkins <matthop88@gmail.com>
2025-05-14 14:36:02 -0700

    Added init() function to tweakAttributes.lua which takes object, incAttributeKey, decAttributeKey, and an attributes table

--- plugins/modules/tweakAttributes.lua
+++ plugins/modules/tweakAttributes.lua
@@ -1,1 +1,15 @@
-return { }
+return {
+    object           = nil,
+    incAttributeKey  = nil,
+    decAttributeKey  = nil,
+    attributes       = { },
+
+    init             = function(self, params)
+        self.object          = params.object
+        self.incAttributeKey = params.incAttributeKey
+        self.decAttributeKey = params.decAttributeKey
+        self.attributes      = params.attributes
+
+        return self
+    end,
+}
