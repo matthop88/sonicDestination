9e3ba4c98b3597b5375bf82161df29d595ab0b2a
Matthew Hopkins <matthop88@gmail.com>
2025-05-14 14:43:43 -0700

    Added draw() function to tweakAttributes.lua to show that an attribute is active

--- plugins/modules/tweakAttributes.lua
+++ plugins/modules/tweakAttributes.lua
@@ -13,6 +13,15 @@ return {
         return self
     end,
 
+    draw             = function(self)
+        love.graphics.setColor(1, 1, 1)
+        for attName, attribute in pairs(self.attributes) do
+            if attribute.active then
+                love.graphics.printf("" .. attName .. " IS ACTIVE", 0, 600, 1024, "center")
+            end
+        end
+    end,
+    
     handleKeypressed = function(self, key)
         -- Check to see if key matches toggleShowKey on any attribute
         for attName, attribute in pairs(self.attributes) do
