9850cb0b65150b4eb89ce1bcbc1a74e53775dbcd
Matthew Hopkins <matthop88@gmail.com>
2025-05-14 14:40:28 -0700

    Added handleKeypressed() function to tweakAttributes.lua to toggle show key for different attributes. Using print to console to test.

--- plugins/modules/tweakAttributes.lua
+++ plugins/modules/tweakAttributes.lua
@@ -12,4 +12,14 @@ return {
 
         return self
     end,
+
+    handleKeypressed = function(self, key)
+        -- Check to see if key matches toggleShowKey on any attribute
+        for attName, attribute in pairs(self.attributes) do
+            if key == attribute.toggleShowKey then
+                attribute.active = not attribute.active
+                print("Attribute " .. attName .. " is active: ", attribute.active)
+            end
+        end
+    end,
 }
