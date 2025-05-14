cd3e3f345bc850c15e2ad3335dcf3671fd13806f
Matthew Hopkins <matthop88@gmail.com>
2025-05-14 11:01:16 -0700

    Rolled image, init(), addQuads(), getCurrentFrame(), advanceFrame() into animations. Also added getImage().

--- game/sprite.lua
+++ game/sprite.lua
@@ -1,75 +1,80 @@
 local COLOR_PURE_WHITE = { 1, 1, 1 }
 
 return ({
-    image    = love.graphics.newImage(relativePath("resources/images/spriteSheets/sonic1Transparent.png")),
     animations = {
-        standing = {
-            rects = {
-                { x =  43, y = 257, w = 32, h = 40 },
+        image    = love.graphics.newImage(relativePath("resources/images/spriteSheets/sonic1Transparent.png")),
+        data = {
+            standing = {
+                rects = {
+                    { x =  43, y = 257, w = 32, h = 40 },
+                },
+                quads = { },
             },
-            quads = { },
-        },
-        running  = {
-            rects = {
-                { x =  46, y = 349, w = 24, h = 40 },
-                { x = 109, y = 347, w = 40, h = 40 },
-                { x = 178, y = 348, w = 32, h = 40 },
-                { x = 249, y = 349, w = 40, h = 40 },
-                { x = 319, y = 347, w = 40, h = 40 },
-                { x = 390, y = 348, w = 40, h = 40 },
+            running  = {
+                rects = {
+                    { x =  46, y = 349, w = 24, h = 40 },
+                    { x = 109, y = 347, w = 40, h = 40 },
+                    { x = 178, y = 348, w = 32, h = 40 },
+                    { x = 249, y = 349, w = 40, h = 40 },
+                    { x = 319, y = 347, w = 40, h = 40 },
+                    { x = 390, y = 348, w = 40, h = 40 },
+                },
+                quads = { },
             },
-            quads = { },
         },
-    },
-    
-    currentAnimation  = nil,
-    currentFrameIndex = 1,
+
+        currentAnimation  = nil,
+        currentFrameIndex = 1,
         
+        init = function(self)
+            self.image:setFilter("nearest", "nearest")
+            self:addQuads()
+            self.currentAnimation  = self.data.running
+            self.currentFrameIndex = 1,
+
+            return self
+        end,
+
+        addQuads = function(self)
+            for _, animation in pairs(self.data) do
+                for _, rect in ipairs(animation.rects) do
+                    table.insert(animation.quads, love.graphics.newQuad(
+                        rect.x, rect.y, rect.w, rect.h,
+                        self.image:getWidth(), self.image:getHeight()))
+                end
+            end
+        end,
+
+        getImage = function(self)
+            return self.image
+        end,
+
+        getCurrentFrame = function(self)
+            return self.currentAnimation.quads[self.currentFrameIndex]
+        end,
+
+        advanceFrame = function(self)
+            self.currentFrameIndex = self.currentFrameIndex + 1
+            if self.currentFrameIndex > #self.currentAnimation.quads then
+                self.currentFrameIndex = 1
+            end
+        end,
+    },
+       
     offset   = { x = 16, y = 20 },
     scale    = { x =  3, y =  3 },
 
-    init = function(self)
-        self:addQuads()
-        
-        self.currentAnimation = self.animations.running
-        self.currentFrameIndex = 3
-            
-        self.image:setFilter("nearest", "nearest")
-        return self
-    end,
-
     draw = function(self, x, y)
         love.graphics.setColor(COLOR_PURE_WHITE)
-        love.graphics.draw(self.image,        self:getCurrentFrame(),
-                           self:getImageX(x), self:getImageY(y),
-                        0, self.scale.x,      self.scale.y)
+        love.graphics.draw(self.animations:getImage(), self.animations:getCurrentFrame(),
+                           self:getImageX(x),          self:getImageY(y),
+                           0, self.scale.x,            self.scale.y)
     end,
 
     --------------------------------------------------------------
     --                  Specialized Functions                   --
     --------------------------------------------------------------
 
-    addQuads = function(self)
-        for _, animation in pairs(self.animations) do
-            for _, rect in ipairs(animation.rects) do
-                table.insert(animation.quads, love.graphics.newQuad(
-                    rect.x, rect.y, rect.w, rect.h,
-                    self.image:getWidth(), self.image:getHeight()))
-            end
-        end
-    end,
-        
-    getCurrentFrame = function(self)
-        return self.currentAnimation.quads[self.currentFrameIndex]
-    end,
-
-    advanceFrame = function(self)
-        self.currentFrameIndex = self.currentFrameIndex + 1
-        if self.currentFrameIndex > #self.currentAnimation.quads then
-            self.currentFrameIndex = 1
-        end
-    end,
-        
     getImageX  = function(self, x) return x - (self.offset.x * self.scale.x) end,
     getImageY  = function(self, y) return y - (self.offset.y * self.scale.y) end,
 
