7b48e9ed6cc655f568aba466189c88cb0c12adc7
Matthew Hopkins <matthop88@gmail.com>
2025-05-17 14:12:41 -0700

    Added sections and dummy keypressed and keyreleased functions to modKeyEnabled plugin

--- plugins/modules/modKeyEnabler.lua
+++ plugins/modules/modKeyEnabler.lua
@@ -17,6 +17,27 @@ return {
     'left'   is pressed  -> if shift is held, 'shiftleft' keypress is transmitted.
     'left'   is released -> 'shiftleft' keyrelease is transmitted,
                             regardless of status of shift
-    
     --]]
+
+    --------------------------------------------------------------
+    --                     Member Variables                     --
+    --------------------------------------------------------------
+    
+    -- ...
+    -- ...
+    -- ...
+    
+    --------------------------------------------------------------
+    --                 LOVE2D Delegated Functions               --
+    --------------------------------------------------------------
+    
+    keypressed  = function(self, key)
+        -- code for transmitting keypressed events goes here
+        -- return keyToTransmit
+    end,
+    
+    keyreleased = function(self, key)
+        -- code for transmitting keyreleased events goes here
+        -- return keyToTransmit
+    end,
 }
