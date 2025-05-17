624c4155759f86b58f767931045d553f6f72f4ae
Matthew Hopkins <matthop88@gmail.com>
2025-05-17 14:02:12 -0700

    Created modKeyEnabler.lua, with basic comments.

---
+++ plugins/modules/modKeyEnabler.lua
@@ -0,0 +1,14 @@
+return {
+    --[[
+    Mod Key Enabler is designed to generate more specific key events
+    based upon a key being pressed while a modifier key is down.
+    
+    For example, an uppercase A is written with a shift keypress and
+    an 'a' keypress. Both of these are ordinarily separate events.
+    However, if the shift is held down, this manager should
+    transmit an 'A' keypressed key.
+    
+    A shift keypress can be either an 'lshift' or an 'rshift'.
+    
+    --]]
+}
