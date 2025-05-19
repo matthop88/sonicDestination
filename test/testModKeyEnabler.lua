67204cb21c9cb55490894c4e52963381afa3783e
Matthew Hopkins <matthop88@gmail.com>
2025-05-19 12:51:05 -0700

    Updated testModKeyEnabler.lua to reflect fixes in method names

--- test/testModKeyEnabler.lua
+++ test/testModKeyEnabler.lua
@@ -8,13 +8,13 @@ function runTests()
     
     print("\nRunning Tests\n-------------")
     
-    local lshiftResult = modKeyEnabler:keypressed("lshift")
-    local rshiftResult = modKeyEnabler:keypressed("rshift")
+    local lshiftResult = modKeyEnabler:handleKeypressed("lshift")
+    local rshiftResult = modKeyEnabler:handleKeypressed("rshift")
     
     if lshiftResult == true and rshiftResult == true then
-        print("PASSED => Test #1: lshift and rshift were consumed by modKeyEnabler")
+        print("PASSED => Test #1: lshift and rshift keypressed were consumed by modKeyEnabler")
     else
-        print("FAILED => Test #1: lshift and rshift were NOT consumed by modKeyEnabler")
+        print("FAILED => Test #1: lshift and rshift keypressed were NOT consumed by modKeyEnabler")
     end
     
     love.event.quit()
