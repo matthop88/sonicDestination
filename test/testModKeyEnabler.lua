7d6cfe401cb325191d7b94949db9cf2e096a1d22
Matthew Hopkins <matthop88@gmail.com>
2025-05-19 12:42:41 -0700

    Extracted delayTests code entirely

--- test/testModKeyEnabler.lua
+++ test/testModKeyEnabler.lua
@@ -1,3 +1,5 @@
+require "test/delayTests"
+
 function runTests()
     -- Test #1
     -- When shift key is pressed (lshift or rshift), no event is transmitted
@@ -5,17 +7,4 @@ function runTests()
     print("Test #1... testing shift key being pressed.")
     
     love.event.quit()
-end
-
-local textTimer    = 0
-local waitForTests = true
-
-function love.update(dt)
-    if waitForTests then
-        textTimer = textTimer + dt
-        if textTimer > 1 then
-            waitForTests = false
-            runTests()
-        end
-    end
 end
