29f9bce625becd5fc22bb8e518c7dc6ed59c4c7c
Matthew Hopkins <matthop88@gmail.com>
2025-05-19 12:48:03 -0700

    Crude code in testModKeyEnabler.lua which implements failing test

--- test/testModKeyEnabler.lua
+++ test/testModKeyEnabler.lua
@@ -1,10 +1,21 @@
 require "test/delayTests"
 
+local modKeyEnabler = require "plugins/modules/modKeyEnabler"
+
 function runTests()
     -- Test #1
     -- When shift key is pressed (lshift or rshift), no event is transmitted
     
-    print("Test #1... testing shift key being pressed.")
+    print("\nRunning Tests\n-------------")
+    
+    local lshiftResult = modKeyEnabler:keypressed("lshift")
+    local rshiftResult = modKeyEnabler:keypressed("rshift")
+    
+    if lshiftResult == true and rshiftResult == true then
+        print("PASSED => Test #1: lshift and rshift were consumed by modKeyEnabler")
+    else
+        print("FAILED => Test #1: lshift and rshift were NOT consumed by modKeyEnabler")
+    end
     
     love.event.quit()
 end
