09d422ff2d84c86873fe65f8d4a15ea2d0c671cb
Matthew Hopkins <matthop88@gmail.com>
2025-05-20 10:46:55 -0700

    Fixed test #1 so that it passes

--- test/testModKeyEnabler.lua
+++ test/testModKeyEnabler.lua
@@ -11,7 +11,7 @@ function runTests()
     local lshiftResult = modKeyEnabler:prehandleKeypressed("lshift")
     local rshiftResult = modKeyEnabler:prehandleKeypressed("rshift")
     
-    if lshiftResult == true and rshiftResult == true then
+    if lshiftResult == nil and rshiftResult == nil then
         print("PASSED => Test #1: lshift and rshift keypressed were consumed by modKeyEnabler")
     else
         print("FAILED => Test #1: lshift and rshift keypressed were NOT consumed by modKeyEnabler")
