2175986b42b24e43bf630a797e896aca894afa8a
Matthew Hopkins <matthop88@gmail.com>
2025-05-23 13:56:03 -0700

    Modified delayTests.lua to run all tests

--- test/delayTests.lua
+++ test/delayTests.lua
@@ -6,7 +6,7 @@ function love.update(dt)
         textTimer = textTimer + dt
         if textTimer > 1 then
             waitForTests = false
-            TESTS:runAll()
+            TESTING:runAll()
         end
     end
 end
