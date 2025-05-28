712edbf485177ebc13ec4975c26f4ed0967ad374
Matthew Hopkins <matthop88@gmail.com>
2025-05-28 12:22:59 -0700

    Added frameTimer and update() function to sprites/sprite.lua. Currently not called.

--- game/sprites/sprite.lua
+++ game/sprites/sprite.lua
@@ -2,8 +2,19 @@ return {
     animations = requireRelative("sprites/animations"):init("sonic1"),
     scale      = { x =  3, y =  3 },
 
+    frameTimer = 0,
+    
     draw = function(self, x, y)
         self.animations:draw(x, y, self.scale.x, self.scale.y)
+    end,
+
+    update = function(self, dt)
+        -- Increase frame index at a rate of 1 fps
+        self.frameTimer = self.frameTimer + dt
+        if self.frameTimer >= 1 then
+            self.frameTimer = 0
+            self.animations:advanceFrame()
+        end
     end,
 
     --------------------------------------------------------------
