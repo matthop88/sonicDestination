44def85777527aec0631bcb04a8c17d4d0a8c5a9
Matthew Hopkins <matthop88@gmail.com>
2025-05-28 12:10:27 -0700

    Added fps fields to animations in sprites/data/sonic1.lua

--- game/sprites/data/sonic1.lua
+++ game/sprites/data/sonic1.lua
@@ -1,11 +1,11 @@
 return {
     imageName  = "sonic1Transparent",
     animations = {
-        standing = { isDefault = true,
+        standing = { fps = 1, isDefault = true,
             -------------------------------------------------------------------
             { x =  43, y = 257, w = 32, h = 40, offset = { x = 12, y = 20 }, },
         },
-        running  = {
+        running  = { fps = 10,
             { x =  46, y = 349, w = 24, h = 40, offset = { x =  8, y = 19 }, },
             { x = 109, y = 347, w = 40, h = 40, offset = { x = 16, y = 19 }, },
             { x = 178, y = 348, w = 32, h = 40, offset = { x = 16, y = 19 }, },
