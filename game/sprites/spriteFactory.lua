8a9551141fd32a354cad33c8fd36296df679705e
Matthew Hopkins <matthop88@gmail.com>
2025-05-29 12:31:54 -0700

    Changed sprite.lua to spriteFactory.lua, added create() function

---
+++ game/sprites/spriteFactory.lua
@@ -0,0 +1,23 @@
+return {
+    create = function(self, name)
+        return {
+            animations = requireRelative("sprites/animations"):init(name),
+            scale      = { x =  3, y =  3 },
+        
+            draw = function(self, x, y)
+                self.animations:draw(x, y, self.scale.x, self.scale.y)
+            end,
+        
+            update = function(self, dt)
+                self.animations:update(dt)
+            end,
+        
+            --------------------------------------------------------------
+            --                  Specialized Functions                   --
+            --------------------------------------------------------------
+        
+            isXFlipped = function(self)    return self.scale.x < 0                   end,
+            flipX      = function(self)    self.scale.x = self.scale.x * -1          end,
+        }
+    end,      
+}
