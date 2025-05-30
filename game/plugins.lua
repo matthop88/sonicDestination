313d762ed2ed63c1e596677a446b94d575d9cefd
Matthew Hopkins <matthop88@gmail.com>
2025-05-30 10:38:07 -0700

    Derived SPRITE and ANIMATIONS inside of plugins.lua to make for easier reading

--- game/plugins.lua
+++ game/plugins.lua
@@ -1,8 +1,10 @@
 return {
     init = function(self, params)
-        self.SONIC    = params.SONIC
-        self.GRAPHICS = params.GRAPHICS
-
+        self.GRAPHICS   = params.GRAPHICS
+        self.SONIC      = params.SONIC
+        self.SPRITE     = self.SONIC.sprite
+        self.ANIMATIONS = self.SPRITE.animations
+        
         return self:initPlugins()
     end,
 
@@ -30,52 +32,52 @@ return {
                     frameIndex = {
                         name = "Frame Index",
                         incrementFn = function()
-                            self.SONIC.sprite:setCurrentFrameIndex(self.SONIC.sprite:getCurrentFrameIndex() + 1)
+                            self.SPRITE:setCurrentFrameIndex(self.SPRITE:getCurrentFrameIndex() + 1)
                         end,
                         decrementFn = function()
-                            self.SONIC.sprite:setCurrentFrameIndex(self.SONIC.sprite:getCurrentFrameIndex() - 1)
+                            self.SPRITE:setCurrentFrameIndex(self.SPRITE:getCurrentFrameIndex() - 1)
                         end,
                         getValueFn  = function()
-                            return self.SONIC.sprite:getCurrentFrameIndex()
+                            return self.SPRITE:getCurrentFrameIndex()
                         end,
                         toggleShowKey = "f",
                     },
                     fps = {
                         name = "FPS",
                         incrementFn = function()
-                            self.SONIC.sprite.animations:setFPS(self.SONIC.sprite.animations:getFPS() + 1)
+                            self.ANIMATIONS:setFPS(self.ANIMATIONS:getFPS() + 1)
                         end,
                         decrementFn = function()
-                            self.SONIC.sprite.animations:setFPS(self.SONIC.sprite.animations:getFPS() - 1)
+                            self.ANIMATIONS:setFPS(self.ANIMATIONS:getFPS() - 1)
                         end,
                         getValueFn  = function()
-                            return self.SONIC.sprite.animations:getFPS()
+                            return self.ANIMATIONS:getFPS()
                         end,
                         toggleShowKey = "F",
                     },
                     offsetX = {
                         name = "X Offset",
                         incrementFn = function()
-                            self.SONIC.sprite:getCurrentOffset().x = self.SONIC.sprite:getCurrentOffset().x + 1
+                            self.SPRITE:getCurrentOffset().x = self.SPRITE:getCurrentOffset().x + 1
                         end,
                         decrementFn = function()
-                            self.SONIC.sprite:getCurrentOffset().x = self.SONIC.sprite:getCurrentOffset().x - 1
+                            self.SPRITE:getCurrentOffset().x = self.SPRITE:getCurrentOffset().x - 1
                         end,
                         getValueFn  = function()
-                            return self.SONIC.sprite:getCurrentOffset().x
+                            return self.SPRITE:getCurrentOffset().x
                         end,
                         toggleShowKey = "x"
                     },
                     offsetY = {
                         name = "Y Offset",
                         incrementFn = function()
-                            self.SONIC.sprite:getCurrentOffset().y = self.SONIC.sprite:getCurrentOffset().y + 1
+                            self.SPRITE:getCurrentOffset().y = self.SPRITE:getCurrentOffset().y + 1
                         end,
                         decrementFn = function()
-                            self.SONIC.sprite:getCurrentOffset().y = self.SONIC.sprite:getCurrentOffset().y - 1
+                            self.SPRITE:getCurrentOffset().y = self.SPRITE:getCurrentOffset().y - 1
                         end,
                         getValueFn  = function()
-                            return self.SONIC.sprite:getCurrentOffset().y
+                            return self.SPRITE:getCurrentOffset().y
                         end,
                         toggleShowKey = "y"
                     },
