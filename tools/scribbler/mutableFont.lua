474a5e3b27bfe8904b2df6a0d3d093aaeda273ed
Matthew Hopkins <matthop88@gmail.com>
2025-06-04 18:28:30 -0700

    In tools/scribbler/mutableFont.lua, simplified font to automagically populate fontsBySize. Added size of 120

--- tools/scribbler/mutableFont.lua
+++ tools/scribbler/mutableFont.lua
@@ -1,28 +1,5 @@
-return {
+return ({
     index = 11,
-
-    fontsBySize = {
-        [9]  = love.graphics.newFont(9),
-        [10] = love.graphics.newFont(10),
-        [11] = love.graphics.newFont(11),
-        [12] = love.graphics.newFont(12),
-        [14] = love.graphics.newFont(14),
-        [16] = love.graphics.newFont(16),
-        [18] = love.graphics.newFont(18),
-        [20] = love.graphics.newFont(20),
-        [24] = love.graphics.newFont(24),
-        [28] = love.graphics.newFont(28),
-        [32] = love.graphics.newFont(32),
-        [36] = love.graphics.newFont(36),
-        [40] = love.graphics.newFont(40),
-        [48] = love.graphics.newFont(48),
-        [56] = love.graphics.newFont(56),
-        [64] = love.graphics.newFont(64),
-        [72] = love.graphics.newFont(72),
-        [80] = love.graphics.newFont(80),
-        [96] = love.graphics.newFont(96),
-
-    },
 
     9,
     10,
@@ -43,6 +20,14 @@ return {
     72,
     80,
     96,
+    120,
+
+    init = function(self)
+        for _, fontSize in ipairs(self) do
+            self.fontsBySize[fontSize] = love.graphics.newFont(fontSize)
+        end
+        return self
+    end,
 
     next = function(self)
         self.index = math.min(#self, self.index + 1)
@@ -63,4 +48,4 @@ return {
     getFontForSize = function(self, fontSize)
         return self.fontsBySize[fontSize]
     end,
-}
+}):init()
