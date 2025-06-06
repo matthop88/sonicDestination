1ca2fe722448a567757ffe1eaf4e683e21cb6737
Matthew Hopkins <matthop88@gmail.com>
2025-06-05 17:09:42 -0700

    Added skeletal manager.lua inside jotTools directory

---
+++ tools/scribbler/jotTools/manager.lua
@@ -0,0 +1,21 @@
+return {
+    draw = function(self)
+        -- ...
+    end,
+
+    update = function(self, dt)
+        -- ...
+    end,
+
+    mousepressed = function(self, mx, my)
+        -- ...
+    end,
+
+    mousereleased = function(self, mx, my)
+        -- ...
+    end,
+
+    keypressed = function(self, key)
+        -- ...
+    end,
+}
