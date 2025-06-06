ef8c658c60cdbf8faf452eb46a63ee56247439f9
Matthew Hopkins <matthop88@gmail.com>
2025-06-05 17:18:10 -0700

    Added mousePosition to manager, implemented draw code

--- tools/scribbler/jotTools/manager.lua
+++ tools/scribbler/jotTools/manager.lua
@@ -1,22 +1,26 @@
 return {
-    scribbleTool = nil,
-    lineTool     = nil,
-    rectTool     = nil,
-    textTool     = nil,
+    mousePosition = require("tools/scribbler/mousePosition")
+    scribbleTool  = nil,
+    lineTool      = nil,
+    rectTool      = nil,
+    textTool      = nil,
 
-    currentTool  = nil,
+    currentTool   = nil,
 
     init = function(self, picture)
+        self:initTools(picture)
+        self.currentTool = self.scribbleTool
+    end,
+
+    initTools = function(self, picture)
         self.scribbleTool = require("tools/scribbler/jotTools/scribble"):init(picture)
         self.lineTool     = require("tools/scribbler/jotTools/line"):init(picture)
         self.rectTool     = require("tools/scribbler/jotTools/rect"):init(picture)
         self.textTool     = require("tools/scribbler/jotTools/text"):init(picture)
-
-        self.currentTool  = self.scribbleTool
     end,
     
     draw = function(self)
-        -- ...
+        self.currentTool:draw(self.mousePosition:get())
     end,
 
     update = function(self, dt)
