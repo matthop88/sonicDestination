938df0eb1ffdf136a0668ae87567d16fd7b07ebd
Matthew Hopkins <matthop88@gmail.com>
2025-06-05 17:13:40 -0700

    Added jot tool references to manager, and an init function to set them up

--- tools/scribbler/jotTools/manager.lua
+++ tools/scribbler/jotTools/manager.lua
@@ -1,4 +1,20 @@
 return {
+    scribbleTool = nil,
+    lineTool     = nil,
+    rectTool     = nil,
+    textTool     = nil,
+
+    currentTool  = nil,
+
+    init = function(self, picture)
+        self.scribbleTool = require("tools/scribbler/jotTools/scribble"):init(picture)
+        self.lineTool     = require("tools/scribbler/jotTools/line"):init(picture)
+        self.rectTool     = require("tools/scribbler/jotTools/rect"):init(picture)
+        self.textTool     = require("tools/scribbler/jotTools/text"):init(picture)
+
+        self.currentTool  = self.scribbleTool
+    end,
+    
     draw = function(self)
         -- ...
     end,
