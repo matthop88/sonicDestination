0953e1ed1c88196559ab40e1950005e80ed0b04b
Matthew Hopkins <matthop88@gmail.com>
2025-06-06 14:32:53 -0700

    Removed idle code from mousePosition

--- tools/scribbler/utils/mousePosition.lua
+++ tools/scribbler/utils/mousePosition.lua
@@ -1,18 +1,10 @@
-local IDLE_TIMER_MAX = 120
-
 return ({
-    idleTimer = IDLE_TIMER_MAX,
-        
     init = function(self)
         self.x,     self.y     = love.mouse.getPosition()
         self.prevX, self.prevY = self.x, self.y
         return self
     end,
 
-    isIdle = function(self)
-        return self.idleTimer <= 0
-    end,
-        
     isChanged = function(self)
         local x, y = self:get()
         return x ~= self.prevX or y ~= self.prevY
@@ -21,13 +13,6 @@ return ({
     update = function(self, dt)
         self.prevX, self.prevY = self:get()
         self.x,     self.y     = love.mouse.getPosition()
-            
-        if self:isChanged() then self:resetIdle()
-        else                     self.idleTimer = self.idleTimer - (60 * dt)  end
-    end,
-
-    resetIdle = function(self)
-        self.idleTimer = IDLE_TIMER_MAX
     end,
 
     get = function(self)
