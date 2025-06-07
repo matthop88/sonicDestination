b09d9a310d7c6fcbac7b9074642e10d7dda11725
Matthew Hopkins <matthop88@gmail.com>
2025-06-06 19:02:42 -0700

    Add files via upload

---
+++ tools/scribbler/data/scribbleRun.lua
@@ -0,0 +1,125 @@
+return { version = "0.3",
+  {
+    name = "rect",
+    data = { x = 64, y = 96, w = 288, h = 128, color = { 1, 1, 0 } },
+  },
+  {
+    name = "rect",
+    data = { x = 672, y = 96, w = 288, h = 256, color = { 1, 1, 0 } },
+  },
+  {
+    name = "text",
+    data = { x = 115, y = 138, fontSize = 32, color = { 0, 1, 1 }, message = "Stand Right", },
+  },
+  {
+    name = "text",
+    data = { x = 742, y = 200, fontSize = 32, color = { 0, 1, 1 }, message = "Run Right", },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 352, y = 128 },
+      { x = 672, y = 128 },
+      { x = 640, y = 96 },
+      { x = 672, y = 128 },
+      { x = 640, y = 160 },
+    },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 672, y = 192 },
+      { x = 352, y = 192 },
+      { x = 384, y = 160 },
+      { x = 352, y = 192 },
+      { x = 384, y = 224 },
+    },
+  },
+  {
+    name = "text",
+    data = { x = 468, y = 88, fontSize = 32, color = { 0, 1, 1 }, message = "R On", },
+  },
+  {
+    name = "text",
+    data = { x = 466, y = 153, fontSize = 32, color = { 0, 1, 1 }, message = "R Off", },
+  },
+  {
+    name = "rect",
+    data = { x = 672, y = 384, w = 288, h = 128, color = { 1, 1, 0 } },
+  },
+  {
+    name = "text",
+    data = { x = 739, y = 429, fontSize = 32, color = { 0, 1, 1 }, message = "Stand Left", },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 352, y = 480 },
+      { x = 672, y = 480 },
+      { x = 640, y = 448 },
+      { x = 672, y = 480 },
+      { x = 640, y = 512 },
+    },
+  },
+  {
+    name = "text",
+    data = { x = 474, y = 441, fontSize = 32, color = { 0, 1, 1 }, message = "L Off", },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 672, y = 416 },
+      { x = 352, y = 416 },
+      { x = 384, y = 384 },
+      { x = 352, y = 416 },
+      { x = 384, y = 448 },
+    },
+  },
+  {
+    name = "text",
+    data = { x = 472, y = 377, fontSize = 32, color = { 0, 1, 1 }, message = "L On", },
+  },
+  {
+    name = "rect",
+    data = { x = 64, y = 256, w = 288, h = 256, color = { 1, 1, 0 } },
+  },
+  {
+    name = "text",
+    data = { x = 143, y = 362, fontSize = 32, color = { 0, 1, 1 }, message = "Run Left", },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 352, y = 352 },
+      { x = 672, y = 352 },
+      { x = 640, y = 320 },
+      { x = 672, y = 352 },
+      { x = 640, y = 384 },
+    },
+  },
+  {
+    name = "line",
+    data = {
+      color = { 1, 0, 0 },
+      { x = 672, y = 288 },
+      { x = 352, y = 288 },
+      { x = 384, y = 256 },
+      { x = 352, y = 288 },
+      { x = 384, y = 320 },
+    },
+  },
+  {
+    name = "text",
+    data = { x = 472, y = 315, fontSize = 32, color = { 0, 1, 1 }, message = "R On", },
+  },
+  {
+    name = "text",
+    data = { x = 472, y = 250, fontSize = 32, color = { 0, 1, 1 }, message = "L On", },
+  },
+
+}
