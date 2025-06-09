33ce177c133b8e9c9d3a691b18008f276131240e
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 16:24:28 -0700

    Outer main points to testWidgetFactory when using the 'test' command

--- main.lua
+++ main.lua
@@ -26,7 +26,7 @@ function love.load(args)
         __VIEWER_FILE = args[2]
         require "tools/stateMachine/viewer"
     elseif args[1] == "test" then
-        require "test/testModKeyEnabler"
+        require "test/testWidgetFactory"
     else
         require "game/main"
     end
