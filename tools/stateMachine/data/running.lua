04a8ac31297f106968282b3897aaea33302fb155
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 13:47:25 -0700

    Added Run Right to running.lua

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -1,5 +1,6 @@
 return {
     { type = "BOX",   label = "Stand Right", x  =  2, y  =  3, w  =  9, h  =  4 },
+    { type = "BOX",   label = "Run Right",   x  = 21, y  =  3, w  =  9, h  =  8 },
     --{ type = "ARROW", label = "L On",        x1 = 20, y1 =  9, x2 = 13, y2 =  9 },
     --{ type = "ARROW", label = "R On",        x1 = 13, y1 = 12, x2 = 20, y2 = 12 },
 }
