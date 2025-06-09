f5d29a4b75a480f0172bce716f2f057f95044845
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 14:58:03 -0700

    Widgets are now referenced from viewer without explicitly including logic

--- tools/stateMachine/viewer.lua
+++ tools/stateMachine/viewer.lua
@@ -39,44 +39,7 @@ local GRAFX                       = require "tools/lib/graphics"
 local PEGBOARD       = require("tools/stateMachine/pegboard"):init(GRID_SIZE, GRAFX)
 local WIDGET_FACTORY = require("tools/stateMachine/widgetFactory"):init(GRID_SIZE, LABEL_FONT_SIZE, GRAFX)
 
-local WIDGETS        = ({
-    "standing",
-    "running",
-
-    currentWidgetList = { },
-    currentIndex      = 1,
-
-    init = function(self)
-        self:refresh()
-        return self
-    end,
-
-    get         = function(self) return self.currentWidgetList                                     end,
-    getDataName = function(self) return self[self.currentIndex]                                    end,
-    getFileName = function(self) return "tools/stateMachine/data/" .. self:getDataName() .. ".lua" end,
-
-    next = function(self)
-        self.currentIndex = self.currentIndex + 1
-        if self.currentIndex > #self then
-            self.currentIndex = 1
-        end
-        self:refresh()
-    end,
-
-    prev = function(self)
-        self.currentIndex = self.currentIndex - 1
-        if self.currentIndex < 1 then
-            self.currentIndex = #self
-        end
-        self:refresh()
-    end,
-
-    refresh = function(self)
-        self.currentWidgetList = WIDGET_FACTORY:createWidgets(dofile(self:getFileName()))
-    end,
-
-}):init()  
-       
+local WIDGETS        = require("tools/stateMachine/widgets"):init(WIDGET_FACTORY)
 
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
