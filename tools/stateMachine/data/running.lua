ed332aaa9eb10d0ff0f0e3961d9b10e2c7560ed4
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 14:12:20 -0700

    Added other arrows (R On, L On x 2, L Off)

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -1,8 +1,12 @@
 return {
-    { type = "BOX",   label = "Stand Right", x  =  2, y  =  3, w  =  9, h  =  4 },
-    { type = "BOX",   label = "Stand Left",  x  = 21, y  = 12, w  =  9, h  =  4 },
-    { type = "BOX",   label = "Run Right",   x  = 21, y  =  3, w  =  9, h  =  8 },
-    { type = "BOX",   label = "Run Left",    x  =  2, y  =  8, w  =  9, h  =  8 },
-    { type = "ARROW", label = "R On",        x1 = 11, y1 =  4, x2 = 21, y2 =  4 },
-    { type = "ARROW", label = "R Off",       x1 = 21, y1 =  6, x2 = 11, y2 =  6 },
+    { type = "BOX",   label = "Stand Right", x  =  2, y  =  3,   w  =  9, h  =  4   },
+    { type = "BOX",   label = "Stand Left",  x  = 21, y  = 12,   w  =  9, h  =  4   },
+    { type = "BOX",   label = "Run Right",   x  = 21, y  =  3,   w  =  9, h  =  8   },
+    { type = "BOX",   label = "Run Left",    x  =  2, y  =  8,   w  =  9, h  =  8   },
+    { type = "ARROW", label = "R On",        x1 = 11, y1 =  4,   x2 = 21, y2 =  4   },
+    { type = "ARROW", label = "R Off",       x1 = 21, y1 =  6,   x2 = 11, y2 =  6   },
+    { type = "ARROW", label = "R On",        x1 = 11, y1 =  8.5, x2 = 21, y2 =  8.5 },
+    { type = "ARROW", label = "L On",        x1 = 21, y1 = 10,   x2 = 11, y2 = 10   },
+    { type = "ARROW", label = "L On",        x1 = 21, y1 = 13,   x2 = 11, y2 = 13   },
+    { type = "ARROW", label = "L Off",       x1 = 11, y1 = 15,   x2 = 21, y2 = 15   },
 }
