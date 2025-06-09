8e26b928e7a18e7dff26a9cb27756e18256f20dc
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 16:16:10 -0700

    Added comments to data to demonstrate a new structure of data for arrows which is much more readable

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -10,3 +10,15 @@ return {
     { type = "ARROW", label = "L On",        x1 = 21, y1 = 15,   x2 = 11, y2 = 15   },
     { type = "ARROW", label = "L Off",       x1 = 11, y1 = 17,   x2 = 21, y2 = 17   },
 }
+
+--[[
+    -- GOAL: Readability
+
+    { type = "ARROW", label = "R On",  from = "Stand Right", to = "Run Right",   y =  5 },
+    { type = "ARROW", label = "R Off", from = "Run Right",   to = "Stand Right", y =  8 },
+    { type = "ARROW", label = "R On",  from = "Run Left",    to = "Run Right",   y = 10.5 },
+    { type = "ARROW", label = "L On",  from = "Run Right",   to = "Run Left",    y = 12.5 },
+    { type = "ARROW", label = "L On",  from = "Stand Left",  to = "Run Left",    y = 15   },
+    { type = "ARROW", label = "L Off", from = "Run Left",    to = "Stand Left",  y = 17   },
+
+--]]
