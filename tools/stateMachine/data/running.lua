5892c2a93da5a42c89818068db7e22b08e26926e
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 14:42:21 -0700

    Gave more breathing room between R On and L On

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -6,7 +6,7 @@ return {
     { type = "ARROW", label = "R On",        x1 = 11, y1 =  4,   x2 = 21, y2 =  4   },
     { type = "ARROW", label = "R Off",       x1 = 21, y1 =  6,   x2 = 11, y2 =  6   },
     { type = "ARROW", label = "R On",        x1 = 11, y1 =  8.5, x2 = 21, y2 =  8.5 },
-    { type = "ARROW", label = "L On",        x1 = 21, y1 = 10,   x2 = 11, y2 = 10   },
+    { type = "ARROW", label = "L On",        x1 = 21, y1 = 10.5, x2 = 11, y2 = 10.5 },
     { type = "ARROW", label = "L On",        x1 = 21, y1 = 13,   x2 = 11, y2 = 13   },
     { type = "ARROW", label = "L Off",       x1 = 11, y1 = 15,   x2 = 21, y2 = 15   },
 }
