8b86b6e382e03ddec8aa647508ece378f4810a7f
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 13:44:59 -0700

    Added Stand Right

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -1,6 +1,5 @@
 return {
-    --{ type = "BOX",   label = "Stand Left",  x  =  4, y  =  7, w  =  9, h  =  7 },
-    --{ type = "BOX",   label = "Stand Right", x  = 20, y  =  7, w  =  9, h  =  7 },
+    { type = "BOX",   label = "Stand Right", x  =  2, y  =  3, w  =  9, h  =  4 },
     --{ type = "ARROW", label = "L On",        x1 = 20, y1 =  9, x2 = 13, y2 =  9 },
     --{ type = "ARROW", label = "R On",        x1 = 13, y1 = 12, x2 = 20, y2 = 12 },
 }
