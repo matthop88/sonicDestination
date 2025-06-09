08e3b56a70765bcfb9e2b30ea225386935ae6fe1
Matthew Hopkins <matthop88@gmail.com>
2025-06-09 13:49:32 -0700

    Added R On and R Off arrows to running.lua

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -1,6 +1,6 @@
 return {
     { type = "BOX",   label = "Stand Right", x  =  2, y  =  3, w  =  9, h  =  4 },
     { type = "BOX",   label = "Run Right",   x  = 21, y  =  3, w  =  9, h  =  8 },
-    --{ type = "ARROW", label = "L On",        x1 = 20, y1 =  9, x2 = 13, y2 =  9 },
-    --{ type = "ARROW", label = "R On",        x1 = 13, y1 = 12, x2 = 20, y2 = 12 },
+    { type = "ARROW", label = "R On",        x1 = 11, y1 =  4, x2 = 21, y2 =  4 },
+    { type = "ARROW", label = "R Off",       x1 = 21, y1 =  6, x2 = 11, y2 =  6 },
 }
