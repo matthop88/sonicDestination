4fbbb958e5cb44a8642a865975b5f2e4d6efc3f9
Matthew Hopkins <matthop88@gmail.com>
2025-06-16 17:22:25 -0700

    Made standing.lua smart

--- tools/stateMachine/data/standing.lua
+++ tools/stateMachine/data/standing.lua
@@ -1,6 +1,6 @@
 return {
-    { type = "BOX",   label = "Stand Left",  x  =  4, y  =  7, w  =  9, h  =  7 },
-    { type = "BOX",   label = "Stand Right", x  = 20, y  =  7, w  =  9, h  =  7 },
-    { type = "ARROW", label = "L On",        x1 = 20, y1 =  9, x2 = 13, y2 =  9 },
-    { type = "ARROW", label = "R On",        x1 = 13, y1 = 12, x2 = 20, y2 = 12 },
+    { type = "BOX",   label = "Stand Left",  x  =  4, w  =  9,   h  =  7,            y =  7 },
+    { type = "BOX",   label = "Stand Right", x  = 20, w  =  9,   h  =  7,            y =  7 },
+    { type = "ARROW", label = "L On",      from = "Stand Right", to = "Stand Left",  y =  9 },
+    { type = "ARROW", label = "R On",      from = "Stand Left",  to = "Stand Right", y = 12 },
 }
