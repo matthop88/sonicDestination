78f184c883aec74a588e91764608fa162ceadb32
Matthew Hopkins <matthop88@gmail.com>
2025-06-17 17:36:33 -0700

    Added additional arrows

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -5,8 +5,10 @@ return {
     { type = "BOX",   label = "Run Left",    x  =  1,   y  =  5,   w  =  6,      h = 13   },
     { type = "ARROW", label = "R On",  from = "Stand Right", to = "Run Right",   y =  6   },
     { type = "ARROW", label = "R Off", from = "Run Right",   to = "Stand Right", y =  8   },
+    { type = "ARROW", label = "L On",  from = "Stand Right", to = "Run Left",    y =  6   },
     { type = "ARROW", label = "R On",  from = "Run Left",    to = "Run Right",   y = 10.5 },
     { type = "ARROW", label = "L On",  from = "Run Right",   to = "Run Left",    y = 12.5 },
     { type = "ARROW", label = "L On",  from = "Stand Left",  to = "Run Left",    y = 15   },
     { type = "ARROW", label = "L Off", from = "Run Left",    to = "Stand Left",  y = 17   },
+    { type = "ARROW", label = "R On",  from = "Stand Left",  to = "Run Right",   y = 15   },
 }
