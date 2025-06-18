977d779bf2930c59f8f80331cb058810371b90c1
Matthew Hopkins <matthop88@gmail.com>
2025-06-17 17:31:22 -0700

    Resized and arranged boxes in running.lua

--- tools/stateMachine/data/running.lua
+++ tools/stateMachine/data/running.lua
@@ -1,8 +1,8 @@
 return {
-    { type = "BOX",   label = "Stand Right", x  =  2, y  =  5,   w  =  9,        h =  4   },
-    { type = "BOX",   label = "Stand Left",  x  = 21, y  = 14,   w  =  9,        h =  4   },
-    { type = "BOX",   label = "Run Right",   x  = 21, y  =  5,   w  =  9,        h =  8   },
-    { type = "BOX",   label = "Run Left",    x  =  2, y  = 10,   w  =  9,        h =  8   },
+    { type = "BOX",   label = "Stand Right", x  = 12.5, y  =  5,   w  =  7,      h =  4   },
+    { type = "BOX",   label = "Stand Left",  x  = 12.5, y  = 14,   w  =  7,      h =  4   },
+    { type = "BOX",   label = "Run Right",   x  = 25,   y  =  5,   w  =  6,      h = 13   },
+    { type = "BOX",   label = "Run Left",    x  =  1,   y  =  5,   w  =  6,      h = 13   },
     { type = "ARROW", label = "R On",  from = "Stand Right", to = "Run Right",   y =  6   },
     { type = "ARROW", label = "R Off", from = "Run Right",   to = "Stand Right", y =  8   },
     { type = "ARROW", label = "R On",  from = "Run Left",    to = "Run Right",   y = 10.5 },
