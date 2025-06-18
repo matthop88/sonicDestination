c362cd5b6ad05b02aa10c61c8eee91ced7a0fa5b
Matthew Hopkins <matthop88@gmail.com>
2025-06-18 13:24:07 -0700

    Updated sonic.lua to make use of states library

--- game/sonic.lua
+++ game/sonic.lua
@@ -1,69 +1,12 @@
-local SONIC
-
-local STAND_LEFT, STAND_RIGHT, RUN_LEFT, RUN_RIGHT
-
-STAND_LEFT = {
-    onEnter    = function(self) 
-        SONIC:faceLeft() 
-        SONIC.sprite:setCurrentAnimation("standing")
-    end,
-    
-    keypressed = function(self, key)
-        if     key == "right" then SONIC:setState(RUN_RIGHT)
-        elseif key == "left"  then SONIC:setState(RUN_LEFT)  end
-    end, 
-}
-
-STAND_RIGHT = {
-    onEnter    = function(self) 
-        SONIC:faceRight()
-        SONIC.sprite:setCurrentAnimation("standing")
-    end,
-    
-    keypressed = function(self, key)
-        if     key == "right" then SONIC:setState(RUN_RIGHT)
-        elseif key == "left"  then SONIC:setState(RUN_LEFT)  end
-    end,
-}
-
-RUN_LEFT = {
-    onEnter    = function(self)
-        SONIC:faceLeft()
-        SONIC.sprite:setCurrentAnimation("running")
-    end,
-    
-    keypressed = function(self, key)
-        if key == "right" then SONIC:setState(RUN_RIGHT) end
-    end,
-    
-    keyreleased = function(self, key)
-        if key == "left" then SONIC:setState(STAND_LEFT) end
-    end,
-}
-
-RUN_RIGHT = {
-    onEnter    = function(self)
-        SONIC:faceRight()
-        SONIC.sprite:setCurrentAnimation("running")
-    end,
-    
-    keypressed = function(self, key)
-       if key == "left" then SONIC:setState(RUN_LEFT) end
-    end,
-    
-    keyreleased = function(self, key)
-        if key == "right" then SONIC:setState(STAND_RIGHT) end
-    end,
-}
+local STATES
 
 return {
     x = 0, y = 0,
         
-    state = STAND_RIGHT,
-            
     init = function(self, params)
         self.sprite = requireRelative("sprites/spriteFactory", { GRAPHICS = params.GRAPHICS }):create("sonic1")
-        SONIC = self
+        STATES      = requireRelative("states/sonic",          { SONIC = self })
+        self.state  = STATES.STAND_RIGHT
         return self
     end,
 
