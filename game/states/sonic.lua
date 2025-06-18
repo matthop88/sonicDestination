6e3927e7439cfa037da12e6a27195ddd92962447
Matthew Hopkins <matthop88@gmail.com>
2025-06-18 13:20:26 -0700

    Made states into an initializable object

--- game/states/sonic.lua
+++ game/states/sonic.lua
@@ -1,57 +1,64 @@
 local SONIC
 
-local STAND_LEFT, STAND_RIGHT, RUN_LEFT, RUN_RIGHT
-
-STAND_LEFT = {
-    onEnter    = function(self) 
-        SONIC:faceLeft() 
-        SONIC.sprite:setCurrentAnimation("standing")
+local STATES = {
+    init = function(self, params)
+        SONIC = params.SONIC
+        return self
     end,
     
-    keypressed = function(self, key)
-        if     key == "right" then SONIC:setState(RUN_RIGHT)
-        elseif key == "left"  then SONIC:setState(RUN_LEFT)  end
-    end, 
+    STAND_LEFT = {
+        onEnter    = function(self) 
+            SONIC:faceLeft() 
+            SONIC.sprite:setCurrentAnimation("standing")
+        end,
+        
+        keypressed = function(self, key)
+            if     key == "right" then SONIC:setState(STATES.RUN_RIGHT)
+            elseif key == "left"  then SONIC:setState(STATES.RUN_LEFT)  end
+        end, 
+    },
+
+    STAND_RIGHT = {
+        onEnter    = function(self) 
+            SONIC:faceRight()
+            SONIC.sprite:setCurrentAnimation("standing")
+        end,
+        
+        keypressed = function(self, key)
+            if     key == "right" then SONIC:setState(STATES.RUN_RIGHT)
+            elseif key == "left"  then SONIC:setState(STATES.RUN_LEFT)  end
+        end,
+    },
+
+    RUN_LEFT = {
+        onEnter    = function(self)
+            SONIC:faceLeft()
+            SONIC.sprite:setCurrentAnimation("running")
+        end,
+        
+        keypressed = function(self, key)
+            if key == "right" then SONIC:setState(STATES.RUN_RIGHT) end
+        end,
+        
+        keyreleased = function(self, key)
+            if key == "left" then SONIC:setState(STATES.STAND_LEFT) end
+        end,
+    },
+
+    RUN_RIGHT = {
+        onEnter    = function(self)
+            SONIC:faceRight()
+            SONIC.sprite:setCurrentAnimation("running")
+        end,
+        
+        keypressed = function(self, key)
+           if key == "left" then SONIC:setState(STATES.RUN_LEFT) end
+        end,
+        
+        keyreleased = function(self, key)
+            if key == "right" then SONIC:setState(STATES.STAND_RIGHT) end
+        end,
+    }
 }
 
-STAND_RIGHT = {
-    onEnter    = function(self) 
-        SONIC:faceRight()
-        SONIC.sprite:setCurrentAnimation("standing")
-    end,
-    
-    keypressed = function(self, key)
-        if     key == "right" then SONIC:setState(RUN_RIGHT)
-        elseif key == "left"  then SONIC:setState(RUN_LEFT)  end
-    end,
-}
-
-RUN_LEFT = {
-    onEnter    = function(self)
-        SONIC:faceLeft()
-        SONIC.sprite:setCurrentAnimation("running")
-    end,
-    
-    keypressed = function(self, key)
-        if key == "right" then SONIC:setState(RUN_RIGHT) end
-    end,
-    
-    keyreleased = function(self, key)
-        if key == "left" then SONIC:setState(STAND_LEFT) end
-    end,
-}
-
-RUN_RIGHT = {
-    onEnter    = function(self)
-        SONIC:faceRight()
-        SONIC.sprite:setCurrentAnimation("running")
-    end,
-    
-    keypressed = function(self, key)
-       if key == "left" then SONIC:setState(RUN_LEFT) end
-    end,
-    
-    keyreleased = function(self, key)
-        if key == "right" then SONIC:setState(STAND_RIGHT) end
-    end,
-}
+return STATES
