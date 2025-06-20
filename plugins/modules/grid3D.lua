249ffa86bde083d2e6908cf52162d4e3cc118378
Matthew Hopkins <matthop88@gmail.com>
2025-06-20 16:39:18 -0700

    Now drawing top markings of grid in red

--- plugins/modules/grid3D.lua
+++ plugins/modules/grid3D.lua
@@ -1,6 +1,7 @@
 return {
     init = function(self, parameters)
         self.graphics = parameters.graphics
+        self.gridSize = parameters.gridSize or 64
         return self
     end,
     
@@ -8,28 +9,42 @@ return {
     --   Methods called by LOVE events go here   --
     -----------------------------------------------
     draw = function(self)
-        local viewPortRect = self.graphics:calculateViewportRect()
+        local viewportRect = self.graphics:calculateViewportRect()
         
         self.graphics:setColor(1, 1, 1)
-        self:drawTopLine(viewPortRect)
-        self:drawMidLine(viewPortRect)
+        self:drawTopLine(viewportRect)
+        self:drawMidLine(viewportRect)
+        self:drawGridTopMarkings(viewportRect)
     end,
 
     -----------------------------------------------
     --        Specialized Methods Go Here        --
     -----------------------------------------------
-    drawTopLine = function(self, viewPortRect)
+    drawTopLine = function(self, viewportRect)
         local topLineY = 512
         self.graphics:setLineWidth(4)
-        self.graphics:line(viewPortRect.x, topLineY, viewPortRect.x + viewPortRect.w, topLineY)
+        self.graphics:line(viewportRect.x, topLineY, viewportRect.x + viewportRect.w, topLineY)
     end,
     
-    drawMidLine = function(self, viewPortRect)
+    drawMidLine = function(self, viepPortRect)
         local midLineY = 534
         self.graphics:setLineWidth(2)
-        self.graphics:line(viewPortRect.x, midLineY, viewPortRect.x + viewPortRect.w, midLineY)
+        self.graphics:line(viewportRect.x, midLineY, viewportRect.x + viewportRect.w, midLineY)
     end,
     
-    -- ...
+    drawGridTopMarkings = function(self, viewportRect)
+        local startX, endX = self:calculateGridTopStartXAndEndX()
 
+        self.graphics:setColor(1, 0, 0)
+        for x = startX, endX, self.gridSize do
+            self.graphics:rectangle("fill", x - 1, 511, 3, 3)
+        end
+    end,
+
+    calculateGridTopStartXAndEndX = function(self)
+        local startX, startY, width, height = self.graphics:calculateViewport()
+        startX = startX - (startX % self.gridSize)
+
+        return startX, startX + width
+    end,
 }
