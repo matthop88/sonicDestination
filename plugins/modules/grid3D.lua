3ca403e5cfb6436249112175a8706885467e14d2
Matthew Hopkins <matthop88@gmail.com>
2025-06-20 15:08:21 -0700

    Added basic draw function to grid3D.lua

--- plugins/modules/grid3D.lua
+++ plugins/modules/grid3D.lua
@@ -1,3 +1,16 @@
 return {
+    -----------------------------------------------
+    --   Methods called by LOVE events go here   --
+    -----------------------------------------------
+    draw = function(self)
+        local topLineY, midLineY = 512, 534
+
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.setLineWidth(4)
+        love.graphics.line(0, topLineY, 1024, topLineY)
+
+        love.graphics.setLineWidth(2)
+        love.graphics.line(0, midLineY, 1024, midLineY)
+    end,
 
 }
