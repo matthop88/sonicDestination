4d1213b3334630b6313b5e6e41dd71aae306aa90
Matthew Hopkins <matthop88@gmail.com>
2025-06-20 15:14:40 -0700

    Updated grid3D to be able to take in graphics object as a parameter

--- plugins/modules/grid3D.lua
+++ plugins/modules/grid3D.lua
@@ -1,16 +1,21 @@
 return {
+    init = function(self, parameters)
+        self.graphics = parameters.graphics
+        return self
+    end,
+    
     -----------------------------------------------
     --   Methods called by LOVE events go here   --
     -----------------------------------------------
     draw = function(self)
         local topLineY, midLineY = 512, 534
 
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.setLineWidth(4)
-        love.graphics.line(0, topLineY, 1024, topLineY)
+        self.graphics:setColor(1, 1, 1)
+        self.graphics:setLineWidth(4)
+        self.graphics:line(0, topLineY, 1024, topLineY)
 
-        love.graphics.setLineWidth(2)
-        love.graphics.line(0, midLineY, 1024, midLineY)
+        self.graphics:setLineWidth(2)
+        self.graphics:line(0, midLineY, 1024, midLineY)
     end,
 
 }
