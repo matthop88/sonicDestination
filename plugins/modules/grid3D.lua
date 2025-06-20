50687e565371d6250b023a418a732e395474dc7b
Matthew Hopkins <matthop88@gmail.com>
2025-06-20 15:24:16 -0700

    Lines now stretch from left to right on screen

--- plugins/modules/grid3D.lua
+++ plugins/modules/grid3D.lua
@@ -8,14 +8,29 @@ return {
     --   Methods called by LOVE events go here   --
     -----------------------------------------------
     draw = function(self)
+        local viewPortRect = self:calculateViewportRect()
+        
         local topLineY, midLineY = 512, 534
 
         self.graphics:setColor(1, 1, 1)
         self.graphics:setLineWidth(4)
-        self.graphics:line(0, topLineY, 1024, topLineY)
+        self.graphics:line(viewPortRect.x, topLineY, viewPortRect.x + viewPortRect.w, topLineY)
 
         self.graphics:setLineWidth(2)
-        self.graphics:line(0, midLineY, 1024, midLineY)
+        self.graphics:line(viewPortRect.x, midLineY, viewPortRect.x + viewPortRect.w, midLineY)
+    end,
+
+    -----------------------------------------------
+    --        Specialized Methods Go Here        --
+    -----------------------------------------------
+    getScreenWidth  = function(self) return love.graphics.getWidth()  end,
+    getScreenHeight = function(self) return love.graphics.getHeight() end,
+
+    calculateViewportRect = function(self)
+        local leftX,  topY    = self.graphics:screenToImageCoordinates(0, 0)
+        local rightX, bottomY = self.graphics:screenToImageCoordinates(self:getScreenWidth(), self:getScreenHeight())
+
+        return { x = leftX, y = topY, w = rightX - leftX, h = bottomY - topY }
     end,
 
 }
