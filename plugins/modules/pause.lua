14e0d0fca65f0250c2b80557198c74e7dcb4be64
Matthew Hopkins <matthop88@gmail.com>
2025-06-20 14:21:58 -0700

    Added keypressed which toggles pause on and off. Just console printout for now.

--- plugins/modules/pause.lua
+++ plugins/modules/pause.lua
@@ -1,3 +1,10 @@
 return {
+    paused = false,
 
+    handleKeypressed = function(self, key)
+        if key == "p" then
+            self.paused = not self.paused
+            print("PAUSED: ", self.paused)
+        end
+    end,
 }
