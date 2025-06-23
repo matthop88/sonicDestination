4b1b48c10c4211b643421c7fd3019a84f44ea7b0
Matthew Hopkins <matthop88@gmail.com>
2025-06-23 11:38:32 -0700

    Updated cameraTracking to allow toggling

--- plugins/modules/cameraTracking.lua
+++ plugins/modules/cameraTracking.lua
@@ -1,18 +1,31 @@
 return {
-    positionFn = nil,
-    graphics   = nil,
+    positionFn      = nil,
+    graphics        = nil,
+    active          = true,
+    toggleCameraKey = nil,
 
     init = function(self, parameters)
         self.graphics   = parameters.graphics
         self.positionFn = parameters.positionFn
+        if parameters.toggleCameraKey then
+            self.toggleCameraKey = parameters.toggleCameraKey
+            self.active = false
+        end
         return self
     end,
 
+    keypressed = function(self, key)
+        if key == self.toggleCameraKey then
+            self.active = not self.active
+        end
+    end,
+    
     update = function(self, dt)
-        local centerX = self.graphics:getScreenWidth()  / 2
-        
-        local objectX, _ = self.positionFn()
-
-        self.graphics:syncImageXWithScreen(objectX, centerX)
+        if self.active then
+            local centerX    = self.graphics:getScreenWidth()  / 2
+            local objectX, _ = self.positionFn()
+    
+            self.graphics:syncImageXWithScreen(objectX, centerX)
+        end
     end,
 }
