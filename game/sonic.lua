b0c76dc1d14c779a86df1146bd11ba20757d2662
Matthew Hopkins <matthop88@gmail.com>
2025-06-23 12:03:43 -0700

    Fixed FPS when going left.

--- game/sonic.lua
+++ game/sonic.lua
@@ -101,7 +101,7 @@ return {
 
         --]]
 
-        self.sprite:setFPS(self.velocity.x / 36)
+        self.sprite:setFPS(math.abs(self.velocity.x / 36))
     end,
     
     updatePosition = function(self, dt)
