e1a9747e8d547594fc2a72150f1d02b17692a222
Matthew Hopkins <matthop88@gmail.com>
2025-07-03 11:55:05 -0700

    Removed gallery code from slicerApp.lua, replaced with a library requirement

--- tools/spriteSheetSlicer/slicerApp.lua
+++ tools/spriteSheetSlicer/slicerApp.lua
@@ -80,60 +80,7 @@ local currentRect = require "tools/spriteSheetSlicer/smartRect"
 
 local imgPath     = "resources/images/sadSlicer.png"
 
-local gallery = {
-    spriteRects = {
-        { x =  41, y = 347, w = 26, h = 39 },
-        { x = 102, y = 346, w = 29, h = 40 },
-        { x = 162, y = 345, w = 39, h = 40 },
-        { x = 224, y = 346, w = 39, h = 39 },
-        { x = 293, y = 347, w = 26, h = 39 },
-        { x = 355, y = 346, w = 28, h = 40 },
-        { x = 412, y = 345, w = 40, h = 38 },
-        { x = 476, y = 346, w = 39, h = 39 },
-    },
-
-    init = function(self)
-        self.image = getImageViewer():getImage()
-        self:initQuads()
-        return self
-    end,
-
-    initQuads = function(self)
-        for _, spriteRect in ipairs(self.spriteRects) do
-            spriteRect.quad = love.graphics.newQuad(spriteRect.x, spriteRect.y, spriteRect.w, spriteRect.h,
-                                                    self.image:getWidth(), self.image:getHeight())
-        end
-    end,
-    
-    draw = function(self)
-        self:drawBackground()
-        self:drawSlots()
-        self:drawSprites()
-    end,
-
-    drawBackground = function(self)
-        love.graphics.setColor(0.5, 0.5, 0.5)
-        love.graphics.rectangle("fill", 0, 684, 1024, 86)
-        love.graphics.setColor(1, 1, 1)
-    end,
-
-    drawSlots = function(self)
-        love.graphics.setLineWidth(2)
-        for x = 8, 1024, 73 do
-            love.graphics.rectangle("line", x, 696, 60, 60)
-        end
-    end,
-    
-    drawSprites = function(self)
-        for n, spriteRect in ipairs(self.spriteRects) do
-            self:drawSpriteCentered(spriteRect, (n * 73) - 35, 726)
-        end
-    end,
-
-    drawSpriteCentered = function(self, spriteRect, x, y)
-        love.graphics.draw(self.image, spriteRect.quad, x - (spriteRect.w / 2), y - (spriteRect.h / 2), 0, 1, 1)
-    end,
-}
+local gallery
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -210,7 +157,7 @@ PLUGINS = require("plugins/engine")
 --             Static code - is executed last               --
 --------------------------------------------------------------
 
-gallery:init()
+gallery = require("tools/spriteSheetSlicer/gallery"):init()
 
 slicer:start({
     imageViewer          = getImageViewer(),
