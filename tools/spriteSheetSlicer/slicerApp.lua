cbe5ec72871465efb2d09158cf5bf3cf26c8f099
Matthew Hopkins <matthop88@gmail.com>
2025-07-03 10:48:50 -0700

    In slicerApp.lua, refactored drawGallery() by adding the draw method into gallery.

--- tools/spriteSheetSlicer/slicerApp.lua
+++ tools/spriteSheetSlicer/slicerApp.lua
@@ -80,15 +80,35 @@ local currentRect = require "tools/spriteSheetSlicer/smartRect"
 
 local imgPath     = "resources/images/sadSlicer.png"
 
-local gallerySpriteRects = {
-    { x =  41, y = 347, w = 26, h = 39 },
-    { x = 102, y = 346, w = 29, h = 40 },
-    { x = 162, y = 345, w = 39, h = 40 },
-    { x = 224, y = 346, w = 39, h = 39 },
-    { x = 293, y = 347, w = 26, h = 39 },
-    { x = 355, y = 346, w = 28, h = 40 },
-    { x = 412, y = 345, w = 40, h = 38 },
-    { x = 476, y = 346, w = 39, h = 39 },
+local gallery = {
+    spriteRects = {
+        { x =  41, y = 347, w = 26, h = 39 },
+        { x = 102, y = 346, w = 29, h = 40 },
+        { x = 162, y = 345, w = 39, h = 40 },
+        { x = 224, y = 346, w = 39, h = 39 },
+        { x = 293, y = 347, w = 26, h = 39 },
+        { x = 355, y = 346, w = 28, h = 40 },
+        { x = 412, y = 345, w = 40, h = 38 },
+        { x = 476, y = 346, w = 39, h = 39 },
+    },
+
+    draw = function(self)
+        love.graphics.setColor(0.5, 0.5, 0.5)
+        love.graphics.rectangle("fill", 0, 684, 1024, 86)
+        love.graphics.setColor(1, 1, 1)
+
+        love.graphics.setLineWidth(2)
+        for x = 8, 1024, 73 do
+            love.graphics.rectangle("line", x, 696, 60, 60)
+        end
+    
+        for n, spriteRect in ipairs(self.spriteRects) do
+            love.graphics.draw(getImageViewer():getImage(),
+                               love.graphics.newQuad(spriteRect.x, spriteRect.y, spriteRect.w, spriteRect.h,
+                               getImageViewer():getImageWidth(), getImageViewer():getImageHeight()),
+                               (n * 73) - 65 + ((60 - spriteRect.w) / 2), 706, 0, 1, 1)
+        end
+    end,
 }
 
 --------------------------------------------------------------
@@ -140,27 +160,7 @@ end
 function drawOverlays()
     slicer:draw()
     currentRect:draw()
-    drawGallery()
-end
-
-function drawGallery()
-    love.graphics.setColor(0.5, 0.5, 0.5)
-    love.graphics.rectangle("fill", 0, 684, 1024, 86)
-    love.graphics.setColor(1, 1, 1)
-
-    love.graphics.setLineWidth(2)
-    for x = 8, 1024, 73 do
-        love.graphics.rectangle("line", x, 696, 60, 60)
-    end
-
-    for n, spriteRect in ipairs(gallerySpriteRects) do
-        love.graphics.draw(getImageViewer():getImage(),
-                love.graphics.newQuad(spriteRect.x, spriteRect.y, spriteRect.w, spriteRect.h,
-                getImageViewer():getImageWidth(), getImageViewer():getImageHeight()),
-                (n * 73) - 65 + ((60 - spriteRect.w) / 2), 706, 0, 1, 1)
-    end
-                    
-                
+    gallery:draw()
 end
 
 --------------------------------------------------------------
