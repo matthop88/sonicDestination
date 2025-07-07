ac0a2f299ef4fbe64e8afecb26675bbe14e1f429
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 16:52:39 -0700

    Created editor.lua

---
+++ tools/spriteSheetSlicer/editor.lua
@@ -0,0 +1,52 @@
+local Editor = {
+    create = function(self)
+        local x, y, w, h = 228, 50, 568, 568
+        
+        local isActive = false
+
+        local scale      = 10
+        local image      = nil
+        local spriteRect = nil
+        
+        return {
+            draw = function(self)
+                if isActive then
+                    love.graphics.setColor(0.3, 0.3, 0.3, 0.7)
+                    love.graphics.rectangle("fill", x,      y,        w,   h)
+                    love.graphics.setColor(0.2, 0.2, 0.2, 0.9)
+                    love.graphics.rectangle("fill", x + 84, y + 34, 400, 400)
+                    love.graphics.setColor(0.5, 0.5, 0.5)
+                    love.graphics.rectangle("line", x + 83, y + 33, 402, 402)
+                    love.graphics.setColor(1, 1, 1)
+                    love.graphics.rectangle("line", x,      y,        w,   h)
+
+                    self:drawSprite()
+                end
+            end,
+
+            mousepressed = function(self, mx, my)
+                return self:isInsideRect(mx, my) and isActive
+            end,
+            
+            setActive = function(self, active) 
+                isActive = active 
+            end,
+
+            setSprite = function(self, img, sprRect)
+                image      = img
+                spriteRect = sprRect
+            end,
+
+            drawSprite = function(self)
+                if image ~= nil then
+                    love.graphics.draw(image, spriteRect.quad, 512 - ((spriteRect.w / 2) * scale),
+                                                               284 - ((spriteRect.h / 2) * scale), 0, scale, scale)
+                end
+            end,
+
+            isInsideRect = function(self, px, py)
+                return px >= x and px <= x + w and py >= y and py <= y + h
+            end,
+        }
+    end,
+}
