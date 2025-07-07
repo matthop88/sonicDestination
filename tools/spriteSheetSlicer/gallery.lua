8986f8bed91f1403493a1b8f02604419bbf2d54f
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 15:21:59 -0700

    Extracted hard-coded spriteRects from gallery; now they are passed in via init()

--- tools/spriteSheetSlicer/gallery.lua
+++ tools/spriteSheetSlicer/gallery.lua
@@ -36,22 +36,11 @@ local GallerySlot = {
 }
 
 return {
-    spriteRects = {
-        { x =  41, y = 347, w = 26, h = 39 },
-        { x = 102, y = 346, w = 29, h = 40 },
-        { x = 162, y = 345, w = 39, h = 40 },
-        { x = 224, y = 346, w = 39, h = 39 },
-        { x = 293, y = 347, w = 26, h = 39 },
-        { x = 355, y = 346, w = 28, h = 40 },
-        { x = 412, y = 345, w = 40, h = 38 },
-        { x = 476, y = 346, w = 39, h = 39 },
-    },
-
     slots = { },
 
-    init = function(self)
+    init = function(self, spriteRects)
         local image = getImageViewer():getImage()
-        for n, spriteRect in ipairs(self.spriteRects) do
+        for n, spriteRect in ipairs(spriteRects) do
             spriteRect.quad = self:createQuad(spriteRect, image)
             table.insert(self.slots, GallerySlot:create((n * 73) - 65, 696, 60, 60, image, spriteRect))
         end
