64845499856a196874098480b03e711ee89a9f6a
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 15:31:50 -0700

    Fleshed out data in sonic1.lua

--- tools/spriteSheetSlicer/data/sonic1.lua
+++ tools/spriteSheetSlicer/data/sonic1.lua
@@ -1,1 +1,12 @@
+return {
+    imagePath = "game/resources/images/spriteSheets/sonic1Transparent.png",
 
+    spriteRects = {
+        { x =  46, y = 349, w = 24, h = 40 },
+        { x = 109, y = 347, w = 40, h = 40 },
+        { x = 178, y = 348, w = 32, h = 40 },
+        { x = 249, y = 349, w = 40, h = 40 },
+        { x = 319, y = 347, w = 40, h = 40 },
+        { x = 390, y = 348, w = 40, h = 40 },
+    },
+}
