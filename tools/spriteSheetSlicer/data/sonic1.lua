69192233ca0695264c8ab4023821b0a952010f4a
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 15:36:03 -0700

    Added SPRITE_BG_COLOR and MARGIN_BG_COLOR constants to sonic1.lua data file

--- tools/spriteSheetSlicer/data/sonic1.lua
+++ tools/spriteSheetSlicer/data/sonic1.lua
@@ -1,6 +1,9 @@
 return {
     imagePath = "game/resources/images/spriteSheets/sonic1Transparent.png",
 
+    MARGIN_BG_COLOR = { r = 0.05, g = 0.28, b = 0.03, a = 1 },
+    SPRITE_BG_COLOR = { r = 0,    g = 0,    b = 0,    a = 0 },
+    
     spriteRects = {
         { x =  46, y = 349, w = 24, h = 40 },
         { x = 109, y = 347, w = 40, h = 40 },
