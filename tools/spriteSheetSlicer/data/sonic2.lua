10043c0e53fc05540a47b4d80959a49973a7a46c
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 15:19:11 -0700

    Fleshed out sonic2.lua to have imagePath, spriteRects

--- tools/spriteSheetSlicer/data/sonic2.lua
+++ tools/spriteSheetSlicer/data/sonic2.lua
@@ -1,1 +1,14 @@
-
+return {
+    imagePath = "resources/images/spriteSheets/sonic2Transparent.png",
+    
+    spriteRects = {
+        { x =  41, y = 347, w = 26, h = 39 },
+        { x = 102, y = 346, w = 29, h = 40 },
+        { x = 162, y = 345, w = 39, h = 40 },
+        { x = 224, y = 346, w = 39, h = 39 },
+        { x = 293, y = 347, w = 26, h = 39 },
+        { x = 355, y = 346, w = 28, h = 40 },
+        { x = 412, y = 345, w = 40, h = 38 },
+        { x = 476, y = 346, w = 39, h = 39 },
+    },
+}
