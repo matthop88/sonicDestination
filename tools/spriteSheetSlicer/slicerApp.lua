5103237e3ba1f18366a12e948adc4c527dc60461
Matthew Hopkins <matthop88@gmail.com>
2025-07-07 15:23:15 -0700

    Now pass in spriteRects to gallery inside slicerApp.lua

--- tools/spriteSheetSlicer/slicerApp.lua
+++ tools/spriteSheetSlicer/slicerApp.lua
@@ -159,7 +159,16 @@ PLUGINS = require("plugins/engine")
 --             Static code - is executed last               --
 --------------------------------------------------------------
 
-gallery = require("tools/spriteSheetSlicer/gallery"):init()
+gallery = require("tools/spriteSheetSlicer/gallery"):init({
+    { x =  41, y = 347, w = 26, h = 39 },
+    { x = 102, y = 346, w = 29, h = 40 },
+    { x = 162, y = 345, w = 39, h = 40 },
+    { x = 224, y = 346, w = 39, h = 39 },
+    { x = 293, y = 347, w = 26, h = 39 },
+    { x = 355, y = 346, w = 28, h = 40 },
+    { x = 412, y = 345, w = 40, h = 38 },
+    { x = 476, y = 346, w = 39, h = 39 }, 
+})
 
 slicer:start({
     imageViewer          = getImageViewer(),
