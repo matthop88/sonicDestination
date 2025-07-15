6daebffd3d7986516d9fc1c533db20712d9e85c1
Matthew Hopkins <matthop88@gmail.com>
2025-07-15 16:37:08 -0700

    Created testPropertyNotifier.lua test

---
+++ test/testPropertyNotifier.lua
@@ -0,0 +1,54 @@
+TESTING = require "test/testFramework"
+
+local PROP_LOADER = require("game/properties/loader")
+
+local LISTENER_A = {
+    onPropertyChange = function(self, propData)
+        -- do nothing
+    end,
+}
+
+local LISTENER_B = {
+    onPropertyChange = function(self, propData)
+        -- do nothing
+    end,
+}
+
+TESTS = {
+    beforeAll = function(self)
+        self.propertyNotifier = PROP_LOADER:getNotifier()
+    end,
+
+    before = function(self)
+        self.propertyNotifier:clearListeners()
+    end,
+
+    --------------- Tests ---------------
+
+    testMultipleNotifications = function(self)
+        local name = "Add Listeners A, B. Listeners list should contain A and B"
+        PROP_LOADER:notifyOnChange(LISTENER_A)
+        PROP_LOADER:notifyOnChange(LISTENER_B)
+
+        local listeners = self.propertyNotifier:getListeners()
+
+        return TESTING:assertTrue(name, #listeners == 2 and listeners[1] == LISTENER_A and listeners[2] == LISTENER_B)
+    end,
+
+    testUniqueNotifications = function(self)
+        local name = "Add Listeners A, B, A, B, B. Listeners list should contain A and B"
+        PROP_LOADER:notifyOnChange(LISTENER_A)
+        PROP_LOADER:notifyOnChange(LISTENER_B)
+        PROP_LOADER:notifyOnChange(LISTENER_A)
+        PROP_LOADER:notifyOnChange(LISTENER_B)
+        PROP_LOADER:notifyOnChange(LISTENER_B)
+
+        local listeners = self.propertyNotifier:getListeners()
+
+        return TESTING:assertTrue(name, #listeners == 2 and listeners[1] == LISTENER_A and listeners[2] == LISTENER_B)
+    end,
+}
+
+TESTING:initTests(TESTS)
+
+require "test/delayTests"
