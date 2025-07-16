d2d4a7cce54ae24f88bebee9756e0e6fd514f5b8
Matthew Hopkins <matthop88@gmail.com>
2025-07-16 16:02:03 -0700

    Removed vertical line from workspace.lua

--- game/workspace.lua
+++ game/workspace.lua
@@ -12,19 +12,18 @@ return {
         self.graphics = params.GRAPHICS
         return self
     end,
-    
+
     draw = function(self)
         self:updateImageCoordinates()
         self:drawBackground()
         self:drawHorizontalLine()
-        self:drawVerticalLine()
     end,
 
     updateImageCoordinates = function(self)
         leftX,  topY    = self.graphics:screenToImageCoordinates(0, 0)
         rightX, bottomY = self.graphics:screenToImageCoordinates(self:getWidth(), self:getHeight())
     end,
-    
+
     drawBackground = function(self)
         self.graphics:setColor(COLOR_GREEN)
         self.graphics:rectangle("fill", leftX, topY, rightX - leftX, bottomY - topY)
@@ -35,7 +34,7 @@ return {
         self.graphics:setLineWidth(1.5)
         self.graphics:line(leftX, self:getHeight() * 3/4, rightX, self:getHeight() * 3/4)
     end,
-    
+
     drawVerticalLine = function(self)
         self.graphics:setColor(COLOR_PURE_WHITE)
         self.graphics:setLineWidth(1.5)
