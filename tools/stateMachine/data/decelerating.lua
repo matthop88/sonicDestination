edec16188fded4b4afe1c679b880848cad290c9c
Matthew Hopkins <matthop88@gmail.com>
2025-07-18 15:01:31 -0700

    Changed Run to Accelerate in state diagram

--- tools/stateMachine/data/decelerating.lua
+++ tools/stateMachine/data/decelerating.lua
@@ -1,8 +1,8 @@
 return {
-    { type = "BOX",   label = "Stand Right", x  = 12.5, y  =  5,   w  =  7,      h =  4   },
-    { type = "BOX",   label = "Stand Left",  x  = 12.5, y  = 14,   w  =  7,      h =  4   },
-    { type = "BOX",   label = "Run Right",   x  = 25,   y  =  5,   w  =  6,      h = 13   },
-    { type = "BOX",   label = "Run Left",    x  =  1,   y  =  5,   w  =  6,      h = 13   },
+    { type = "BOX",   label = "Stand Right",      x  = 12.5, y  =  5,   w  =  7, h =  4   },
+    { type = "BOX",   label = "Stand Left",       x  = 12.5, y  = 14,   w  =  7, h =  4   },
+    { type = "BOX",   label = "Accelerate Right", x  = 25,   y  =  5,   w  =  6, h = 13   },
+    { type = "BOX",   label = "Accelerate Left",  x  =  1,   y  =  5,   w  =  6, h = 13   },
     { type = "ARROW", label = "R On",  from = "Stand Right", to = "Run Right",   y =  6   },
     { type = "ARROW", label = "R Off", from = "Run Right",   to = "Stand Right", y =  8   },
     { type = "ARROW", label = "L On",  from = "Stand Right", to = "Run Left",    y =  6   },
