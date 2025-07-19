726b35e807b02c16f0e9fd97b6ff759b16f9806d
Matthew Hopkins <matthop88@gmail.com>
2025-07-18 17:09:24 -0700

    Made acceleration boxes taller

--- tools/stateMachine/data/decelerating.lua
+++ tools/stateMachine/data/decelerating.lua
@@ -1,8 +1,8 @@
 return {
     { type = "BOX",   label = "Stand Right",      x  = 12.5, y  =  5,   w  =  7, h =  4   },
     { type = "BOX",   label = "Stand Left",       x  = 12.5, y  = 14,   w  =  7, h =  4   },
-    { type = "BOX",   label = "Accelerate Right", x  = 35,   y  =  5,   w  =  6, h = 13   },
-    { type = "BOX",   label = "Accelerate Left",  x  =  1,   y  =  5,   w  =  6, h = 13   },
+    { type = "BOX",   label = "Accelerate Right", x  = 35,   y  =  3,   w  =  6, h = 21   },
+    { type = "BOX",   label = "Accelerate Left",  x  =  1,   y  =  3,   w  =  6, h = 21   },
     { type = "ARROW", label = "R On",  from = "Stand Right",      to = "Accelerate Right", y =  6   },
     { type = "ARROW", label = "R Off", from = "Accelerate Right", to = "Stand Right",      y =  8   },
     { type = "ARROW", label = "L On",  from = "Stand Right",      to = "Accelerate Left",  y =  6   },
