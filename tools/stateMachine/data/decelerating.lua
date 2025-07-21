4e247ecd18b912183f7697cfd0aca8d29b1fd6a3
Matthew Hopkins <matthop88@gmail.com>
2025-07-21 15:51:06 -0700

    Fixed y coordinates of speed = 0 arrows

--- tools/stateMachine/data/decelerating.lua
+++ tools/stateMachine/data/decelerating.lua
@@ -9,7 +9,7 @@ return {
     { type = "ARROW", label = "R Off",     from = "Accelerate Right", to = "Decelerate Right", y =  6   },
     { type = "ARROW", label = "R On",      from = "Decelerate Right", to = "Accelerate Right", y =  8.5 },
     { type = "ARROW", label = "L On",      from = "Decelerate Right", to = "Accelerate Left",  y =  8.5 },
-    { type = "ARROW", label = "Speed = 0", from = "Decelerate Right", to = "Stand Right",      y =  9   },
+    { type = "ARROW", label = "Speed = 0", from = "Decelerate Right", to = "Stand Right",      y =  6   },
     { type = "ARROW", label = "L On",      from = "Stand Right",      to = "Accelerate Left",  y =  6   },
     { type = "ARROW", label = "R On",      from = "Accelerate Left",  to = "Accelerate Right", y = 10.5 },
     { type = "ARROW", label = "L On",      from = "Accelerate Right", to = "Accelerate Left",  y = 12.5 },
@@ -17,6 +17,6 @@ return {
     { type = "ARROW", label = "L Off",     from = "Accelerate Left",  to = "Decelerate Left",  y = 17   },
     { type = "ARROW", label = "L On",      from = "Decelerate Left",  to = "Accelerate Left",  y = 19.5 },
     { type = "ARROW", label = "R On",      from = "Decelerate Left",  to = "Accelerate Right", y = 19.5 },
-    { type = "ARROW", label = "Speed = 0", from = "Decelerate Left",  to = "Stand Left",       y = 20.5 },
+    { type = "ARROW", label = "Speed = 0", from = "Decelerate Left",  to = "Stand Left",       y = 17.5 },
     { type = "ARROW", label = "R On",      from = "Stand Left",       to = "Accelerate Right", y = 15   },
 }
