c87d98b8a8e6d789d1e06f2e4dd10cc34c1c8077
Matthew Hopkins <matthop88@gmail.com>
2025-07-25 14:30:35 -0700

    Created standing.lua state

---
+++ game/states/sonic/standing.lua
@@ -0,0 +1,12 @@
+return {
+    onEnter    = function(self) 
+        SONIC:faceLeft()
+        SONIC.sprite:setCurrentAnimation("standing")
+        SONIC.velocity.x = 0
+    end,
+
+    keypressed = function(self, key)
+        if     key == "right" then SONIC:setState(STATES.RUN_RIGHT)
+        elseif key == "left"  then SONIC:setState(STATES.RUN_LEFT)  end
+        end, 
+}
