5a27243971d6219eb87a70637d89fd003d0c6cc4
Matthew Hopkins <matthop88@gmail.com>
2025-07-25 14:47:49 -0700

    Added standRight.lua state

---
+++ game/states/sonic/standRight.lua
@@ -0,0 +1,21 @@
+local SONIC, STATES
+
+return {
+    init = function(self, params)
+        SONIC  = params.SONIC
+        STATES = params.STATES
+        return self
+    end,
+
+    onEnter    = function(self) 
+        SONIC:faceRight()
+        SONIC.sprite:setCurrentAnimation("standing")
+        SONIC.velocity.x = 0
+    end,
+    
+    keypressed = function(self, key)
+        if     key == "right" then SONIC:setState(STATES.RUN_RIGHT)
+        elseif key == "left"  then SONIC:setState(STATES.RUN_LEFT)  end
+    end,
+}
+    
