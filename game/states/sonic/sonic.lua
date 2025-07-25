a6dfc0e0ba8f92bf0a5c290120e4fe303005dd54
Matthew Hopkins <matthop88@gmail.com>
2025-07-25 14:53:59 -0700

    Updated states/sonic/sonic.lua to reference states properly

--- game/states/sonic/sonic.lua
+++ game/states/sonic/sonic.lua
@@ -1,8 +1,10 @@
 local SONIC, STATES
 
 STATES = {
-    STAND_LEFT  = "standLeft",
-    STAND_RIGHT = "standRight",
+    STAND_LEFT       = "standLeft",
+    STAND_RIGHT      = "standRight",
+    ACCELERATE_LEFT  = "accelerateLeft",
+    ACCELERATE_RIGHT = "accelerateRight",
     
     init = function(self, params)
         SONIC = params.SONIC
@@ -13,46 +15,6 @@ STATES = {
         end
         return self
     end,
-
-    RUN_LEFT = {
-        onEnter    = function(self)
-            SONIC:faceLeft()
-            SONIC.sprite:setCurrentAnimation("running")
-            SONIC.velocity.x = 0
-        end,
-        
-        keypressed = function(self, key)
-            if key == "right" then SONIC:setState(STATES.RUN_RIGHT) end
-        end,
-        
-        keyreleased = function(self, key)
-            if key == "left" then SONIC:setState(STATES.STAND_LEFT) end
-        end,
-
-        update = function(self, dt)
-            SONIC.velocity.x = math.max(-SONIC.MAX_RUNNING_SPEED, SONIC.velocity.x - (SONIC.RUNNING_ACCELERATION * dt))
-        end,
-    },
-
-    RUN_RIGHT = {
-        onEnter    = function(self)
-            SONIC:faceRight()
-            SONIC.sprite:setCurrentAnimation("running")
-            SONIC.velocity.x = 0
-        end,
-        
-        keypressed = function(self, key)
-           if key == "left" then SONIC:setState(STATES.RUN_LEFT) end
-        end,
-        
-        keyreleased = function(self, key)
-            if key == "right" then SONIC:setState(STATES.STAND_RIGHT) end
-        end,
-
-        update = function(self, dt)
-            SONIC.velocity.x = math.min(SONIC.MAX_RUNNING_SPEED, SONIC.velocity.x + (SONIC.RUNNING_ACCELERATION * dt))
-        end,
-    }
 }
 
 return STATES
