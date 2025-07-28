79e78bcd7f74645d927f44d14982ee943197de50
Matthew Hopkins <matthop88@gmail.com>
2025-07-28 15:20:34 -0700

    Split screen plugin now can smoothly transition between screens

--- plugins/modules/splitScreen.lua
+++ plugins/modules/splitScreen.lua
@@ -1,4 +1,7 @@
 return {
+    height     = 768,
+    destHeight = 768,
+    
     init = function(self, params)
         self.GFX1 = params.GFX1
         self.GFX2 = params.GFX2
@@ -7,7 +10,24 @@ return {
     end,
 
     draw = function(self)
-        self.GFX1:blitToScreen(0, 384, { 1, 1, 1 }, 0, 1, 0.5)
-        self.GFX2:blitToScreen(0, 0,   { 1, 1, 1 }, 0, 1, 0.5)
+        self.yScale = self.height / 768
+        self.GFX1:blitToScreen(0, 768 - self.height, { 1, 1, 1 }, 0, 1,     self.yScale)
+        self.GFX2:blitToScreen(0, 0,                 { 1, 1, 1 }, 0, 1, 1 - self.yScale)
+    end,
+
+    update = function(self, dt)
+        if self.destHeight > self.height then
+            self.height = math.min(self.destHeight, self.height + (1536 * dt))
+        else
+            self.height = math.max(self.destHeight, self.height - (1536 * dt))
+        end
+    end,
+
+    handleKeypressed = function(self, key)
+        if key == "1" then
+            self.destHeight = math.min(768, self.destHeight + 384)
+        elseif key == "2" then
+            self.destHeight = math.max(0,   self.destHeight - 384)
+        end
     end,
 }
