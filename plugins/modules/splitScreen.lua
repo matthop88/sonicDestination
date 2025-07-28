cbe3da05b56e5d43ca9e6aa027607ee7ed2a54ed
Matthew Hopkins <matthop88@gmail.com>
2025-07-28 14:43:18 -0700

    Added splitScreen plugin

---
+++ plugins/modules/splitScreen.lua
@@ -0,0 +1,13 @@
+return {
+    init = function(self, params)
+        self.GFX1 = params.GFX1
+        self.GFX2 = params.GFX2
+
+        return self
+    end,
+
+    draw = function(self)
+        self.GFX1:blitToScreen(0, 384, { 1, 1, 1 }, 0, 1, 0.5)
+        self:GFX2:blitToScreen(0, 0,   { 1, 1, 1 }, 0, 1, 0.5)
+    end,
+}
