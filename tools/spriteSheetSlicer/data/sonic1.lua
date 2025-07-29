44d2e8005124dc52de3bf275f1a18fbeb95b7968
Matthew Hopkins <matthop88@gmail.com>
2025-07-29 10:27:45 -0700

    Added running animation info to spriteSheetSlicer/data/sonic1.lua (backwards compatible)

--- tools/spriteSheetSlicer/data/sonic1.lua
+++ tools/spriteSheetSlicer/data/sonic1.lua
@@ -3,7 +3,19 @@ return {
 
     MARGIN_BG_COLOR = { r = 0.05, g = 0.28, b = 0.03, a = 1 },
     SPRITE_BG_COLOR = { r = 0,    g = 0,    b = 0,    a = 0 },
-    
+
+    animations  = {
+        running = {
+            sprites = {
+                { x =  46, y = 349, w = 24, h = 40, offset = { x = 12, y = 20 }, },
+                { x = 109, y = 347, w = 40, h = 40, offset = { x = 20, y = 20 }, },
+                { x = 178, y = 348, w = 32, h = 40, offset = { x = 20, y = 20 }, },
+                { x = 249, y = 349, w = 40, h = 40, offset = { x = 20, y = 20 }, },
+                { x = 319, y = 347, w = 40, h = 40, offset = { x = 20, y = 20 }, },
+                { x = 390, y = 348, w = 40, h = 40, offset = { x = 20, y = 20 }, },
+            },
+        },
+    },
     spriteRects = {
         { x =  46, y = 349, w = 24, h = 40, offset = { x = 12, y = 20 }, },
         { x = 109, y = 347, w = 40, h = 40, offset = { x = 20, y = 20 }, },
