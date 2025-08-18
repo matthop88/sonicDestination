d7ded15a360ecb11599ff1d7094c8db968b35362
Matthew Hopkins <matthop88@gmail.com>
2025-08-18 15:28:12 -0700

    Moved grounded box in data so that it is drawn last

--- tools/stateMachine/data/jumping.lua
+++ tools/stateMachine/data/jumping.lua
@@ -8,7 +8,6 @@ return {
     { type = "BOX",   label = "Brake Left",                    x  = 13,   y  = 23.5, w  =  6,  h =   4  },
     { type = "BOX",   label = "Decelerate Right",              x  = 23,   y  =  5.5, w  =  7,  h =   5  },
     { type = "BOX",   label = "Brake Right",                   x  = 23,   y  = -1.5, w  =  7,  h =   4  },
-    { type = "BOX",   label = "Grounded",         alpha = 0.5, x  = -1,   y  = -3.5, w  = 45,  h =  33  },
     { type = "ARROW", label = "R On",      from = "Stand Right",      to = "Accelerate Right", y =  12  },
     { type = "ARROW", label = "R Off",     from = "Accelerate Right", to = "Decelerate Right", y =  7   },
     { type = "ARROW", label = "R On",      from = "Decelerate Right", to = "Accelerate Right", y =  9.5 },
@@ -28,6 +27,8 @@ return {
     { type = "ARROW", label = "L Off",     from = "Brake Right",      to = "Decelerate Right", x = 25.5 }, 
     { type = "ARROW", label = "Speed = 0", from = "Brake Right",      to = "Accelerate Left",  y =  0.5 },
     { type = "ARROW", label = "L On",      from = "Accelerate Right", to = "Brake Right",      y = -0.5 },
-    { type = "ARROW", label = "R On",      from = "Brake Right",      to = "Accelerate Right", y =  1.5 }, 
+    { type = "ARROW", label = "R On",      from = "Brake Right",      to = "Accelerate Right", y =  1.5 },
+    { type = "BOX",   label = "Grounded",         alpha = 0.5, x  = -1,   y  = -3.5, w  = 45,  h =  33  },
+    
     
 }
