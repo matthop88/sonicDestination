c0d6b45c2d2d6370ac75e2065a6c50d91a95856e
Matthew Hopkins <matthop88@gmail.com>
2025-08-26 15:08:37 -0700

    Added code in accelerateRight to transition to air accelerate right

--- game/states/sonic/grounded/accelerateRight.lua
+++ game/states/sonic/grounded/accelerateRight.lua
@@ -17,7 +17,8 @@ return {
     end,
     
     keyreleased = function(self, key)
-        if key == "right" then SONIC:setState(STATES.DECELERATE_RIGHT) end
+        if     key == "right" then SONIC:setState(STATES.DECELERATE_RIGHT) 
+        elseif key == "space" then SONIC:setState(STATES.AIR_ACCELERATE_RIGHT) end
     end,
 
     update = function(self, dt)
