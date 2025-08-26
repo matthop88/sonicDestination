7bf4dffeebead425736268eb0b311add7562bf51
Matthew Hopkins <matthop88@gmail.com>
2025-08-26 15:44:01 -0700

    Air decelerate left can now transition to 0

--- game/states/sonic/airborne/decelerateLeft.lua
+++ game/states/sonic/airborne/decelerateLeft.lua
@@ -17,6 +17,11 @@ return {
     end,
 
     update     = function(self, dt)
-        if SONIC:isGrounded() then SONIC:setState(STATES.DECELERATE_LEFT) end
+        if SONIC:isGrounded() then 
+            SONIC:setState(STATES.DECELERATE_LEFT) 
+        else
+            SONIC.velocity.x = math.min(0, SONIC.velocity.x + (SONIC.AIR_ACCELERATION * dt))
+            if SONIC.velocity.x == 0 then SONIC:setState(STATES.AIR_STATIONARY_LEFT) end
+        end
     end,
 }
