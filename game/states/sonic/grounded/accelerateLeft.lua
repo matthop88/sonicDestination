fb2f74cea6c3ea8417e2086c8cfe391487d0cfce
Matthew Hopkins <matthop88@gmail.com>
2025-08-26 15:48:21 -0700

    Update accelerateLeft.lua

--- game/states/sonic/grounded/accelerateLeft.lua
+++ game/states/sonic/grounded/accelerateLeft.lua
@@ -14,7 +14,10 @@ return {
     
     keypressed = function(self, key)
         if     key == "right" then SONIC:setState(STATES.BRAKE_LEFT) 
-        elseif key == "space" then SONIC:setState(STATES.AIR_ACCELERATE_LEFT) end
+        elseif key == "space" then 
+            SONIC:startJump()
+            SONIC:setState(STATES.AIR_ACCELERATE_LEFT) 
+        end
     end,
     
     keyreleased = function(self, key)
