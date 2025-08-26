cd725f454b4420bb52df61253ca55dc82486843a
Matthew Hopkins <matthop88@gmail.com>
2025-08-26 15:50:32 -0700

    Update standRight.lua

--- game/states/sonic/grounded/standRight.lua
+++ game/states/sonic/grounded/standRight.lua
@@ -16,7 +16,9 @@ return {
     keypressed = function(self, key)
         if     key == "right" then SONIC:setState(STATES.ACCELERATE_RIGHT)
         elseif key == "left"  then SONIC:setState(STATES.ACCELERATE_LEFT)  
-        elseif key == "space" then SONIC:setState(STATES.AIR_STATIONARY_RIGHT)
+        elseif key == "space" then
+            SONIC:startJump()
+            SONIC:setState(STATES.AIR_STATIONARY_RIGHT)
         end
     end,
 }
