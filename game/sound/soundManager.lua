203ed5d8d6eb7e42338a7e72bcfdec7aed022abb
Matthew Hopkins <matthop88@gmail.com>
2025-08-29 15:37:56 -0700

    Fixed bug in soundManager.lua

--- game/sound/soundManager.lua
+++ game/sound/soundManager.lua
@@ -16,12 +16,12 @@ return ({
 
     initSoundData = function(self)
         for name, element in pairs(self.data) do
-            data.load = function(self)
+            element.load = function(self)
                 if self.sound == nil then
                     self.sound = love.audio.newSource("game/resources/sounds/" .. self.filename, "static")
                 end
             end
-            data.play = function(self)
+            element.play = function(self)
                 self:load()
                 self.sound:play()
             end
