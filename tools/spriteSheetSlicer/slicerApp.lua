97d059e5afa9cadc64343fdd97e5c17ce53bc624
Matthew Hopkins <matthop88@gmail.com>
2025-09-03 20:20:25 +0000

    Wrote code in slicerApp to apply FPS for each animation

--- tools/spriteSheetSlicer/slicerApp.lua
+++ tools/spriteSheetSlicer/slicerApp.lua
@@ -82,6 +82,7 @@ local animRects   = {
             if v.rect then
                 local smartRect = require("tools/spriteSheetSlicer/smartRect"):create():initFromRect(v.rect)
                 smartRect.sprites = v.sprites
+                smartRect.fps     = v.fps or 1
                 table.insert(self, smartRect)
             end
         end
@@ -110,6 +111,12 @@ local animRects   = {
     getSelectedSprites = function(self)
         if self.selectedIndex ~= 0 then
             return self[self.selectedIndex].sprites
+        end
+    end,
+
+    getSelectedFPS = function(self)
+        if self.selectedIndex ~= 0 then
+            return self[self.selectedIndex].fps
         end
     end,
 
@@ -179,7 +186,7 @@ function love.mousepressed(mx, my)
     if not gallery:mousepressed(mx, my) and currentRect:isValid() then
         currentRect:select(true)
         animRects:select(mx, my)
-        gallery:refresh(animRects:getSelectedSprites())
+        refreshGallery()
         printToReadout(currentRect:toString())
     end
 end
@@ -193,7 +200,7 @@ end
 --------------------------------------------------------------
 
 function refreshGallery()
-    gallery:refresh(animRects:getSelectedSprites())
+    gallery:refresh(animRects:getSelectedSprites(), animRects:getSelectedFPS())
     gallery:updateEditor()
 end
 
@@ -248,7 +255,7 @@ PLUGINS = require("plugins/engine")
 --------------------------------------------------------------
 
 initAnimationInfo()
-gallery = require("tools/spriteSheetSlicer/gallery"):init(sheetInfo.animations)
+gallery = require("tools/spriteSheetSlicer/gallery")
 
 slicer:start({
     imageViewer          = getImageViewer(),
