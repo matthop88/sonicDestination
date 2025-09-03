e2201cb9f514a34a451a88cda8048645a4d3ca37
Matthew Hopkins <matthop88@gmail.com>
2025-09-03 01:33:33 -0700

    Added code in animationPane to animate sprite and set all frames

--- tools/spriteSheetSlicer/animationPane.lua
+++ tools/spriteSheetSlicer/animationPane.lua
@@ -3,9 +3,15 @@ return {
 		local enabled = false
 
 		return {
+			spriteData = {},
+
+			index = 1,
+			fps   = 10,
+			
 			draw = function(self)
 				if enabled then
 					self:drawTranslucentPane()
+					self:drawAnimationFrame()
 				end
 			end,
 
@@ -14,12 +20,38 @@ return {
 				love.graphics.rectangle("fill", 0, 0, 1024, 768)
 			end,
 
+			drawAnimationFrame = function(self)
+				local scale = 15
+
+				local currentSpriteInfo = self.spriteData[math.floor(self.index)]
+
+				if currentSpriteInfo then
+					love.graphics.setColor(1, 1, 1)
+					love.graphics.draw(currentSpriteInfo.image, currentSpriteInfo.rect.quad,
+						512 - (currentSpriteInfo.rect.offset.x * scale), 374 - (currentSpriteInfo.rect.offset.y * scale),
+						0, scale, scale)
+				end
+			end,
+
+			update = function(self, dt)
+				if enabled then
+					self.index = self.index + (60 / self.fps * dt)
+					if math.floor(self.index) > #self.spriteData then
+						self.index = self.index - #self.spriteData
+					end
+				end
+			end,
+
 			enable = function(self)
 				enabled = true
 			end,
 
 			disable = function(self)
 				enabled = false
+			end,
+
+			setSpriteData = function(self, spriteData)
+				self.spriteData = spriteData
 			end,
 
 		}
