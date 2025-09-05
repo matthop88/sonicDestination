6a2edf031e8a7059c93ff83f8832b158d626c029
Matthew Hopkins <matthop88@gmail.com>
2025-09-05 21:58:19 +0000

    Added basics inside of height tracker to allow line to display

--- plugins/modules/heightTracker.lua
+++ plugins/modules/heightTracker.lua
@@ -1,2 +1,23 @@
 return {
+    toggleShowKey = nil,
+    showTracker   = false,
+
+    init = function(self, params)
+        self.toggleShowKey = params.toggleShowKey
+        return self
+    end,
+    
+    draw = function(self)
+        if self.showHeightTracker then
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.setLineWidth(2)
+            love.graphics.line(1000, 0, 1000, 576)
+        end
+    end,
+
+    handleKeypressed = function(self, key)
+        if key == self.toggleShowKey then
+            self.showTracker = not self.showTracker
+        end
+    end,
 }
