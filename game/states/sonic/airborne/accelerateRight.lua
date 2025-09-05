4bda7926bba3f75023e4a1b3f84f1f402de4fb92
Matthew Hopkins <matthop88@gmail.com>
2025-09-05 01:05:21 +0000

    Added animation adjustment code to airborne accelerateRight

--- game/states/sonic/airborne/accelerateRight.lua
+++ game/states/sonic/airborne/accelerateRight.lua
@@ -24,6 +24,11 @@ return {
             SONIC:setState(STATES.ACCELERATE_RIGHT) 
         else
             SONIC.velocity.x = math.min(SONIC.MAX_RUNNING_SPEED, SONIC.velocity.x + (SONIC.AIR_ACCELERATION * dt))
+            if SONIC.sprite:getFPS() >= 60 then
+                SONIC.sprite:setCurrentAnimation("fastJumping")
+            else
+                SONIC.sprite:setCurrentAnimation("jumping")
+            end
         end
     end,
 }
