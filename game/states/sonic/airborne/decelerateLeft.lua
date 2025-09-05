b924a3f75846f44ee3c3d1a24cf63cbb2289a542
Matthew Hopkins <matthop88@gmail.com>
2025-09-05 01:05:44 +0000

    Added animation adjustment code to airborne decelerateLeft

--- game/states/sonic/airborne/decelerateLeft.lua
+++ game/states/sonic/airborne/decelerateLeft.lua
@@ -22,6 +22,11 @@ return {
         else
             SONIC.velocity.x = math.min(0, SONIC.velocity.x + (SONIC.AIR_ACCELERATION * dt))
             if SONIC.velocity.x == 0 then SONIC:setState(STATES.AIR_STATIONARY_LEFT) end
+            if SONIC.sprite:getFPS() >= 60 then
+                SONIC.sprite:setCurrentAnimation("fastJumping")
+            else
+                SONIC.sprite:setCurrentAnimation("jumping")
+            end
         end
     end,
 }
