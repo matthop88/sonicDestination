d35434242bc43aca0ff955a200de718b17a2246a
Matthew Hopkins <matthop88@gmail.com>
2025-09-09 15:49:46 -0700

    Added jump throttling to airborne accelerate right state

--- game/states/sonic/airborne/accelerateRight.lua
+++ game/states/sonic/airborne/accelerateRight.lua
@@ -16,7 +16,8 @@ return {
     end,
 
     keyreleased = function(self, key)
-        if key == "right" then SONIC:setState(STATES.AIR_DECELERATE_RIGHT) end
+        if     key == "right" then SONIC:setState(STATES.AIR_DECELERATE_RIGHT)
+        elseif key == "space" then SONIC:throttleJump()                    end
     end,
 
     update     = function(self, dt)
