7dbf7e1a6e60f93458ffd4f18a46390fdd7d1b46
Matthew Hopkins <matthop88@gmail.com>
2025-09-10 12:07:46 -0700

    Created tracer plugin in skeletal, non-functional form

---
+++ plugins/modules/tracer.lua
@@ -0,0 +1,27 @@
+return {
+    toggleShowKey = nil,
+    showTracer    = false,
+    graphics      = nil,
+
+    init = function(self, params)
+        self.toggleShowKey = params.toggleShowKey
+        self.graphics      = params.graphics
+        return self
+    end,
+
+    draw = function(self)
+        if self.showTracer then
+            -- Drawing happens here
+        end
+    end,
+
+    update = function(self, dt)
+        -- Do nothing
+    end,
+
+    handleKeypressed = function(self, key)
+        if key == self.toggleShowKey then
+            self.showTracer = not self.showTracer
+        end
+    end,
+}
