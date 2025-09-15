b6bc8504091bc895e2690c04a6b7d09ef334c37f
Matthew Hopkins <matthop88@gmail.com>
2025-09-15 13:07:41 -0700

    Added image to chunkalyzerApp, with margins

--- tools/chunkalyzer/chunkalyzerApp.lua
+++ tools/chunkalyzer/chunkalyzerApp.lua
@@ -4,6 +4,12 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
+local imgPath
+
+if __WORLD_MAP_FILE ~= nil then
+    imgPath = "tools/chunkalyzer/data/" .. __WORLD_MAP_FILE .. ".png"
+end
+
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
@@ -31,6 +37,15 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 --                          Plugins                         --
 --------------------------------------------------------------
 
--- ...
--- ...
--- ...
+PLUGINS = require("plugins/engine")
+    :add("imageViewer",
+    {
+        imagePath      = imgPath,
+        horizMargins   = 16,
+        vertMargins    = 16,
+        accessorFnName = "getImageViewer",
+    })
+    :add("zooming",      { imageViewer = getImageViewer() })
+    :add("scrolling",    { imageViewer = getImageViewer() })
+
+        
