61d8fca5f970e734dfa9c7f76283851a70cadf18
Matthew Hopkins <matthop88@gmail.com>
2025-09-18 14:24:09 -0700

    Added imageViewer methods for scrolling inside of chunkPane.lua

--- tools/chunkalyzer/chunkPane.lua
+++ tools/chunkalyzer/chunkPane.lua
@@ -8,6 +8,8 @@ return {
 	draw = function(self)
         GRAFX:setColor(0, 0, 0)
         GRAFX:rectangle("fill", 0, 0, GRAFX:calculateViewport())
+        GRAFX:setColor(1, 1, 0)
+        GRAFX:rectangle("fill", 200, 100, 300, 200)
     end,
 
     update = function(self, dt)
@@ -21,5 +23,18 @@ return {
     handleMousepressed = function(self, mx, my)
         -- Do nothing
     end,
+
+    --------------------------------------------------------------
+    --              Specialized Update Functions                --
+    --------------------------------------------------------------
+
+    keepImageInBounds = function(self)
+        -- not sure what to do yet
+    end,
+
+    moveImage = function(self, deltaX, deltaY)
+        GRAFX:moveImage(deltaX, deltaY)
+    end,
+
 
 }
