c540fc00b392fb347dcdb0fa5636f3ab87e74ec4
Matthew Hopkins <matthop88@gmail.com>
2025-09-22 10:36:25 -0700

    Extracted modes from worldPane.lua

--- tools/chunkalyzer/worldPane.lua
+++ tools/chunkalyzer/worldPane.lua
@@ -7,48 +7,7 @@ local GRAFX = require("tools/lib/bufferedGraphics"):create(require("tools/lib/gr
 
 local OFFSET  = { x = 0, y = 0 }
 
-local chunkAttributes = {
-	leftMost   = nil,
-	topMost    = nil,
-	rightMost  = nil,
-	bottomMost = nil,
-}
-
-local modes = {
-	
-	{	message = "Select the leftmost Chunk on the Map.",   fn = function(x, y) chunkAttributes.leftMost   = x end	},
-	{	message = "Select the topMost Chunk on the Map.",    fn = function(x, y) chunkAttributes.topMost    = y end },
-	{	message = "Select the rightmost Chunk on the Map.",  fn = function(x, y) chunkAttributes.rightMost  = x end	},
-	{	message = "Select the bottomMost Chunk on the Map.", fn = function(x, y) chunkAttributes.bottomMost = y end },
-
-	
-	{   message = "Click anywhere to begin Chunkalyzing.",   fn = function(x, y) getReadout():setSustain(180)   end },
-	
-	index = 1,
-
-	get   = function(self) return self[self.index]                      end,
-	
-    next  = function(self) 
-        self.index = math.min(self.index + 1, #self) 
-        self:refresh()
-    end,
-
-	prev  = function(self) 
-        self.index = math.max(self.index - 1, 1)  
-        self:refresh()   
-    end,
-	
-    reset = function(self) 
-        self.index = 1                               
-        getReadout():setSustain(1800)
-        self:refresh()
-    end,
-
-    refresh = function(self)
-        local currentMode = self:get()
-        if currentMode.message then printToReadout(currentMode.message) end
-    end,
-}
+local modes = require("tools/chunkalyzer/modes")
 
 local prevGraphics = { x = GRAFX.x, y = GRAFX.y }
 
