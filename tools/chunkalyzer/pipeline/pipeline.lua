5878df7f1227e12be5109516fb2dcec6defeba5a
Matthew Hopkins <matthop88@gmail.com>
2025-09-25 12:19:28 -0700

    Streamlined design of pipeline.lua

--- tools/chunkalyzer/pipeline/pipeline.lua
+++ tools/chunkalyzer/pipeline/pipeline.lua
@@ -1,18 +1,8 @@
---[[
-	PIPELINE:add("Map Chunk Gobbler", processMapChunk)
-
-	...
-
-	PIPELINE:push(MAP_CHUNKS) -- Turns MAP_CHUNKS into a Feeder
-
-	local timeElapsed = PIPELINE:execute(5) -- 5 milliseconds
---]]
-
 local STAGE = require("tools/chunkalyzer/pipeline/stage")
 
 -- When we call execute() on the pipeline, the pipeline iterates through the stages, and
 -- executes the first stage with a successor that is complete.
--- When all stages are complete, capture the result of stage 1.
+-- Pipeline is complete when the first stage has a result.
 
 return {
 	create = function(self, name)
@@ -42,7 +32,7 @@ return {
 				end
 
 				if executeStageIndex == nil then
-					-- ERROR!
+					print("ERROR: no stage to execute!")
 				else
 					local stageToExecute  = self.stages[executeStageIndex]
 					local downStreamStage = self.stages[executeStageIndex + 1]
@@ -60,15 +50,11 @@ return {
 			end,
 
 			isComplete = function(self)
-				for _, s in ipairs(self.stages) do
-					if s:isProcessing() then return false end
-				end
-
-				return true
+				return self:getResults() ~= nil
 			end,
 
-			getResult = function(self)
-				return self.stages[1].result
+			getResults = function(self)
+				return self.stages[1].results
 			end,
 		}
 	end,
