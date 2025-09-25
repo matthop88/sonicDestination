b6dbdf9f159d65d75a821d81a50f3d5db77ea577
Matthew Hopkins <matthop88@gmail.com>
2025-09-25 15:56:59 -0700

    Now chunk pipeline implements compare chunks by rows. Works fabulously

--- tools/chunkalyzer/chunkPipeline.lua
+++ tools/chunkalyzer/chunkPipeline.lua
@@ -1,7 +1,7 @@
 local PIPELINE = require("tools/chunkalyzer/pipeline/pipeline"):create("Chunk Pipeline")
 local FEEDER   = require("tools/chunkalyzer/pipeline/feeder")
 
-local TASK_SLICE_TIME_IN_MS = 0.5
+local TASK_SLICE_TIME_IN_MS = 5
 
 local chunkNum = 0
 local IMAGE_DATA, CHUNK_VIEW
@@ -20,23 +20,15 @@ local colorsMatch = function(c1, c2)
        and math.abs(c1.a - c2.a) < 0.005
 end
 
-local compareWholeChunks = function(c1X, c1Y, c2X, c2Y)
+local comparePixelsInChunkRow = function(c1X, c1Y, c2X, c2Y)
     local x1, y1, x2, y2 = c1X, c1Y, c2X, c2Y
 
     for i = 0, 255 do
-        x1 = c1X
-        x2 = c2X
-        for j = 0, 255 do
-            if not colorsMatch(getPixelColorAt(x1, y1), getPixelColorAt(x2, y2)) then
-                local c1 = getPixelColorAt(x1, y1)
-                local c2 = getPixelColorAt(x2, y2)
-                return false
-            end
-            x1 = x1 + 1
-            x2 = x2 + 1
+        if not colorsMatch(getPixelColorAt(x1, y1), getPixelColorAt(x2, y2)) then
+            return false
         end
-        y1 = y1 + 1
-        y2 = y2 + 1
+        x1 = x1 + 1
+        x2 = x2 + 1
     end
 
     return true
@@ -48,6 +40,16 @@ local CHUNK_REPO = {
 		return #self
 	end,
 }
+
+local sliceChunkIntoRows = function(chunk)
+	local rows = {}
+
+	for y = chunk.y, chunk.y + 255 do
+		table.insert(rows, { x = chunk.x, y = y} )
+	end
+
+	return rows
+end
 
 local processMapChunks = function(results, dataIn, dataOut)
 	if results then
@@ -85,20 +87,42 @@ end
 local compareChunks = function(results, dataIn, dataOut)
 	if dataIn.repoChunk == nil then
 		return { chunkIsUnique = true, chunk = dataIn.mapChunk }
-	else
-		local areChunksTheSame = compareWholeChunks(dataIn.mapChunk.x, dataIn.mapChunk.y, dataIn.repoChunk.x, dataIn.repoChunk.y)
+	elseif results then
+		return { chunkIsUnique = not results.chunksAreEqual, chunk = dataIn.mapChunk, repoChunkID = dataIn.repoChunkID }
+	end		
 		
-		return { chunkIsUnique = not areChunksTheSame, chunk = dataIn.mapChunk, repoChunkID = dataIn.repoChunkID }
+	dataOut.mapChunkRows  = FEEDER:create("Map Chunk Row",  sliceChunkIntoRows(dataIn.mapChunk))
+	dataOut.repoChunkRows = FEEDER:create("Repo Chunk Row", sliceChunkIntoRows(dataIn.repoChunk))
+end
+
+local compareChunkRows = function(results, dataIn, dataOut)
+	if results then
+		if not results.rowsAreEqual then
+			return { chunksAreEqual = false }
+		elseif dataIn.mapChunkRows:isComplete() then
+			return { chunksAreEqual = true }
+		end
 	end
+
+	dataOut.mapChunkRow  = dataIn.mapChunkRows:next()
+	dataOut.repoChunkRow = dataIn.repoChunkRows:next() 
+end
+
+local compareChunkRow = function(results, dataIn, dataOut)
+	local rowsAreEqual = comparePixelsInChunkRow(dataIn.mapChunkRow.x, dataIn.mapChunkRow.y, dataIn.repoChunkRow.x, dataIn.repoChunkRow.y)
+			
+	return { rowsAreEqual = rowsAreEqual }
 end
 
 return {
 	setup = function(self, chunks, imageData, chunkalyzerView)
 		IMAGE_DATA = imageData
 		CHUNK_VIEW = chunkalyzerView
-		PIPELINE:add("Map Processor", processMapChunks)
+		PIPELINE:add("Map Processor",         processMapChunks)
 		PIPELINE:add("Add Map Chunk to Repo", addMapChunkToRepo)
-		PIPELINE:add("Compare Chunks", compareChunks)
+		PIPELINE:add("Compare Chunks",        compareChunks)
+		PIPELINE:add("Compare Chunk Rows",    compareChunkRows)
+		PIPELINE:add("Compare Chunk Row",     compareChunkRow)
 		PIPELINE:push({ MAP_CHUNKS = FEEDER:create("Map Chunks", chunks) })
 	end,
 
