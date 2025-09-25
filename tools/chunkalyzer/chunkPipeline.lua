d56927c0483ba1c3aae85333571f94769c81ea9a
Matthew Hopkins <matthop88@gmail.com>
2025-09-25 12:20:23 -0700

    Chunk pipeline is now streamlined

--- tools/chunkalyzer/chunkPipeline.lua
+++ tools/chunkalyzer/chunkPipeline.lua
@@ -1,31 +1,32 @@
 local PIPELINE = require("tools/chunkalyzer/pipeline/pipeline"):create("Chunk Pipeline")
+local FEEDER   = require("tools/chunkalyzer/pipeline/feeder")
 
 local TASK_SLICE_TIME_IN_MS = 0.5
 
 local chunkNum = 0
 
-local processMapChunks = function(chunks, printStage)
-	local myChunk = chunks:next()
-
-	if myChunk ~= nil then
-		printStage:push(myChunk)
+local processMapChunks = function(results, inData, outData)
+	if results then
+		if inData.MAP_CHUNKS:isComplete() then
+			return { completed = true }
+		end
 	end
+				
+	outData.mapChunk = inData.MAP_CHUNKS:next()
 end
 
-local printChunk = function(chunk)
-	local myChunk = chunk:next()
+local printChunk = function(results, inData, outData)
+	chunkNum = chunkNum + 1
+	print("Chunk # " .. chunkNum .. ": x = ", inData.mapChunk.x, "y = ", inData.mapChunk.y)
 
-	if myChunk ~= nil then
-		chunkNum = chunkNum + 1
-		print("Chunk # " .. chunkNum .. ": x = ", myChunk.x, "y = ", myChunk.y)
-	end
+	return { printed = true }
 end
 
 return {
 	setup = function(self, chunks)
 		PIPELINE:add("Map Processor", processMapChunks)
 		PIPELINE:add("Chunk Printer", printChunk)
-		PIPELINE:push(chunks)
+		PIPELINE:push({ MAP_CHUNKS = FEEDER:create("Map Chunks", chunks) })
 	end,
 
 	execute = function(self)
