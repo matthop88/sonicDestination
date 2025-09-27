e4c57b5837efa1a2d4a7b8887d065c1de0a65be2
Matthew Hopkins <matthop88@gmail.com>
2025-09-27 10:33:37 -0700

    Added encoding capacity to savableMapLayout.lua, print to screen

--- tools/chunkalyzer/savableMapLayout.lua
+++ tools/chunkalyzer/savableMapLayout.lua
@@ -4,9 +4,9 @@ return {
 			mapChunks = mapChunks,
 			
 			init = function(self)
-				local width, height = self:calculateMapDimensions()
+				self.width, self.height = self:calculateMapDimensions()
 
-				print("Creating a Map Layout of dimensions " .. width .. "x" .. height .. "...")
+				print("Creating a Map Layout of dimensions " .. self.width .. "x" .. self.height .. "...")
 				return self
 			end,
 
@@ -19,6 +19,40 @@ return {
 				end
 
 				return math.floor(maxChunkX / 256) + 1, math.floor(maxChunkY / 256) + 1
+			end,
+
+			save = function(self)
+				self:encodeMapData()
+				self:printMapData()
+			end,
+
+			encodeMapData = function(self)
+				self.mapData = {}
+
+				for i = 1, self.height do table.insert(self.mapData, {}) end
+				
+				local currentRow = {}
+
+				for n, c in ipairs(self.mapChunks) do
+					local rowNum = math.floor((n - 1) / self.width) + 1
+					table.insert(currentRow, c.id)
+					if #currentRow == self.width then
+						self.mapData[rowNum] = currentRow
+						currentRow = {}
+					end
+				end
+			end,
+
+			printMapData = function(self)
+				print("return {")
+				for _, row in ipairs(self.mapData) do
+					local rowString = "  { "
+					for _, chunkID in ipairs(row) do
+						rowString = rowString .. (string.rep(" ", 3 - string.len("" .. chunkID))) .. chunkID .. ", "
+					end
+					print(rowString .. "},")
+				end
+				print("}")
 			end,
 
 		}):init()
