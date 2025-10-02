676f21c07f8fe61822c63729ddc3f5bb11b63f8d
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 16:15:52 -0700

    Updated cmdSchema.lua to add descriptions for each command

--- tools/chunkalyzer/cmdSchema.lua
+++ tools/chunkalyzer/cmdSchema.lua
@@ -3,15 +3,18 @@ return {
 
 	COMMANDS = {
 		mapImageIn = {
+			description = "image file with raw map of Zone act",
 			shortcut = "i",
 			required = true,
 		},
 
 		mapFileOut = {
+			description = "image data file to write out",
 			shortcut = "m",
 		},
 
 		chunkImageOut = {
+			description = "chunks image to write based upon map compression",
 			shortcut = "c",
 		},
 	},
