1d3d4473e21fa80f9904ebdc759665d139277263
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 15:10:41 -0700

    Updated commandLineTools to initialize with built-in schema

--- commandLineTools.lua
+++ commandLineTools.lua
@@ -1,41 +1,48 @@
+local getSchema = function(path)
+	local cmdSchema
+	local status, err = pcall(function() cmdSchema = require(path .. "/cmdSchema") end)
+
+	if status == true then
+		return cmdSchema
+	else
+		print("XXX Command Schema not found for path " .. path .. "!")
+		return nil
+	end
+end
+
 return {
-	listParams = function(self, args)
-		local params = self:getParams(args)
+	create = function(self, appDirectory)
+		return {
+			schema = getSchema(appDirectory),
 
-		for k, v in pairs(params) do
-			print(k .. " = " .. v)
-		end
+			listParams = function(self, args)
+				local params = self:getParams(args)
+
+				for k, v in pairs(params) do
+					print(k .. " = " .. v)
+				end
+			end,
+
+			getParams = function(self, args)
+				local key = nil
+				params = {}
+
+				for n, arg in ipairs(args) do
+					if     string.sub(arg, 1, 2) == "--" then
+						key = string.sub(arg, 3, -1)
+						params[key] = true
+					elseif string.sub(arg, 1, 1) == "-" then
+						key = string.sub(arg, 2, -1)
+						params[key] = true
+					elseif key ~= nil then
+						params[key] = arg
+						key = nil
+					end
+				end
+
+				return params
+			end,
+		}
 	end,
 
-	getParams = function(self, args)
-		local key = nil
-		params = {}
-
-		for n, arg in ipairs(args) do
-			if     string.sub(arg, 1, 2) == "--" then
-				key = string.sub(arg, 3, -1)
-				params[key] = true
-			elseif string.sub(arg, 1, 1) == "-" then
-				key = string.sub(arg, 2, -1)
-				params[key] = true
-			elseif key ~= nil then
-				params[key] = arg
-				key = nil
-			end
-		end
-
-		return params
-	end,
-
-	getSchema = function(self, path)
-		local cmdSchema
-		local status, err = pcall(function() cmdSchema = require(path .. "/cmdSchema") end)
-
-		if status == true then
-			return cmdSchema
-		else
-			print("XXX Command Schema not found for path " .. path .. "!")
-			return nil
-		end
-	end,
 }
