231d2a5f45f434c32c0aeec79ad9f224407adc98
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 14:28:55 -0700

    Added more tests for command line tool. testOneArgument() fails

--- testing/tests/testCommandLineTool.lua
+++ testing/tests/testCommandLineTool.lua
@@ -15,7 +15,7 @@ return {
     end,
 
     testNoArguments = function(self)
-        local name = "No command-line arguments                            => { }"
+        local name = "No  command-line arguments                              => { }"
 
         local params = self.commandLineTools:getParams({ })
 
@@ -25,6 +25,30 @@ return {
         end
 
         return TESTING:assertTrue(name, keyCount == 0)
+    end,
+
+    testTwoArgumentsWithShorthand = function(self)
+        local name = [[Two command-line arguments w/shorthand (-i myFile)      => { "i" = "myFile" }]]
+
+        local params = self.commandLineTools:getParams({ "-i", "myFile" })
+        
+        return TESTING:assertEquals(name, "myFile", params["i"])
+    end,
+
+    testTwoArgumentsFullLength = function(self)
+        local name = [[Two command-line arguments full-length (--inMap myFile) => { "inMap" = "myFile" }]]
+
+        local params = self.commandLineTools:getParams({ "--inMap", "myFile" })
+        
+        return TESTING:assertEquals(name, "myFile", params["inMap"])
+    end,
+
+    testOneArgument = function(self)
+        local name = [[One command-line argument (--silentMode)                => { "silentMode" = true }]]
+
+        local params = self.commandLineTools:getParams({ "--silentMode" })
+        
+        return TESTING:assertEquals(name, true, params["silentMode"])
     end,
 
 }
