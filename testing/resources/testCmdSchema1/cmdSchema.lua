231c08bc52e8ea9bc8232e9fe3c5924c2b9f5bf6
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 15:02:59 -0700

    Added testing resource testCmdSchema1/cmdSchema.lua

---
+++ testing/resources/testCmdSchema1/cmdSchema.lua
@@ -0,0 +1,17 @@
+return {
+	VERSION = 0.1,
+
+	COMMANDS = {
+		zizzerZazzerZuzz = {
+			shortcut = "z",
+		},
+
+		woogieWoogieWoogie = {
+			shortcut = "w",
+		},
+
+		ekiEkiFoo = {
+			shortcut = "e",
+		},
+	},
+}
