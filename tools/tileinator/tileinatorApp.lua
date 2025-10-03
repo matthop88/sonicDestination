fc2f68c9b47d0aef0f3e92c9387a938407be33f2
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 18:07:22 -0700

    Chunks are now drawn with separations in tileinator app

--- tools/tileinator/tileinatorApp.lua
+++ tools/tileinator/tileinatorApp.lua
@@ -8,6 +8,27 @@ local GRAFX   = require("tools/lib/graphics"):create()
 
 local chunkImg = love.graphics.newImage("resources/zones/chunks/" .. __PARAMS["chunkImageIn"] .. ".png")
 
+local chunks = ({
+    init = function(self)
+        local chunkCount = self:calculateChunkCount()
+        for i = 1, chunkCount do
+            local chunkX = ((i - 1) % 9)           * 256
+            local chunkY = math.floor((i - 1) / 9) * 256
+            
+            table.insert(self, love.graphics.newQuad(chunkX, chunkY, 256, 256, chunkImg:getWidth(), chunkImg:getHeight()))
+        end
+
+        return self
+    end,
+
+    calculateChunkCount = function(self)
+        local width, height = chunkImg:getWidth(), chunkImg:getHeight()
+
+        return math.floor(width / 256) * math.floor(height / 256)
+    end,
+
+}):init()
+
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
@@ -22,7 +43,13 @@ chunkImg:setFilter("nearest", "nearest")
 --------------------------------------------------------------
 
 function love.draw()
-    GRAFX:draw(chunkImg, 0, 0)
+    for i, chunk in ipairs(chunks) do
+        local chunkX = ((i - 1) % 9)           
+        local chunkY = math.floor((i - 1) / 9)
+        
+        GRAFX:setColor(1, 1, 1)
+        GRAFX:draw(chunkImg, chunk, (chunkX * 256) + 8, (chunkY * 256) + 8, 0, 0.94, 0.94)
+    end
 end
 
 function love.update(dt)
