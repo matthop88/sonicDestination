e98ffb1c2e24433ebf0ab2247ba33675a8634118
Matthew Hopkins <matthop88@gmail.com>
2025-10-03 12:38:13 -0700

    Updated tilePipeline.lua to add tile processing

--- tools/tileinator/tilePipeline.lua
+++ tools/tileinator/tilePipeline.lua
@@ -17,7 +17,26 @@ local processChunks = function(results, dataIn, dataOut)
 end
 
 local processChunk = function(results, dataIn, dataOut)
+	if results then
+		return { completed = true }
+	end
+
 	print("Processing Chunk #" .. dataIn.chunkID)
+
+	dataOut.CHUNK_TILES = FEEDER:create("Chunk Tiles", dataIn.chunk.tiles)
+end
+
+local processTiles = function(results, dataIn, dataOut)
+	if dataIn.CHUNK_TILES:isComplete() then
+		return { completed = true }
+	end
+
+	dataOut.tileID = dataIn.CHUNK_TILES:getIndex()
+	dataOut.tile   = dataIn.CHUNK_TILES:next()
+end
+
+local processTile = function(results, dataIn, dataOut)
+	print("Processing Tile #" .. dataIn.tileID)
 
 	return { completed = true }
 end
@@ -29,6 +48,8 @@ return {
 		
 		PIPELINE:add("Chunks Processor",      processChunks)
 		PIPELINE:add("Process Chunk",         processChunk)
+		PIPELINE:add("Process Tiles",         processTiles)
+		PIPELINE:add("Process Tile",          processTile)
 		PIPELINE:push({ ALL_CHUNKS = FEEDER:create("All Chunks", CHUNKS) })
 	end,
 
