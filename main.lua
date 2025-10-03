1783029c72ee5880509231f06fd0cd9cb1dc1b1c
Matthew Hopkins <matthop88@gmail.com>
2025-10-02 17:51:23 -0700

    Updated main.lua to have less repetition; also added tileinator functionality

--- main.lua
+++ main.lua
@@ -33,25 +33,15 @@ local launchSoundGraph           = function(args)
 end
 
 local launchChunkalyzer          = function(args)
-    local cmdLineTools = require("commandLineTools"):create("tools/chunkalyzer")
-    
-    __PARAMS = cmdLineTools:getParams(args)
-    if __PARAMS["help"] then
-        cmdLineTools:printHelp()
-    elseif cmdLineTools:validateParams(args) then
-        require "tools/chunkalyzer/chunkalyzerApp"
-    end
+    require "tools/chunkalyzer/chunkalyzerApp"
 end
 
 local launchMapViewer            = function(args)
-    local cmdLineTools = require("commandLineTools"):create("tools/mapViewer")
-    
-    __PARAMS = cmdLineTools:getParams(args)
-    if __PARAMS["help"] then
-        cmdLineTools:printHelp()
-    elseif cmdLineTools:validateParams(args) then
-        require "tools/mapViewer/mapViewerApp"
-    end
+    require "tools/mapViewer/mapViewerApp"
+end
+
+local launchTileinator           = function(args)
+    require "tools/tileinator/tileinatorApp"
 end
 
 local launchTestingFramework     = function(args)
@@ -71,7 +61,14 @@ local APP_LAUNCHER = {
     soundGraph   = launchSoundGraph,
     chunkalyzer  = launchChunkalyzer,
     mapViewer    = launchMapViewer,
+    tileinator   = launchTileinator,
     test         = launchTestingFramework,
+}
+
+local APP_PATH = {
+    chunkalyzer  = "tools/chunkalyzer",
+    mapViewer    = "tools/mapViewer",
+    tileinator   = "tools/tileinator",
 }
 
 --------------------------------------------------------------
@@ -88,8 +85,18 @@ function love.load(args)
 
     local appName = args[1]
     
-    if APP_LAUNCHER[appName] ~= nil then APP_LAUNCHER[appName](args)
-    else                                 require "game/main"     end
+    if APP_PATH[appName] ~= nil then
+        local cmdLineTools = require("commandLineTools"):create(APP_PATH[appName])
+        __PARAMS = cmdLineTools:getParams(args)
+        if __PARAMS["help"] then
+            cmdLineTools:printHelp()
+            return
+        elseif not cmdLineTools:validateParams(args) then
+            return
+        end
+    end   
+    if   APP_LAUNCHER[appName] ~= nil then APP_LAUNCHER[appName](args)
+    else                                   require "game/main"     end
 end
 
 function relativePath(path)
