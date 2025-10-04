4b28940e0c5bf77f1da950b3cba354afe93b6963
Matthew Hopkins <matthop88@gmail.com>
2025-10-04 16:43:28 -0700

    Extracted pixel color comparison code from tilePipeline.lua

--- tools/tileinator/tilePipeline.lua
+++ tools/tileinator/tilePipeline.lua
@@ -1,30 +1,18 @@
-local PIPELINE = require("tools/lib/pipeline/pipeline"):create("Tile Pipeline")
-local FEEDER   = require("tools/lib/pipeline/feeder")
+local PIPELINE   = require("tools/lib/pipeline/pipeline"):create("Tile Pipeline")
+local FEEDER     = require("tools/lib/pipeline/feeder")
+
+local PIXEL_UTIL = require("tools/lib/pixelUtil")
 
 local TASK_SLICE_TIME_IN_MS = 12
 
 local CHUNKS, TILE_REPO, GARBAGE_HEAP
-
-local getPixelColorAt = function(imageData, x, y)
-    local r, g, b, a = imageData:getPixel(math.floor(x), math.floor(y))
-    return { r = r, g = g, b = b, a = a }
-end
-
-local colorsMatch = function(c1, c2)
-    return c1 ~= nil 
-       and c2 ~= nil
-       and math.abs(c1.r - c2.r) < 0.005
-       and math.abs(c1.g - c2.g) < 0.005 
-       and math.abs(c1.b - c2.b) < 0.005
-       and math.abs(c1.a - c2.a) < 0.005
-end
 
 local comparePixelsOfTiles = function(tile1, tile2)
     local x1, y1, x2, y2 = tile1.x, tile1.y, tile2.x, tile2.y
 
     for i = 0, 15 do
     	for j = 0, 15 do
-    		if not colorsMatch(getPixelColorAt(tile1.IMG_DATA, x1 + j, y1 + i), getPixelColorAt(tile2.IMG_DATA, x2 + j, y2 + i)) then 
+    		if not PIXEL_UTIL:pixelsMatch(tile1.IMG_DATA, x1 + j, y1 + i, tile2.IMG_DATA, x2 + j, y2 + i) then
         		return false
         	end
         end
