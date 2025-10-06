7da1fdc01df7afa25dace5828a53159615774399
Matthew Hopkins <matthop88@gmail.com>
2025-10-06 13:45:59 -0700

    Simplified tilePipeline.lua

--- tools/tileinator/tilePipeline.lua
+++ tools/tileinator/tilePipeline.lua
@@ -21,115 +21,62 @@ local comparePixelsOfTiles = function(tile1, tile2)
     return true
 end
 
-local processChunks = function(results, dataIn, dataOut)
-	if dataIn.ALL_CHUNKS:isComplete() then
-		print("Tileination complete in " .. PIPELINE:getTotalElapsedTime() .. " seconds.")
-		print("Number of tiles in repo: " .. #TILE_REPO)
-		return { completed = true }
-	end
-				
-	dataOut.chunkID = dataIn.ALL_CHUNKS:getIndex()
-	dataOut.chunk   = dataIn.ALL_CHUNKS:next()
-	
-end
-
---[[
 local processChunks = function(params, nextParams)
 	if params.ALL_CHUNKS:isComplete() then
 		print("Tileination complete in " .. PIPELINE:getTotalElapsedTime() .. " seconds.")
 		print("Number of tiles in repo: " .. #TILE_REPO)
 		return true
 	end
-	nextParams.chunk = params.ALL_CHUNKS:next()
---]]
-
-local processChunk = function(results, dataIn, dataOut)
-	if results then
-		return { completed = true }
-	end
-
-	dataOut.CHUNK_TILES = FEEDER:create("Chunk Tiles", dataIn.chunk.tiles)
+				
+	nextParams:init {
+		chunk = params.ALL_CHUNKS:next(),
+	}
+	
 end
 
---[[
 local processChunk = function(params, nextParams)
-	nextParams.CHUNK_TILES = FEEDER:create("Chunk Tiles", params.chunk.tiles)
+	
+	nextParams:init {
+		CHUNK_TILES = FEEDER:create("Chunk Tiles", params.chunk.tiles),
+	}
+
 	return true
---]]
-
-local processTiles = function(results, dataIn, dataOut)
-	if dataIn.CHUNK_TILES:isComplete() then
-		return { completed = true }
-	end
-
-	dataOut.tileID = dataIn.CHUNK_TILES:getIndex()
-	dataOut.tile   = dataIn.CHUNK_TILES:next()
-	dataOut.TILE_REPO = FEEDER:create("Tile Repo", TILE_REPO)
 end
 
---[[
 local processTiles = function(params, nextParams)
 	if params.CHUNK_TILES:isComplete() then
 		return true
 	end
-	nextParams.chunkTile = params.CHUNK_TILES:next()
-	nextParams.TILE_REPO = FEEDER:create("Tile Repo", TILE_REPO)
---]]
 
-local addTileToRepo = function(results, dataIn, dataOut)
-	if results then
-		if not results.tilesAreEqual then
-			if dataIn.TILE_REPO:isComplete() then
-				table.insert(TILE_REPO, dataIn.tile)
-				dataIn.tile.tileID = #TILE_REPO
-				return { wasAdded = true }
-			end
-		else
-			dataIn.tile.garbage = true
-			table.insert(GARBAGE_HEAP, dataIn.tile)
-			return { wasAdded = false }
-		end
-	end		
-	
-	dataOut.tile       = dataIn.tile
-	dataOut.repoTileID = dataIn.TILE_REPO:getIndex()
-	dataOut.repoTile   = dataIn.TILE_REPO:next()
+	nextParams:init {
+		tile      = params.CHUNK_TILES:next(),
+		TILE_REPO = FEEDER:create("Tile Repo", TILE_REPO),
+	}
+
 end
 
---[[
 local addTileToRepo = function(params, nextParams)
-	if params.chunkTile.isDuplicate then
-		table.insert(GARBAGE_HEAP, params.chunkTile)
+	if params.tile.garbage then
+		table.insert(GARBAGE_HEAP, params.tile)
 		return true
 	elseif params.TILE_REPO:isComplete() then
-		table.insert(TILE_REPO, params.chunkTile)
-		params.chunkTile.tileID = #TILE_REPO
+		table.insert(TILE_REPO, params.tile)
+		params.tile.tileID = #TILE_REPO
 		return true
-	end
+	end		
+	
+	nextParams:init {
+		repoTile = params.TILE_REPO:next(),
+	}
 
-	nextParams.repoTile = params.TILE_REPO:next()
 end
 
---]]
-
-local compareTiles = function(results, dataIn, dataOut)
-	if dataIn.repoTile ~= nil then
-		local tilesAreEqual = comparePixelsOfTiles(dataIn.tile, dataIn.repoTile)
-		return { tilesAreEqual = tilesAreEqual }
-	end
-
-	return { tilesAreEqual = false }
-end
-
---[[
 local compareTiles = function(params, nextParams)
-	if comparePixelsOfTiles(params.chunkTile, params.repoTile) then
-		params.chunkTile.isDuplicate = true
-	end
+	local tilesAreEqual = comparePixelsOfTiles(params.tile, params.repoTile)
+	if tilesAreEqual then params.tile.garbage = true end
 
 	return true
 end
---]]
 
 return {
 	setup = function(self, chunks, tileRepo, garbageHeap)
