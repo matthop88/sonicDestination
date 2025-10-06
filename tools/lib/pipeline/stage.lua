86aaefc60a0373b7a0d586c3a5439e9e922d8a14
Matthew Hopkins <matthop88@gmail.com>
2025-10-06 13:44:41 -0700

    Revamped pipeline stage.lua; removed results, made nextParams easier to initialize

--- tools/lib/pipeline/stage.lua
+++ tools/lib/pipeline/stage.lua
@@ -1,48 +1,73 @@
+local NEXT_PARAMS = {
+	nextParams = {},
+
+	init = function(self, params)
+		for k, v in pairs(params) do
+			if self.nextParams[k] == nil then
+				self.nextParams[k] = v
+			end
+		end
+	end,
+
+	get = function(self)
+		return self.nextParams
+	end,
+
+	clear = function(self)
+		self.nextParams = {}
+	end,
+
+	count = function(self)
+		local numKeys = 0
+		for _, _ in pairs(self.nextParams) do numKeys = numKeys + 1 end
+		return numKeys
+	end,
+
+}
+
 return {
 	create = function(self, name, taskFn)
 		return {
 			name    = name,
 			results = nil,
-			dataIn  = nil,
+			params  = nil,
 			taskFn  = taskFn,
 
 			getName       = function(self) return self.name                                 end,
 
-			isReady       = function(self) return self.results ~= nil or self.dataIn == nil end,
+			isReady       = function(self) return self.results ~= nil or self.params == nil end,
 			isComplete    = function(self) return self:isReady()                            end,
 			isProcessing  = function(self) return not self:isReady()                        end,
 			
 			execute = function(self, downStreamStage)
-				if self.dataIn == nil then
+				if self.params == nil then
 					-- ERROR!
 				else
-					local downStreamResult = nil
-					if downStreamStage then downStreamResults = downStreamStage:popResults() end
+					if downStreamStage then 
+						downStreamStage.results = nil 
+						downStreamStage.params  = nil
+					end
 
-					local dataOut = {}
-					self.results = self.taskFn(downStreamResults, self.dataIn, dataOut)
+					NEXT_PARAMS:clear()
+					self.results = self.taskFn(self.params, NEXT_PARAMS)
 					
-					local numKeys = 0
-					for _, _ in pairs(dataOut) do numKeys = numKeys + 1 end
+					local numKeys = NEXT_PARAMS:count()
+
+					for k, v in pairs(NEXT_PARAMS:get()) do
+						self.params[k] = v
+					end
 
 					if numKeys > 0 and downStreamStage then
-						downStreamStage:push(dataOut)
+						NEXT_PARAMS:init(self.params)
+						downStreamStage:push(NEXT_PARAMS:get())
 					end
 
 				end
 			end,
 
-			push = function(self, dataIn)
-				self.dataIn = dataIn
+			push = function(self, params)
+				self.params = params
 			end,
-
-			popResults = function(self)
-				local myResults = self.results
-				self.results = nil
-				self.dataIn = nil
-				return myResults
-			end,
-
 		}
 	end,
 }
