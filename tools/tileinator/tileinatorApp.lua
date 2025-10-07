825570cdf6343fd9fd842541a0cce602cd7d0a2d
Matthew Hopkins <matthop88@gmail.com>
2025-10-07 15:20:54 -0700

    Extracted guts from tileinatorApp.lua

--- tools/tileinator/tileinatorApp.lua
+++ tools/tileinator/tileinatorApp.lua
@@ -4,203 +4,7 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
-local GRAFX   = require("tools/lib/graphics"):create()
-
-local CHUNK_IMG_DATA = love.image.newImageData("resources/zones/chunks/" .. __PARAMS["chunkImageIn"] .. ".png")
-local CHUNK_IMG      = love.graphics.newImage(CHUNK_IMG_DATA)
-
-local tileinationDone = false
-
-local GRAVITY_FORCE  = 787.5
-
-local TILE_REPO      = {
-    draw = function(self)
-        for _, tile in ipairs(self) do
-            tile:draw()
-        end
-    end,
-
-    update = function(self, dt)
-        for n, tile in ipairs(self) do
-            if tile.targetX == nil then
-                tile.delay = math.random(25) + 50
-                local chunkID = math.floor((tile.tileID - 1) / 256)
-                local x = (tile.tileID - 1) % 16
-                local y = math.floor(((tile.tileID - 1) % 256) / 16)
-
-                tile.origX   = tile.posX
-                tile.origY   = tile.posY
-
-                tile.targetX = (chunkID * 350) + (x * 20) + 8
-                tile.targetY = (y * 20) + 8
-            end
-
-            tile.delay = math.max(0, tile.delay - 1)
-            if tile.delay == 0 then
-                if not tile.reachedX then
-                    tile.posX = tile.posX + (tile.targetX - tile.origX) * dt
-                    if math.abs(tile.origX - tile.posX) > math.abs(tile.origX - tile.targetX) then
-                        tile.posX = tile.targetX
-                        tile.reachedX = true
-                    end
-                end
-                if not tile.reachedY then
-                    tile.posY = tile.posY + (tile.targetY - tile.origY) * dt
-                    if math.abs(tile.origY - tile.posY) > math.abs(tile.origY - tile.targetY) then
-                        tile.posY = tile.targetY
-                        tile.reachedY = true
-                    end
-                end
-            end
-
-            if n == #self and tile.reachedX and tile.reachedY and tileinationDone == false then
-                tileinationDone = true
-                printToReadout("Press 'return' to save the tiles to disk.")
-            end
-        end
-    end,
-}
-
-local GARBAGE_HEAP   = {
-    draw = function(self)
-        for _, tile in ipairs(self) do
-            tile:draw()
-        end
-    end,
-
-    update = function(self, dt)
-        for _, tile in ipairs(self) do
-            if tile.velocity == nil then 
-                tile.delay = math.random(50)
-                tile.velocity = 0
-                tile.alpha = 1
-            end
-            
-            tile.delay = math.max(0, tile.delay - 1)
-            if tile.delay == 0 then
-                tile.velocity = tile.velocity + (GRAVITY_FORCE * dt)
-                tile.posY     = tile.posY     + (tile.velocity * dt)
-                tile.alpha    = tile.alpha - (0.333 * dt)
-            end
-        end
-    end,
-}
-
-local TILE = {
-    create = function(self, img, imgData, x, y, posX, posY)
-        return {
-            IMG_DATA = imgData,
-            IMG      = img,
-
-            x        = x,
-            y        = y,
-
-            posX     = posX,
-            posY     = posY,
-
-            quad     = love.graphics.newQuad(x, y, 16, 16, imgData:getWidth(), imgData:getHeight()),
-        
-            draw     = function(self)
-                GRAFX:setColor(1, 1, 1, self.alpha or 1)
-                GRAFX:draw(self.IMG, self.quad, self.posX + 1, self.posY + 1, 0, 0.875, 0.875)
-            end,
-        }
-    end,
-}
-
-local CHUNK = {
-    create = function(self, img, chunkX, chunkY)
-        return ({
-            chunkImg = img,
-            
-            quad = love.graphics.newQuad(chunkX * 256, chunkY * 256, 256, 256, img:getWidth(), img:getHeight()),
-
-            tiles = { },
-
-            init = function(self)
-                local n = 0
-                for y = chunkY * 256, (chunkY * 256) + 240, 16 do
-                    for x = chunkX * 256, (chunkX * 256) + 240, 16 do
-                        n = n + 1
-                        local posX = ((chunkX * 256) +   8) +           (((n - 1) % 16) * (16 * 0.94))
-                        local posY = ((chunkY * 256) +   8) + (math.floor((n - 1) / 16) * (16 * 0.94))
-                     
-                        table.insert(self.tiles, TILE:create(CHUNK_IMG, CHUNK_IMG_DATA, x, y, posX, posY))
-                    end
-                end
-
-                return self
-            end,
-
-            draw = function(self, tileMode)
-                if self:isOnScreen() then
-                    if tileMode then self:drawTiles()
-                    else             self:drawChunk() end
-                end
-            end,
-
-            drawTiles = function(self)
-                for n, tile in ipairs(self.tiles) do
-                    if not tile.garbage and not tile.tileID then
-                        tile:draw()
-                    end
-                end
-            end,
-
-            drawChunk = function(self)
-                GRAFX:setColor(1, 1, 1)
-                GRAFX:draw(self.chunkImg, self.quad, (chunkX * 256) + 8, (chunkY * 256) + 8, 0, 0.94, 0.94)
-            end,
-
-            isOnScreen = function(self, cX, cY)
-                local leftX, topY     = GRAFX:screenToImageCoordinates(0, 0)
-                local rightX, bottomY = GRAFX:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
-
-                return chunkX * 256 < rightX and (chunkX * 256) + 256 > leftX and chunkY * 256 < bottomY and (chunkY * 256) + 256 > topY
-            end,
-
-        }):init()
-    end,
-}
-
-local chunks = ({
-    tileMode = false,
-    chunkImg = nil,
-
-    init = function(self, img)
-        self.chunkImg = img
-
-        local chunkCount = self:calculateChunkCount()
-
-        for i = 1, chunkCount do
-            local chunkX = ((i - 1) % 9)         
-            local chunkY = math.floor((i - 1) / 9)
-            
-            table.insert(self, CHUNK:create(self.chunkImg, chunkX, chunkY))
-        end
-
-        return self
-    end,
-
-    draw = function(self)
-        for i, chunk in ipairs(self) do
-            GRAFX:setColor(1, 1, 1)
-            chunk:draw(self.tileMode)
-        end
-    end,
-
-    calculateChunkCount = function(self)
-        local width, height = self.chunkImg:getWidth(), self.chunkImg:getHeight()
-
-        return math.floor(width / 256) * math.floor(height / 256)
-    end,
-
-    toggleTileMode = function(self)
-        self.tileMode = not self.tileMode
-    end,
-
-
-}):init(CHUNK_IMG)
+local TILE_VIEW = require("tools/tileinator/view") 
 
 local TILE_PIPELINE = require("tools/tileinator/tilePipeline")
 local okayToTileinate = false
@@ -212,34 +16,28 @@ local okayToTileinate = false
 love.window.setTitle("Tileinator")
 love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 
-CHUNK_IMG:setFilter("nearest", "nearest")
-
-TILE_PIPELINE:setup(chunks, TILE_REPO, GARBAGE_HEAP)
+TILE_PIPELINE:setup(TILE_VIEW)
 
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
 --------------------------------------------------------------
 
 function love.draw()
-    chunks:draw()
-    TILE_REPO:draw()
-    GARBAGE_HEAP:draw()
+    TILE_VIEW:draw()
 end
 
 function love.update(dt)
 	if not TILE_PIPELINE:isComplete() and okayToTileinate then
         TILE_PIPELINE:execute()
     end
-    GARBAGE_HEAP:update(dt)
-    TILE_REPO:update(dt)
+    TILE_VIEW:update(dt)
 end
 
 function love.keypressed(key)
-    if     key == "t"     then chunks:toggleTileMode() 
-    elseif key == "space" then 
-        chunks.tileMode = true
+    if key == "space" then 
+        TILE_VIEW:setTileMode(true)
         okayToTileinate = true 
-    end
+    else TILE_VIEW:handleKeypressed(key) end
 end
 
 --------------------------------------------------------------
@@ -256,10 +54,10 @@ end
 
 PLUGINS = require("plugins/engine")
     :add("modKeyEnabler")
-    :add("zooming",      { imageViewer = GRAFX, })
+    :add("zooming",      { imageViewer = TILE_VIEW, })
     :add("scrolling",    
     { 
-    	imageViewer = GRAFX,
+    	imageViewer = TILE_VIEW,
     	scrollSpeed = 2400,          
     })
 	:add("readout",      
