4f03de7ba1a988153c3909837b6474759ff9c6ee
Matthew Hopkins <matthop88@gmail.com>
2025-10-09 14:19:36 -0700

    Updated resource manager to utilize map constructor instead of handling it internally

--- tools/mapViewer/resourceManager.lua
+++ tools/mapViewer/resourceManager.lua
@@ -47,34 +47,8 @@ return ({
 		return img
 	end,
 
-	-- Map Constructor object?
 	constructMap = function(self)
-		local width, height = self:calculateMapDimensions()
-
-		self:createMapData(width, height)
-	end,	
-
-	calculateMapDimensions = function(self)
-		local width  = math.floor(self.chunkImg:getWidth() / 256)
-		local height = math.floor(self.chunkImg:getHeight() / 256)
-
-		return width, height
-	end,
-
-	createMapData = function(self, width, height)
-		self.mapData = {}
-
-		local chunkID = 1
-
-		for i = 1, height do 
-			local currentRow = {}
-				
-			for j = 1, width do
-				table.insert(currentRow, chunkID)
-				chunkID = chunkID + 1
-			end
-			table.insert(self.mapData, currentRow)
-		end
+		self.mapData = require("tools/mapViewer/mapConstructor"):create(self.chunkImg)
 	end,
 
 	loadChunkDataAndConstructMap = function(self, chunksDataName)
