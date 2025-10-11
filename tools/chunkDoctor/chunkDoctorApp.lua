7688bcffd0f2d31a717a54a703a64d71bee11967
Matthew Hopkins <matthop88@gmail.com>
2025-10-11 16:06:33 -0700

    Chunk Doctor app now displays a chunk on screen (chunk #1)

--- tools/chunkDoctor/chunkDoctorApp.lua
+++ tools/chunkDoctor/chunkDoctorApp.lua
@@ -11,11 +11,17 @@ local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 love.window.setTitle("Chunk Doctor")
 love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 
+local CHUNKS_DATA_PATH = "resources/zones/chunks/" .. __PARAMS["chunkDataIn"] .. ".lua"
+
+local CHUNK_ARTIST = require("tools/chunkDoctor/chunkArtist"):create(CHUNKS_DATA_PATH)
+
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
 --------------------------------------------------------------
 
--- ...
+function love.draw()
+    CHUNK_ARTIST:draw(1, 200, 200)
+end
 -- ...
 -- ...
 
