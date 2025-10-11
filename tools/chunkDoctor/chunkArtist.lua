97d1dfae1e99606337c3bdd545d99548da544e3e
Matthew Hopkins <matthop88@gmail.com>
2025-10-11 16:06:02 -0700

    Create chunkArtist.lua

---
+++ tools/chunkDoctor/chunkArtist.lua
@@ -0,0 +1,30 @@
+local TILES_BUILDER = require("tools/chunkDoctor/tilesBuilder")
+
+return {
+	create = function(self, chunksDataPath)
+        self.chunksData = dofile(chunksDataPath)
+        local tilesImgPath = "resources/zones/tiles/" .. self.chunksData.tilesImageName .. ".png"
+        
+        self.tiles = TILES_BUILDER:create(tilesImgPath)
+
+        return self
+	end,
+
+    draw = function(self, chunkID, x, y)
+        local chunk = self.chunksData[chunkID]
+        
+        for _, row in ipairs(chunk) do
+            self:drawRow(row, x, y)
+            y = y + 16
+        end
+    end,
+
+    drawRow = function(self, row, x, y)
+        for n, tileID in ipairs(row) do
+            self.tiles:draw(x, y, tileID)
+            x = x + 16 
+        end
+    end,
+}
+
+
