fc2c3c3cf523a67a1add3633d302cf9df94047bf
Matthew Hopkins <matthop88@gmail.com>
2025-10-11 20:01:54 -0700

    Updated chunkArtist.lua to utilize scale, in a fault-tolerant way

--- tools/chunkDoctor/chunkArtist.lua
+++ tools/chunkDoctor/chunkArtist.lua
@@ -10,19 +10,20 @@ return {
         return self
 	end,
 
-    draw = function(self, chunkID, x, y)
+    draw = function(self, chunkID, x, y, scale)
+        scale = scale or 1
         local chunk = self.chunksData[chunkID]
         
         for _, row in ipairs(chunk) do
-            self:drawRow(row, x, y)
-            y = y + 16
+            self:drawRow(row, x, y, scale)
+            y = y + (16 * scale)
         end
     end,
 
-    drawRow = function(self, row, x, y)
+    drawRow = function(self, row, x, y, scale)
         for n, tileID in ipairs(row) do
-            self.tiles:draw(x, y, tileID)
-            x = x + 16 
+            self.tiles:draw(x, y, tileID, scale)
+            x = x + (16 * scale)
         end
     end,
 
