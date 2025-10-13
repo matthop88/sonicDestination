4553f108593a273362ab4c615693afd2de61232a
Matthew Hopkins <matthop88@gmail.com>
2025-10-13 10:10:20 -0700

    Created tweenableValue.lua in library

---
+++ tools/lib/tweenableValue.lua
@@ -0,0 +1,35 @@
+return {
+	create = function(self, initialValue, params)
+		params = params or {}
+
+		return {
+			value       = initialValue,
+			origin      = initialValue,
+			destination = initialValue,
+
+			speed       = params.speed or 1,
+			delay       = params.delay or 0,
+			timer       = 0,
+
+			get = function(self)
+				return math.floor(self.value)
+			end,
+
+			setDestination = function(self, destination)
+				self.destination = destination
+			end,
+
+			update = function(self, dt)
+				if self.timer <= self.delay then
+					self.timer = self.timer + dt
+				elseif self.value ~= self.destination then
+					self.value = self.value + (self.destination - self.origin) * dt * self.speed
+	                if math.abs(self.origin - self.value) > math.abs(self.destination - self.origin) then
+	                    self.value  = self.destination
+	                    self.origin = self.value
+	                end 
+				end
+			end,
+		}
+	end,
+}
