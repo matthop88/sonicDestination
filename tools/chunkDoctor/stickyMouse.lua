099c7e328f5e77df7790135a7f22df5ca277b493
Matthew Hopkins <matthop88@gmail.com>
2025-10-16 10:39:01 -0700

    Added stickyMouse class for transporting tiles

---
+++ tools/chunkDoctor/stickyMouse.lua
@@ -0,0 +1,39 @@
+local CHUNK_ARTIST
+
+local MOUSE_GRAFX = require("tools/lib/graphics"):create()
+
+return {
+	tileBeingHeld = nil,
+	chunkID       = nil,
+
+	init = function(self, chunkArtist)
+		CHUNK_ARTIST = chunkArtist
+		return self
+	end,
+	
+	holdTile = function(self, chunkID, tile)
+		self.chunkID       = chunkID
+		self.tileBeingHeld = tile
+	end,
+
+	releaseTile = function(self)
+		self.chunkID       = nil
+		self.tileBeingHeld = nil
+	end,
+
+	draw = function(self)
+		local mX, mY = love.mouse.getPosition()
+
+		if self.tileBeingHeld ~= nil then
+			local tileID = CHUNK_ARTIST:getTileID(self.chunkID, self.tileBeingHeld.x, self.tileBeingHeld.y)
+			CHUNK_ARTIST:drawTileAt(tileID, mX - 32, mY - 32, MOUSE_GRAFX, 4, { 1, 1, 1, 0.9 })
+			MOUSE_GRAFX:setColor(1, 1, 1)
+			MOUSE_GRAFX:setLineWidth(3)
+			MOUSE_GRAFX:rectangle("line", mX - 33, mY - 33, 66, 66)
+        end
+	end,
+
+	setVisible = function(self, visibility)
+		love.mouse.setVisible(visibility or (self.tileBeingHeld ~= nil))
+	end,
+}
