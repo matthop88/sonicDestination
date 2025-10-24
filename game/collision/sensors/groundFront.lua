3fcad22e41ec59da629c2671ec8baba1e500c8dc
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 15:03:57 -0700

    Updated groundFront.lua; added white dot indicating sensor position

--- game/collision/sensors/groundFront.lua
+++ game/collision/sensors/groundFront.lua
@@ -10,7 +10,9 @@ return {
 
 			draw = function(self)
 				self.graphics:setColor(0.13, 0.93, 0.19)
-				self.graphics:line(self.owner:getX() + 9, self.owner:getY(), self.owner:getX() + 9, self.owner:getY() + 20)
+				self.graphics:line(self.owner:getX() + 9, self.owner:getY(),      self.owner:getX() + 9, self.owner:getY() + 20)
+				self.graphics:setColor(1,    1,    1)
+				self.graphics:line(self.owner:getX() + 9, self.owner:getY() + 20, self.owner:getX() + 9, self.owner:getY() + 20)
 			end,
 		}
 	end,
