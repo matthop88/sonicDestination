50cb6c06a685fca5d087af83e71d7ecb92018079
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 16:02:19 -0700

    Adjusted position of grid3D in sonicPlugins.lua

--- sonicPlugins.lua
+++ sonicPlugins.lua
@@ -52,7 +52,7 @@ return {
             :add("grid3D",         { 
                 graphics       = self.GRAPHICS,
                 topLineYOffset = 0,
-                standingLine   = 898,
+                standingLine   = 1262,
                 toggleGridKey  = "g",
             })
             :add("tracer", {
