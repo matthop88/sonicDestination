bb4f6133d95db324a901a7d777589e57b52e6e85
Matthew Hopkins <matthop88@gmail.com>
2025-10-23 18:14:23 -0700

    Create solidsBuilder.lua

---
+++ game/terrain/solidsBuilder.lua
@@ -0,0 +1,36 @@
+return {
+	create = function(self, chunkData)
+		local solids = {
+			draw = function(self, graphics, rowNum, colNum, chunkID)
+				local y = ((rowNum - 1) * 256) - 384
+        		local x = (colNum - 1) * 256
+
+				local chunkSolids = self[chunkID]
+				local rowY = y
+				for n, rowOfSolids in ipairs(chunkSolids) do
+					local rowX = x
+					for _, solid in ipairs(rowOfSolids) do
+						if solid == 1 then
+							graphics:setColor(1, 1, 1)
+							graphics:setLineWidth(2)
+                			graphics:line(rowX, rowY + 16, rowX + 16, rowY + 16)
+                		end
+                		rowX = rowX + 16
+                	end
+                	rowY = rowY + 16
+				end
+		    end,
+		}
+
+		for n, chunk in ipairs(chunkData) do
+			local chunkSolids = {}
+			for _, row in ipairs(chunk) do
+				if row.S == nil then row.S = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, } end
+				table.insert(chunkSolids, row.S)
+			end
+			table.insert(solids, chunkSolids)
+		end
+
+		return solids
+	end,
+}
