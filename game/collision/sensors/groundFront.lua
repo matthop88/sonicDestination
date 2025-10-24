13b31789ddfb8e9016168c8b15ad584e9ff2e5c9
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 14:53:03 -0700

    Updated groundFront.lua to add draw() method

--- game/collision/sensors/groundFront.lua
+++ game/collision/sensors/groundFront.lua
@@ -2,10 +2,16 @@ local WORLD
 
 return {
 	init = function(self, params)
-		WORLD = params.WORLD
+		WORLD    = params.WORLD
 		
 		return {
-			owner = params.OWNER,
+			owner    = params.OWNER,
+			graphics = params.GRAPHICS,
 		}
+	end,
+
+	draw = function(self)
+		self.graphics:setColor(0.13, 0.93, 0.19)
+		self.graphics:line(self.owner:getX() + 15, self.owner:getY(), self.owner:getX() + 15, self.owner:getY() + 20)
 	end,
 }
