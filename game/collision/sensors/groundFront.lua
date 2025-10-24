6a59705c3f6fdb4d394f9362692a84dc805a0591
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 15:05:42 -0700

    Fixed visibility of white dot

--- game/collision/sensors/groundFront.lua
+++ game/collision/sensors/groundFront.lua
@@ -12,7 +12,7 @@ return {
 				self.graphics:setColor(0.13, 0.93, 0.19)
 				self.graphics:line(self.owner:getX() + 9, self.owner:getY(),      self.owner:getX() + 9, self.owner:getY() + 20)
 				self.graphics:setColor(1,    1,    1)
-				self.graphics:line(self.owner:getX() + 9, self.owner:getY() + 20, self.owner:getX() + 9, self.owner:getY() + 20)
+				self.graphics:line(self.owner:getX() + 9, self.owner:getY() + 19, self.owner:getX() + 9, self.owner:getY() + 20)
 			end,
 		}
 	end,
