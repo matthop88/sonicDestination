68294e59dbfd59bb659687c041bef77b17d2216b
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 15:10:55 -0700

    Drawing of ground front sensor switches position when Sonic changes facing

--- game/collision/sensors/groundFront.lua
+++ game/collision/sensors/groundFront.lua
@@ -9,10 +9,13 @@ return {
 			graphics = params.GRAPHICS,
 
 			draw = function(self)
+				local xOffset = 9
+				if self.owner:isFacingLeft() then xOffset = xOffset * -1 end
+
 				self.graphics:setColor(0.13, 0.93, 0.19)
-				self.graphics:line(self.owner:getX() - 9, self.owner:getY(),      self.owner:getX() - 9, self.owner:getY() + 20)
+				self.graphics:line(self.owner:getX() + xOffset, self.owner:getY(),      self.owner:getX() + xOffset, self.owner:getY() + 20)
 				self.graphics:setColor(1,    1,    1)
-				self.graphics:line(self.owner:getX() - 9, self.owner:getY() + 19, self.owner:getX() - 9, self.owner:getY() + 20)
+				self.graphics:line(self.owner:getX() + xOffset, self.owner:getY() + 19, self.owner:getX() + xOffset, self.owner:getY() + 20)
 			end,
 		}
 	end,
