8e975b1e6fea0d2ac737decb3af953f1a74fb449
Matthew Hopkins <matthop88@gmail.com>
2025-10-24 14:59:58 -0700

    Updated groundFront.lua; moved front sensor closer in

--- game/collision/sensors/groundFront.lua
+++ game/collision/sensors/groundFront.lua
@@ -10,7 +10,7 @@ return {
 
 			draw = function(self)
 				self.graphics:setColor(0.13, 0.93, 0.19)
-				self.graphics:line(self.owner:getX() + 15, self.owner:getY(), self.owner:getX() + 15, self.owner:getY() + 20)
+				self.graphics:line(self.owner:getX() + 10, self.owner:getY(), self.owner:getX() + 10, self.owner:getY() + 20)
 			end,
 		}
 	end,
