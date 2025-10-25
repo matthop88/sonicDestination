5c836b8381126f560dfb4585af8ec01689a94570
Matthew Hopkins <matthop88@gmail.com>
2025-10-25 11:06:21 -0700

    Updated cmdSchema.lua to add outline flag

--- tools/transparencyEditor/cmdSchema.lua
+++ tools/transparencyEditor/cmdSchema.lua
@@ -11,5 +11,10 @@ return {
 			description = "path of image. Defaults to resources/images/spriteSheets/",
 			shortcut = "p",
 		},
+
+		outline = {
+			description = "this flag signifies outline mode; everything but black will be made transparent",
+			shortcut = "l",
+		}
 	},
 }
