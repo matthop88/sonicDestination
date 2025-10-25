16a6237f76a6cc4a59d5c154896426337d48baed
Matthew Hopkins <matthop88@gmail.com>
2025-10-25 11:48:52 -0700

    Image lineage can be peeled apart in progress.lua

--- fun/applications/progress/progress.lua
+++ fun/applications/progress/progress.lua
@@ -7,6 +7,10 @@ local WINDOW_WIDTH, WINDOW_HEIGHT = 550, 900
 local COLOR_IMAGE   = love.graphics.newImage("fun/resources/images/sonicAdventure.png")
 local OUTLINE_IMAGE = love.graphics.newImage("fun/resources/images/sonicAdventureTransparent.png")
 
+local LAYER_SPEED   = 120
+local zSpeed        = 0
+local z             = 0
+
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
@@ -21,8 +25,24 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("fill",   0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-    love.graphics.draw(COLOR_IMAGE,   0, 0, 0, 0.5, 0.5)
-    love.graphics.draw(OUTLINE_IMAGE, 0, 0, 0, 0.5, 0.5)
+    love.graphics.draw(COLOR_IMAGE,   -z, -z, 0, 0.5, 0.5)
+    love.graphics.draw(OUTLINE_IMAGE, z, z, 0, 0.5, 0.5)
+end
+
+function love.keypressed(key)
+    if key == "right" then
+        zSpeed = LAYER_SPEED
+    elseif key == "left" then
+        zSpeed = -LAYER_SPEED
+    end
+end
+
+function love.keyreleased(key)
+    zSpeed = 0
+end
+
+function love.update(dt)
+    z = math.min(150, math.max(0, z + (zSpeed * dt)))
 end
 
 
