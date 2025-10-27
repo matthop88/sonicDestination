6c13809d757e8539d7fd3fd2e0aa92430f441e07
Matthew Hopkins <matthop88@gmail.com>
2025-10-27 11:48:39 -0700

    Updated progress.lua to show percentage on screen in a cool way

--- fun/applications/progress/progress.lua
+++ fun/applications/progress/progress.lua
@@ -7,13 +7,15 @@ local WINDOW_WIDTH, WINDOW_HEIGHT = 550, 900
 local COLOR_IMAGE   = love.graphics.newImage("fun/resources/images/sonicAdventure.png")
 local OUTLINE_IMAGE = love.graphics.newImage("fun/resources/images/sonicAdventureTransparent.png")
 
+local FONT             = love.graphics.newFont(30)
+
 local LAYER_SPEED      = 120
 local FILL_SPEED       = 120
 local ySpeed           = 0
 local y                = 0
 local zSpeed           = 0
 local z                = 0
-local targetedProgress = 50.0
+local targetedProgress = tonumber(__PROGRESS) or 50.0
 local targetedY        = nil
 
 --------------------------------------------------------------
@@ -36,6 +38,8 @@ function love.draw()
     love.graphics.rectangle("fill",   0, y, WINDOW_WIDTH, WINDOW_HEIGHT)
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(OUTLINE_IMAGE, z, z, 0, 0.5 + (z / 1500), 0.5 + (z / 1500))
+
+    drawProgress()
 end
 
 function love.keypressed(key)
@@ -55,8 +59,6 @@ end
 function love.update(dt)
     if targetedY ~= nil then updateBasedUponTargetedY(dt)
     else                     updateBasedUponManualControls(dt) end
-
-    if ySpeed ~= 0 then printProgress() end
 end
 
 --------------------------------------------------------------
@@ -80,4 +82,16 @@ end
 
 function markTargetedProgress()
     targetedY = -((targetedProgress / 100 * 865) + 35)
+end
+
+function drawProgress()
+    local progress = math.max(0, -(y + 35) / 865) * 100
+    love.graphics.setFont(FONT)
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.rectangle("fill", 400, 882 + y, 150, 40)
+    love.graphics.setLineWidth(3)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("line", 400, 882 + y, 150, 40)
+    love.graphics.setColor(1, 1, 0)
+    love.graphics.printf(string.format("%.2f%%", progress), 0, 885 + y, 540, "right")
 end
