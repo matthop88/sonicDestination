44bcf51fbe6f22c914955f20c9ff6ac4134cff9a
Matthew Hopkins <matthop88@gmail.com>
2025-10-29 09:05:01 -0700

    Created sonic1CommonObjects.lua data file

---
+++ tools/spriteSheetSlicer/data/sonic1CommonObjects.lua
@@ -0,0 +1,10 @@
+return {
+    imagePath = "tools/ringMaster/resources/commonObj.png",
+
+    MARGIN_BG_COLOR = { r = 0.15, g = 0.40, b = 0.10, a = 1 },
+    SPRITE_BG_COLOR = { r = 0.05, g = 0.28, b = 0.03, a = 1 },
+
+    animations  = {},
+
+    spriteRects = {},
+}
