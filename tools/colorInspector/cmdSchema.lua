9a27a3ae403d5755df25a3c3128ce4400f46d21c
Matthew Hopkins <matthop88@gmail.com>
2025-10-29 08:48:45 -0700

    Create cmdSchema.lua for color inspector

---
+++ tools/colorInspector/cmdSchema.lua
@@ -0,0 +1,15 @@
+return {
+	VERSION = 0.1,
+
+	COMMANDS = {
+		image = {
+			description = "name of image to read in",
+			shortcut = "i",
+		},
+
+		path = {
+			description = "path of image. Defaults to resources/images/spriteSheets/",
+			shortcut = "p",
+		},
+	},
+}
