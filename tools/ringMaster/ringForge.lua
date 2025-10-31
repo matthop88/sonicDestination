83ed45da332b8cab0d0a1f776cd5ab58918dee9d
Matthew Hopkins <matthop88@gmail.com>
2025-10-31 15:37:21 -0700

    Created ringForge.lua, which creates a ring image

---
+++ tools/ringMaster/ringForge.lua
@@ -0,0 +1,28 @@
+--------------------------------------------------------------
+--                     Local Variables                      --
+--------------------------------------------------------------
+
+local GRAFX      = require("tools/lib/graphics"):create()
+local RING_GRAFX = require("tools/lib/bufferedGraphics"):create(GRAFX, 16, 16)
+
+local OBJ_IMG    = love.graphics.newImage("tools/ringMaster/resources/commonObj.png")
+local RING_QUAD  = love.graphics.newQuad(24, 198, 16, 16, OBJ_IMG:getWidth(), OBJ_IMG:getHeight())
+
+--------------------------------------------------------------
+--              Static code - is executed first             --
+--------------------------------------------------------------
+
+OBJ_IMG:setFilter("nearest", "nearest")
+
+return {
+    create = function(self)
+        RING_GRAFX:setColor(1, 1, 1)
+        RING_GRAFX:draw(OBJ_IMG, RING_QUAD, 0, 0, 0, 1, 1)
+
+        return {
+            getImage = function(self)
+                return RING_GRAFX:getBuffer()
+            end,
+        }
+    end,
+}
