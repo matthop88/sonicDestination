0674024460bc779631a51bbdabb7b70297253b2a
Matthew Hopkins <matthop88@gmail.com>
2025-10-31 13:30:10 -0700

    Optimized code of pixelUtil class; now runs 15% faster

--- tools/lib/pixelUtil.lua
+++ tools/lib/pixelUtil.lua
@@ -1,9 +1,40 @@
-return {
-    getPixelColorAt = function(self, imageData, x, y)
-        local r, g, b, a = imageData:getPixel(math.floor(x), math.floor(y))
-        return { r = r, g = g, b = b, a = a }
-    end,
+-- More optimized version of pixel matching algorithm.
+-- Reuses same two tables rather than dynamically creating and destroying millions.
+-- Performance difference:
+-- Chunkalyzation of 10240 x 1280 image -> 7.30 sec (was 8.36 sec)
+-- Tileination of     2304 x 1280 image -> 7.57 sec (was 9.57 sec)
 
+local PIXEL_COLOR_1 = {
+    r = nil, g = nil, b = nil, a = nil,
+
+    init = function(self, r, g, b, a)
+        self.r, self.g, self.b, self.a = r, g, b, a
+        
+        return self
+    end,
+}
+
+local PIXEL_COLOR_2 = {
+    r = nil, g = nil, b = nil, a = nil,
+
+    init = function(self, r, g, b, a)
+        self.r, self.g, self.b, self.a = r, g, b, a
+        
+        return self
+    end,
+}
+
+local getPixelColor1At = function(imageData, x, y)
+    local r, g, b, a = imageData:getPixel(math.floor(x), math.floor(y))
+    return PIXEL_COLOR_1:init(r, g, b, a)
+end
+
+local getPixelColor2At = function(imageData, x, y)
+    local r, g, b, a = imageData:getPixel(math.floor(x), math.floor(y))
+    return PIXEL_COLOR_2:init(r, g, b, a)
+end
+
+return {
     colorsMatch = function(self, c1, c2)
         return c1 ~= nil 
            and c2 ~= nil
@@ -14,7 +45,7 @@ return {
     end,
 
     pixelsMatch = function(self, imageData1, x1, y1, imageData2, x2, y2)
-        return self:colorsMatch(self:getPixelColorAt(imageData1, x1, y1), self:getPixelColorAt(imageData2, x2, y2))
+        return self:colorsMatch(getPixelColor1At(imageData1, x1, y1), getPixelColor2At(imageData2, x2, y2))
     end, 
                 
 }
