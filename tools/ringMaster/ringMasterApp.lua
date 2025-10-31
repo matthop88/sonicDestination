4fe69aa547a137aa7591f987b8ffbee88ba55e19
Matthew Hopkins <matthop88@gmail.com>
2025-10-31 09:25:08 -0700

    Update ringMasterApp.lua; only draw rings to buffer 1x

--- tools/ringMaster/ringMasterApp.lua
+++ tools/ringMaster/ringMasterApp.lua
@@ -4,19 +4,20 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
+local GRAFX     = require("tools/lib/graphics"):create()
 local RING_IMG  = love.graphics.newImage("tools/ringMaster/resources/commonObj.png")
 local RING_QUAD = love.graphics.newQuad(24, 198, 16, 16, RING_IMG:getWidth(), RING_IMG:getHeight())
 
 local map = ({
-    rings = {},
-    isZooming = false,
+    rings      = {},
+    isZooming  = false,
+    ringsDrawn = false,
 
     init = function(self)
         self.pageWidth  = 1200
         self.pageHeight =  800
 
-        self.GRAFX = require("tools/lib/bufferedGraphics")
-                        :create(require("tools/lib/graphics"):create(), self.pageWidth, self.pageHeight)
+        self.BUF_GRAFX = require("tools/lib/bufferedGraphics"):create(GRAFX, self.pageWidth, self.pageHeight)
 
         self:initRings()
         return self
@@ -32,15 +33,22 @@ local map = ({
     end,
 
     draw = function(self)
-        self.GRAFX:setColor(0, 0, 0)
-        self.GRAFX:rectangle("fill", self.GRAFX:calculateViewport())
-        self.GRAFX:setColor(0.3, 0.3, 0.3)
-        self.GRAFX:rectangle("fill", 10, 10, self:getPageWidth() - 20, self:getPageHeight() - 20)
-        self.GRAFX:setColor(1, 1, 1)
+        if not self.ringsDrawn then self:drawRingsToBuffer() end
+            
+        GRAFX:setColor(1, 1, 1)
+        GRAFX:drawImage(self.BUF_GRAFX:getBuffer(), 0, 0)
+    end,
+
+    drawRingsToBuffer = function(self)
+        self.BUF_GRAFX:setColor(0, 0, 0)
+        self.BUF_GRAFX:rectangle("fill", self.BUF_GRAFX:calculateViewport())
+        self.BUF_GRAFX:setColor(0.3, 0.3, 0.3)
+        self.BUF_GRAFX:rectangle("fill", 10, 10, self:getPageWidth() - 20, self:getPageHeight() - 20)
+        self.BUF_GRAFX:setColor(1, 1, 1)
         for _, ring in ipairs(self.rings) do
-            self.GRAFX:draw(RING_IMG, RING_QUAD, ring.x, ring.y, 0, 1, 1)
+            self.BUF_GRAFX:draw(RING_IMG, RING_QUAD, ring.x, ring.y, 0, 1, 1)
         end
-        self.GRAFX:blitToScreen(0, 0)
+        self.ringsDrawn = true
     end,
 
     getPageWidth  = function(self) return self.pageWidth  end,
@@ -48,27 +56,27 @@ local map = ({
     
     keepImageInBounds = function(self)
         if not self.isZooming then
-            self.GRAFX:setX(math.min(0, math.max(self.GRAFX:getX(), (love.graphics:getWidth()  / self.GRAFX:getScale()) - self:getPageWidth())))
-            self.GRAFX:setY(math.min(0, math.max(self.GRAFX:getY(), (love.graphics:getHeight() / self.GRAFX:getScale()) - self:getPageHeight())))
+            self.BUF_GRAFX:setX(math.min(0, math.max(self.BUF_GRAFX:getX(), (love.graphics:getWidth()  / self.BUF_GRAFX:getScale()) - self:getPageWidth())))
+            self.BUF_GRAFX:setY(math.min(0, math.max(self.BUF_GRAFX:getY(), (love.graphics:getHeight() / self.BUF_GRAFX:getScale()) - self:getPageHeight())))
         end
         self.isZooming = false
     end,
 
     moveImage = function(self, deltaX, deltaY)
-        self.GRAFX:moveImage(deltaX, deltaY)
+        self.BUF_GRAFX:moveImage(deltaX, deltaY)
     end,
 
     screenToImageCoordinates = function(self, screenX, screenY)
-        return self.GRAFX:screenToImageCoordinates(screenX, screenY)
+        return self.BUF_GRAFX:screenToImageCoordinates(screenX, screenY)
     end,
 
     adjustScaleGeometrically = function(self, deltaScale)
-        self.GRAFX:adjustScaleGeometrically(deltaScale)
+        self.BUF_GRAFX:adjustScaleGeometrically(deltaScale)
         if deltaScale > 0 then self.isZooming = true end
     end,
 
     syncImageCoordinatesWithScreen = function(self, imageX, imageY, screenX, screenY)
-       self.GRAFX:syncImageCoordinatesWithScreen(imageX, imageY, screenX, screenY)
+       self.BUF_GRAFX:syncImageCoordinatesWithScreen(imageX, imageY, screenX, screenY)
     end,
 
 }):init()
