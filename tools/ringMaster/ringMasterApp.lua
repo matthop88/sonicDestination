144fd5bed6dd43fe608e6047dc8f3c70bd12ff70
Matthew Hopkins <matthop88@gmail.com>
2025-10-31 20:22:06 -0700

    Updated ringMasterApp.lua; cartographer code is extracted

--- tools/ringMaster/ringMasterApp.lua
+++ tools/ringMaster/ringMasterApp.lua
@@ -5,51 +5,24 @@
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
 local GRAFX      = require("tools/lib/graphics"):create()
+
 local RING_FORGE = require("tools/ringMaster/ringForge")
+local MAP_MAKER  = require("tools/ringMaster/cartographer")
 
-local map = ({
-    isZooming = false,
-    ring      = nil,
-
-    init = function(self)
-        self.pageWidth  = 1200
-        self.pageHeight =  800
-
-        self.MAP_GRAFX  = require("tools/lib/bufferedGraphics"):create(GRAFX, self.pageWidth, self.pageHeight)
-        self.imageData = self.MAP_GRAFX:getBuffer():newImageData()
-        return self
-    end,
+local map = {
+    isZooming  = false,
+    ring       = nil,
+    pageWidth  = 1200,
+    pageHeight = 800,
 
     draw = function(self)
         if not self.ring then 
             self.ring = RING_FORGE:create()
-            self:drawRingsToBuffer() 
+            self.map  = MAP_MAKER:create(self.ring, self.pageWidth, self.pageHeight)
         end
             
         GRAFX:setColor(1, 1, 1)
-        GRAFX:drawImage(self.MAP_GRAFX:getBuffer(), 0, 0)
-    end,
-
-    drawRingsToBuffer = function(self)
-        self:drawBackground()
-        self:drawRings()
-    end,
-
-    drawBackground = function(self)
-        self.MAP_GRAFX:setColor(0, 0, 0)
-        self.MAP_GRAFX:rectangle("fill", self.MAP_GRAFX:calculateViewport())
-        self.MAP_GRAFX:setColor(0.3, 0.3, 0.3)
-        self.MAP_GRAFX:rectangle("fill", 10, 10, self:getPageWidth() - 20, self:getPageHeight() - 20)
-    end,
-
-    drawRings = function(self)
-        self.MAP_GRAFX:setColor(1, 1, 1)
-        for i = 1, 150 do
-            local ringX = math.random(1, (self:getPageWidth()  / 16) - 3) * 16
-            local ringY = math.random(1, (self:getPageHeight() / 16) - 3) * 16
-            self.MAP_GRAFX:drawImage(self.ring:getImage(), ringX, ringY)
-        end
-        self.MAP_GRAFX:getBuffer():setFilter("nearest", "nearest")
+        GRAFX:drawImage(self.map:getImage(), 0, 0)
     end,
 
     getPageWidth  = function(self) return self.pageWidth  end,
@@ -57,30 +30,30 @@ local map = ({
     
     keepImageInBounds = function(self)
         if not self.isZooming then
-            self.MAP_GRAFX:setX(math.min(0, math.max(self.MAP_GRAFX:getX(), (love.graphics:getWidth()  / self.MAP_GRAFX:getScale()) - self:getPageWidth())))
-            self.MAP_GRAFX:setY(math.min(0, math.max(self.MAP_GRAFX:getY(), (love.graphics:getHeight() / self.MAP_GRAFX:getScale()) - self:getPageHeight())))
+            GRAFX:setX(math.min(0, math.max(GRAFX:getX(), (love.graphics:getWidth()  / GRAFX:getScale()) - self:getPageWidth())))
+            GRAFX:setY(math.min(0, math.max(GRAFX:getY(), (love.graphics:getHeight() / GRAFX:getScale()) - self:getPageHeight())))
         end
         self.isZooming = false
     end,
 
     moveImage = function(self, deltaX, deltaY)
-        self.MAP_GRAFX:moveImage(deltaX, deltaY)
+        GRAFX:moveImage(deltaX, deltaY)
     end,
 
     screenToImageCoordinates = function(self, screenX, screenY)
-        return self.MAP_GRAFX:screenToImageCoordinates(screenX, screenY)
+        return GRAFX:screenToImageCoordinates(screenX, screenY)
     end,
 
     adjustScaleGeometrically = function(self, deltaScale)
-        self.MAP_GRAFX:adjustScaleGeometrically(deltaScale)
+        GRAFX:adjustScaleGeometrically(deltaScale)
         if deltaScale > 0 then self.isZooming = true end
     end,
 
     syncImageCoordinatesWithScreen = function(self, imageX, imageY, screenX, screenY)
-       self.MAP_GRAFX:syncImageCoordinatesWithScreen(imageX, imageY, screenX, screenY)
+       GRAFX:syncImageCoordinatesWithScreen(imageX, imageY, screenX, screenY)
     end,
 
-}):init()
+}
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -95,7 +68,6 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 
 function love.draw()
     map:draw()
-    love.graphics.setColor(1, 1, 1)
 end
 
 function love.mousepressed(mx, my)
