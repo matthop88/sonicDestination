b732ab357daf3202ec8c5524d7ec9ff8124a948b
Matthew Hopkins <matthop88@gmail.com>
2025-10-31 20:20:19 -0700

    Updated ringForge.lua to utilize new filter code

--- tools/ringMaster/ringForge.lua
+++ tools/ringMaster/ringForge.lua
@@ -5,21 +5,21 @@
 local GRAFX      = require("tools/lib/graphics"):create()
 local RING_GRAFX = require("tools/lib/bufferedGraphics"):create(GRAFX, 16, 16)
 
-local OBJ_IMG    = love.graphics.newImage("tools/ringMaster/resources/commonObj.png")
-local RING_QUAD  = love.graphics.newQuad(24, 198, 16, 16, OBJ_IMG:getWidth(), OBJ_IMG:getHeight())
+local OBJECT_IMG = love.graphics.newImage("tools/ringMaster/resources/commonObj.png")
+local RING_QUAD  = love.graphics.newQuad(24, 198, 16, 16, OBJECT_IMG:getWidth(), OBJECT_IMG:getHeight())
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
 
-OBJ_IMG:setFilter("nearest", "nearest")
+OBJECT_IMG:setFilter("nearest", "nearest")
+RING_GRAFX:setFilter("nearest", "nearest")
 
 return {
     create = function(self)
         RING_GRAFX:setColor(1, 1, 1)
-        RING_GRAFX:draw(OBJ_IMG, RING_QUAD, 0, 0, 0, 1, 1)
-        RING_GRAFX:getBuffer():setFilter("nearest", "nearest")
-
+        RING_GRAFX:draw(OBJECT_IMG, RING_QUAD, 0, 0, 0, 1, 1)
+        
         return {
             getImage = function(self)
                 return RING_GRAFX:getBuffer()
