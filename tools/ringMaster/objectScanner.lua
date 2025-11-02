4a153f39a5112694941a867bfba38bdab1feffbb
Matthew Hopkins <matthop88@gmail.com>
2025-11-02 00:28:13 -0700

    Updated objectScanner.lua; fleshed out scanning algorithm

--- tools/ringMaster/objectScanner.lua
+++ tools/ringMaster/objectScanner.lua
@@ -1,3 +1,5 @@
+local PIXEL_UTIL = require("tools/lib/pixelUtil")
+
 return {
 	create = function(self, OBJECT_DATA, MAP_DATA)
 		local OBJECT_WIDTH  = OBJECT_DATA:getWidth()
@@ -11,11 +13,36 @@ return {
   				local x = 0
   				for y = 0, MAP_HEIGHT - OBJECT_HEIGHT do
   					while x < MAP_WIDTH - OBJECT_WIDTH do
-  						-- Look for object at x, y in map
+  						self:scanForObjectAt(x, y)
   						x = x + 1
   					end
   				end
 			end,
-		}
-	end,
-}
+
+			scanForObjectAt = function(self, x, y)
+				for objY = 0, OBJECT_HEIGHT - 1 do
+					if not self:scanlineMatches(objY, x, y) then
+						return false
+					end
+				end
+
+				return true
+			end,
+
+			scanlineMatches = function(self, objY, x, y)
+				for objX = 0, OBJECT_WIDTH - 1 do
+    				local mapX, mapY = objX + x, objY + y
+    				if not self:pixelsMatch(objX, objY, mapX, mapY) then
+    					return false
+    				end
+    			end
+
+    			return true
+    		end,
+
+    		pixelsMatch = function(self, objX, objY, mapX, mapY)
+    			return PIXEL_UTIL:pixelsMatchWithWildcardTransparency(OBJECT_DATA, objX, objY, MAP_DATA, mapX, mapY) 
+ 			end,
+ 		}
+ 	end,
+ }
