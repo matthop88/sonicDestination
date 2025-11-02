fc09c2d0a3611eccee259ba7eb926da2109e5d91
Matthew Hopkins <matthop88@gmail.com>
2025-11-02 01:23:13 -0800

    Made tolerance more lenient in pixelUtil. Now finds majority of rings.

--- tools/lib/pixelUtil.lua
+++ tools/lib/pixelUtil.lua
@@ -5,11 +5,12 @@
 -- Over all, a 20-40% improvement in performance.
 
 return {
-    colorsMatch = function(self, r1, g1, b1, a1, r2, g2, b2, a2)
-        return math.abs(r1 - r2) < 0.005
-           and math.abs(g1 - g2) < 0.005 
-           and math.abs(b1 - b2) < 0.005
-           and math.abs(a1 - a2) < 0.005
+    colorsMatch = function(self, r1, g1, b1, a1, r2, g2, b2, a2, tolerance)
+        tolerance = tolerance or 0.005
+        return math.abs(r1 - r2) < tolerance
+           and math.abs(g1 - g2) < tolerance
+           and math.abs(b1 - b2) < tolerance
+           and math.abs(a1 - a2) < tolerance
     end,
 
     pixelsMatch = function(self, imageData1, x1, y1, imageData2, x2, y2)
@@ -26,7 +27,7 @@ return {
             if debug then
                 print("{ r = " .. r1 .. ", g = " .. g1 .. ", b = " .. b1 .. ", a = " .. a1 .. " } vs { r = " .. r2 .. ", g = " .. g2 .. ", b = " .. b2 .. ", a = " .. a2 .. "}")
             end
-            return self:colorsMatch(r1, g1, b1, a1, r2, g2, b2, a2)
+            return self:colorsMatch(r1, g1, b1, a1, r2, g2, b2, a2, 0.1)
         end
     end,        
 }
