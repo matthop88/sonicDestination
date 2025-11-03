c24ee88a76e84102fc4785f7638d5db4b11d7fd9
Matthew Hopkins <matthop88@gmail.com>
2025-11-03 13:40:10 -0800

    Updated progressBar.lua to have updatable progress

--- tools/ringMaster/progressBar.lua
+++ tools/ringMaster/progressBar.lua
@@ -1,10 +1,26 @@
+local CALLBACK
+
 return {
 	create = function(self, callback)
+		CALLBACK = callback
 		return {
+			progress = 0.0,
+
 			draw = function(self)
-				love.graphics.setColor(1, 1, 1)
-				love.graphics.setLineWidth(1)
-				love.graphics.rectangle("line", 100, 380, 1000, 40)
+				if self.progress ~= 0 then
+					love.graphics.setColor(1, 1, 1)
+					love.graphics.setLineWidth(1)
+					love.graphics.rectangle("line", 100, 380, 1000, 40)
+					love.graphics.setColor(1, 1, 0)
+					love.graphics.rectangle("fill", 103, 382, 994 * self.progress, 36)
+				end
+			end,
+
+			update = function(self, dt, callback)
+				CALLBACK = CALLBACK or callback
+				if CALLBACK then
+					self.progress = CALLBACK()
+				end
 			end,
 		}
 	end,
