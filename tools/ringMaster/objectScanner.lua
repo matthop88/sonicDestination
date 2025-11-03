c5a8c26aa2f0adb07c3f23697c46cca96c300cbb
Matthew Hopkins <matthop88@gmail.com>
2025-11-02 16:03:55 -0800

    Update objectScanner.lua; removed all functions and made them local

--- tools/ringMaster/objectScanner.lua
+++ tools/ringMaster/objectScanner.lua
@@ -15,6 +15,54 @@ local MAP_WIDTH,      MAP_HEIGHT
 local MAP_START_X,    MAP_START_Y
 local MAP_END_X,      MAP_END_Y
 
+local OBJECTS_FOUND = {}
+
+local scanAll, scanForObjectsAtLine, scanForObjectAt, scanForObjectScanlineAt, pixelsMatch
+
+doScanning = function()
+  	for y = MAP_START_Y, MAP_END_Y do
+  		scanForObjectsAtLine(y)
+  	end
+end
+
+scanForObjectsAtLine = function(y)
+	local x = MAP_START_X
+	while x < MAP_END_X do
+		if scanForObjectAt(x, y) then
+			table.insert(OBJECTS_FOUND, { x = x, y = y })
+			print("" .. #OBJECTS_FOUND .. ": Found ring at { " .. x .. ", " .. y .. " }")
+			x = x + 16
+		else
+			x = x + 1
+		end
+	end
+end
+
+scanForObjectAt = function(x, y)
+	for objY = 0, OBJECT_HEIGHT - 1 do
+		if not scanForObjectScanlineAt(objY, x, y) then
+			return false
+		end
+	end
+
+	return true
+end
+
+scanForObjectScanlineAt = function(objY, x, y)
+	for objX = 0, OBJECT_WIDTH - 1 do
+		local mapX, mapY = objX + x, objY + y
+		if not pixelsMatch(OBJECT_START_X + objX, OBJECT_START_Y + objY, mapX, mapY) then
+			return false
+		end
+	end
+
+	return true
+end
+
+pixelsMatch = function(objX, objY, mapX, mapY)
+    return PIXEL_UTIL:pixelsMatchWithWildcardTransparency(OBJECT_DATA, objX, objY, MAP_DATA, mapX, mapY) 	
+end
+
 return {
 	create = function(self, objectInfo, mapInfo)
 		OBJECT_DATA                    = objectInfo.data
@@ -30,54 +78,12 @@ return {
   		MAP_END_Y                      = MAP_START_Y + MAP_HEIGHT - OBJECT_HEIGHT
 
   		return {
-			objectsFound = {},
-
-			scanAll = function(self)
-  				for y = MAP_START_Y, MAP_END_Y do
-  					self:scanForObjectsAtLine(y)
-  				end
+  			scanAll = function(self)
+  				doScanning()
   			end,
-
-  			scanForObjectsAtLine = function(self, y)
-  				local x = MAP_START_X
-  				while x < MAP_END_X do
-  					if self:scanForObjectAt(x, y) then
-  						table.insert(self.objectsFound, { x = x, y = y })
-  						print("" .. #self.objectsFound .. ": Found ring at { " .. x .. ", " .. y .. " }")
-  						x = x + 16
-  					else
-  						x = x + 1
-  					end
-  				end
-			end,
-
-			scanForObjectAt = function(self, x, y)
-				for objY = 0, OBJECT_HEIGHT - 1 do
-					if not self:scanForObjectScanlineAt(objY, x, y) then
-						return false
-					end
-				end
-
-				return true
-			end,
-
-			scanForObjectScanlineAt = function(self, objY, x, y)
-				for objX = 0, OBJECT_WIDTH - 1 do
-    				local mapX, mapY = objX + x, objY + y
-    				if not self:pixelsMatch(OBJECT_START_X + objX, OBJECT_START_Y + objY, mapX, mapY) then
-    					return false
-    				end
-    			end
-
-    			return true
-    		end,
-
-    		pixelsMatch = function(self, objX, objY, mapX, mapY)
-    			return PIXEL_UTIL:pixelsMatchWithWildcardTransparency(OBJECT_DATA, objX, objY, MAP_DATA, mapX, mapY) 	
- 			end,
-
+  			
 			getObjectsFound = function(self)
- 				return self.objectsFound
+ 				return OBJECTS_FOUND
  			end,
  		}
  	end,
