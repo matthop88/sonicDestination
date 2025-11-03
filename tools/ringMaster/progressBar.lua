1a0103ba00886a0eaa5c81774e365507bbf7de62
Matthew Hopkins <matthop88@gmail.com>
2025-11-03 14:30:53 -0800

    Added transparent black rect behind progress bar

--- tools/ringMaster/progressBar.lua
+++ tools/ringMaster/progressBar.lua
@@ -12,7 +12,9 @@ return {
 			alpha = 0.0,
 			
 			draw = function(self)
-				if self.progress ~= 0 then
+				if self.progress ~= 0 and self.alpha > 0.0 then
+					love.graphics.setColor(0, 0, 0, self.alpha * 0.5)
+					love.graphics.rectangle("fill", 90, 320, 1020, 120)
 					love.graphics.setColor(1, 1, 1, self.alpha)
 					love.graphics.setLineWidth(1)
 					love.graphics.rectangle("line", 100, 380, 1000, 40)
