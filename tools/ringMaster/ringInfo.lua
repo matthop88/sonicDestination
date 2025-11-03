110b99a085bf2f718de0d89a6d23a826d0f4ea76
Matthew Hopkins <matthop88@gmail.com>
2025-11-03 12:43:13 -0800

    Added capacity to draw ring in ringInfo.lua

--- tools/ringMaster/ringInfo.lua
+++ tools/ringMaster/ringInfo.lua
@@ -4,6 +4,7 @@
 
 local OBJECT_DATA = love.image.newImageData("tools/ringMaster/resources/commonObj.png")
 local OBJECT_IMG  = love.graphics.newImage(OBJECT_DATA)
+local RING_QUAD   = love.graphics.newQuad(24, 198, 16, 16, OBJECT_IMG:getWidth(), OBJECT_IMG:getHeight())
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -17,4 +18,9 @@ return {
     height = 16, 
     startX = 24, 
     startY = 198,
+
+    draw = function(self, x, y, scale, color)
+        love.graphics.setColor(color)
+        love.graphics.draw(OBJECT_IMG, RING_QUAD, x, y, 0, scale, scale)
+    end,
 }
