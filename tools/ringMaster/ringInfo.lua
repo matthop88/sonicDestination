8e347e6da58feb760efb91b601b7a466da2b9f46
Matthew Hopkins <matthop88@gmail.com>
2025-11-05 13:51:39 -0800

    Updated ringInfo.lua to utilize color position model and max strikes

--- tools/ringMaster/ringInfo.lua
+++ tools/ringMaster/ringInfo.lua
@@ -2,10 +2,11 @@
 --                     Local Variables                      --
 --------------------------------------------------------------
 
-local OBJECT_DATA = love.image.newImageData("tools/ringMaster/resources/commonObj.png")
-local OBJECT_IMG  = love.graphics.newImage(OBJECT_DATA)
-local RING_QUAD   = love.graphics.newQuad(24, 198, 16, 16, OBJECT_IMG:getWidth(), OBJECT_IMG:getHeight())
-local COLOR_FREQ  = require("tools/ringMaster/colorClassifier"):classifyImageData(OBJECT_DATA, 24, 198, 16, 16)
+local OBJECT_DATA   = love.image.newImageData("tools/ringMaster/resources/commonObj.png")
+local OBJECT_IMG    = love.graphics.newImage(OBJECT_DATA)
+local RING_QUAD     = love.graphics.newQuad(24, 198, 16, 16, OBJECT_IMG:getWidth(), OBJECT_IMG:getHeight())
+local COLOR_FREQ    = require("tools/ringMaster/colorClassifier"):classifyImageData(OBJECT_DATA, 24, 198, 16, 16)
+local MATCH_PERCENT = 50
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -13,23 +14,24 @@ local COLOR_FREQ  = require("tools/ringMaster/colorClassifier"):classifyImageDat
 
 OBJECT_IMG:setFilter("nearest", "nearest")
 
-local minInstances = 256
+local maxInstances = 0
 local bestKey      = 0
 
 for n, v in ipairs(COLOR_FREQ) do
-    if minInstances > v.frequency then
-        minInstances = v.frequency
+    if maxInstances < v.frequency and v.color.a ~= 0 then
+        maxInstances = v.frequency
         bestKey = n
     end
 end
 
 return {
-    data     = OBJECT_DATA, 
-    width    = 16,
-    height   = 16, 
-    startX   = 24, 
-    startY   = 198,
-    keyColor = COLOR_FREQ[bestKey],
+    data       = OBJECT_DATA, 
+    width      = 16,
+    height     = 16, 
+    startX     = 24, 
+    startY     = 198,
+    keyColor   = COLOR_FREQ[bestKey],
+    maxStrikes = math.ceil(COLOR_FREQ[bestKey].frequency * (100 - MATCH_PERCENT) / 100),
 
     draw = function(self, x, y, scale, color)
         love.graphics.setColor(color)
