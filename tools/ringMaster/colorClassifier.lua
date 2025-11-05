96591d1a3817455024c1e0d5fa2e30b9fdc2b2f8
Matthew Hopkins <matthop88@gmail.com>
2025-11-04 18:19:37 -0800

    Refactored colorClassifier.lua - now return color frequency list

--- tools/ringMaster/colorClassifier.lua
+++ tools/ringMaster/colorClassifier.lua
@@ -1,19 +1,49 @@
+local isFullyTransparent = function(color) return color.a == 0 end
+
+local buildColor = function(r, g, b, a)
+	local rb, gb, bb, ab = love.math.colorToBytes(r, g, b, a)
+	return { r = rb, g = gb, b = bb, a = ab }
+end
+
+local buildColorKey = function(color)
+	return "" .. ((color.r * 16777216) + (color.g * 65536) + (color.b * 256) + color.a)
+end
+
 return {
 	classifyImageData = function(self, imgData, startX, startY, width, height)
-		local colorFrequencies = {}
+		local colorFrequencyMap = self:buildColorFrequencyMap(imgData, startX, startY, width, height)
+
+		return self:convertToColorFrequencyList(colorFrequencyMap)
+	end,
+
+	buildColorFrequencyMap = function(self, imgData, startX, startY, width, height)
+		local colorFrequencyMap = {}
 
 		for y = startY, startY + height - 1 do
 			for x = startX, startX + width - 1 do
-				local r, g, b, a = love.math.colorToBytes(imgData:getPixel(x, y))
-				local colorKey = "" .. ((r * 16777216) + (g * 65536) + (b * 256) + a)
-				if not colorFrequencies[colorKey] then
-					colorFrequencies[colorKey] = { color = { r = r, g = g, b = b, a = a }, frequency = 1 }
-				else
-					colorFrequencies[colorKey].frequency = colorFrequencies[colorKey].frequency + 1
+				local myColor = buildColor(imgData:getPixel(x, y))
+				if not isFullyTransparent(myColor) then
+					local colorKey = buildColorKey(myColor)
+					if not colorFrequencyMap[colorKey] then
+						colorFrequencyMap[colorKey] = { color = myColor, frequency = 1 }
+					else
+						colorFrequencyMap[colorKey].frequency = colorFrequencyMap[colorKey].frequency + 1
+					end
 				end
 			end
 		end
 
+		return colorFrequencyMap
+	end,
+
+	convertToColorFrequencyList = function(self, colorFrequencyMap)
+		local colorFrequencies = {}
+
+		for k, colorFrequency in pairs(colorFrequencyMap) do
+			table.insert(colorFrequencies, colorFrequency)
+		end
+
 		return colorFrequencies
 	end,
+
 }
