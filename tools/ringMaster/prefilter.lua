46c71219eeed8890a8c6ab097d51cf9223f273d2
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 15:29:24 -0800

    Created prefilter.lua

---
+++ tools/ringMaster/prefilter.lua
@@ -0,0 +1,57 @@
+local PIXEL_UTIL = require("tools/lib/pixelUtil")
+
+local HOT_LIST   = require("tools/ringMaster/rectList"):create()
+local COLD_LIST  = require("tools/ringMaster/rectList"):create()
+
+return {
+	prefilter = function(self, imageData, hotColor)
+		print("Hot Color: r = " .. hotColor.r .. ", g = " .. hotColor.g .. ", b = " .. hotColor.b .. ", a = " .. hotColor.a)
+		local myColor = { r = hotColor.r / 255, g = hotColor.r / 255, b = hotColor.b / 255, a = hotColor.a / 255 }
+		local isHot = false
+		for x = 0, imageData:getWidth() - 1 do
+			for y = 0, imageData:getHeight() - 1 do
+				if PIXEL_UTIL:pixelMatchesColor(imageData, x, y, myColor, 0.1, false) then
+					HOT_LIST:add(x)
+					isHot = true
+					break
+				end
+			end
+			if not isHot then COLD_LIST:add(x) end
+			isHot = false
+		end
+
+		COLD_LIST = self:compressList(COLD_LIST)
+
+		--[[
+		print("Hot List:")
+		print("---------")
+		self:printList(self:getHotList())
+
+		print("Cold List:")
+		print("----------")
+		self:printList(self:getColdList())
+		--]]
+
+		return self:getHotList(), self:getColdList()
+	end,
+
+	getHotList  = function(self) return HOT_LIST  end,
+	getColdList = function(self) return COLD_LIST end,
+
+	printList = function(self, myList)
+		for n, elt in ipairs(myList) do
+			print("" .. n .. ": x = " .. elt.x .. ", w = " .. elt.w)
+		end
+	end,
+
+	compressList = function(self, myList)
+		local newList = {}
+		for _, elt in ipairs(myList) do
+			if elt.w > 15 then
+				table.insert(newList, { x = elt.x - 3, w = elt.w - 12 })
+			end
+		end
+
+		return newList
+	end,
+}
