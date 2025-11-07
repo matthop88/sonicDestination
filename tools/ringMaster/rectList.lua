49d3f756b5197faedf6108be7812094a5dc62a0b
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 18:02:47 -0800

    Made rectList.lua more generic

--- tools/ringMaster/rectList.lua
+++ tools/ringMaster/rectList.lua
@@ -3,24 +3,24 @@ return {
 		return {
 			getLast = function(self) return self[#self] end,
 
-			add = function(self, x)
-				if self:canAppend(x) then
-					self:getLast().w = self:getLast().w + 1
+			add = function(self, offset)
+				if self:canAppend(offset) then
+					self:getLast().size = self:getLast().size + 1
 				else
-					if #self == 0 or x ~= self:getLast().x then
-						table.insert(self, { x = x, w = 1 })
+					if #self == 0 or offset ~= self:getLast().offset then
+						table.insert(self, { offset = offset, size = 1 })
 					end
 				end
 			end,
 
-			canAppend = function(self, x)
+			canAppend = function(self, offset)
 				if #self > 0 then
-					return self:canJoinTogether(self:getLast(), x)
+					return self:canJoinTogether(self:getLast(), offset)
 				end
 			end,
 
-			canJoinTogether = function(self, rect, x)
-				return rect.x + rect.w == x
+			canJoinTogether = function(self, rect, offset)
+				return rect.offset + rect.size == offset
 			end,
 
 		}
