0830e565993becd4c9da5f1b7fe30f19f441f641
Matthew Hopkins <matthop88@gmail.com>
2025-11-07 11:50:15 -0800

    Added new class to prefilter results of previous prefiltering.

---
+++ tools/ringMaster/prefilter2.lua
@@ -0,0 +1,66 @@
+local PIXEL_UTIL = require("tools/lib/pixelUtil")
+
+local HOT_LIST
+
+return {
+	prefilter = function(self, imageData, hotColor, hotList)
+		HOT_LIST = hotList
+		for _, block in ipairs(HOT_LIST) do
+			block.coldList = require("tools/ringMaster/rectList"):create()
+			self:prefilterAtBlock(imageData, hotColor, block) 
+		end
+
+		return self:getResults()
+	end,
+
+	prefilterAtBlock = function(self, imageData, hotColor, block)
+		for y = 0, imageData:getHeight() - 1 do
+			self:prefilterAtScanLine(imageData, hotColor, block, y)
+		end
+	end,
+
+	prefilterAtScanLine = function(self, imageData, hotColor, block, scanY)
+		local myColor = { r = hotColor.r / 255, g = hotColor.r / 255, b = hotColor.b / 255, a = hotColor.a / 255 }
+		local isHot = false
+		for x = block.offset, block.offset + block.size - 1 do
+			if PIXEL_UTIL:pixelMatchesColor(imageData, x, scanY, myColor, 0.1, false) then
+				isHot = true
+				break
+			end
+		end
+		if not isHot then block.coldList:add(scanY) end
+	end,
+
+	getResults = function(self)
+		HOT_LIST = self:compressList(HOT_LIST)
+
+		return self:getHotList()
+	end,
+
+	compressList = function(self, myList)
+		local newList = {}
+		for _, elt in ipairs(myList) do
+			if elt.size > 15 then
+				if elt.coldList then
+					table.insert(newList, { offset = elt.offset, size = elt.size, coldList = self:compressList(elt.coldList) })
+				else
+					table.insert(newList, { offset = elt.offset - 3, size = elt.size - 12 })
+				end
+			end
+		end
+
+		return newList
+	end,
+
+	getHotList = function(self) return HOT_LIST end,
+
+	printList = function(self, myList, indent)
+		indent = indent or ""
+		for n, elt in ipairs(myList) do
+			print(indent .. n .. ": offset = " .. elt.offset .. ", size = " .. elt.size)
+			if elt.coldList then
+				self:printList(elt.coldList, "  ")
+			end
+		end
+	end,
+}
