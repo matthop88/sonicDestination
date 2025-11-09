d61aaccd4a8d06364215a23bf2f847ab9711ddcb
Matthew Hopkins <matthop88@gmail.com>
2025-11-09 14:56:55 -0800

    Added command schema to ringMaster

---
+++ tools/ringMaster/cmdSchema.lua
@@ -0,0 +1,16 @@
+return {
+	VERSION = 0.1,
+
+	COMMANDS = {
+		mapIn = {
+			description = "map image file to scan and find rings in",
+			shortcut = "i",
+			required = true,
+		},
+
+		ringDataOut = {
+			description = "data file to write out which specifies ring locations",
+			shortcut = "o",
+		},
+	},
+}
