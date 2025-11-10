dd18140efe442d2ed57df35ee2a73c9f4473f35b
Matthew Hopkins <matthop88@gmail.com>
2025-11-10 15:19:21 -0800

    Changed ringSmarts.lua so that ring descriptions are above the borders

--- tools/ringMaster/ringSmarts.lua
+++ tools/ringMaster/ringSmarts.lua
@@ -22,7 +22,7 @@ local drawRingDetails = function(ring, imageViewer)
     local mx, my = love.mouse.getPosition()
     love.graphics.setColor(1, 1, 1, 0.7)
     love.graphics.rectangle("fill", imageViewer:pageToScreenRect(ring.x - 10, ring.y - 10, 20, 20))
-    love.graphics.setColor(0, 0, 0)
+    love.graphics.setColor(0, 0, 0, 0.5)
     local scale = imageViewer:getScale()
     love.graphics.rectangle("fill", mx + (16 * scale), my + (16 * scale), 100 * scale, 20 * scale)
     love.graphics.setColor(1, 1, 1)
@@ -31,26 +31,27 @@ local drawRingDetails = function(ring, imageViewer)
     love.graphics.printf("" .. ring.x .. ", " .. ring.y, mx + (16 * scale), my + (16 * scale), 100 * scale, "center")
 end
 
+local drawRingBorder = function(ring, imageViewer)
+    love.graphics.setColor(1, 1, 0, (1 - ring.alpha) * 0.7)
+    love.graphics.setLineWidth(1 * imageViewer:getScale())
+    local px, py = imageViewer:imageToScreenCoordinates(ring.x, ring.y)
+    love.graphics.push()
+    love.graphics.translate(px, py)
+    love.graphics.rotate(ROTATION)
+    love.graphics.translate(-px, -py)
+    love.graphics.rectangle("line", imageViewer:pageToScreenRect(ring.x - 9, ring.y - 9, 17, 17))
+    love.graphics.pop()
+end
+
 local drawRing = function(ring, imageViewer, ringInfo)
     if ring.alpha ~= nil then
         local ringScale = math.max(1, (ring.alpha * ring.alpha * 400))
         local deltaX = ring.deltaX * (ringScale - 1)
         local deltaY = ring.deltaY * (ringScale - 1)
-        if ringScale == 1 and isInsideRing(ring, imageViewer:screenToImageCoordinates(love.mouse.getPosition())) then
-            drawRingDetails(ring, imageViewer)
-        end
+        drawRingBorder(ring, imageViewer)
         local x, y = imageViewer:imageToScreenCoordinates(ring.x + deltaX, ring.y + deltaY)
         local scale = imageViewer:getScale() * ringScale
         ringInfo:draw(x, y, scale, { 1, 0, 0, RING_PULSE * (1 - ring.alpha) *  (1 - ring.alpha) * 0.7})
-        love.graphics.setColor(1, 1, 0, (1 - ring.alpha) * 0.7)
-        love.graphics.setLineWidth(1 * imageViewer:getScale())
-        local px, py = imageViewer:imageToScreenCoordinates(ring.x, ring.y)
-        love.graphics.push()
-        love.graphics.translate(px, py)
-        love.graphics.rotate(ROTATION)
-        love.graphics.translate(-px, -py)
-        love.graphics.rectangle("line", imageViewer:pageToScreenRect(ring.x - 9, ring.y - 9, 17, 17))
-        love.graphics.pop()
     end
 end
 
@@ -69,6 +70,11 @@ local drawRings = function(self, imageViewer, ringInfo)
 	for _, ring in ipairs(self) do
 		drawRing(ring, imageViewer, ringInfo)
 	end
+    for _, ring in ipairs(self) do
+        if ring.alpha == 0 and isInsideRing(ring, imageViewer:screenToImageCoordinates(love.mouse.getPosition())) then
+            drawRingDetails(ring, imageViewer)
+        end
+    end
 end
 
 local updateRings = function(self, dt, isComplete)
