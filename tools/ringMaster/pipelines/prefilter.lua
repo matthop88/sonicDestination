0c5d33fa122cafb5438629826d4d35a905fdb04b
Matthew Hopkins <matthop88@gmail.com>
2025-11-10 11:07:51 -0800

    Removed unnecessary cold list from prefilter

--- tools/ringMaster/pipelines/prefilter.lua
+++ tools/ringMaster/pipelines/prefilter.lua
@@ -1,75 +1,90 @@
+local PIPELINE   = require("tools/lib/pipeline/pipeline"):create("Prefilter Pipeline")
+local FEEDER     = require("tools/lib/pipeline/feeder")
+
 local PIXEL_UTIL = require("tools/lib/pixelUtil")
 
-local HOT_LIST
-local COLD_LIST  = require("tools/ringMaster/rectList"):create()
+local TASK_SLICE_TIME_IN_MS = 12
 
-local IMAGE_DATA, HOT_COLOR
+local COMPARISON_COLOR
+
+local PREFILTER_ENGINE = require("tools/ringMaster/pipelines/prefilter")
+local MAP_VLINE
+
+local HOT_LIST
+
+local doPrefiltering, prefilterAtVLine, createMapFeeder
+
+doPrefiltering = function(params, nextParams)
+	if params.MAP_VLINES:isComplete() then
+		HOT_LIST = PREFILTER_ENGINE:getResults()
+		
+		MAP_VLINE = MAP_WIDTH
+		return true
+	end
+
+	nextParams:init {
+		x = params.MAP_VLINES:next()
+	}
+
+end
+
+prefilterAtVLine = function(params, nextParams)
+	MAP_VLINE = params.x
+	PREFILTER_ENGINE:prefilterAtVScan(MAP_DATA, COMPARISON_COLOR, params.x)
+
+	return true
+end
+
+createMapFeeder = function()
+	local myList = {}
+	for x = MAP_START_X, MAP_END_X do
+		table.insert(myList, x)
+	end
+
+	return FEEDER:create("Map VLines", myList)
+end
 
 return {
-	prefilter = function(self, imageData, hotColor)
-
-		print("Hot Color: r = " .. hotColor.r .. ", g = " .. hotColor.g .. ", b = " .. hotColor.b .. ", a = " .. hotColor.a)
-		
-		for x = 0, imageData:getWidth() - 1 do
-			self:prefilterAtVScan(imageData, hotColor, x)
-		end
-
-		return self:getResults()
+	setup = function(self, objectInfo, mapInfo)
+		OBJECT_WIDTH      = objectInfo.width
+  		COMPARISON_COLOR  = objectInfo.keyColor.color
+  		MAP_DATA          = mapInfo.data
+		MAP_WIDTH         = mapInfo.width
+  		MAP_START_X       = mapInfo.startX
+  		MAP_END_X         = MAP_START_X + MAP_WIDTH - OBJECT_WIDTH
+  		
+  		PIPELINE:add("Prefilter All",  doPrefiltering)
+		PIPELINE:add("Scan at VLine",  prefilterAtVLine)
+		PIPELINE:push { MAP_VLINES = createMapFeeder() }
 	end,
 
-	prefilterAtVScan = function(self, imageData, hotColor, scanX)
-		IMAGE_DATA, HOT_COLOR = imageData, hotColor
-		local myColor = { r = hotColor.r / 255, g = hotColor.r / 255, b = hotColor.b / 255, a = hotColor.a / 255 }
-		local isHot = false
-		for y = 0, imageData:getHeight() - 1 do
-			if PIXEL_UTIL:pixelMatchesColor(imageData, scanX, y, myColor, 0.1, false) then
-				isHot = true
-				break
-			end
-		end
-		if not isHot then COLD_LIST:add(scanX) end
+	execute = function(self)
+		PIPELINE:execute(TASK_SLICE_TIME_IN_MS)
 	end,
 
-	getResults = function(self)
-		self:compressColdList()
-		
-		HOT_LIST = self:generateHotList()
-		
-		return self:getHotList()
+	isComplete = function(self)
+		return PIPELINE:isComplete()
 	end,
 
-	compressColdList = function(self)
-		local newList = {}
-		for n, elt in ipairs(COLD_LIST) do
-			if n == #COLD_LIST then
-				table.insert(newList, { offset = elt.offset - 3, size = IMAGE_DATA:getWidth() - elt.offset + 2 })
-			elseif elt.size > 15 then
-				table.insert(newList, { offset = elt.offset, size = elt.size - 15 })
-			end
-		end
-
-		COLD_LIST = newList
+	isReady = function(self)
+		return COMPARISON_COLOR ~= nil and not PIPELINE:isComplete()
 	end,
 
-	generateHotList = function(self)
-		local offset, size = 0, 0
-		local newList = {}
-		for _, elt in ipairs(COLD_LIST) do
-			if offset < elt.offset then
-				size = elt.offset - offset
-				table.insert(newList, { offset = offset, size = size })
-				offset = elt.offset + elt.size
-			end
-		end
-		return newList
+	getVScanX = function(self)
+		return MAP_VLINE
 	end,
 
-	getHotList  = function(self) return HOT_LIST  end,
-	getColdList = function(self) return COLD_LIST end,
-
-	printList = function(self, myList)
-		for n, elt in ipairs(myList) do
-			print("" .. n .. ": offset = " .. elt.offset .. ", size = " .. elt.size)
-		end
+	getProgress = function(self)
+		if   MAP_VLINE == nil then return 0
+		else                       return MAP_VLINE / MAP_WIDTH end
 	end,
+
+	getHotList = function(self)
+		return HOT_LIST
+	end,
+
+	getTotalElapsedTime = function(self)
+		return PIPELINE:getTotalElapsedTime()
+	end,
+
 }
