427f8dfcb73ddbde73e4acdfd7db7b6f7b30180f
Matthew Hopkins <matthop88@gmail.com>
2025-11-10 10:55:12 -0800

    Updated ringSmarts.lua so that borders around discovered rings rotate

--- tools/ringMaster/ringSmarts.lua
+++ tools/ringMaster/ringSmarts.lua
@@ -1,7 +1,8 @@
 local RING_PULSE       = 1
 local TIME             = 0
 
-local FONT           = love.graphics.newFont(16)
+local FONT             = love.graphics.newFont(16)
+local ROTATION         = 0
 
 FONTS = {
     get = function(self, size)
@@ -35,7 +36,6 @@ local drawRing = function(ring, imageViewer, ringInfo)
         local ringScale = math.max(1, (ring.alpha * ring.alpha * 400))
         local deltaX = ring.deltaX * (ringScale - 1)
         local deltaY = ring.deltaY * (ringScale - 1)
-
         if ringScale == 1 and isInsideRing(ring, imageViewer:screenToImageCoordinates(love.mouse.getPosition())) then
             drawRingDetails(ring, imageViewer)
         end
@@ -44,7 +44,13 @@ local drawRing = function(ring, imageViewer, ringInfo)
         ringInfo:draw(x, y, scale, { 1, 0, 0, RING_PULSE * (1 - ring.alpha) *  (1 - ring.alpha) * 0.7})
         love.graphics.setColor(1, 1, 0, (1 - ring.alpha) * 0.7)
         love.graphics.setLineWidth(1 * imageViewer:getScale())
-        love.graphics.rectangle("line", imageViewer:pageToScreenRect(ring.x - 8, ring.y - 8, 16, 16))
+        local px, py = imageViewer:imageToScreenCoordinates(ring.x, ring.y)
+        love.graphics.push()
+        love.graphics.translate(px, py)
+        love.graphics.rotate(ROTATION)
+        love.graphics.translate(-px, -py)
+        love.graphics.rectangle("line", imageViewer:pageToScreenRect(ring.x - 9, ring.y - 9, 17, 17))
+        love.graphics.pop()
     end
 end
 
@@ -69,6 +75,8 @@ local updateRings = function(self, dt, isComplete)
 	for _, ring in ipairs(self) do
 		updateRing(ring, dt)
 	end
+
+    ROTATION = ROTATION + (math.pi / 90)
 
 	if isComplete then
         RING_PULSE = 0.5 + math.sin(TIME * 5) * 0.5
