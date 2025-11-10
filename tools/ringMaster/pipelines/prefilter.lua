b13efdf9f84da0ea358cf43eb3a8c818ffef8fca
Matthew Hopkins <matthop88@gmail.com>
2025-11-10 11:28:22 -0800

    Changed prefilter to generate hot list. Now only finds 141 rings.

--- tools/ringMaster/pipelines/prefilter.lua
+++ tools/ringMaster/pipelines/prefilter.lua
@@ -1,7 +1,6 @@
 local PIXEL_UTIL = require("tools/lib/pixelUtil")
 
-local HOT_LIST
-local COLD_LIST  = require("tools/ringMaster/rectList"):create()
+local HOT_LIST   = require("tools/ringMaster/rectList"):create()
 
 local IMAGE_DATA, HOT_COLOR
 
@@ -20,53 +19,30 @@ return {
 	prefilterAtVScan = function(self, imageData, hotColor, scanX)
 		IMAGE_DATA, HOT_COLOR = imageData, hotColor
 		local myColor = { r = hotColor.r / 255, g = hotColor.r / 255, b = hotColor.b / 255, a = hotColor.a / 255 }
-		local isHot = false
 		for y = 0, imageData:getHeight() - 1 do
 			if PIXEL_UTIL:pixelMatchesColor(imageData, scanX, y, myColor, 0.1, false) then
-				isHot = true
+				HOT_LIST:add(scanX)
 				break
 			end
 		end
-		if not isHot then COLD_LIST:add(scanX) end
 	end,
 
 	getResults = function(self)
-		self:compressColdList()
+		self:normalizeHotList()
 		
-		HOT_LIST = self:generateHotList()
-		
+		self:printList(HOT_LIST)
 		return self:getHotList()
 	end,
 
-	compressColdList = function(self)
-		local newList = {}
-		for n, elt in ipairs(COLD_LIST) do
-			if n == #COLD_LIST then
-				table.insert(newList, { offset = elt.offset - 3, size = IMAGE_DATA:getWidth() - elt.offset + 2 })
-			elseif elt.size > 15 then
-				table.insert(newList, { offset = elt.offset, size = elt.size - 15 })
-			end
+	normalizeHotList = function(self)
+		for n, elt in ipairs(HOT_LIST) do
+			elt.offset = math.max(0, elt.offset - 3)
+			elt.size = elt.size + 3
 		end
-
-		COLD_LIST = newList
-	end,
-
-	generateHotList = function(self)
-		local offset, size = 0, 0
-		local newList = {}
-		for _, elt in ipairs(COLD_LIST) do
-			if offset < elt.offset then
-				size = elt.offset - offset
-				table.insert(newList, { offset = offset, size = size })
-				offset = elt.offset + elt.size
-			end
-		end
-		return newList
 	end,
 
 	getHotList  = function(self) return HOT_LIST  end,
-	getColdList = function(self) return COLD_LIST end,
-
+	
 	printList = function(self, myList)
 		for n, elt in ipairs(myList) do
 			print("" .. n .. ": offset = " .. elt.offset .. ", size = " .. elt.size)
