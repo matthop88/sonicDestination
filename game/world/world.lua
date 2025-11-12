17fe46d4c4c65d93f05dbd8fb3b4eef14834dca2
Matthew Hopkins <matthop88@gmail.com>
2025-11-12 13:48:47 -0800

    Updated world.lua to add a ring, draw and update as well

--- game/world/world.lua
+++ game/world/world.lua
@@ -3,17 +3,29 @@ local TERRAIN
 local WORKSPACE
 
 return {
+    objects = {},
+
     init = function(self, params)
         GRAPHICS = params.GRAPHICS
         TERRAIN  = requireRelative("world/terrain/terrain", { GRAPHICS = GRAPHICS })
         WORKSPACE = requireRelative("world/workspace",      { GRAPHICS = GRAPHICS })
         
+        table.insert(self.objects, requireRelative("world/gameObjects/object"):create("objects/ring", 500, 500, GRAPHICS))
         return self
     end,
 
     draw = function(self)
         TERRAIN:draw()
         WORKSPACE:draw()
+        for _, obj in ipairs(self.objects) do
+            obj:draw()
+        end
+    end,
+
+    update = function(self, dt)
+        for _, obj in ipairs(self.objects) do
+            obj:update(dt)
+        end
     end,
 
     refresh     = function(self)       TERRAIN:refresh()                end,
