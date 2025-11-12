a74cfbcc75e2663f206f5dfded1a764e2e507975
Matthew Hopkins <matthop88@gmail.com>
2025-11-12 12:56:33 -0800

    Added basic ring spinning animation to sonic1CommonObjects.lua data file

--- tools/spriteSheetSlicer/data/sonic1CommonObjects.lua
+++ tools/spriteSheetSlicer/data/sonic1CommonObjects.lua
@@ -2,5 +2,17 @@ return {
     imagePath = "tools/ringMaster/resources/commonObj.png",
 
     MARGIN_BG_COLOR = { r = 0.15, g = 0.40, b = 0.10 },
-    SPRITE_BG_COLOR = { r = 0.05, g = 0.28, b = 0.03 },
+    SPRITE_BG_COLOR = { r = 0, g = 0, b = 0, a = 0 },
+
+    animations  = {
+        spinning = { fps = 8,
+            rect    = { x = 24, y = 198, w = 78, h = 16 },
+            sprites = {
+                { x =  24, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
+                { x =  50, y = 198, w = 12, h = 16, offset = { x =  5, y =  8 }, },
+                { x =  73, y = 198, w =  6, h = 16, offset = { x =  3, y =  8 }, },
+                { x =  90, y = 198, w = 12, h = 16, offset = { x =  6, y =  8 }, },
+            },
+        },
+    },
 }
