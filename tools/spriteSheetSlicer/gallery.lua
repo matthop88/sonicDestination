1113b4a0b12e95025f7bcc58605bd71967c44301
Matthew Hopkins <matthop88@gmail.com>
2025-11-12 13:05:47 -0800

    Fixed animation capability in gallery (option A)

--- tools/spriteSheetSlicer/gallery.lua
+++ tools/spriteSheetSlicer/gallery.lua
@@ -172,7 +172,7 @@ return {
 
     keypressed = function(self, key)
         if     key == "escape"                then self.editor:setActive(false)
-        elseif key == "lalt" or key == "ralt" then 
+        elseif key == "optiona" then 
             self.animationPane:enable()
             self.animationPane:setSpriteData(self.slots:getSpriteData())
         elseif self.animationPane:keypressed(key) then
@@ -182,7 +182,7 @@ return {
     end,
 
     keyreleased = function(self, key)
-        if key == "lalt" or key == "ralt" then self.animationPane:disable() end
+        if key == "optiona" then self.animationPane:disable() end
     end,
 
     handleKeypressedInEditor = function(self, key)
