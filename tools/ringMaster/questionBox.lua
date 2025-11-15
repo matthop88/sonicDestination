c6935172c50ac0ef1d6227b4a98c97f6bef35ed0
Matthew Hopkins <matthop88@gmail.com>
2025-11-15 14:22:27 -0800

    Added code to open question box

--- tools/ringMaster/questionBox.lua
+++ tools/ringMaster/questionBox.lua
@@ -2,10 +2,15 @@ local QUESTION_FONT = love.graphics.newFont(64)
 
 return {
 	create = function(self, x, y)
+		local origX = x
+		local w = 80
+		local h = 80
+		local mousePressed = false
+		local opened       = false
+		
 		return {
 			draw = function(self)
-				local mx, my = love.mouse.getPosition()
-				if mx >= x and mx < x + 80 and my >= y and my < y + 80 then
+				if self:isInside(love.mouse.getPosition()) then
 					if love.mouse.isDown(1) then self:drawClicked()
 					else                         self:drawHighlighted() end
 				else
@@ -13,6 +18,20 @@ return {
 				end
 			end,
 
+			setOpened = function(self) 
+				x = 10
+				w = 1180 
+			end,
+
+			setClosed = function(self) 
+				x = origX
+				w = 80   
+			end,
+
+			isInside = function(self, px, py)
+				return px >= x and px < x + w and py >= y and py < y + h
+			end,
+				
 			drawClicked     = function(self)
 				self:drawPanel        { 1,   1,   1,   0.6 }
     			self:drawBorder       { 0,   0,   0,   0.6 }
@@ -33,19 +52,34 @@ return {
 
     		drawPanel = function(self, color)
     			love.graphics.setColor(color)
-    			love.graphics.rectangle("fill", x, y, 80, 80)
+    			love.graphics.rectangle("fill", x, y, w, h)
     		end,
 
     		drawBorder = function(self, color)
     			love.graphics.setColor(color)
     			love.graphics.setLineWidth(3)
-    			love.graphics.rectangle("line", x, y, 80, 80)
+    			love.graphics.rectangle("line", x, y, w, h)
     		end,
 
     		drawQuestionMark = function(self, color)
     			love.graphics.setColor(color)
     			love.graphics.setFont(QUESTION_FONT)
     			love.graphics.printf("?", x, y + 5, 80, "center")
+			end,
+
+			handleMousepressed = function(self, mx, my)
+				if self:isInside(mx, my) then
+					mousePressed = true
+				end
+			end,
+
+			handleMousereleased = function(self, mx, my)
+				if mousePressed then
+					mousePressed = false
+					opened = not opened
+					if opened then self:setOpened()
+					else           self:setClosed() end
+				end
 			end,
 		}
 	end,
