ceafbdacf710030147e6c4b7949f26a66b7f8932
Matthew Hopkins <matthop88@gmail.com>
2025-11-17 16:44:14 -0800

    Implemented fading of question mark

--- plugins/modules/questionBox/questionBox.lua
+++ plugins/modules/questionBox/questionBox.lua
@@ -9,7 +9,7 @@ return {
 	opened = false,
 
 	draw = function(self)
-		if self:isInside(love.mouse.getPosition()) or self.opened then
+		if self:isInside(love.mouse.getPosition()) or self.opened or self.x:inFlux() then
 			if love.mouse.isDown(1) then self:drawClicked()
 			else                         self:drawHighlighted() end
 		else
@@ -18,7 +18,7 @@ return {
 	end,
 
 	isInside = function(self, px, py)
-		return px >= self.x:get() and px < self.x:get() + self.w:get() and py >= self.y and py < self.y + self.h
+		return px >= self.x:get() and px < self.x:get() + self:getWidth() and py >= self.y and py < self.y + self.h
 	end,
 					
 	drawUnhighlighted = function(self)
@@ -31,32 +31,41 @@ return {
 		local alpha = 0.6
 		self:drawPanel        { 0.3, 0.3, 0.3, alpha }
 		self:drawBorder       { 1,   1,   0,   alpha }
-		self:drawQuestionMark { 1,   1,   0,   alpha + 0.2 }
+		self:drawQuestionMark { 1,   1,   0,   (alpha + 0.2) * self:getQuestionMarkAlpha() }
 	end,
 
 	drawClicked = function(self)
-		if self.opened then self:drawPanel { 0.3, 0.3, 0.3, 0.1 }
-		else                self:drawPanel { 1,   1,   1,   0.6 } end
+		local alpha = 0.6
+		if self.opened or self.x:inFlux() then self:drawPanel { 0.3, 0.3, 0.3, 0.1 }
+		else                                   self:drawPanel { 1,   1,   1,   0.6 } end
 
 		self:drawBorder       { 0,   0,   0,   0.6 }
-    	self:drawQuestionMark { 0,   0,   0,   0.9 }
+    	self:drawQuestionMark { 0,   0,   0,   (alpha + 0.2) * self:getQuestionMarkAlpha() }
     end,
 	
     drawPanel = function(self, color)
     	love.graphics.setColor(color)
-    	love.graphics.rectangle("fill", self.x:get(), self.y, self.w:get(), self.h)
+    	love.graphics.rectangle("fill", self.x:get(), self.y, self:getWidth(), self.h)
     end,
 
     drawBorder = function(self, color)
     	love.graphics.setColor(color)
     	love.graphics.setLineWidth(3)
-    	love.graphics.rectangle("line", self.x:get(), self.y, self.w:get(), self.h)
+    	love.graphics.rectangle("line", self.x:get(), self.y, self:getWidth(), self.h)
     end,
 
     drawQuestionMark = function(self, color)
     	love.graphics.setColor(color)
     	love.graphics.setFont(QUESTION_FONT)
-    	love.graphics.printf("?", self.x:get(), self.y + 2, self.w:get(), "center")
+    	love.graphics.printf("?", self.x:get(), self.y + 2, self:getWidth(), "center")
+	end,
+
+	getWidth = function(self)
+		return self.w:get()
+	end,
+
+	getQuestionMarkAlpha = function(self)
+		return 1 - (self.w:get() - 40) / 720
 	end,
 
 	update = function(self, dt)
