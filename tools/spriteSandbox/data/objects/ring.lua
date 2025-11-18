0bbaa938d00482694521f9c4e706f38f00c820a3
Matthew Hopkins <matthop88@gmail.com>
2025-11-18 14:07:32 -0800

    Added ring sprite data

---
+++ tools/spriteSandbox/data/objects/ring.lua
@@ -0,0 +1,12 @@
+return {
+    imageName  = "commonObj",
+    animations  = {
+        spinning = { fps = 8, offset = { x = 8, y = 8 }, isDefault = true,
+            --------------------------------------------------------------
+            { x =  24, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
+            { x =  48, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
+            { x =  72, y = 198, w =  8, h = 16, offset = { x =  4, y =  8 }, },
+            { x =  88, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
+        },
+    },
+}
