c201a7f9372fafb4d3842f4a287dafde5fd109bd
Matthew Hopkins <matthop88@gmail.com>
2025-11-18 15:57:45 -0800

    Updated sprite.lua so that ring is drawn

--- tools/spriteSandbox/sprite.lua
+++ tools/spriteSandbox/sprite.lua
@@ -1,17 +1,33 @@
 return {
 	create = function(self, path, x, y)
+		local data     = require("tools/spriteSandbox/data/" .. path)
+		local sheetImg = love.graphics.newImage("resources/images/spriteSheets/" .. data.imageName .. ".png")
+		sheetImg:setFilter("nearest", "nearest")
+
+		local currentAnimation
+		for _, animation in pairs(data.animations) do
+			if currentAnimation == nil then currentAnimation = animation end
+			if animation.isDefault     then currentAnimation = animation end
+
+			for _, frame in ipairs(animation) do
+				frame.QUAD = love.graphics.newQuad(frame.x, frame.y, frame.w, frame.h, sheetImg:getWidth(), sheetImg:getHeight())
+			end
+		end
+
 		return ({
 			init = function(self)
-				self.data     = require("tools/spriteSandbox/data/" .. path)
-				self.sheetImg = love.graphics.newImage("resources/images/spriteSheets/" .. self.data.imageName .. ".png")
-				self.sheetImg:setFilter("nearest", "nearest")
+				self.animations       = data.animations
+				self.currentAnimation = currentAnimation
+				self.currentFrame     = 1
 
 				return self
 			end,
 
 			draw = function(self)
+				local frame = self.currentAnimation[self.currentFrame]
+
 				love.graphics.setColor(1, 1, 1)
-				love.graphics.draw(self.sheetImg, 0, 0)
+				love.graphics.draw(sheetImg, frame.QUAD, x, y, 0, 1, 1)
 			end,
 		}):init()
 	end,
