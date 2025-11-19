dda30553dc4c7f94f3c4a3bb8be2618ed763e906
Matthew Hopkins <matthop88@gmail.com>
2025-11-18 16:26:53 -0800

    Updated sandbox.lua to have ring chase mouse around screen

--- tools/spriteSandbox/sandbox.lua
+++ tools/spriteSandbox/sandbox.lua
@@ -1,15 +1,17 @@
 return {
     graphics   = require("tools/lib/graphics"):create(),
 
-    ringSprite = require("tools/spriteSandbox/sprite"):create("objects/ring", 300, 200),
+    ringSprite = require("tools/spriteSandbox/sprite"):create("objects/ring", 0, 0),
 
     draw = function(self)
         love.graphics.setColor(0.1, 0.1, 0.1)
         love.graphics.rectangle("fill", 0, 0, 1200, 800)
+
         self.ringSprite:draw(self.graphics)
     end,
 
     update = function(self, dt)
+        self.ringSprite.x, self.ringSprite.y = self:screenToImageCoordinates(love.mouse.getPosition())
         self.ringSprite:update(dt)
     end,
 
