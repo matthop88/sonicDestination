8368665375ca23d00b79a4f1e8d95168be64bafc
Matthew Hopkins <matthop88@gmail.com>
2025-11-19 09:01:39 -0800

    Updated ring.lua to include w, h in general animation info

--- tools/spriteSandbox/data/objects/ring.lua
+++ tools/spriteSandbox/data/objects/ring.lua
@@ -1,7 +1,7 @@
 return {
     imageName  = "commonObj",
     animations  = {
-        spinning = { fps = 8, offset = { x = 8, y = 8 }, isDefault = true, synchronized = true,
+        spinning = { fps = 8, offset = { x = 8, y = 8 }, w = 16, h = 16, isDefault = true, synchronized = true,
             --------------------------------------------------------------
             { x =  24, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
             { x =  48, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
