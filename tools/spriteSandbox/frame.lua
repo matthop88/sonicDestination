6880debf273402c6110b6ffd76acb170f27d4061
Matthew Hopkins <matthop88@gmail.com>
2025-11-18 16:51:28 -0800

    Added frame repository to frame.lua

--- tools/spriteSandbox/frame.lua
+++ tools/spriteSandbox/frame.lua
@@ -1,5 +1,18 @@
+local FRAME_REPOSITORY = {}
+
 return {
-	create = function(self, animation)
+	create = function(self, animation, syncName)
+		if syncName ~= nil then
+			if not FRAME_REPOSITORY[syncName] then
+				FRAME_REPOSITORY[syncName] = self:createIntern(animation)
+			end
+			return FRAME_REPOSITORY[syncName]
+		else
+			return self:createIntern(animation)
+		end
+	end,
+
+	createIntern = function(self, animation)
 		return {
 			frameNumber = 1,
 			animation   = animation,
