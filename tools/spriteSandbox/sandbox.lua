7157d5a3de2dd1ea9061d2735f3416ec33d6c9dd
Matthew Hopkins <matthop88@gmail.com>
2025-11-18 16:20:11 -0800

    Created sandbox.lua for drawing purposes

---
+++ tools/spriteSandbox/sandbox.lua
@@ -0,0 +1,32 @@
+return {
+    graphics   = require("tools/lib/graphics"):create(),
+
+    ringSprite = require("tools/spriteSandbox/sprite"):create("objects/ring", 300, 200),
+
+    draw = function(self)
+        love.graphics.setColor(0.1, 0.1, 0.1)
+        love.graphics.rectangle("fill", 0, 0, 1200, 800)
+        self.ringSprite:draw(self.graphics)
+    end,
+
+    update = function(self, dt)
+        self.ringSprite:update(dt)
+    end,
+
+    moveImage = function(self, deltaX, deltaY)
+        self.graphics:moveImage(deltaX, deltaY)
+    end,
+
+    screenToImageCoordinates = function(self, screenX, screenY)
+        return self.graphics:screenToImageCoordinates(screenX, screenY)
+    end,
+
+    adjustScaleGeometrically = function(self, deltaScale)
+        self.graphics:adjustScaleGeometrically(deltaScale)
+    end,
+
+    syncImageCoordinatesWithScreen = function(self, imageX, imageY, screenX, screenY)
+        self.graphics:syncImageCoordinatesWithScreen(imageX, imageY, screenX, screenY)
+    end,
+
+}
