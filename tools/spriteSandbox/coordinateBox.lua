67cb1fbf90adc501012bd5877b07c264f699b4ec
Matthew Hopkins <matthop88@gmail.com>
2025-11-19 16:23:58 -0800

    Coordinates fade as mouse moves away

--- tools/spriteSandbox/coordinateBox.lua
+++ tools/spriteSandbox/coordinateBox.lua
@@ -7,14 +7,21 @@ return {
             end,
 
             draw = function(self, GRAFX)
-                GRAFX:setColor(0, 0, 0, 0.5)
+                local alpha = self:getAlpha(GRAFX)
+                GRAFX:setColor(0, 0, 0, 0.5 * alpha)
                 GRAFX:rectangle("fill", self.x + 6, self.y + 6, 40, 9)
-                GRAFX:setColor(1, 1, 1, 0.7)
+                GRAFX:setColor(1, 1, 1, 0.7 * alpha)
                 GRAFX:setLineWidth(0.5)
                 GRAFX:rectangle("line", self.x + 6, self.y + 6, 40, 9)
-                GRAFX:setColor(1, 1, 1)
+                GRAFX:setColor(1, 1, 1, alpha)
                 GRAFX:setFontSize(6)
                 GRAFX:printf("" .. math.floor(self.x) .. ", " .. math.floor(self.y), self.x + 7, self.y + 7, 40, "center")
+            end,
+
+            getAlpha = function(self, GRAFX)
+                local px, py = GRAFX:screenToImageCoordinates(love.mouse.getPosition())
+                local distance = math.sqrt(((px - self.x) * (px - self.x)) + ((py - self.y) * (py - self.y)))
+                return math.max(0.1, ((10 - math.sqrt(distance)) / 10) + 0.2)
             end,
 
             updateCoordinates = function(self, x, y)
