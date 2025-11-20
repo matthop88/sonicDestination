8a1afab4b5d9a20339f1467ad7641d5c98aba815
Matthew Hopkins <matthop88@gmail.com>
2025-11-19 17:16:22 -0800

    Can now zoom in to a tile grid

--- tools/spriteSandbox/grid.lua
+++ tools/spriteSandbox/grid.lua
@@ -1,18 +1,30 @@
 local CHUNK_SIZE = 256
+local TILE_SIZE  = 16
 
 return {
 	draw = function(self, GRAFX)
 		local leftX, topY = GRAFX:screenToImageCoordinates(0, 0)
 		local rightX, bottomY = GRAFX:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
 		
-		GRAFX:setColor(1, 1, 1)
-        GRAFX:setLineWidth(0.25)
+		if (rightX - leftX) <= 512 then
+			self:drawTileGrid(GRAFX, leftX, topY, rightX, bottomY)
+		end
 
-        self:drawChunkGrid(GRAFX, leftX, topY, rightX, bottomY)
+		self:drawChunkGrid(GRAFX, leftX, topY, rightX, bottomY)
+	end,
+
+	drawTileGrid  = function(self, GRAFX, leftX, topY, rightX, bottomY)
+		GRAFX:setColor(0.5, 0.5, 0.5, (512 - (rightX - leftX)) / 512)
+		GRAFX:setLineWidth(0.25)
+
+		self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, TILE_SIZE)
 	end,
 
 	drawChunkGrid = function(self, GRAFX, leftX, topY, rightX, bottomY)
-		self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, CHUNK_SIZE)
+		GRAFX:setColor(1, 1, 1)
+        GRAFX:setLineWidth(0.25)
+
+        self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, CHUNK_SIZE)
 	end,
 	
 	drawGrid = function(self, GRAFX, leftX, topY, rightX, bottomY, gridSize)
