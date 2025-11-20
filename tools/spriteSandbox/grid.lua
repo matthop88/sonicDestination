8d1a2ac93ad9e15c3fb11cc5976ede9db8d2f859
Matthew Hopkins <matthop88@gmail.com>
2025-11-19 17:32:58 -0800

    Added pixel grid to grid.lua

--- tools/spriteSandbox/grid.lua
+++ tools/spriteSandbox/grid.lua
@@ -1,11 +1,16 @@
 local CHUNK_SIZE = 256
 local TILE_SIZE  = 16
+local PIXEL_SIZE = 1
 
 return {
 	draw = function(self, GRAFX)
 		local leftX, topY = GRAFX:screenToImageCoordinates(0, 0)
 		local rightX, bottomY = GRAFX:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
 		
+		if (rightX - leftX) <= 32 then
+			self:drawPixelGrid(GRAFX, leftX, topY, rightX, bottomY)
+		end
+
 		if (rightX - leftX) <= 512 then
 			self:drawTileGrid(GRAFX, leftX, topY, rightX, bottomY)
 		end
@@ -13,16 +18,23 @@ return {
 		self:drawChunkGrid(GRAFX, leftX, topY, rightX, bottomY)
 	end,
 
+	drawPixelGrid  = function(self, GRAFX, leftX, topY, rightX, bottomY)
+		GRAFX:setColor(0.5, 0.5, 0.3, (32 - (rightX - leftX)) / 32)
+		GRAFX:setLineWidth(0.125)
+
+		self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, PIXEL_SIZE)
+	end,
+
 	drawTileGrid  = function(self, GRAFX, leftX, topY, rightX, bottomY)
 		GRAFX:setColor(0.5, 0.5, 0.5, (512 - (rightX - leftX)) / 512)
 		GRAFX:setLineWidth(0.25)
 
-		self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, TILE_SIZE)
+		self:drawGrid(GRAFX, leftX + 0.25, topY + 0.25, rightX + 0.25, bottomY + 0.25, TILE_SIZE)
 	end,
 
 	drawChunkGrid = function(self, GRAFX, leftX, topY, rightX, bottomY)
 		GRAFX:setColor(1, 1, 1)
-        GRAFX:setLineWidth(0.25)
+        GRAFX:setLineWidth(0.5)
 
         self:drawGrid(GRAFX, leftX, topY, rightX, bottomY, CHUNK_SIZE)
 	end,
