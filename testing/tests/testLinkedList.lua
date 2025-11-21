2495b524cd42f291a308bf57e4cde2a15210a71a
Matthew Hopkins <matthop88@gmail.com>
2025-11-21 14:50:46 -0800

    Added new test to  testLinkedList.lua

--- testing/tests/testLinkedList.lua
+++ testing/tests/testLinkedList.lua
@@ -27,4 +27,23 @@ return {
                                     and self.LINKED_LIST:size()   == 0)
     end,
 
+    testSingleListElement = function(self)
+
+        local name = "Single List Element Test",
+
+        self.LINKED_LIST:add(3)
+
+        return TESTING:assertTrue(name, self.LINKED_LIST:head()   == true
+                                    and self.LINKED_LIST:tail()   == true
+                                    and self.LINKED_LIST:get()    == 3
+                                    and self.LINKED_LIST:next()   == self.LINKED_LIST
+                                    and self.LINKED_LIST:prev()   == self.LINKED_LIST
+                                    and self.LINKED_LIST:isEnd()  == false
+                                    and self.LINKED_LIST:size()   == 1
+                                    and self.LINKED_LIST:remove() == 3
+                                    and self.LINKED_LIST:isEnd()  == true
+                                    and self.LINKED_LIST:size()   == 0)
+    end,
+                                    
+
 }
