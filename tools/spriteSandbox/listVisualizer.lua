a1b630c4bbf6db6b44a606d283422f67412e53bd
Matthew Hopkins <matthop88@gmail.com>
2025-11-21 16:27:40 -0800

    Added listVisualizer class

---
+++ tools/spriteSandbox/listVisualizer.lua
@@ -0,0 +1,27 @@
+return {
+	create = function(self, list)
+
+		return {
+			list     = list,
+			graphics = require("tools/lib/graphics"):create(), 
+    
+			draw = function(self)
+				if self.list:size() > 0 then
+					self.graphics:setColor(0.5, 0.5, 0.5, 0.8)
+					self.graphics:rectangle("fill", 0, 700, 1200, 100)
+
+					self.list:head()
+					local x = 50
+					while not self.list:isEnd() do
+						local sprite = self.list:get()
+						self.list:next()
+						self.graphics:setColor(1, 1, 1, 0.9)
+						self.graphics:setLineWidth(3)
+						self.graphics:rectangle("line", x, 725, 50, 50)
+						x = x + 100
+					end
+				end
+			end,
+		}
+	end,
+}
