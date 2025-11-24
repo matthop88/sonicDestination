baec832f6445477c4748eb5e99ad8489456dd80a
Matthew Hopkins <matthop88@gmail.com>
2025-11-24 15:29:50 -0800

    Updated stack.lua; now all tests pass

--- tools/lib/dataStructures/stack.lua
+++ tools/lib/dataStructures/stack.lua
@@ -1,7 +1,34 @@
 return {
 	create = function(self)
 		return {
+			list = {},
 
+			__sz = 0,
+
+			size = function(self) return self.__sz end,
+
+			push = function(self, item)
+				if #self.list == self:size() then table.insert(self.list, item)
+				else                              self.list[self:size() + 1] = item end
+
+				self.__sz = self.__sz + 1
+
+				return self
+			end,
+
+			pop  = function(self)
+				if self:size() == 0 then
+					return nil
+				else
+					local item = self.list[self:size()]
+					self.__sz = self.__sz - 1
+					return item
+				end
+			end,
+
+			peek = function(self)
+				return self.list[self:size()]
+			end,
 		}
 	end,
 }
