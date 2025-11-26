919b9b23cdccbf62ff5a8d56c214b180cb263ee9
Matthew Hopkins <matthop88@gmail.com>
2025-11-25 16:59:03 -0800

    Added hitBoxes to sonic1.lua

--- game/sprites/data/sonic1.lua
+++ game/sprites/data/sonic1.lua
@@ -2,10 +2,12 @@ return {
     imageName  = "sonic1Transparent",
     animations = {
         standing    = { fps = 1,  offset = { x = 14, y = 20 }, isDefault = true,
-            -------------------------------------------------------------------
+            hitBox = { rX = 8, rY = 16 },
+            --------------------------------------------------------------------
             { x =  43, y = 257, w = 32, h = 40, offset = { x = 16, y = 20 }, },
         },
         running     = { fps = 10, offset = { x = 16, y = 20 },
+            hitBox = { rX = 8, rY = 16 },
             { x =  46, y = 349, w = 24, h = 40, offset = { x = 12, y = 19 }, },
             { x = 109, y = 347, w = 40, h = 40, offset = { x = 20, y = 19 }, },
             { x = 178, y = 348, w = 32, h = 40, offset = { x = 20, y = 19 }, },
@@ -15,11 +17,13 @@ return {
         },
 
         braking     = { fps = 2, offset = { x = 14, y = 17 },
+            hitBox = { rX = 8, rY = 16 },
             { x = 481, y = 352, w = 30, h = 36, offset = { x = 13, y = 18 }, },
             { x = 547, y = 352, w = 34, h = 36, offset = { x = 17, y = 18 }, },
         },
 
         jumping     = { fps = 12, offset = { x = 16, y = 14 },
+            hitBox = { rX = 8, rY = 12 },
             { x =  44, y = 626, w = 29, h = 30, offset = { x = 16, y = 14 }, },
             { x = 114, y = 626, w = 30, h = 29, offset = { x = 16, y = 15 }, },
             { x = 185, y = 626, w = 29, h = 30, offset = { x = 11, y = 15 }, },
@@ -27,6 +31,7 @@ return {
             { x = 324, y = 626, w = 30, h = 30, offset = { x = 15, y = 14 }, },
         },
         fastJumping = { fps = 60, offset = { x = 16, y = 14 },
+            hitBox = { rX = 8, rY = 12 },
             { x =  44, y = 626, w = 29, h = 30, offset = { x = 16, y = 14 }, },
             { x = 114, y = 626, w = 30, h = 29, offset = { x = 16, y = 15 }, },
             { x = 324, y = 626, w = 30, h = 30, offset = { x = 15, y = 14 }, },
