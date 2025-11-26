60780e906d57b08e5cbdaea02e3b3f3b010260eb
Matthew Hopkins <matthop88@gmail.com>
2025-11-25 20:23:12 -0800

    Updated object.lua to generate hitbox object

--- game/world/gameObjects/object.lua
+++ game/world/gameObjects/object.lua
@@ -7,20 +7,34 @@ return {
             x        = x,
             y        = y,
             graphics = graphics,
+            HITBOX   = nil,
 
             draw = function(self)
                 SPRITE:draw(self.x, self.y)
             end,
 
             drawHitBox = function(self)
-                local hitBox = SPRITE:getHitBox()
+                local hitBox = self:getHitBox()
                 if hitBox then
-                    hitBox:draw(self.graphics, self.x, self.y, { 1, 0, 0, 0.8 }, 2)
+                    hitBox:draw(self.graphics, { 1, 0, 0, 0.8 }, 2)
                 end
+            end,
+
+            getHitBox = function(self)
+                if self.HITBOX == nil then
+                    self.HITBOX = requireRelative("collision/hitBoxes/hitBox"):create(SPRITE:getHitBox())
+                end
+                return self.HITBOX
             end,
 
             update = function(self, dt)
                 SPRITE:update(dt)
+                self:updateHitBox(dt)
+            end,
+
+            updateHitBox = function(self, dt)
+                local hitBox = self:getHitBox()
+                if hitBox then hitBox:update(self.x, self.y) end
             end,
 
             isForeground = function(self)
