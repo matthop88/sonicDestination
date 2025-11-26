4c00e2e5999d910da92294f2c6132c2fde8fe7c9
Matthew Hopkins <matthop88@gmail.com>
2025-11-25 18:21:43 -0800

    Added drawHitBox() method to game object

--- game/world/gameObjects/object.lua
+++ game/world/gameObjects/object.lua
@@ -4,11 +4,18 @@ return {
         local SPRITE = spriteFactory:create(spriteName)
 
         return {
-            x = x,
-            y = y,
+            x        = x,
+            y        = y,
+            graphics = graphics,
 
             draw = function(self)
                 SPRITE:draw(self.x, self.y)
+            end,
+
+            drawHitBox = function(self)
+                self.graphics:setColor(1, 0, 0, 0.5)
+                self.graphics:setLineWidth(2)
+                SPRITE:drawHitBox(self.x, self.y)
             end,
 
             update = function(self, dt)
