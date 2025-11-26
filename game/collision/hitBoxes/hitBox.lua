0bb1ed2860163114c9685ce2ad193a71a39c3742
Matthew Hopkins <matthop88@gmail.com>
2025-11-25 20:17:43 -0800

    Updated hitBox.lua; now has update() method which takes coordinates

--- game/collision/hitBoxes/hitBox.lua
+++ game/collision/hitBoxes/hitBox.lua
@@ -1,17 +1,27 @@
 return {
-	create = function(self, radiusX, radiusY)
-		return {
-			radiusX = radiusX,
-			radiusY = radiusY,
-			width   = radiusX * 2,
-			height  = radiusY * 2,
+	create = function(self, hitBoxData)
+		if hitBoxData == nil then 
+			return nil
+		else
+			return {
+				radiusX = hitBoxData.rX,
+				radiusY = hitBoxData.rY,
+				width   = hitBoxData.rX * 2,
+				height  = hitBoxData.rY * 2,
+				x       = 0,
+				y       = 0,
 
-			draw = function(self, GRAFX, x, y, color, thickness, scaleX, scaleY)
-				scaleX, scaleY = (scaleX or 1), (scaleY or 1)
-				GRAFX:setColor(color)
-				GRAFX:setLineWidth(thickness)
-				GRAFX:rectangle("line", x - (self.radiusX * scaleX), y - (self.radiusY * scaleY), self.width * scaleX, self.height * scaleY)
-            end,
-		}
+				draw = function(self, GRAFX, color, thickness, scaleX, scaleY)
+					scaleX, scaleY = (scaleX or 1), (scaleY or 1)
+					GRAFX:setColor(color)
+					GRAFX:setLineWidth(thickness)
+					GRAFX:rectangle("line", self.x - (self.radiusX * scaleX), self.y - (self.radiusY * scaleY), self.width * scaleX, self.height * scaleY)
+	            end,
+
+	            update = function(self, x, y)
+	            	self.x, self.y = x, y
+	            end,
+			}
+		end
 	end,
 }
