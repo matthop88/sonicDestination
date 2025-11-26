dd17aa0d3eaca8d6b18c928d50165f094804d3b1
Matthew Hopkins <matthop88@gmail.com>
2025-11-25 21:03:01 -0800

    Fixed hit box intersection algorithm, and cached last hitbox we intersect with

--- game/collision/hitBoxes/hitBox.lua
+++ game/collision/hitBoxes/hitBox.lua
@@ -4,16 +4,19 @@ return {
 			return nil
 		else
 			return {
-				radiusX = hitBoxData.rX,
-				radiusY = hitBoxData.rY,
-				width   = hitBoxData.rX * 2,
-				height  = hitBoxData.rY * 2,
-				x       = 0,
-				y       = 0,
+				radiusX          = hitBoxData.rX,
+				radiusY          = hitBoxData.rY,
+				width            = hitBoxData.rX * 2,
+				height           = hitBoxData.rY * 2,
+				x                = 0,
+				y                = 0,
+
+				lastIntersection = nil,
 
 				draw = function(self, GRAFX, color, thickness, scaleX, scaleY)
 					scaleX, scaleY = (scaleX or 1), (scaleY or 1)
-					GRAFX:setColor(color)
+					if self:intersectsLast() then GRAFX:setColor(1, 1, 1)
+					else                          GRAFX:setColor(color)  end
 					GRAFX:setLineWidth(thickness)
 					GRAFX:rectangle("line", self.x - (self.radiusX * scaleX), self.y - (self.radiusY * scaleY), self.width * scaleX, self.height * scaleY)
 	            end,
@@ -22,12 +25,28 @@ return {
 	            	self.x, self.y = x, y
 	            end,
 
-	            intersects = function(self, otherHitBox)
-	            	return self.x + self.width  > otherHitBox.x 
-	            	   and self.x               < otherHitBox.x + otherHitBox.width
-	            	   and self.y + self.height > otherHitBox.y
-	            	   and self.y               < otherHitBox.y + otherHitBox.height
+	            intersectsIntern = function(self, otherHitBox)
+	            	return self.x + self.radiusX  > otherHitBox.x - otherHitBox.radiusX
+	            	   and self.x - self.radiusX  < otherHitBox.x + otherHitBox.radiusX
+	            	   and self.y + self.radiusY  > otherHitBox.y - otherHitBox.radiusY
+	            	   and self.y - self.radiusY  < otherHitBox.y + otherHitBox.radiusY
 	            end,
+
+	            intersects = function(self, otherHitBox)
+	            	local result = self:intersectsIntern(otherHitBox)
+	            	if result then self.lastIntersection = otherHitBox end
+	            	return result
+	            end,
+
+	            intersectsLast = function(self)
+	            	if self.lastIntersection and self:intersectsIntern(self.lastIntersection) then
+	            		return true
+	            	else
+	            		self.lastIntersection = nil
+	            		return false
+	            	end
+	            end,
+
 			}
 		end
 	end,
