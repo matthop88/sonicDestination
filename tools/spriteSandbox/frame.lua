daf90136ee71a71efeeb3cd48bf08487afc38a05
Matthew Hopkins <matthop88@gmail.com>
2025-11-26 17:28:29 -0800

    Added flag to frame indicating animation has rolled over

--- tools/spriteSandbox/frame.lua
+++ tools/spriteSandbox/frame.lua
@@ -17,14 +17,24 @@ return {
 			frameNumber  = 1,
 			animation    = animation,
 			creationTime = love.timer.getTime(),
-
+			rolledOver   = false,
+			
 			update = function(self, dt)
 				local time = love.timer.getTime() - self.creationTime
+				local prevFrameNumber = self.frameNumber
 				self.frameNumber = (math.floor(time * self.animation.fps) % #self.animation) + 1
+				if self.frameNumber < prevFrameNumber then self.rolledOver = true end
 			end,
 
 			get = function(self)
 				return self.animation[math.floor(self.frameNumber)]
+			end,
+
+			isRolledOver = function(self)
+				if self.rolledOver then
+					self.rolledOver = false
+					return true
+				end
 			end,
 
 			getFirst = function(self)
