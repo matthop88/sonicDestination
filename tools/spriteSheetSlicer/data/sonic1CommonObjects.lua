7502368b418d3bf5e820a4c19137f198a3b2f139
Matthew Hopkins <matthop88@gmail.com>
2025-11-30 17:28:15 -0800

    Added giant spinning ring to common objects in sprite sheet slicer

--- tools/spriteSheetSlicer/data/sonic1CommonObjects.lua
+++ tools/spriteSheetSlicer/data/sonic1CommonObjects.lua
@@ -5,7 +5,7 @@ return {
     SPRITE_BG_COLOR = { r = 0,    g = 0,    b = 0,    a = 0 },
 
     animations  = {
-        spinning = { fps = 8,
+        spinningRing = { fps = 8,
             rect    = { x = 24,  y = 198, w = 78, h = 16 },
             sprites = {
                 { x =  24, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
@@ -14,13 +14,22 @@ return {
                 { x =  88, y = 198, w = 16, h = 16, offset = { x =  8, y =  8 }, },
             },
         },
-        dissolving = { fps = 8,
+        dissolvingRing = { fps = 8,
             rect    = { x = 112, y = 198, w = 88, h = 14 },
             sprites = {
                 { x = 112, y = 198, w = 16, h = 14, offset = { x =  8, y =  7 }, },
                 { x = 136, y = 200, w = 16, h = 14, offset = { x =  8, y =  7 }, },
                 { x = 160, y = 198, w = 16, h = 14, offset = { x =  8, y =  7 }, },
                 { x = 184, y = 200, w = 16, h = 14, offset = { x =  8, y =  7 }, },
+            },
+        },
+        spinningGiantRing = { fps = 8,
+            rect    = { x = 24, y = 239, w = 208, h = 64 },
+            sprites = {
+                { x =  24, y = 239, w = 64, h = 64, offset = { x = 32, y = 32 }, },
+                { x =  96, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
+                { x = 152, y = 239, w = 24, h = 64, offset = { x = 12, y = 32 }, },
+                { x = 184, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
             },
         },
     },
