fc228de36deb3f1bd1fbc7e213258d1dd0881a7f
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 14:23:28 -0800

    Utilizing concept of super to create special update functionality in Giant Ring Object

--- game/world/gameObjects/objects/giantRing.lua
+++ game/world/gameObjects/objects/giantRing.lua
@@ -1,6 +1,14 @@
 return {
 	create = function(self)
 		return {
+			update = function(self, dt)
+				self.super:update(dt)
+				self:updateGiantRingActivity(dt)
+			end,
+
+			updateGiantRingActivity = function(self, dt)
+				print("Updating Giant Ring Activity...")
+			end,
 
 		}
 	end,
