be16f0668b4b3f0eb93f44cf177c6b392fc8c955
Matthew Hopkins <matthop88@gmail.com>
2025-11-30 18:29:10 -0800

    Created giantRing.lua for use in Sprite Sandbox

---
+++ tools/spriteSandbox/data/objects/giantRing.lua
@@ -0,0 +1,12 @@
+return {
+    imageName  = "commonObj",
+    animations  = {
+        giantSpinning = { fps = 8, offset = { x = 32, y = 32 }, w = 64, h = 64, isDefault = true, synchronized = true,
+            ----------------------------------------------------------------------------------------------------------
+            { x =  24, y = 239, w = 64, h = 64, offset = { x = 32, y = 32 }, },
+            { x =  96, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
+            { x = 152, y = 239, w = 24, h = 64, offset = { x = 12, y = 32 }, },
+            { x = 184, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
+        },
+    },
+}
