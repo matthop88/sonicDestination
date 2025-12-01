f46de2079a2ea16dfcc3204758cc2238300df8b7
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 15:23:29 -0800

    Giant ring now scales in and out of sight depending on ring count

--- game/world/gameObjects/objects/giantRing.lua
+++ game/world/gameObjects/objects/giantRing.lua
@@ -1,13 +1,22 @@
 return {
 	create = function(self)
 		return {
+			scale         = 0.1,
+			
 			update = function(self, dt)
 				self.super:update(dt)
 				self:updateGiantRingActivity(dt)
+				self.sprite.scale.x = self.scale
+				self.sprite.scale.y = self.scale
 			end,
 
 			updateGiantRingActivity = function(self, dt)
-				self.active       = GLOBALS:getPlayer():getRingCount() >= 50
+				local active       = GLOBALS:getPlayer():getRingCount() >= 50
+				
+				if   active then self.scale = math.min(1,   self.scale + (dt * 3))
+				else             self.scale = math.max(0.1, self.scale - (dt * 3)) end
+
+				self.active       = self.scale > 0.1
 				self.super.active = self.active
 			end,
 		}
