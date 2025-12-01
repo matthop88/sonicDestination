be11c36b057b2eff3f6763a9d4ac64190f7156fc
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 15:57:18 -0800

    Updated giantRing.lua so dissolving is in foreground. SUPER KEWL

--- tools/spriteSandbox/data/objects/giantRing.lua
+++ tools/spriteSandbox/data/objects/giantRing.lua
@@ -8,7 +8,7 @@ return {
             { x = 152, y = 239, w = 24, h = 64, offset = { x = 12, y = 32 }, },
             { x = 184, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
         },
-        giantDissolving = { fps = 8, offset = { x = 32, y = 32 }, w = 64, h = 64,
+        giantDissolving = { fps = 8, offset = { x = 32, y = 32 }, w = 64, h = 64, foreground = true,
             { x =  24, y = 311, w = 64, h = 64, offset = { x = 32, y = 32 }, },
             { x = 114, y = 311, w = 46, h = 64, offset = { x = 14, y = 32 }, },
             { x = 176, y = 311, w = 56, h = 64, offset = { x = 24, y = 32 }, },
