02d0e444542d6d1a2e3aac0ffa1ad7ccfa56a334
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 14:10:54 -0800

    Updated objectFactory.lua to pull general methods from generic object can give to specific object

--- game/world/gameObjects/objectFactory.lua
+++ game/world/gameObjects/objectFactory.lua
@@ -1,6 +1,18 @@
 return {
 	create = function(self, objectData, GRAPHICS)
-		return requireRelative("world/gameObjects/object"):create(objectData, GRAPHICS)
+		local generalObj  = requireRelative("world/gameObjects/object"):create(objectData, GRAPHICS)
+		local specificObj = requireRelative("world/gameObjects/objects/" .. objectData.obj):create()
+
+		self:addGeneralFunctionality(generalObj, specificObj)
+		return specificObj
+	end,
+
+	addGeneralFunctionality = function(self, generalObj, specificObj)
+		for k, v in pairs(generalObj) do
+			if not specificObj[k] then
+				specificObj[k] = v
+			end
+		end
 	end,
         
 }
