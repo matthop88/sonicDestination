248be8fc12503c1ab57f70735b8b3e6f25e0e5de
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 16:24:55 -0800

    Removed use of super in giant ring

--- game/world/gameObjects/objects/giantRing.lua
+++ game/world/gameObjects/objects/giantRing.lua
@@ -4,21 +4,31 @@ return {
 			scale         = 0.1,
 			
 			update = function(self, dt)
-				self.super:update(dt)
-				self:updateGiantRingActivity(dt)
+                if self.active then
+                    self.sprite:update(dt)
+                    self:updateHitBox(dt)
+                    self.deleted = self.sprite.deleted
+                end
+                self:updateGiantRingActivity(dt)
 				self.sprite.scale.x = self.scale
 				self.sprite.scale.y = self.scale
-			end,
+            end,
 
-			updateGiantRingActivity = function(self, dt)
+            updateGiantRingActivity = function(self, dt)
 				local active       = GLOBALS:getPlayer():getRingCount() >= 50
 				
 				if   active then self.scale = math.min(1,   self.scale + (dt * 3))
 				else             self.scale = math.max(0.1, self.scale - (dt * 3)) end
 
 				self.active       = self.scale > 0.1
-				self.super.active = self.active
 			end,
+
+			drawHitBox = function(self)
+                local hitBox = self:getHitBox()
+                if hitBox then 
+                	hitBox:draw(self.graphics, { 1, 0, 0, 0.8 }, 2) 
+                end
+            end,
 		}
 	end,            
 }
