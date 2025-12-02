5600795643336afcb605f8b47f2be5f44f79fce1
Matthew Hopkins <matthop88@gmail.com>
2025-12-02 14:02:49 -0800

    Added vanish sound to soundManager.lua

--- game/sound/soundManager.lua
+++ game/sound/soundManager.lua
@@ -14,6 +14,7 @@ return ({
         sonicCDJumping = { filename = "sonicCDJump.mp3",    volume     = 0.9,  startPoint = 19520, },
         ringCollectL   = { filename = "ring-collect-L.mp3", volume     = 0.4, },
         ringCollectR   = { filename = "ring-collect-R.mp3", volume     = 0.4, },
+        vanish         = { filename = "vanish.mp3",         volume     = 0.5,  startPoint =  3968, },
     },
 
     initSoundData = function(self)
