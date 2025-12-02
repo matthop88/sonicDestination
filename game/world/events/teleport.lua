70b6253f870aef9dfc6e3abb70a4581a3dad2782
Matthew Hopkins <matthop88@gmail.com>
2025-12-02 12:57:47 -0800

    First stab at creating teleport event

---
+++ game/world/events/teleport.lua
@@ -0,0 +1,47 @@
+local WORLD
+
+local STAGES = { 
+	READY =      { duration =  0,                                                 },
+	FADING_OUT = { duration = 60, activate = function(self) WORLD:fadeOut()  end, }, 
+	RESETTING  = { duration = 60, activate = function(self) WORLD:reset()    end, }, 
+	FADING_IN  = { duration = 60, activate = function(self) WORLD:fadeIn()   end, },
+	DONE       = {},
+
+	index           = 1,
+	get             = function(self) return self[self.index]                           end,
+	next            = function(self) self.index = math.min(self.index + 1, #self)      end,
+	completed       = function(self) return self.index == #self                        end,
+	
+	readyForNext    = function(self, timer)
+		local stage = self:get()
+		return stage.duration and timer > stage.duration
+	end,
+
+	activateCurrent = function(self) 
+		if self:get().activate then self:get():activate() end
+	end,
+}
+
+return {
+	create = function(self, world)
+		WORLD = world
+		return {
+			timer      = 0,
+			
+			update = function(self, dt)
+				self.timer = self.timer + dt
+				if STAGES:readyForNext(self.timer) then self:nextStage() end
+			end,
+
+			nextStage = function(self)
+				self.timer = 0
+				STAGES:next()
+				STAGES:activateCurrent()
+			end,
+
+			isComplete = function(self)
+				return STAGES:isComplete()
+			end,
+		}
+	end,
+}
