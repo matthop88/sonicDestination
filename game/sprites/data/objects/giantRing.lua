c52c4b898c532f5ae3573d3f794dd6a234627caa
Matthew Hopkins <matthop88@gmail.com>
2025-12-01 17:40:36 -0800

    Updated giantRing.lua; made hitBox extremely small

--- game/sprites/data/objects/giantRing.lua
+++ game/sprites/data/objects/giantRing.lua
@@ -2,7 +2,7 @@ return {
     imageName  = "commonObj",
     animations  = {
         giantSpinning = { fps = 8, offset = { x = 32, y = 32 }, w = 64, h = 64, isDefault = true, synchronized = true,
-            hitBox = { rX = 5, rY = 5 },
+            hitBox = { rX = 1, rY = 1 },
             ----------------------------------------------------------------------------------------------------------
             { x =  24, y = 239, w = 64, h = 64, offset = { x = 32, y = 32 }, },
             { x =  96, y = 239, w = 48, h = 64, offset = { x = 24, y = 32 }, },
