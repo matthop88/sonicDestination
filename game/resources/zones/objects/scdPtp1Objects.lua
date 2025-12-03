26fb6e2934ecc156c42f0d6f2a2715fc5fbd9749
Matthew Hopkins <matthop88@gmail.com>
2025-12-02 19:12:39 -0800

    Updated scdPtp1Objects.lua to contain proper origin

--- game/resources/zones/objects/scdPtp1Objects.lua
+++ game/resources/zones/objects/scdPtp1Objects.lua
@@ -1,9 +1,5 @@
 return {
-  sourceImage = "resources/zones/maps/SCD_PTP_Map_Act1.png",
-  
-  -- XXX We don't know what origin really is
-  origin           = { x = 512,  y = 940,  },
-
+  sourceImage      = "resources/zones/maps/SCD_PTP_Map_Act1.png",
+  origin           = { x = 56,  y = 238,  },
   -------------------------------------------
-
 }
