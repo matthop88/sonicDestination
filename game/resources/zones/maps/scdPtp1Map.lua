4cdbf6f31061c532fda63537112edf3618e6b439
Matthew Hopkins <matthop88@gmail.com>
2025-12-02 19:15:36 -0800

    Added reference to scdPtp1Objects in scdPtp1Map.lua

--- game/resources/zones/maps/scdPtp1Map.lua
+++ game/resources/zones/maps/scdPtp1Map.lua
@@ -1,6 +1,8 @@
 return {
-  chunksDataName = "scdPtpChunks",
+  chunksDataName  = "scdPtpChunks",
   chunksImageName = "scdPtpChunks",
+  objectsDataName = "scdPtp1Objects",
+  -----------------------------------
   {   1,   2,   3,   4, },
   {   5,   5,   5,   5, },
 }
