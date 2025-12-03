7a3b646d55174dac31f1ee6eb5e8926ea6db47f0
Matthew Hopkins <matthop88@gmail.com>
2025-12-02 19:02:43 -0800

    Create scdPtp1Objects.lua

---
+++ game/resources/zones/objects/scdPtp1Objects.lua
@@ -0,0 +1,9 @@
+return {
+  sourceImage = "resources/zones/maps/SCD_PTP_Map_Act1.png",
+  
+  -- XXX We don't know what origin really is
+  origin           = { x = 512,  y = 940,  },
+
+  -------------------------------------------
+
+}
