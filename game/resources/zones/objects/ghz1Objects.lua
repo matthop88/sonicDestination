fc5850ac1d3fb97f42e2c21fda62d5285664d24f
Matthew Hopkins <matthop88@gmail.com>
2025-12-03 13:00:55 -0800

    Updated ghz1Objects.lua; now there is destination info on the giant ring

--- game/resources/zones/objects/ghz1Objects.lua
+++ game/resources/zones/objects/ghz1Objects.lua
@@ -155,5 +155,5 @@ return {
   { obj = "ring",      x = 6536, y = 116,  },
   { obj = "ring",      x = 9274, y = 1206, },
   { obj = "ring",      x = 1728, y = 932,  },
-  { obj = "giantRing", x = 9640, y = 1104, inactive = true },
+  { obj = "giantRing", x = 9640, y = 1104, inactive = true, destination = { map = "scdPtp1Map", coordinates = { x = 56,  y = 238, }} },
 }
