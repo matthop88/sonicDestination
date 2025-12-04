17e7a1434758dbd42ff05e673746b3c173b25f15
Matthew Hopkins <matthop88@gmail.com>
2025-12-03 18:56:08 -0800

    Added many more stages to teleport

--- game/world/events/teleport.lua
+++ game/world/events/teleport.lua
@@ -2,7 +2,15 @@ local WORLD
 local STAGE_BUILDER = requireRelative("world/events/stageBuilder")
 local SOUND_MANAGER = requireRelative("sound/soundManager")
 
-local READY =      { duration =  60,                                                }
+local READY       = { duration = 5, }
+
+local PLAYER_GONE = {
+	duration = 50,
+
+	activate = function(self, params)
+		params.player:deactivate()
+	end,                                               
+}
 
 local FADING_OUT = { 
 	duration = 60, 
@@ -16,35 +24,47 @@ local RESETTING  = {
 	duration =  0, 
 	activate = function(self, params) 
 		WORLD:reset(params.map, params.x, params.y)
-		params.giantRing:arriving(params.x, params.y)
+		params.player:deactivate()
+	end,
+}
+
+local FADING_IN    = { duration = 60, activate = function(self) WORLD:fadeIn() end }
+
+local RING_ARRIVAL = { 
+	duration = 60,
+	activate = function(self, params)
+		params.giantRing:arrive(params.x, params.y, params.player)
+		params.player:activate()
+		params.player:freeze()
 		WORLD:addPreexistingObject(params.giantRing)
 	end,
 }
 
-local FADING_IN  = { duration = 60, activate = function(self) WORLD:fadeIn()            end, }
-local DONE       = {}
-
-local STAGES = STAGE_BUILDER:build { READY, FADING_OUT, RESETTING, FADING_IN, DONE  }
+local UNFREEZING   = { duration = 60, activate = function(self, params) params.player:unfreeze()  end, }
+local DEPARTING    = { duration = 60, activate = function(self, params) params.giantRing:depart() end, }
+local DONE         = {}
 
 return {
 	create = function(self, world, params)
 		WORLD = world
 		return {
-			timer      = 0,
+			STAGES = STAGE_BUILDER:build { READY, PLAYER_GONE, FADING_OUT, RESETTING, FADING_IN, RING_ARRIVAL, UNFREEZING, DEPARTING, DONE  },
+
+			timer  = 0,
 			
 			update = function(self, dt)
 				self.timer = self.timer + (60 * dt)
-				if STAGES:readyForNext(self.timer) then self:nextStage() end
+				if self.STAGES:readyForNext(self.timer) then self:nextStage() end
 			end,
 
 			nextStage = function(self)
 				self.timer = 0
-				STAGES:next()
-				STAGES:activateCurrent(params)
+				self.STAGES:next()
+				self.STAGES:activateCurrent(params)
 			end,
 
 			isComplete = function(self)
-				return STAGES:isComplete()
+				return self.STAGES:completed()
 			end,
 
 			getName = function(self)
