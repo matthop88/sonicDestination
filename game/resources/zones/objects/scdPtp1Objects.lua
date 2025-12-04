eed1e49db34c48818ba4e7d3cb684937eea1488f
Matthew Hopkins <matthop88@gmail.com>
2025-12-04 09:57:57 -0800

    Added multiple destinations to scdPtp1Objects

--- game/resources/zones/objects/scdPtp1Objects.lua
+++ game/resources/zones/objects/scdPtp1Objects.lua
@@ -2,6 +2,12 @@ return {
   sourceImage      = "resources/zones/maps/SCD_PTP_Map_Act1.png",
   origin           = { x = 56,  y = 238,  },
   -------------------------------------------
-  { obj = "giantRing", x = 963, y = 128, inactive = true, destination = { map = "ghz1Map", coordinates = { x = 256,  y = 500, }} },
+  { obj = "giantRing", x = 963, y = 128, inactive = true, 
+    destination = { 
+      { map = "ghz1Map", coordinates = { x = 256,  y = 500, } },
+      { map = "ghz1Map", coordinates = { x = 2048, y = 100, } },
+      { map = "ghz1Map", coordinates = { x = 8192, y = 800, } },
+    },
+  },
 
 }
