2d80f2e403319075e01ed7d4a2ce3181ded68caa
Matthew Hopkins <matthop88@gmail.com>
2025-12-03 18:48:43 -0800

    Updated scdPtp1Objects.lua to have a ring targeting the GHZ Act 1 Map

--- game/resources/zones/objects/scdPtp1Objects.lua
+++ game/resources/zones/objects/scdPtp1Objects.lua
@@ -2,4 +2,6 @@ return {
   sourceImage      = "resources/zones/maps/SCD_PTP_Map_Act1.png",
   origin           = { x = 56,  y = 238,  },
   -------------------------------------------
+  { obj = "giantRing", x = 963, y = 128, inactive = true, destination = { map = "ghz1Map", coordinates = { x = 256,  y = 500, }} },
+
 }
