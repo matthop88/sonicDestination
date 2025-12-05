1a8d97b30050a4bc09dc7edb705fe78d0d367c3d
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 15:17:33 -0800

    Create puffOfSmoke.lua object part

---
+++ tools/spriteSandbox/data/objects/parts/puffOfSmoke.lua
@@ -0,0 +1,23 @@
+return {
+    imageName = "sonic1BadniksTransparent",
+    animations = {
+        puffing = { fps = 16, offset = { x = -19, y = 5 }, w = 8, h = 8,
+            { x = 287, y = 277, w =  4, h =  4, offset = { x = -21, y =  3 }, },
+            { x = 287, y = 277, w =  4, h =  4, offset = { x = -22, y =  3 }, },
+			{ x = 287, y = 277, w =  4, h =  4, offset = { x = -23, y =  3 }, },
+            { x = 287, y = 277, w =  4, h =  4, offset = { x = -24, y =  3 }, },
+            { x = 285, y = 291, w =  8, h =  8, offset = { x = -23, y =  5 }, },
+            { x = 285, y = 291, w =  8, h =  8, offset = { x = -24, y =  5 }, },
+			{ x = 285, y = 291, w =  8, h =  8, offset = { x = -25, y =  5 }, },
+            { x = 285, y = 291, w =  8, h =  8, offset = { x = -26, y =  5 }, },
+            { x = 285, y = 307, w =  8, h =  8, offset = { x = -27, y =  5 }, },
+            { x = 285, y = 307, w =  8, h =  8, offset = { x = -28, y =  5 }, },
+            { x = 285, y = 307, w =  8, h =  8, offset = { x = -29, y =  5 }, },
+            { x = 285, y = 307, w =  8, h =  8, offset = { x = -30, y =  5 }, },
+            {},
+            {},
+            {},
+            {},
+        },
+    }
+}
