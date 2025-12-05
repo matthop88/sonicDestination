8d04eb080daa18c4fc8364dfbb6e15062033ef34
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 14:18:13 -0800

    Added desired format in comments in motobug data

--- tools/spriteSandbox/data/objects/motobug.lua
+++ tools/spriteSandbox/data/objects/motobug.lua
@@ -16,3 +16,29 @@ return {
         },
     },
 }
+
+--[[
+
+return {
+    imageName = "sonic1BadniksTransparent",
+    animations  = {
+        motobugRoving = { isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
+            -------------------------------------------------------------------------------------
+            sprites = {
+                motobugBody = { fps = 4, offset = { x = 20, y = 14 }, w = 40, h = 28,
+                    { x = 174, y = 276, w = 39, h = 29, offset = { x = 20, y = 14 }, },
+                    { x = 229, y = 276, w = 40, h = 29, offset = { x = 21, y = 14 }, },
+                    { x = 174, y = 316, w = 39, h = 28, offset = { x = 20, y = 14 }, },
+                    { x = 229, y = 316, w = 40, h = 28, offset = { x = 21, y = 14 }, },
+                },
+                puffOfSmoke = { fps = 4, offset = { x = -15, y = 4 }, w = 8, h = 8,
+                    { x = 287, y = 277, w =  4, h =  4, offset = { x = -17, y =  2 }, },
+                    { x = 285, y = 291, w =  8, h =  8, offset = { x = -15, y =  4 }, },
+                    { x = 285, y = 307, w =  8, h =  8, offset = { x = -15, y =  4 }, },
+                }
+            },
+        },
+    },
+}
+
+--]]
