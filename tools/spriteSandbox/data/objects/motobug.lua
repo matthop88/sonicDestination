9407c007b01027f549a6b9bd1b2f0420541d538e
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 14:58:21 -0800

    Updated motobug.lua to create more frames of animation for smoke, offsets gradually move

--- tools/spriteSandbox/data/objects/motobug.lua
+++ tools/spriteSandbox/data/objects/motobug.lua
@@ -11,8 +11,11 @@ return {
         },
         motobugPutt = { fps = 4, offset = { x = -19, y = 5 }, w = 8, h = 8,
             { x = 287, y = 277, w =  4, h =  4, offset = { x = -21, y =  3 }, },
-			{ x = 285, y = 291, w =  8, h =  8, offset = { x = -19, y =  5 }, },
-			{ x = 285, y = 307, w =  8, h =  8, offset = { x = -19, y =  5 }, },
+            { x = 287, y = 277, w =  4, h =  4, offset = { x = -22, y =  3 }, },
+			{ x = 285, y = 291, w =  8, h =  8, offset = { x = -21, y =  5 }, },
+            { x = 285, y = 291, w =  8, h =  8, offset = { x = -22, y =  5 }, },
+			{ x = 285, y = 307, w =  8, h =  8, offset = { x = -23, y =  5 }, },
+            { x = 285, y = 307, w =  8, h =  8, offset = { x = -24, y =  5 }, },
         },
     },
 }
