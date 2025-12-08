71ae037ace4ba884987925d58ca8de51b9e5694c
Matthew Hopkins <matthop88@gmail.com>
2025-12-08 13:15:44 -0800

    Fleshed out animation.lua to delegate methods to animation data

--- tools/spriteSandbox/animation.lua
+++ tools/spriteSandbox/animation.lua
@@ -1,5 +1,25 @@
 return {
 	create = function(self, animationData)
-		return animationData
+		return {
+			data = animationData,
+
+			isDefault    = function(self)    return self.data.isDefault    end,
+
+			getFPS       = function(self)    return self.data.fps          end,
+
+			size         = function(self)    return #self.data             end,
+			get          = function(self, n) return self.data[n]           end,
+			frames       = function(self)    return self.data              end,
+
+			width        = function(self)    return self.data.w            end,
+			height       = function(self)    return self.data.h            end,
+
+			offsetX      = function(self)    return self.data.offset.x     end,
+			offsetY      = function(self)    return self.data.offset.y     end,
+			
+			reps         = function(self)    return self.data.reps         end,
+			synchronized = function(self)    return self.data.synchronized end,
+			foreground   = function(self)    return self.data.foreground   end,
+		}
 	end,
 }
