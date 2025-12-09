9c0fa757a98215057ad300d51498ed1b0ed14ba2
Matthew Hopkins <matthop88@gmail.com>
2025-12-09 10:11:56 -0800

    Added motobug.lua to game/sprites/data

---
+++ game/sprites/data/objects/motobug.lua
@@ -0,0 +1,13 @@
+return {
+    imageName = "sonic1BadniksTransparent",
+    animations  = {
+        motobugRoving = { isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
+            ----------------------------------------------------------------------------
+            parts = {
+				{	name = "motobugBody", animation = "roving",  },
+				{   name = "puffOfSmoke", animation = "puffing", },
+			}
+        },
+    },
+}
+
