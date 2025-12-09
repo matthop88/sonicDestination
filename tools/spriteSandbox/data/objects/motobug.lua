18bbf7204efa811efa15ad249329b98646a735ef
Matthew Hopkins <matthop88@gmail.com>
2025-12-08 16:32:20 -0800

    Simplified motobug.lua data

--- tools/spriteSandbox/data/objects/motobug.lua
+++ tools/spriteSandbox/data/objects/motobug.lua
@@ -2,39 +2,6 @@
 return {
     imageName = "sonic1BadniksTransparent",
     animations  = {
-        motobugRoving = { fps = 4, isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
-            ---------------------------------------------------------------------------------------------------
-            { x = 174, y = 276, w = 39, h = 29, offset = { x = 20, y = 14 }, },
-			{ x = 229, y = 276, w = 40, h = 29, offset = { x = 21, y = 14 }, },
-			{ x = 174, y = 316, w = 39, h = 28, offset = { x = 20, y = 14 }, },
-			{ x = 229, y = 316, w = 40, h = 28, offset = { x = 21, y = 14 }, },
-        },
-        motobugPutt = { fps = 16, offset = { x = -19, y = 5 }, w = 8, h = 8,
-            { x = 287, y = 277, w =  4, h =  4, offset = { x = -21, y =  3 }, },
-            { x = 287, y = 277, w =  4, h =  4, offset = { x = -22, y =  3 }, },
-			{ x = 287, y = 277, w =  4, h =  4, offset = { x = -23, y =  3 }, },
-            { x = 287, y = 277, w =  4, h =  4, offset = { x = -24, y =  3 }, },
-            { x = 285, y = 291, w =  8, h =  8, offset = { x = -23, y =  5 }, },
-            { x = 285, y = 291, w =  8, h =  8, offset = { x = -24, y =  5 }, },
-			{ x = 285, y = 291, w =  8, h =  8, offset = { x = -25, y =  5 }, },
-            { x = 285, y = 291, w =  8, h =  8, offset = { x = -26, y =  5 }, },
-            { x = 285, y = 307, w =  8, h =  8, offset = { x = -27, y =  5 }, },
-            { x = 285, y = 307, w =  8, h =  8, offset = { x = -28, y =  5 }, },
-            { x = 285, y = 307, w =  8, h =  8, offset = { x = -29, y =  5 }, },
-            { x = 285, y = 307, w =  8, h =  8, offset = { x = -30, y =  5 }, },
-            {},
-            {},
-            {},
-            {},
-        },
-    },
-}
-
---[[
-
-return {
-    imageName = "sonic1BadniksTransparent",
-    animations  = {
         motobugRoving = { isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
             ----------------------------------------------------------------------------
             parts = {
@@ -45,4 +12,3 @@ return {
     },
 }
 
---]]
