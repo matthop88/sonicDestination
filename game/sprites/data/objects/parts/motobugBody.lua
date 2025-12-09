6eb08e09790e88b816af6430a197b275797998cb
Matthew Hopkins <matthop88@gmail.com>
2025-12-09 10:09:43 -0800

    Added motobugBody to parts in game/sprites

---
+++ game/sprites/data/objects/parts/motobugBody.lua
@@ -0,0 +1,12 @@
+return {
+    imageName = "sonic1BadniksTransparent",
+    animations  = {
+        roving = { fps = 4, isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
+            ------------------------------------------------------------------------------
+            { x = 174, y = 276, w = 39, h = 29, offset = { x = 20, y = 14 }, },
+			{ x = 229, y = 276, w = 40, h = 29, offset = { x = 21, y = 14 }, },
+			{ x = 174, y = 316, w = 39, h = 28, offset = { x = 20, y = 14 }, },
+			{ x = 229, y = 316, w = 40, h = 28, offset = { x = 21, y = 14 }, },
+        },
+    },
+}
