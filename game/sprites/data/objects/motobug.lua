8e82f985dc0dacc95cfc21a47ee926c505e45ced
Matthew Hopkins <matthop88@gmail.com>
2025-12-10 17:42:34 -0800

    Added hitBox to motobug

--- game/sprites/data/objects/motobug.lua
+++ game/sprites/data/objects/motobug.lua
@@ -2,6 +2,7 @@ return {
     imageName = "sonic1BadniksTransparent",
     animations  = {
         motobugRoving = { isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
+            hitBox = { rX = 15, rY = 10 },
             ----------------------------------------------------------------------------
             parts = {
 				{	name = "motobugBody", animation = "roving",  },
