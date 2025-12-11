f7bcdc55b79389daf084f9f1283b2c93d74aaebe
Matthew Hopkins <matthop88@gmail.com>
2025-12-11 10:21:26 -0800

    Updated motobug.lua to play badnik death sound upon destruction

--- game/world/gameObjects/objects/motobug.lua
+++ game/world/gameObjects/objects/motobug.lua
@@ -1,9 +1,13 @@
+local SOUND_MANAGER  = requireRelative("sound/soundManager")
+
 return {
 	create = function(self)
 		return {
 			onCollisionWithPlayer = function(self, player)
 				if player:isSpinning() then
 					self:setAnimation("motobugDying")
+					SOUND_MANAGER:play("badnikDeath")
+        
 				end
 			end,
 		}
