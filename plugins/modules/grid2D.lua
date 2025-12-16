abd4ceffb962b6fa2c0ea407630dda8128bd3407
Matthew Hopkins <matthop88@gmail.com>
2025-12-15 17:47:25 -0800

    Updated grid2D.lua to introduce super chunks and uber super chunks

--- plugins/modules/grid2D.lua
+++ plugins/modules/grid2D.lua
@@ -1,6 +1,8 @@
-local CHUNK_SIZE = 256
-local TILE_SIZE  = 16
-local PIXEL_SIZE = 1
+local UBER_SUPER_CHUNK_SIZE = 65536
+local SUPER_CHUNK_SIZE      = 4096
+local CHUNK_SIZE            = 256
+local TILE_SIZE             = 16
+local PIXEL_SIZE            = 1
 
 return {
 	init = function(self, params)
@@ -14,36 +16,54 @@ return {
 		local leftX,  topY    = self.graphics:screenToImageCoordinates(0, 0)
 		local rightX, bottomY = self.graphics:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
 		
-		if (rightX - leftX) <= 96 then
+		if (rightX - leftX) <= 192 then
 			self:drawPixelGrid(leftX, topY, rightX, bottomY)
 		end
 
-		if (rightX - leftX) <= 768 then
+		if (rightX - leftX) <= 1536 then
 			self:drawTileGrid(leftX, topY, rightX, bottomY)
 		end
 
-		self:drawChunkGrid(leftX, topY, rightX, bottomY)
+		if (rightX - leftX) <= 24576 then
+			self:drawChunkGrid(leftX, topY, rightX, bottomY)
+		end
+
+		self:drawSuperChunkGrid(leftX, topY, rightX, bottomY)
+
+		self:drawUberSuperChunkGrid(leftX, topY, rightX, bottomY)
 	end,
 
 	drawPixelGrid  = function(self, leftX, topY, rightX, bottomY)
-		self.graphics:setColor(0.5, 0.5, 0.3, (96 - (rightX - leftX)) / 96)
+		self.graphics:setColor(0.5, 0.5, 0.3, (192 - (rightX - leftX)) / 192)
 		self.graphics:setLineWidth(0.125)
 
 		self:drawGrid(leftX, topY, rightX, bottomY, PIXEL_SIZE)
 	end,
 
 	drawTileGrid  = function(self, leftX, topY, rightX, bottomY)
-		self.graphics:setColor(0.5, 0.5, 0.5, (768 - (rightX - leftX)) / 768)
+		self.graphics:setColor(0.5, 0.5, 0.5, (1536 - (rightX - leftX)) / 1536)
 		self.graphics:setLineWidth(0.25)
 
 		self:drawGrid(leftX + 0.25, topY + 0.25, rightX + 0.25, bottomY + 0.25, TILE_SIZE)
 	end,
 
 	drawChunkGrid = function(self, leftX, topY, rightX, bottomY)
-		self.graphics:setColor(1, 1, 1)
+		self.graphics:setColor(0.8, 0.8, 0.8, (24576 - (rightX - leftX)) / 24576)
         self.graphics:setLineWidth(0.5)
 
         self:drawGrid(leftX, topY, rightX, bottomY, CHUNK_SIZE)
+	end,
+
+	drawSuperChunkGrid = function(self, leftX, topY, rightX, bottomY)
+		self.graphics:setColor(0.9, 0.9, 0.9, (393216 - (rightX - leftX)) / 393216)
+		self.graphics:setLineWidth(1)
+		self:drawGrid(leftX, topY, rightX, bottomY, SUPER_CHUNK_SIZE)
+	end,
+
+	drawUberSuperChunkGrid = function(self, leftX, topY, rightX, bottomY)
+		self.graphics:setColor(1, 1, 1)
+		self.graphics:setLineWidth(1)
+		self:drawGrid(leftX, topY, rightX, bottomY, UBER_SUPER_CHUNK_SIZE)
 	end,
 	
 	drawGrid = function(self, leftX, topY, rightX, bottomY, gridSize)
