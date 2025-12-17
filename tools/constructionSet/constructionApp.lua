a43dfd5d732361c0183c1302d1282c6e8ff94736
Matthew Hopkins <matthop88@gmail.com>
2025-12-17 05:36:35 -0800

    Updated constructionApp.lua; troubleshooting blank chunk image

--- tools/constructionSet/constructionApp.lua
+++ tools/constructionSet/constructionApp.lua
@@ -14,19 +14,29 @@ end
 local CHUNKS_PATH = "game/resources/zones/chunks/ghzChunks.lua"
 local CHUNKS_INFO = require("tools/constructionSet/terrain/chunkImageBuilder"):create(CHUNKS_PATH)
 local CHUNKS      = require("tools/constructionSet/terrain/chunksBuilder"):create(CHUNKS_INFO.image)
-            
+    
+local CHUNKS_DATA = dofile(CHUNKS_PATH)
+local TILES_PATH  = "game/resources/zones/tiles/" .. CHUNKS_DATA.tilesImageName .. ".png"
+local TILES_IMG   = love.graphics.newImage(TILES_PATH)
+
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
 
 love.window.setTitle("Construction Set")
 love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
+TILES_IMG:setFilter("nearest", "nearest")
 
 --------------------------------------------------------------
 --                     LOVE2D Functions                     --
 --------------------------------------------------------------
 
--- ...
+function love.draw()
+    GRAFX:setColor(1, 1, 1)
+    local x, y = GRAFX:screenToImageCoordinates(love.mouse.getPosition())
+    GRAFX:draw(TILES_IMG, x, y)
+end
+
 -- ...
 -- ...
 
@@ -54,8 +64,8 @@ PLUGINS = require("plugins/engine")
     })
     :add("scrolling",      { imageViewer = GRAFX })
     :add("zooming",        { imageViewer = GRAFX })    
-    :add("grid2d",         { 
+    --[[:add("grid2d",         { 
         graphics    = GRAFX,
         bounds      = { x = 0, y = 0, w = 256, h = 256 },
-    })
+    })]]
     
