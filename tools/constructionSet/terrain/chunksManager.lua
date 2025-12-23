2689ee78b03f9307a509a6f2572742d14a0f6c5c
Matthew Hopkins <matthop88@gmail.com>
2025-12-23 08:38:35 -0500

    Expanded functionality of chunksManager.lua

--- tools/constructionSet/terrain/chunksManager.lua
+++ tools/constructionSet/terrain/chunksManager.lua
@@ -1,13 +1,31 @@
 return {
-	chunkBanks = {},
+	chunkBanks = require("tools/lib/dataStructures/navigableList"):create {},
+	chunksRepo = {},
 
 	register = function(self, path)
-		local fullPath = "game/resources/zones/chunks/" .. path .. ".lua"
+		local fullPath   = "game/resources/zones/chunks/" .. path .. ".lua"
 		local chunksInfo = require("tools/constructionSet/terrain/chunkImageBuilder"):create(fullPath)
         local chunks     = require("tools/constructionSet/terrain/chunksBuilder"):create(chunksInfo.image) 
-            
-        table.insert(self.chunkBanks, chunks)
+        local chunkList  = require("tools/constructionSet/terrain/chunkList"):create(chunks)
 
-        return chunks 
+        self.chunkBanks:add(chunkList)
+        table.insert(self.chunksRepo, chunks)
+
+        return self 
 	end,
+
+	nextBank   = function(self)        self.chunkBanks:next()        end,
+	prevBank   = function(self)        self.chunkBanks:prev()        end,
+	nextChunk  = function(self)        self.chunkBanks:get():next()  end,
+	prevChunk  = function(self)        self.chunkBanks:get():prev()  end,
+	chunkIndex = function(self) return self.chunkBanks:get():index() end,
+	bankIndex  = function(self) return self.chunkBanks:index()       end,
+	
+	get       = function(self, n)
+		if n then return self.chunkBanks:get():get(n)
+		else      return self.chunkBanks:get():get()  end
+	end,
+
+	getChunksRepo = function(self) return self.chunksRepo end,
+
 }
