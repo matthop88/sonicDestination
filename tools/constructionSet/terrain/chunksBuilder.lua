b5a93ad6e9211f7b7ce8d1fc3da1ac0af28a78e7
Matthew Hopkins <matthop88@gmail.com>
2025-12-23 08:49:57 -0500

    Updated chunksBuilder.lua to take fixed chunk count rather than dynamically generated one

--- tools/constructionSet/terrain/chunksBuilder.lua
+++ tools/constructionSet/terrain/chunksBuilder.lua
@@ -1,11 +1,10 @@
 return {
-    create = function(self, chunksImage)
+    create = function(self, chunksImage, chunkCount)
         return ({
             init = function(self, chunksImage)
     	        self.image = chunksImage
                 self.data  = {}
 
-	            local chunkCount = self:calculateChunkCount()
 	            for i = 1, chunkCount do
 	                local chunkX = ((i - 1) % 9)           * 256
 	                local chunkY = math.floor((i - 1) / 9) * 256
@@ -14,12 +13,6 @@ return {
 	            end
 	    
                 return self
-            end,
-
-            calculateChunkCount = function(self)
-                local width, height = self.image:getWidth(), self.image:getHeight()
-
-                return math.floor(width / 256) * math.floor(height / 256)
             end,
 
             get = function(self, chunkID)
