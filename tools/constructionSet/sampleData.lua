7962f78c6a7c4c25ca940b12a8aa098180758507
Matthew Hopkins <matthop88@gmail.com>
2025-12-25 11:07:54 -0500

    Created sampleData.lua

---
+++ tools/constructionSet/sampleData.lua
@@ -0,0 +1,5 @@
+return {
+  { row = 1, data = {} },
+  { row = 2, data = { { "ghzChunks_2", 6 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  2 }, { "ghzChunks_2",  3 }, { "ghzChunks_2", 4 }, { "ghzChunks_2", 5 } } },
+  { row = 3, data = { { "ghzChunks_2", 7 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 8 }, { "ghzChunks_2", 9 } } },
+}
