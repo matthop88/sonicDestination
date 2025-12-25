bbdaf7dd2e44fdbae3e96d13c71ce2983e17381c
Matthew Hopkins <matthop88@gmail.com>
2025-12-25 07:52:58 -0500

    Updated map.lua so it can be printed as a table

--- tools/constructionSet/map.lua
+++ tools/constructionSet/map.lua
@@ -1,3 +1,5 @@
+local STRING_UTIL = require("tools/lib/stringUtil")
+
 return {
 	create = function(self, params)
 		local widthInChunks  = params.w        or 256
@@ -15,13 +17,14 @@ return {
 			chunkHeight    = chunkHeight,
 			CHUNKS_MANAGER = manager,
 			chunkMap       = chunkMap,
+			maxRow         = 0,
 
 			draw = function(self, GRAFX)
 				GRAFX:setColor(1, 1, 1)
     			for i = 1, self.height do
         			local c = self.chunkMap[i]
         			for j = 1, self.width do
-            			if c[j] then
+            			if c[j] ~= 0 then
                 			c[j]:draw(GRAFX, (j - 1) * self.chunkWidth, (i - 1) * self.chunkHeight)
                 		end
                 	end
@@ -41,8 +44,35 @@ return {
     			local x, y = GRAFX:screenToImageCoordinates(mx, my)
         		local j, i = math.floor(x / self.chunkWidth) + 1, math.floor(y / self.chunkHeight) + 1
         		self.chunkMap[i][j] = self.CHUNKS_MANAGER:get()
+        		self.chunkMap[i].hasData = true
+        		self.maxRow = math.max(self.maxRow, i)
+        		self.chunkMap[i].maxCell = math.max(self.chunkMap[i].maxCell, j)
         	end,
 
+        	printAsTable = function(self)
+        		print("{")
+        		for rowNum, chunkRow in ipairs(self.chunkMap) do
+        			if rowNum <= self.maxRow then
+        				if chunkRow.hasData then
+	        				print("  { row = " .. rowNum .. ", data = " .. self:getRowData(chunkRow) .. " },")
+	        			else
+	        				print("  { row = " .. rowNum .. ", data = {} },")
+	        			end
+	        		end
+        		end
+        		print("}")
+        	end,
+
+        	getRowData = function(self, chunkRow)
+        		local results = {}
+        		for n, v in ipairs(chunkRow) do
+        			if n <= chunkRow.maxCell then
+        				if v == 0 then table.insert(results, "0")
+        				else           table.insert(results, "{ \"" .. v.name .. "\", " .. v.id .. " }") end
+        			end
+        		end
+        		return "{ " .. (STRING_UTIL:join(results, ", ") or "") .. " }"
+        	end,
 		}
 	end,
 
@@ -50,8 +80,8 @@ return {
 		local chunkMap = {}
 
 		for i = 1, heightInChunks do
-		    local c = {}
-		    for j = 1, widthInChunks do table.insert(c, nil) end
+		    local c = { maxCell = 0, }
+		    for j = 1, widthInChunks do table.insert(c, 0) end
 		    table.insert(chunkMap, c)
 		end
 
