66d698b0ebcf952fced99cd16bd7d7f8d5169f81
Matthew Hopkins <matthop88@gmail.com>
2025-12-26 07:12:06 -0500

    Updated sampleData.lua to contain chunks from 2 different zones

--- tools/constructionSet/sampleData.lua
+++ tools/constructionSet/sampleData.lua
@@ -1,5 +1,5 @@
 return {
   { row = 1, data = {} },
-  { row = 2, data = { { "ghzChunks_2", 6 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  2 }, { "ghzChunks_2",  3 }, { "ghzChunks_2", 4 }, { "ghzChunks_2", 5 } } },
-  { row = 3, data = { { "ghzChunks_2", 7 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 8 }, { "ghzChunks_2", 9 } } },
+  { row = 2, data = { { "ghzChunks_2", 6 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  1 }, { "ghzChunks_2",  2 }, { "ghzChunks_2",  3 }, { "ghzChunks_2", 4 }, { "ghzChunks_2", 5 }, { "scdPtpChunks", 1 }, { "scdPtpChunks", 2 }, { "scdPtpChunks", 3 }, { "scdPtpChunks", 4 } } },
+  { row = 3, data = { { "ghzChunks_2", 7 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 10 }, { "ghzChunks_2", 8 }, { "ghzChunks_2", 9 }, { "scdPtpChunks", 5 }, { "scdPtpChunks", 5 }, { "scdPtpChunks", 5 }, { "scdPtpChunks", 5 } } },
 }
