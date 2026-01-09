66027e4833c9947182d2c4057dc74084e8f1d78e
Matthew Hopkins <matthop88@gmail.com>
2026-01-08 17:48:15 -0800

    Updated sprite paths in animationFactory.lua

--- tools/spriteSandbox/animationFactory.lua
+++ tools/spriteSandbox/animationFactory.lua
@@ -2,7 +2,7 @@ local IMAGE_LOADER = require("tools/lib/util/imageLoader")
 
 return {
 	create = function(self, pathPrefix, objectName, animationName)
-        local object    = require("tools/spriteSandbox/data/objects/" .. pathPrefix .. objectName)
+        local object    = require("game/sprites/data/objects/" .. pathPrefix .. objectName)
         local animation = object.animations[animationName]
         local image     = IMAGE_LOADER:loadImage("resources/images/spriteSheets/" .. object.imageName .. ".png")
         
