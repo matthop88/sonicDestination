05d31e9bf1351f19752b96ad1f11c053644ad55c
Matthew Hopkins <matthop88@gmail.com>
2026-01-11 17:59:30 -0800

    Update and rename badnikFactory.lua to badnikTemplateFactory.lua

--- tools/badnikUniversity/badniks/badnikTemplateFactory.lua
+++ tools/badnikUniversity/badniks/badnikTemplateFactory.lua
@@ -1,22 +1,32 @@
 local NO_BUMP_ID = true
 
 return {
-    create = function(self, name, path)
+    createTemplate = function(self, name, path)
         return {
-            name = name,
-            path = path,
+            name  = name,
+            path  = path,
             spritePreview = require("tools/lib/sprites/sprite"):create(path, 0, 0, NO_BUMP_ID),
-
+            xFlip = false,
+            
             drawPreviewSprite = function(self, GRAFX, x, y)
+                GRAFX:setColor(1, 1, 1)
                 self.spritePreview:drawThumbnail(GRAFX, x, y, 1, 1)
             end,
 
+            flipX = function(self) 
+                self.spritePreview:flipX() 
+                self.xFlip = not self.xFlip
+            end,
+
             create = function(self, x, y)
+                local sprite = require("tools/lib/sprites/sprite"):create(self.path, x, y)
+                if self.xFlip then sprite:flipX() end
+
                 return {
                     name  = self.name,
                     x     = x,
                     y     = y,
-                    sprite = require("tools/lib/sprites/sprite"):create(self.path, x, y),
+                    sprite = sprite,
 
                     getX  = function(self) return self.x     end,
                     getY  = function(self) return self.y     end,
@@ -26,7 +36,7 @@ return {
                     draw  = function(self, GRAFX)
                         self.sprite:draw(GRAFX)
                     end,
-                end
+                }
             end,
         }
     end,
