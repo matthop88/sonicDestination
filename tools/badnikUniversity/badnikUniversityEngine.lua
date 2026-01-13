201999e3ad01ce2c1b60350afa48263553140eb2
Matthew Hopkins <matthop88@gmail.com>
2026-01-12 17:36:10 -0800

    Updated badnikUniversityEngine.lua to allow paging through badniks via tab and shift tab

--- tools/badnikUniversity/badnikUniversityEngine.lua
+++ tools/badnikUniversity/badnikUniversityEngine.lua
@@ -5,10 +5,28 @@ return {
             graphics   = require("tools/lib/graphics"):create(), 
             badnikMode = false,
             badniks    = require("tools/badnikUniversity/badnikList"),
-            badnikTemplate = require("tools/badnikUniversity/factories/badnikTemplateFactory"):createTemplate("motobug", "objects/motobug"),
-
+            badnikRoster = {
+                "motobug",
+                "patabata",
+            },
+            badnikTemplates = {
+                index = 1,
+                get  = function(self) return self[self.index] end,
+                next = function(self)
+                    self.index = self.index + 1
+                    if self.index > #self then self.index = 1 end
+                end,
+                prev = function(self)
+                    self.index = self.index - 1
+                    if self.index < 1 then self.index = #self end
+                end,
+            },
+            
             init = function(self, params)
                 local x, y = self:screenToImageCoordinates(love.mouse.getPosition())
+                for _, badnikName in ipairs(self.badnikRoster) do
+                    table.insert(self.badnikTemplates, require("tools/badnikUniversity/factories/badnikTemplateFactory"):createTemplate(badnikName))
+                end
                 return self
             end,
 
@@ -21,7 +39,7 @@ return {
                 if self.badnikMode then
                     love.mouse.setVisible(false)
                     local x, y = self:screenToImageCoordinates(love.mouse.getPosition())
-                    self.badnikTemplate:drawPreviewSprite(self.graphics, math.floor(x), math.floor(y))
+                    self.badnikTemplates:get():drawPreviewSprite(self.graphics, math.floor(x), math.floor(y))
                 else
                     love.mouse.setVisible(true)
                 end
@@ -35,14 +53,18 @@ return {
                 if key == "b" then
                     self.badnikMode = not self.badnikMode
                 elseif key == "x" and self.badnikMode then
-                    self.badnikTemplate:flipX()
+                    self.badnikTemplates:get():flipX()
+                elseif key == "tab" and self.badnikMode then
+                    self.badnikTemplates:next()
+                elseif key == "shifttab" and self.badnikMode then
+                    self.badnikTemplates:prev()
                 end
             end,
 
             handleMousepressed = function(self, mx, my)
                 if self.badnikMode then
                     local x, y = self:screenToImageCoordinates(mx, my)
-                    self.badniks:placeBadnik(self.badnikTemplate:create(math.floor(x), math.floor(y)), self.graphics)
+                    self.badniks:placeBadnik(self.badnikTemplates:get():create(math.floor(x), math.floor(y)), self.graphics)
                     self.badnikMode = false
                 end
             end,
