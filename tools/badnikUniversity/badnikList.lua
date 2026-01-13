9edf7737191bf3cbaf54b34f8ac39f428e4e7d4e
Matthew Hopkins <matthop88@gmail.com>
2026-01-13 11:46:50 -0800

    Updated badnikList.lua to support mouseover functionality

--- tools/badnikUniversity/badnikList.lua
+++ tools/badnikUniversity/badnikList.lua
@@ -2,14 +2,22 @@ return {
     badniks = require("tools/lib/dataStructures/linkedList"):create(),  
      
     draw = function(self, GRAFX)
+        local x, y = GRAFX:screenToImageCoordinates(love.mouse.getPosition())
+
         self.badniks:head()
         while not self.badniks:isEnd() do 
             local badnik = self.badniks:getNext()
             badnik:draw(GRAFX)
+            if badnik:isInside(x, y) then
+                GRAFX:setColor(1, 1, 1)
+                GRAFX:setLineWidth(2)
+                GRAFX:rectangle("line", badnik:getX() - (badnik:getW() / 2) - 2, badnik:getY() - (badnik:getH() / 2) - 2, badnik:getW() + 4, badnik:getH() + 4)
+            end
         end
     end,
     
     placeBadnik = function(self, newBadnik, GRAFX)
         self.badniks:add(newBadnik)
     end,
+
 }
