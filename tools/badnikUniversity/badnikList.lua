fae82580a5cb588ca0fa5bb8f490c1f49b9d93bf
Matthew Hopkins <matthop88@gmail.com>
2026-01-13 12:05:19 -0800

    Updated badnikList.lua; separated draw and mouseover, and changed border color

--- tools/badnikUniversity/badnikList.lua
+++ tools/badnikUniversity/badnikList.lua
@@ -2,14 +2,21 @@ return {
     badniks = require("tools/lib/dataStructures/linkedList"):create(),  
      
     draw = function(self, GRAFX)
+        self.badniks:head()
+        while not self.badniks:isEnd() do 
+            local badnik = self.badniks:getNext()
+            badnik:draw(GRAFX)
+        end
+    end,
+
+    drawMouseOver = function(self, GRAFX)
         local x, y = GRAFX:screenToImageCoordinates(love.mouse.getPosition())
 
         self.badniks:head()
         while not self.badniks:isEnd() do 
             local badnik = self.badniks:getNext()
-            badnik:draw(GRAFX)
             if badnik:isInside(x, y) then
-                GRAFX:setColor(1, 1, 1)
+                GRAFX:setColor(0.5, 0.5, 1)
                 GRAFX:setLineWidth(2)
                 GRAFX:rectangle("line", badnik:getX() - (badnik:getW() / 2) - 2, badnik:getY() - (badnik:getH() / 2) - 2, badnik:getW() + 4, badnik:getH() + 4)
             end
