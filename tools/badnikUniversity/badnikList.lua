b31f78b7db625031d2f14ef83c1e8025036cf0ce
Matthew Hopkins <matthop88@gmail.com>
2026-01-15 16:54:19 -0500

    Modified badnik list to have getSelected() method rather than tagging selection on object level

--- tools/badnikUniversity/badnikList.lua
+++ tools/badnikUniversity/badnikList.lua
@@ -84,4 +84,5 @@ return {
     size    = function(self)     return self.badniks:size()      end,
     remove  = function(self)     return self.badniks:remove()    end,
     forEach = function(self, fn) return self.badniks:forEach(fn) end,
+    getSelected = function(self) return self.selectedBadnik      end,
 }
