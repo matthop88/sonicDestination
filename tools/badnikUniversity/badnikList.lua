1263d7a0e33815897385fc001b17ec6f3b020dde
Matthew Hopkins <matthop88@gmail.com>
2026-01-16 14:12:21 -0500

    Updated badnikList.lua to support dual get/set considered architecture

--- tools/badnikUniversity/badnikList.lua
+++ tools/badnikUniversity/badnikList.lua
@@ -1,26 +1,39 @@
 return {
     badniks = require("tools/lib/dataStructures/linkedList"):create(),  
      
-    selectedBadnik = nil,
+    selectedBadnik       = nil,
+    consideredBadnik     = nil,
+    externallyConsidered = nil,
 
     draw = function(self, GRAFX)
         self.badniks:forEach(function(badnik) badnik:draw(GRAFX) end)
     end,
 
     drawMouseOver = function(self, GRAFX)
+        local c = self.consideredBadnik or self.externallyConsidered
+        if c then
+            GRAFX:setColor(0.5, 0.5, 1)
+            GRAFX:setLineWidth(2)
+            GRAFX:rectangle("line", c:getX() - (c:getW() / 2) - 2, c:getY() - (c:getH() / 2) - 2, c:getW() + 4, c:getH() + 4)
+        end
+    end,
+
+    calculateMouseOver = function(self, GRAFX) 
         local x, y = GRAFX:screenToImageCoordinates(love.mouse.getPosition())
+
+        self.consideredBadnik = nil
 
         self.badniks:forEach(function(badnik)
             if badnik:isInside(x, y) then
-                GRAFX:setColor(0.5, 0.5, 1)
-                GRAFX:setLineWidth(2)
-                GRAFX:rectangle("line", badnik:getX() - (badnik:getW() / 2) - 2, badnik:getY() - (badnik:getH() / 2) - 2, badnik:getW() + 4, badnik:getH() + 4)
+                self.consideredBadnik = badnik
+                self.externallyConsidered = nil
+                return true
             end
         end)
     end,
 
-    update = function(self, dt)
-        -- Do nothing
+    update = function(self, dt, GRAFX)
+        self:calculateMouseOver(GRAFX)
     end,
 
     nudgeSelected = function(self, dx, dy)
@@ -81,8 +94,16 @@ return {
         self:selectBadnik(newBadnik)
     end,
 
-    size    = function(self)     return self.badniks:size()      end,
-    remove  = function(self)     return self.badniks:remove()    end,
-    forEach = function(self, fn) return self.badniks:forEach(fn) end,
-    getSelected = function(self) return self.selectedBadnik      end,
+    size    = function(self)     return  self.badniks:size()          end,
+    remove  = function(self)      
+        self.badniks:remove()  
+        self.selectedBadnik = nil
+        self.consideredBadnik = nil  
+        self.externallyConsidered = nil    
+    end,
+    forEach       = function(self, fn)     return self.badniks:forEach(fn)     end,
+    getSelected   = function(self)         return self.selectedBadnik          end,
+    setSelected   = function(self, badnik) self.selectedBadnik = badnik        end,
+    getConsidered = function(self)         return self.consideredBadnik        end,
+    setConsidered = function(self, badnik) self.externallyConsidered = badnik  end,
 }
