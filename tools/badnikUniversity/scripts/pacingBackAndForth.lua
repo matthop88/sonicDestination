847d9c02aa1dbb70a246f0b65613a20c356c3cae
Matthew Hopkins <matthop88@gmail.com>
2026-01-20 08:44:52 -0500

    Updated pacingBackAndForth.lua; now is actual skeletal code

--- tools/badnikUniversity/scripts/pacingBackAndForth.lua
+++ tools/badnikUniversity/scripts/pacingBackAndForth.lua
@@ -1,17 +1,28 @@
---[[
+local SET_X_SPEED = function(xSpeed)
+	-- do nothing
+end
 
-{
-	name: pacingBackAndForth,
-	title: "Pacing Back And Forth",
-	instructions: {
-		SET_X_SPEED(100)
-		MOVE(3)
-		SET_X_SPEED(0)
-		WAIT(1)
-		FLIPX()
-		WAIT(1)
-		REPEAT()
-	}
+local MOVE        = function(numSeconds)
+	-- do nothing
+end
+
+local WAIT        = function(numSeconds)
+	-- do nothing
+end
+
+local FLIPX       = function()
+	-- do nothing
+end
+
+return {
+	name         = "pacingBackAndForth",
+	title        = "Pacing Back and Forth",
+	instructions = {
+		SET_X_SPEED(100),
+		MOVE(3),
+		SET_X_SPEED(0),
+		WAIT(1),
+		FLIPX(),
+		WAIT(1),
+	},
 }
-
---]]
