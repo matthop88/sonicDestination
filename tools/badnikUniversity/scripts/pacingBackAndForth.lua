ec380f20fea91832e5898b631eaea9a400152755
Matthew Hopkins <matthop88@gmail.com>
2026-01-19 21:15:14 -0500

    Added pseudo code for pacing back and forth

---
+++ tools/badnikUniversity/scripts/pacingBackAndForth.lua
@@ -0,0 +1,17 @@
+--[[
+
+{
+	name: pacingBackAndForth,
+	title: "Pacing Back And Forth",
+	instructions: {
+		SET_X_SPEED(100)
+		MOVE(3)
+		SET_X_SPEED(0)
+		WAIT(1)
+		FLIPX()
+		WAIT(1)
+		REPEAT()
+	}
+}
+
+--]]
