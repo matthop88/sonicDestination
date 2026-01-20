6a70ae8f6662694567e816165e28c7ca7437addc
Matthew Hopkins <matthop88@gmail.com>
2026-01-20 10:54:28 -0500

    Updated move.lua with parameters

--- tools/badnikUniversity/scripts/commands/move.lua
+++ tools/badnikUniversity/scripts/commands/move.lua
@@ -1,3 +1,17 @@
-return function(xSpeed, numSeconds)
-    -- do nothing
+return function(params)
+    return {
+        xSpeed   =  params.xSpeed     or 100,
+        duration = (params.numSeconds or 1)   * 60,
+        elapsed  =  0,
+
+        reset = function(self)
+            self.elapsed = 0
+        end,
+
+        execute = function(self, dt, actor)
+            actor:setXSpeed(self.xSpeed)
+            self.elapsed = self.elapsed + dt
+            return self.elapsed > self.duration
+        end,
+    }
 end
