467245abc41aba66b28b34cc7e5da0c3c50e00bd
Matthew Hopkins <matthop88@gmail.com>
2026-01-20 10:58:48 -0500

    Updated wait.lua to be fleshed out

--- tools/badnikUniversity/scripts/commands/wait.lua
+++ tools/badnikUniversity/scripts/commands/wait.lua
@@ -1,3 +1,16 @@
-return function(numSeconds)
-    -- do nothing
+return function(params)
+    return {
+        duration = (params.numSeconds or 1) * 60,
+        elapsed  =  0,
+
+        reset = function(self)
+            self.elapsed = 0
+        end,
+
+        execute = function(self, dt, actor)
+            actor:setXSpeed(0)
+            self.elapsed = self.elapsed + dt
+            return self.elapsed > self.duration
+        end,
+    }
 end
