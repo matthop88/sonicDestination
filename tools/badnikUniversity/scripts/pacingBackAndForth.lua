64cdfbf4620a35ef215921515e4fa1561616b39b
Matthew Hopkins <matthop88@gmail.com>
2026-01-22 10:33:04 -0800

    Updated pacingBackAndForth.lua; program for script now becomes navigableList

--- tools/badnikUniversity/scripts/pacingBackAndForth.lua
+++ tools/badnikUniversity/scripts/pacingBackAndForth.lua
@@ -1,11 +1,11 @@
-local MOVE        = require("tools/badnikUniversity/scripts/commands/move")
-local WAIT        = require("tools/badnikUniversity/scripts/commands/wait")
-local FLIPX       = require("tools/badnikUniversity/scripts/commands/flipX")
+local MOVE  = require("tools/badnikUniversity/scripts/commands/move")
+local WAIT  = require("tools/badnikUniversity/scripts/commands/wait")
+local FLIPX = require("tools/badnikUniversity/scripts/commands/flipX")
 
 return {
-	name         = "pacingBackAndForth",
-	title        = "Pacing Back and Forth",
-	instructions = {
+	name    = "pacingBackAndForth",
+	title   = "Pacing Back and Forth",
+	program = require("tools/lib/dataStructures/navigableList"):create {
 		MOVE { numSeconds = 3, xSpeed = 100, },
 		WAIT { numSeconds = 1,               },
 		FLIPX(),
