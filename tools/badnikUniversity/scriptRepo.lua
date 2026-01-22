62604ef14ef7644255cd6d307b9efb31bdba6dc0
Matthew Hopkins <matthop88@gmail.com>
2026-01-22 13:36:21 -0800

    Create scriptRepo.lua

---
+++ tools/badnikUniversity/scriptRepo.lua
@@ -0,0 +1,25 @@
+local STRING_UTIL = require("tools/lib/stringUtil")
+
+return ({
+    scriptNames = {},
+
+    init = function(self)
+        local scriptFilenames = love.filesystem.getDirectoryItems("tools/badnikUniversity/scripts")
+        for _, scriptFilename in ipairs(scriptFilenames) do
+            if STRING_UTIL:endsWith(scriptFilename, ".lua") then
+                print(scriptFilename)
+                table.insert(self.scriptNames, string.sub(scriptFilename, 1, string.len(scriptFilename) - 4))
+            end
+        end
+
+        return self
+    end,
+        
+    get = function(self, scriptName)
+        local script = dofile("tools/badnikUniversity/scripts" .. scriptName .. ".lua")
+        script.program = require("tools/lib/dataStructures/navigableList"):create(script.program)
+
+        return script
+    end,
+        
+}):init()
