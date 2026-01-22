eba51a8813d3d8a386ff0d9c135f1ed2b1e2ef78
Matthew Hopkins <matthop88@gmail.com>
2026-01-22 15:02:06 -0800

    Create sineWaveBackAndForth.lua

---
+++ tools/badnikUniversity/scripts/sineWaveBackAndForth.lua
@@ -0,0 +1,12 @@
+local SINE_WAVE  = require("tools/badnikUniversity/scripts/commands/sineWave")
+local WAIT       = require("tools/badnikUniversity/scripts/commands/wait")
+local FLIPX      = require("tools/badnikUniversity/scripts/commands/flipX")
+
+return {
+	name    = "sineWaveBackAndForth",
+	title   = "Sine Wave Back and Forth",
+	program = {
+		SINE_WAVE { numSeconds = 3, speed = 50, amplitude = 50, wavelength = 100 },
+		FLIPX(),
+	},
+}
