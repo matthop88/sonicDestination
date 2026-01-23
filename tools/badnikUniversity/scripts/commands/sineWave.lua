75491a51be9ceb6531abb7cabd20fceadfb8e8d3
Matthew Hopkins <matthop88@gmail.com>
2026-01-22 16:40:17 -0800

    Updated sineWave.lua to fix horizontal motion

--- tools/badnikUniversity/scripts/commands/sineWave.lua
+++ tools/badnikUniversity/scripts/commands/sineWave.lua
@@ -1,9 +1,9 @@
 return function(params)
     return {
         speed      = params.speed      or 1,
+        xSpeed     = params.xSpeed     or 100,
         duration   = params.numSeconds or 1,
         amplitude  = params.amplitude  or 100,
-        wavelength = params.wavelength or 100,
         elapsed    = 0,
         radians    = 0,
 
@@ -13,8 +13,8 @@ return function(params)
 
         execute = function(self, dt, actor)
             self.radians = self.radians + (self.speed * dt)
-            actor:setXSpeed(math.abs(math.cos(self.radians) * self.wavelength))
-            actor:setYSpeed(         math.sin(self.radians) * self.amplitude)
+            actor:setXSpeed(self.xSpeed)
+            actor:setYSpeed(math.sin(self.radians) * self.amplitude * self.speed)
             
             self.elapsed = self.elapsed + dt
             if self.elapsed > self.duration then
