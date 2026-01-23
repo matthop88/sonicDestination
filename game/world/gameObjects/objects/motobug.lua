4f667bd437d9319aca53c4278f8e9993cf6cd778
Matthew Hopkins <matthop88@gmail.com>
2026-01-23 15:38:03 -0800

    Updated motobug.lua to have pacing back and forth script

--- game/world/gameObjects/objects/motobug.lua
+++ game/world/gameObjects/objects/motobug.lua
@@ -1,8 +1,11 @@
 local SOUND_MANAGER  = requireRelative("sound/soundManager")
+local SCRIPT_REPO    = requireRelative("world/badniks/scripts/lib/scriptRepo")
 
 return {
 	create = function(self)
 		return {
+			script   = SCRIPT_REPO:get("pacingBackAndForth"),
+
 			onCollisionWithPlayer = function(self, player)
 				if player:isSpinning() then
 					self:setAnimation("motobugDying")
