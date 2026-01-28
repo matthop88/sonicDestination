2c5d7e677bbf8630b30b5570cfbc9adc41625faf
Matthew Hopkins <matthop88@gmail.com>
2026-01-27 17:31:03 -0800

    Updated badnikGroundSensor.lua; added scanning function

--- tools/badnikUniversity/factories/badnikGroundSensor.lua
+++ tools/badnikUniversity/factories/badnikGroundSensor.lua
@@ -8,6 +8,13 @@ return {
 				GRAFX:setColor(0.5, 1, 0, 0.7)
 				GRAFX:rectangle("fill", x + self.dx - 2, y + self.dy - 2, 4, 4)
 			end,
+
+			scan = function(self, x, y, world)
+				local solid = world:getSolidAt(x + self.dx, y + self.dy)
+				if solid then
+					solid.redAlpha = 1
+				end
+			end,
 		}
 	end,
 }
