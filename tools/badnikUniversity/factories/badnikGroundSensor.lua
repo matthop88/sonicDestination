e150a75f7b0cbc1007f9a59cf52ecb3cd0ca25df
Matthew Hopkins <matthop88@gmail.com>
2026-01-27 17:39:25 -0800

    Updated badnikGroundSensor.lua to account for xFlip

--- tools/badnikUniversity/factories/badnikGroundSensor.lua
+++ tools/badnikUniversity/factories/badnikGroundSensor.lua
@@ -4,13 +4,18 @@ return {
 			dx = dx,
 			dy = dy,
 
-			draw = function(self, GRAFX, x, y)
-				GRAFX:setColor(0.5, 1, 0, 0.7)
-				GRAFX:rectangle("fill", x + self.dx - 2, y + self.dy - 2, 4, 4)
+			getDx = function(self, xFlip)
+				if xFlip then return  self.dx
+				else          return -self.dx end
 			end,
 
-			scan = function(self, x, y, world)
-				local solid = world:getSolidAt(x + self.dx, y + self.dy)
+			draw = function(self, GRAFX, x, y, xFlip)
+				GRAFX:setColor(0.5, 1, 0, 0.7)
+				GRAFX:rectangle("fill", x + self:getDx(xFlip) - 2, y + self.dy - 2, 4, 4)
+			end,
+
+			scan = function(self, x, y, world, xFlip)
+				local solid = world:getSolidAt(x + self:getDx(xFlip), y + self.dy)
 				if solid then
 					solid.redAlpha = 1
 				end
