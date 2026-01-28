ee31bc51a7c533e215aefcbd38f36f6cc966bae1
Matthew Hopkins <matthop88@gmail.com>
2026-01-28 13:14:39 -0800

    Updated motobug.lua to scan ground

--- game/world/gameObjects/objects/motobug.lua
+++ game/world/gameObjects/objects/motobug.lua
@@ -3,6 +3,9 @@ local SCRIPT_REPO    = requireRelative("world/badniks/scripts/lib/scriptRepo")
 
 return {
 	create = function(self)
+		local SENSOR_DX = 10
+		local SENSOR_DY = 20
+
 		return {
 			script   = SCRIPT_REPO:get("pacingBackAndForth"),
 
@@ -13,6 +16,15 @@ return {
         			player:reboundIfPossible(self.y, 180)
 				end
 			end,
+
+			scanGround = function(self)
+				if not self.groundScanner then
+					self.groundScanner = requireRelative("collision/sensors/badniks/groundScanner"):create { OWNER = self, GRAPHICS = self.graphics, WORLD = self.world, dx = SENSOR_DX, dy = SENSOR_DY }
+				end
+
+				return self.groundScanner:scan()
+			end,
+
 		}
 	end,
 }
