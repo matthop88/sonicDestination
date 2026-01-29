da2fa2e783d6f4b5463070425a3b0716ee61636b
Matthew Hopkins <matthop88@gmail.com>
2026-01-29 14:32:35 -0800

    Updated sonicPlugins.lua to allow flashing in debug mode. Removed fade in and fade out from debug mode

--- sonicPlugins.lua
+++ sonicPlugins.lua
@@ -107,8 +107,7 @@ return {
                     {   key = ">", fn = function() self.SONIC:moveTo(self.SONIC:getX() + 1000, self.SONIC:getY()) end, },
                     {   key = "<", fn = function() self.SONIC:moveTo(self.SONIC:getX() - 1000, self.SONIC:getY()) end, },
                     {   key = "#", fn = function() self.SONIC:getWorld():reset()            end, },
-                    {   key = "f", fn = function() self.SONIC:getWorld():fadeOut()          end, },
-                    {   key = "F", fn = function() self.SONIC:getWorld():fadeIn()           end, },
+                    {   key = "f", fn = function() self.SONIC:toggleFlash()                 end, },
                     {   key = "*", fn = function() self.SONIC:getWorld():teleport()         end, },
                     {   key = "V", fn = function() getListVisualizer():toggleActive()       end, },
                     {   key = "P", fn = function() 
