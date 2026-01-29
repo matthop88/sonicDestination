93d4d22d67c015809c014e84b89286db859c83cf
Matthew Hopkins <matthop88@gmail.com>
2026-01-29 14:43:07 -0800

    Updated flashEngine.lua to give account of flashing status

--- game/collision/flashing/flashEngine.lua
+++ game/collision/flashing/flashEngine.lua
@@ -12,11 +12,13 @@ return {
 					self.timer = self.timer + (dt * 60)
 					if self.timer % (self.frameCount * 2) < self.frameCount then self.visible = true
 					else                                                         self.visible = false end
+				else
+					self.visible = true
 				end
 			end,
 
-			isVisible = function(self) return self.visible end,
-
+			isVisible   = function(self) return self.visible           end,
+			isFlashing  = function(self) return self.active            end,
 			toggleFlash = function(self) self.active = not self.active end,
 		}
 	end,
