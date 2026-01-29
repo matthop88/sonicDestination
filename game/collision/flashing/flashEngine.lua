0b50c258aa2f62e24a5fb49a9fc18e92a2d9ece7
Matthew Hopkins <matthop88@gmail.com>
2026-01-29 14:31:00 -0800

    Create flashEngine.lua

---
+++ game/collision/flashing/flashEngine.lua
@@ -0,0 +1,23 @@
+return {
+	create = function(self, params)
+		return {
+			frameCount = params.frameCount or 4,
+
+			active  = false,
+			timer   = 0,
+			visible = true,
+
+			update = function(self, dt)
+				if self.active then
+					self.timer = self.timer + (dt * 60)
+					if self.timer % (self.frameCount * 2) < self.frameCount then self.visible = true
+					else                                                         self.visible = false end
+				end
+			end,
+
+			isVisible = function(self) return self.visible end,
+
+			toggleFlash = function(self) self.active = not self.active end,
+		}
+	end,
+}
