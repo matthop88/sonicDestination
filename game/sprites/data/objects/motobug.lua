2a4c2bceb813affa3766be4c200cbd3d166b41ba
Matthew Hopkins <matthop88@gmail.com>
2026-01-29 15:13:27 -0800

    Updated motobug.lua sprite; added danger of 1

--- game/sprites/data/objects/motobug.lua
+++ game/sprites/data/objects/motobug.lua
@@ -2,7 +2,7 @@ return {
     imageName = "sonic1BadniksTransparent",
     animations  = {
         motobugRoving = { isDefault = true, offset = { x = 20, y = 14 }, w = 40, h = 28,
-            hitBox = { rX = 15, rY = 10 },
+            hitBox = { rX = 15, rY = 10, danger = 1 },
             ----------------------------------------------------------------------------
             parts = {
                 {   name = "motobugBody", animation = "roving",  },
