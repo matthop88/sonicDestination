de112ccd7de75992842b3c5ed48fce34f697e9ec
Matthew Hopkins <matthop88@gmail.com>
2026-01-30 15:03:21 -0800

    Created simpleSound.lua, which improves the mechanics of the old soundManager

---
+++ game/sound/simpleSound.lua
@@ -0,0 +1,32 @@
+return {
+	create = function(self, params)
+		return {
+			filename   = params.filename,
+			volume     = params.volume or 1,
+			startPoint = params.startPoint,
+            
+            tracks     = { nil, nil, nil },
+           	trackIndex = 1,
+
+            load = function(self)
+                if self:getSound() == nil then self:setSound(love.audio.newSource(relativePath("resources/sounds/") .. self.filename, "static"))
+                else                      love.audio.stop(self:getSound())                                                                   end
+            end,
+
+            play = function(self)
+                self:load()
+                self:getSound():setVolume(self.volume)
+                self:getSound():play()
+                if self.startPoint then self:getSound():seek(self.startPoint, "samples") end
+                self:next()
+            end,
+
+            getSound = function(self) return self.tracks[self.trackIndex]         end,
+            setSound = function(self, sound) self.tracks[self.trackIndex] = sound end,
+            next     = function(self)
+                self.trackIndex = self.trackIndex + 1
+                if self.trackIndex > #self.tracks then self.trackIndex = 1 end
+            end,
+        }
+    end,
+}
