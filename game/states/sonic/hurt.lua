81849fa1866011fd77a44dd177c5cf306f65d184
Matthew Hopkins <matthop88@gmail.com>
2026-01-29 17:14:41 -0800

    Create hurt.lua (new state)

---
+++ game/states/sonic/hurt.lua
@@ -0,0 +1,24 @@
+local SONIC, STATES
+
+return {
+    init = function(self, params)
+        SONIC  = params.SONIC
+        STATES = params.STATES
+        return self
+    end,
+
+    onEnter    = function(self) 
+        SONIC.velocity.y = -240
+        if SONIC:isFacingRight() then SONIC.velocity.x = -120
+        else                          SONIC.velocity.x =  120 end
+    end,
+
+    update     = function(self, dt)
+        if SONIC:isGrounded() then SONIC:setStanding() end
+    end,
+    
+    keypressed = function(self, key)
+        -- When hurt, no key press works
+    end,
+}
+    
