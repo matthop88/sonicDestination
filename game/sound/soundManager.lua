8ff4c43d3e8ca9ac3e846957a4e069d7c05201c0
Matthew Hopkins <matthop88@gmail.com>
2026-01-30 14:43:10 -0800

    Updated soundManager.lua to be savvy towards ice explode

--- game/sound/soundManager.lua
+++ game/sound/soundManager.lua
@@ -14,15 +14,16 @@ return ({
     setOverride    = function(self, key, value) self.overrides[key] = value end,
     
     data = {
-        sonicBraking   = { filename = "brake.ogg",          volume     = 0.2, },
-        sonicJumping   = { filename = "jump.ogg",           volume     = 0.2, },
-        sonicCDJumping = { filename = "sonicCDJump.mp3",    volume     = 0.9,  startPoint = 19520, },
-        ringCollectL   = { filename = "ring-collect-L.mp3", volume     = 0.4, },
-        ringCollectR   = { filename = "ring-collect-R.mp3", volume     = 0.4, },
-        giantRing      = { filename = "giantRing.mp3",      volume     = 0.5,  startPoint =  4352, },
-        vanish         = { filename = "vanish.mp3",         volume     = 0.5,  startPoint =  3968, },
-        badnikDeath    = { filename = "badnik-death.ogg",   volume     = 0.5, },
-        sonicHit       = { filename = "sonicHit.ogg",       volime     = 0.8, },
+        sonicBraking   = { filename = "brake.ogg",             volume     = 0.2, },
+        sonicJumping   = { filename = "jump.ogg",              volume     = 0.2, },
+        sonicCDJumping = { filename = "sonicCDJump.mp3",       volume     = 0.9,  startPoint = 19520, },
+        ringCollectL   = { filename = "ring-collect-L.mp3",    volume     = 0.4, },
+        ringCollectR   = { filename = "ring-collect-R.mp3",    volume     = 0.4, },
+        giantRing      = { filename = "giantRing.mp3",         volume     = 0.5,  startPoint =  4352, },
+        vanish         = { filename = "vanish.mp3",            volume     = 0.5,  startPoint =  3968, },
+        badnikDeath    = { filename = "badnik-death.ogg",      volume     = 0.5, },
+        sonicHit       = { filename = "sonicHit.ogg",          volume     = 0.8, },
+        iceExplode     = { filename = "extra/IceExplode1.wav", volume     = 0.8, },
     },
 
     initSoundData = function(self)
