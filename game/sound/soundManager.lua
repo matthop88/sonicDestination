40d11cef3a906a07bf4eb9429ff8ddd21638451e
Matthew Hopkins <matthop88@gmail.com>
2026-01-30 16:17:52 -0800

    Updated soundManager.lua to have a queue of delayed sounds

--- game/sound/soundManager.lua
+++ game/sound/soundManager.lua
@@ -1,23 +1,41 @@
 return ({
-    sounds    = {},
-    overrides = {},
+    sounds       = {},
+    overrides    = {},
+    queuedSounds = requireRelative("util/dataStructures/linkedList"):create(),
+
+    setOverride = function(self, key, value) self.overrides[key] = value end,
 
     init = function(self)
         self:initSoundData()
         return self
     end,
 
-    play = function(self, soundName)
-        if self.overrides[soundName] then soundName = self.overrides[soundName] end
-        self.sounds[soundName]:play()
-    end,
-
-    setOverride    = function(self, key, value) self.overrides[key] = value end,
-
     initSoundData = function(self)
         for name, element in pairs(requireRelative("sound/soundData")) do
             self.sounds[name] = requireRelative("sound/simpleSound"):create(element)
         end
+    end,
+
+    play = function(self, soundName)
+        if self.overrides[soundName] then soundName = self.overrides[soundName] end
+        local sound = self.sounds[soundName]
+        if sound.delay then self:addToQueue(sound)
+        else                sound:play()       end
+    end,
+
+    addToQueue = function(self, sound)
+        self.queuedSounds:add({ timer = sound.delay, sound = sound })
+    end,
+
+    update = function(self, dt)
+        self.queuedSounds:forEach(function(delayedSound)
+            delayedSound.timer = delayedSound.timer - dt
+            if delayedSound.timer <= 0 then
+                delayedSound.sound:play()
+                self.queuedSounds:remove()
+                return true
+            end
+        end)
     end,
         
 }):init()
