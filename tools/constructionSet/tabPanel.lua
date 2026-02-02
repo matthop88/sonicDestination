2b0c956bc9ffb83307457e598c69aa96abc55ab2
Matthew Hopkins <matthop88@gmail.com>
2026-02-02 13:50:35 -0800

    Updated tabPanel.lua to calculate tab data separately

--- tools/constructionSet/tabPanel.lua
+++ tools/constructionSet/tabPanel.lua
@@ -1,13 +1,27 @@
+local calculateTabData = function(tabs, attrs)
+	local tabsWithMeta = {}
+	local x = 100 - attrs.TAB_MARGIN
+	for _, t in ipairs(tabs) do
+		local w = attrs.FONT:getWidth(t) + (attrs.TAB_MARGIN * 2)
+		local tabWithMeta = { title = t, x = x, y = 470, w = w, h = 30 }
+		table.insert(tabsWithMeta, tabWithMeta)
+		x = x + w + attrs.TAB_SPACING
+	end
+
+	return tabsWithMeta
+end
+
+
 return {
 	create = function(self, params)
 		local FONT_SIZE      = 24
 		local FONT           = love.graphics.newFont(FONT_SIZE)
 
 		local TAB_MARGIN  = params.TAB_MARGIN  or 15
-		local TAB_SPACING = params.TAB_SPACING or 40
+		local TAB_SPACING = params.TAB_SPACING or 25
 
 		return {
-			TABS      = params.TABS or { "Sample", "TabPanel", "Tabs" },
+			TABS      = calculateTabData(params.TABS, { TAB_MARGIN = TAB_MARGIN, TAB_SPACING = TAB_SPACING, FONT = FONT }),
 			TAB_INDEX = 1,
 
 			draw = function(self)
@@ -15,25 +29,22 @@ return {
 			    love.graphics.line(  5,  795, 1195, 795)
 			    love.graphics.line(1195, 795, 1195, 500)
 			    love.graphics.line(  5,  500,    5, 795)
-			    love.graphics.line(  5,  500,  100 - TAB_MARGIN, 500)
+			    love.graphics.line(  5,  500,  self.TABS[1].x, 500)
 
 			    love.graphics.setFont(FONT)
-			    local x = 100
 			    local index = 1
+			    local x = 0
 			    for _, t in ipairs(self.TABS) do
-			        local w = FONT:getWidth(t)
-			        love.graphics.line(x - TAB_MARGIN,     470, x + w + TAB_MARGIN, 470)
-			        love.graphics.line(x - TAB_MARGIN,     470, x - TAB_MARGIN,     499)
-			        love.graphics.line(x + w + TAB_MARGIN, 470, x + w + TAB_MARGIN, 499)
-			        if index ~= self.TAB_INDEX then
-			            love.graphics.line(x - TAB_MARGIN + 1, 500, x + w + TAB_MARGIN - 1, 500)
-			        end
-			        love.graphics.line(x + w + TAB_MARGIN, 500, x + w + TAB_SPACING - TAB_MARGIN, 500)
+			    	love.graphics.line(t.x,       t.y, t.x + t.w, t.y)
+			        love.graphics.line(t.x,       t.y, t.x,       t.y + t.h - 1)
+			        love.graphics.line(t.x + t.w, t.y, t.x + t.w, t.y + t.h - 1)
+			        if index ~= self.TAB_INDEX then love.graphics.line(t.x + 1, 500, t.x + t.w - 1, 500) end
+			        love.graphics.line(t.x + t.w, 500, t.x + t.w + TAB_SPACING, 500)
+			        love.graphics.printf(t.title, t.x + TAB_MARGIN, 470, 500, "left")
+			        x = t.x + t.w + TAB_SPACING
 			        index = index + 1
-			        love.graphics.printf(t, x, 470, 500, "left")
-			        x = x + w + TAB_SPACING
 			    end
-			    love.graphics.line(x - TAB_MARGIN + 1, 500, 1195, 500)
+			    love.graphics.line(x + 1, 500, 1195, 500)
 			end,
 
 			next = function(self)
