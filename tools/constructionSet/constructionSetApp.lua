29eb7238987c95a9a3a7418c7185b4a334a249ef
Matthew Hopkins <matthop88@gmail.com>
2026-02-02 13:21:36 -0800

    Updated constructionSetApp.lua to rely upon tabPanel

--- tools/constructionSet/constructionSetApp.lua
+++ tools/constructionSet/constructionSetApp.lua
@@ -3,12 +3,7 @@
 --------------------------------------------------------------
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
-local FONT_SIZE      = 24
-local FONT           = love.graphics.newFont(FONT_SIZE)
-local TABS           = { "Chunks", "Badniks", "Items" }
-local TAB_INDEX      = 1
-local TAB_MARGIN     = 15
-local TAB_SPACING    = 40
+local TAB_PANEL = require("tools/constructionSet/tabPanel"):create { TABS = { "Chunks", "Badniks", "Items" } }
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -22,34 +17,12 @@ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { display = 2 })
 --------------------------------------------------------------
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.line(  5,  795, 1195, 795)
-    love.graphics.line(1195, 795, 1195, 500)
-    love.graphics.line(  5,  500,    5, 795)
-    love.graphics.line(  5,  500,  100 - TAB_MARGIN, 500)
-
-    love.graphics.setFont(FONT)
-    local x = 100
-    local index = 1
-    for _, t in ipairs(TABS) do
-        local w = FONT:getWidth(t)
-        love.graphics.line(x - TAB_MARGIN,     470, x + w + TAB_MARGIN, 470)
-        love.graphics.line(x - TAB_MARGIN,     470, x - TAB_MARGIN,     499)
-        love.graphics.line(x + w + TAB_MARGIN, 470, x + w + TAB_MARGIN, 499)
-        if index ~= TAB_INDEX then
-            love.graphics.line(x - TAB_MARGIN + 1, 500, x + w + TAB_MARGIN - 1, 500)
-        end
-        love.graphics.line(x + w + TAB_MARGIN, 500, x + w + TAB_SPACING - TAB_MARGIN, 500)
-        index = index + 1
-        love.graphics.printf(t, x, 470, 500, "left")
-        x = x + w + TAB_SPACING
-    end
-    love.graphics.line(x - TAB_MARGIN + 1, 500, 1195, 500)
+    TAB_PANEL:draw()
 end
 
 function love.keypressed(key)
-    TAB_INDEX = TAB_INDEX + 1
-    if TAB_INDEX > #TABS then TAB_INDEX = 1 end
+    if     key == "tab"      then TAB_PANEL:next()
+    elseif key == "shifttab" then TAB_PANEL:prev() end
 end
 
 -- ...
@@ -59,8 +32,7 @@ end
 --------------------------------------------------------------
 
 PLUGINS = require("plugins/engine")
---        ...
+    :add("modKeyEnabler")
 --        ...
 --        ...
 
-    
