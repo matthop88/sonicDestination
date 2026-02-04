55ae150257f07cf2d0c2c4f907662449b1bd25c0
Matthew Hopkins <matthop88@gmail.com>
2026-02-04 12:49:31 -0800

    Updated chunksPanel.lua to set up containers and iterate through them

--- tools/constructionSet/chunksPanel.lua
+++ tools/constructionSet/chunksPanel.lua
@@ -1,11 +1,36 @@
+local CONTAINER = {
+    create = function(self, x, y, w, h)
+        return {
+            x = x,
+            y = y,
+            w = w,
+            h = h,
+
+            draw = function(self, graphics)
+                graphics:setColor(1, 1, 1)
+                graphics:setLineWidth(1)
+                graphics:rectangle("line", self.x - 1, self.y - 1, self.w + 2, self.h + 2)
+            end,
+        }
+    end,
+}
+
 return {
-	draw = function(self, graphics)
-        graphics:setColor(1, 1, 1)
-        graphics:setLineWidth(1)
+    create = function(self)
+        local containers = {}
+
         for y = 16, 290, 143 do
             for x = 31, 1080, 143 do
-                graphics:rectangle("line", x - 1, y - 1, 130, 130)
+                table.insert(containers, CONTAINER:create(x, y, 128, 128))
             end
         end
+
+        return {
+            draw = function(self, graphics)
+                for _, c in ipairs(containers) do
+                    c:draw(graphics)
+                end
+            end,
+        }
     end,
 }
