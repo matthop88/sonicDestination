8a6e8f345a94bfa06b31adcbab6c92b00b76c0fb
Matthew Hopkins <matthop88@gmail.com>
2026-02-04 14:45:38 -0800

    Added more complexity to tabbedPane (mainly, the external function)

--- plugins/modules/tabbedPane/tabbedPane.lua
+++ plugins/modules/tabbedPane/tabbedPane.lua
@@ -1,13 +1,38 @@
+local COLOR = require("tools/lib/colors")
+
+local calculateTabData = function(tabs, attrs)
+	local tabsWithMeta = {}
+	local x = 0
+	for _, tab in ipairs(tabs) do
+		local t = tab.label
+		local w = attrs.FONT:getWidth(t) + (attrs.TAB_MARGIN * 2)
+		local tabWithMeta = { label = t, x = x, w = w, panel = tab.panel }
+		table.insert(tabsWithMeta, tabWithMeta)
+		x = x + w + attrs.TAB_SPACING
+	end
+	local xOffsetToCenter = attrs.WIDTH - (x / 2)
+	for _, t in ipairs(tabsWithMeta) do t.x = t.x + xOffsetToCenter end
+
+	tabsWithMeta.y = require("tools/lib/tweenableValue"):create(attrs.HEIGHT - 40, { speed = 12 })
+	tabsWithMeta.h = 30
+	tabsWithMeta.opened = false
+	return tabsWithMeta
+end
+
 return {
 	init = function(self, params)
+		local FONT_SIZE     = 24
+		self.FONT           = love.graphics.newFont(FONT_SIZE)
+
+		self.WIDTH          = params.WIDTH       or 1200
+		self.HEIGHT         = params.HEIGHT      or 800
 		self.PANE_HEIGHT    = params.PANE_HEIGHT or 300
-		self.FONT_SIZE      = 24
-		self.WIDTH          = params.WIDTH or 1190
-		self.FONT           = love.graphics.newFont(self.FONT_SIZE)
-		self.GRAFX          = require("tools/lib/graphics"):create()
-		self.BUFFERED_GRAFX = require("tools/lib/bufferedGraphics"):create(self.GRAFX, self.WIDTH, self.PANE_HEIGHT)
+		
 		self.TAB_MARGIN     = params.TAB_MARGIN  or 20
 		self.TAB_SPACING    = params.TAB_SPACING or 15
+		
+		local GRAFX         = require("tools/lib/graphics"):create()
+		self.BUFFERED_GRAFX = require("tools/lib/bufferedGraphics"):create(GRAFX, self.WIDTH, self.PANE_HEIGHT)
 		
 		return self
     end,
