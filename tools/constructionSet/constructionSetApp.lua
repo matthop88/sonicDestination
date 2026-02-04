980c8bc7aa806edf51e9cc2a56794f88c41ea762
Matthew Hopkins <matthop88@gmail.com>
2026-02-04 11:07:20 -0800

    Extracted chunksPanel from construction set app

--- tools/constructionSet/constructionSetApp.lua
+++ tools/constructionSet/constructionSetApp.lua
@@ -4,17 +4,7 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
-local CHUNKS_PANEL = {
-    draw = function(self, graphics)
-        graphics:setColor(1, 1, 1)
-        graphics:setLineWidth(1)
-        for y = 16, 290, 143 do
-            for x = 31, 1080, 143 do
-                graphics:rectangle("line", x - 1, y - 1, 130, 130)
-            end
-        end
-    end,
-}
+local CHUNKS_PANEL = require("tools/constructionSet/chunksPanel")
 
 local TAB_PANEL = require("tools/constructionSet/tabPanel"):create 
 { 
