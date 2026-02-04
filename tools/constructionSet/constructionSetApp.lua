7148bea5ee1f27fef3d19701aab125d0d1f7dd58
Matthew Hopkins <matthop88@gmail.com>
2026-02-04 10:22:07 -0800

    Updated constructionSetApp.lua so that chunks panel shows 2 rows of 8 containers

--- tools/constructionSet/constructionSetApp.lua
+++ tools/constructionSet/constructionSetApp.lua
@@ -3,10 +3,23 @@
 --------------------------------------------------------------
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
+
+local CHUNKS_PANEL = {
+    draw = function(self, graphics)
+        graphics:setColor(1, 1, 1)
+        graphics:setLineWidth(1)
+        for y = 16, 290, 143 do
+            for x = 31, 1080, 143 do
+                graphics:rectangle("line", x - 1, y - 1, 130, 130)
+            end
+        end
+    end,
+}
+
 local TAB_PANEL = require("tools/constructionSet/tabPanel"):create 
 { 
     TABS = { 
-             { label = "Chunks",  panel = nil, },
+             { label = "Chunks",  panel = CHUNKS_PANEL, },
              { label = "Badniks", panel = nil, },
              { label = "Items",   panel = nil, },
     }
