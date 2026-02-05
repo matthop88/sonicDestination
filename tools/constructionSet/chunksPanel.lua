c32d013c3eb5e255fe7e57eb61802c61be7c51e9
Matthew Hopkins <matthop88@gmail.com>
2026-02-05 10:19:12 -0800

    Updated chunksPanel.lua to refer to CONTAINER externally

--- tools/constructionSet/chunksPanel.lua
+++ tools/constructionSet/chunksPanel.lua
@@ -1,4 +1,6 @@
-local COLOR = require("tools/lib/colors")
+local COLOR     = require("tools/lib/colors")
+
+local CONTAINER = require("tools/constructionSet/container")
 
 local CHUNKS
 
@@ -22,57 +24,6 @@ local CHUNK = {
     end,
 }
         
-local CONTAINER = {
-    create = function(self, object, x, y, w, h)
-        return {
-            object     = object,
-            x          = x,
-            y          = y,
-            w          = w,
-            h          = h,
-            hasFocus   = false,
-            isSelected = false,
-            
-
-            draw = function(self, graphics)
-                object:draw(graphics, self.x, self.y, self.w, self.h)
-                graphics:setColor(self:getOverlayColor())
-                graphics:rectangle("fill", self.x, self.y, self.w, self.h)
-                graphics:setColor(self:getOutlineColor())
-                graphics:setLineWidth(self:getOutlineWidth())
-                graphics:rectangle("line", self.x - 1, self.y - 1, self.w + 2, self.h + 2)
-                graphics:setLineWidth(1)
-            end,
-
-            isInside = function(self, x, y)
-                return x >= self.x and x <= self.x + self.w and y >= self.y and y <= self.y + self.h
-            end,
-
-            gainFocus = function(self) self.hasFocus = true  end,
-            loseFocus = function(self) self.hasFocus = false end,
-
-            getOutlineColor = function(self)
-                if     self.isSelected then return COLOR.PURE_WHITE
-                elseif self.hasFocus   then return COLOR.LIGHT_YELLOW
-                else                        return COLOR.LIGHT_GREY end
-            end,
-
-            getOverlayColor = function(self)
-                if     self.isSelected then return { 0, 0, 0, 0   }
-                elseif self.hasFocus   then return { 1, 1, 0, 0.5 }
-                else                        return { 0, 0, 0, 0.5 } end
-            end,
-
-            getOutlineWidth = function(self)
-                if self.hasFocus or self.isSelected then return 3
-                else                                     return 1 end
-            end,
-
-            select   = function(self) self.isSelected = true  end,
-            unselect = function(self) self.isSelected = false end,
-        }
-    end,
-}
 
 return {
     create = function(self)
