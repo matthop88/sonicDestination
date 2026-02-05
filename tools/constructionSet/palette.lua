885226ce8d7bc85a802817732bd141c81ad3f76f
Matthew Hopkins <matthop88@gmail.com>
2026-02-05 10:45:45 -0800

    Create palette.lua

---
+++ tools/constructionSet/palette.lua
@@ -0,0 +1,49 @@
+local COLOR     = require("tools/lib/colors")
+local CONTAINER = require("tools/constructionSet/container")
+
+return {
+    create = function(self, params)
+        local LEFTMOST = 31
+        local TOPMOST  = 16
+
+        local containers = {}
+
+        return ({
+            init = function(self, objects)
+                local x, y = LEFTMOST, TOPMOST
+                for _, obj in ipairs(objects) do
+                    table.insert(containers, CONTAINER:create(obj, x, y, 128, 128))
+                    x = x + 143
+                    if x > 1080 then
+                        x = LEFTMOST
+                        y = y + 143
+                    end
+                end
+
+                return self
+            end,
+
+            draw = function(self, graphics)
+                graphics:clear(COLOR.DARK_GREY)
+                for _, c in ipairs(containers) do
+                    c:draw(graphics)
+                end
+            end,
+
+            update = function(self, dt, mx, my)
+                for _, c in ipairs(containers) do
+                    if c:isInside(mx, my) then c:gainFocus()
+                    else                       c:loseFocus() end
+                end
+            end,
+
+            handleMousepressed = function(self, mx, my)
+                for _, c in ipairs(containers) do
+                    if c:isInside(mx, my) then c:select()
+                    else                       c:unselect() end
+                end
+            end,
+        }):init(params.objects)
+    end,
+}
+ 
