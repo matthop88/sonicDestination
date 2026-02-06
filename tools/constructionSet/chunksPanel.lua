cbb64e4a69a144729516b307b5a0d921c4aa5be9
Matthew Hopkins <matthop88@gmail.com>
2026-02-06 14:05:19 -0800

    Updated chunksPanel.lua to use a chunk template

--- tools/constructionSet/chunksPanel.lua
+++ tools/constructionSet/chunksPanel.lua
@@ -7,41 +7,59 @@ local CHUNK = {
     create = function(self, chunkID)
         return {
             chunkID    = chunkID,
+            
+            draw = function(self, graphics, x, y, w, h)
+                if CHUNKS then
+                    graphics:setColor(COLOR.PURE_WHITE)
+                    local sX, sY = 1, 1
+                    if w and h then sX, sY = w / 256, h / 256 end
+                    CHUNKS:drawAt(graphics, x, y, self.chunkID, sX, sY) 
+                end
+            end,  
+        }
+    end,
+}
+
+-- A CHUNK_TEMPLATE is what solely resides in the container. It is capable of creating chunk objects, which can be placed on the map.
+
+local CHUNK_TEMPLATE = {
+    create = function(self, chunkID)
+        local coreChunk = CHUNK:create(chunkID)
+
+        return {
             hasFocus   = false,
             isSelected = false,
+            chunkID    = chunkID
 
             drawInContainer = function(self, graphics, x, y, w, h)
-                if CHUNKS then 
-                    graphics:setColor(COLOR.PURE_WHITE)
-                    CHUNKS:drawAt(graphics, x - (w / 2), y - (h / 2), self.chunkID, w / 256, h / 256) 
-                end
+                x, y = x - (w / 2), y - (h / 2)
+                coreChunk:draw(graphics, x, y, w, h)
 
                 if self.hasFocus and not self.isSelected then
                     graphics:setColor(1, 1, 0, 0.5)
-                    graphics:rectangle("fill", x - (w / 2), y - (h / 2), w, h)
+                    graphics:rectangle("fill", x, y, w, h)
                 end
             end,  
-
-            draw = function(self, graphics, x, y)
-                if CHUNKS then
-                    graphics:setColor(COLOR.PURE_WHITE)
-                    CHUNKS:drawAt(graphics, x, y, self.chunkID, 1, 1)
-                end
-            end,
 
             gainFocus = function(self) self.hasFocus = true    end,
             loseFocus = function(self) self.hasFocus = false   end, 
 
             select    = function(self) self.isSelected = true  end,
             unselect  = function(self) self.isSelected = false end,
+
+            newObject = function(self)
+                return CHUNK:create(self.chunkID)
+            end,
         }
     end,
 }
+
+
         
 return {
     create = function(self)
         local chunkList = {}
-        for id = 2, 17 do table.insert(chunkList, CHUNK:create(id)) end
+        for id = 2, 17 do table.insert(chunkList, CHUNK_TEMPLATE:create(id)) end
 
         local palette   = require("tools/constructionSet/palette"):create { objects = chunkList }
 
