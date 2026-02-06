aa0b50466ea246a0568a28db71b8dfbf6c2bb53e
Matthew Hopkins <matthop88@gmail.com>
2026-02-06 15:04:29 -0800

    Updated itemsPanel.lua to use item templates

--- tools/constructionSet/itemsPanel.lua
+++ tools/constructionSet/itemsPanel.lua
@@ -6,29 +6,50 @@ local ITEMS
 local ITEM = {
     create = function(self, name, spritePath)
         return {
-            name   = name,
-            sprite = require("tools/lib/sprites/sprite"):create(spritePath, 0, 0),
+            name       = name,
+            sprite     = require("tools/lib/sprites/sprite"):create(spritePath, 0, 0),
+            
+            draw = function(self, graphics, x, y, w, h)
+                local scale = 1
+                if w and h then scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH())) end
+                
+                graphics:setColor(COLOR.PURE_WHITE)
+                self.sprite:drawAt(graphics, x, y, scale, scale)
+            end,
+
+            drawThumbnail = function(self, graphics, x, y, w, h)
+                local scale = 1
+                if w and h then scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH())) end
+                
+                graphics:setColor(COLOR.PURE_WHITE)
+                self.sprite:drawThumbnail(graphics, x, y, scale, scale)
+            end,  
+
+            update = function(self, dt)
+                self.sprite:update(dt)
+            end,
+        }
+    end,
+}
+
+local ITEM_TEMPLATE = {
+    create = function(self, name, spritePath)
+        local coreItem = ITEM:create(name, spritePath)
+
+        return {
+            name       = name,
+            spritePath = spritePath,
             hasFocus   = false,
             isSelected = false,
             
             drawInContainer = function(self, graphics, x, y, w, h)
-                local scale = 1
-                if w and h then 
-                    scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH()))
-                end
-                
-                graphics:setColor(COLOR.PURE_WHITE)
-                if self.hasFocus or self.isSelected then
-                    self.sprite:drawAt(graphics, x, y, scale, scale)
-                else
-                    self.sprite:drawThumbnail(graphics, x, y, scale, scale)
-                end
-                
+                if self.isSelected or self.hasFocus then coreItem:draw(graphics, x, y, w, h)
+                else                                     coreItem:drawThumbnail(graphics, x, y, w, h) end 
             end,  
 
             updateInContainer = function(self, dt)
                 if self.hasFocus or self.isSelected then
-                    self.sprite:update(dt)
+                    coreItem:update(dt)
                 end
             end,
 
@@ -38,6 +59,7 @@ local ITEM = {
             select    = function(self) self.isSelected = true  end,
             unselect  = function(self) self.isSelected = false end,
       
+            newObject = function(self) return ITEM:create(self.name, self.spritePath) end,
         }
     end,
 }
@@ -45,8 +67,8 @@ local ITEM = {
 return {
     create = function(self)
         local itemList = {}
-        table.insert(itemList, ITEM:create("ring", "objects/ring"))
-        table.insert(itemList, ITEM:create("giantRing", "objects/giantRing"))
+        table.insert(itemList, ITEM_TEMPLATE:create("ring", "objects/ring"))
+        table.insert(itemList, ITEM_TEMPLATE:create("giantRing", "objects/giantRing"))
 
         local palette   = require("tools/constructionSet/palette"):create { objects = itemList, CONTAINER_WIDTH = 64, CONTAINER_HEIGHT = 64 }
 
