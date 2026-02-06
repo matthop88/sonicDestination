5bed6f5c0fb76f12ee27db733430acb9b65f4bc8
Matthew Hopkins <matthop88@gmail.com>
2026-02-06 15:16:09 -0800

    Removed dimension calculation from draw method of items, put in init function

--- tools/constructionSet/itemsPanel.lua
+++ tools/constructionSet/itemsPanel.lua
@@ -4,37 +4,39 @@ local ITEMS
 -- An ITEM is an object. It contains a sprite, and can draw itself into a space of optionally specified dimensions.
 
 local ITEM = {
-    create = function(self, name, spritePath)
-        return {
-            name       = name,
-            sprite     = require("tools/lib/sprites/sprite"):create(spritePath, 0, 0),
+    create = function(self, name, spritePath, containerWidth, containerHeight)
+        return ({
+            init = function(self, name, spritePath, containerWidth, containerHeight)
+                self.name   = name
+                self.sprite = require("tools/lib/sprites/sprite"):create(spritePath, 0, 0)
+                self.scale  = 1
+                if containerWidth and containerHeight then
+                    self.scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH()))
+                end
+
+                return self
+            end,
             
             draw = function(self, graphics, x, y, w, h)
-                local scale = 1
-                if w and h then scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH())) end
-                
                 graphics:setColor(COLOR.PURE_WHITE)
-                self.sprite:drawAt(graphics, x, y, scale, scale)
+                self.sprite:drawAt(graphics, x, y, self.scale, self.scale)
             end,
 
             drawThumbnail = function(self, graphics, x, y, w, h)
-                local scale = 1
-                if w and h then scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH())) end
-                
                 graphics:setColor(COLOR.PURE_WHITE)
-                self.sprite:drawThumbnail(graphics, x, y, scale, scale)
+                self.sprite:drawThumbnail(graphics, x, y, self.scale, self.scale)
             end,  
 
             update = function(self, dt)
                 self.sprite:update(dt)
             end,
-        }
+        }):init(name, spritePath, containerWidth, containerHeight)
     end,
 }
 
 local ITEM_TEMPLATE = {
-    create = function(self, name, spritePath)
-        local coreItem = ITEM:create(name, spritePath)
+    create = function(self, name, spritePath, containerWidth, containerHeight)
+        local coreItem = ITEM:create(name, spritePath, containerWidth, containerHeight)
 
         return {
             name       = name,
@@ -67,10 +69,12 @@ local ITEM_TEMPLATE = {
 return {
     create = function(self)
         local itemList = {}
-        table.insert(itemList, ITEM_TEMPLATE:create("ring", "objects/ring"))
-        table.insert(itemList, ITEM_TEMPLATE:create("giantRing", "objects/giantRing"))
+        local WIDTH, HEIGHT = 64, 64
 
-        local palette   = require("tools/constructionSet/palette"):create { objects = itemList, CONTAINER_WIDTH = 64, CONTAINER_HEIGHT = 64 }
+        table.insert(itemList, ITEM_TEMPLATE:create("ring", "objects/ring", WIDTH, HEIGHT))
+        table.insert(itemList, ITEM_TEMPLATE:create("giantRing", "objects/giantRing", WIDTH, HEIGHT))
+
+        local palette   = require("tools/constructionSet/palette"):create { objects = itemList, CONTAINER_WIDTH = WIDTH, CONTAINER_HEIGHT = HEIGHT }
 
         return {
             draw               = function(self, graphics)   palette:draw(graphics)             end,
