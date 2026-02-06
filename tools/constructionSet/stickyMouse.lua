0d9ba491bc0ced6f8d11910edf00ab97d488db46
Matthew Hopkins <matthop88@gmail.com>
2026-02-06 15:34:19 -0800

    Create stickyMouse.lua

---
+++ tools/constructionSet/stickyMouse.lua
@@ -0,0 +1,29 @@
+return {
+	create = function(self)
+		return {
+			onSelect = function(self, container)
+				if self.selected ~= container then
+					self.selected = container
+					self.object   = container:newObject()
+				end
+			end,
+
+			onDeselect = function(self, container)
+				if self.selected == container then
+					self.selected = nil
+					self.object   = nil
+				end
+			end,
+
+			draw = function(self, graphics, mx, my)
+				if self.object then
+					love.mouse.setVisible(false)
+					self.object:draw(graphics, mx, my)
+				else
+					love.mouse.setVisible(true)
+				end
+			end,
+		}
+	end,
+
+}
