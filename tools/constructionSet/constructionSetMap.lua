1b15206fe1588bdaddd9f4db278d4a02e52de632
Matthew Hopkins <matthop88@gmail.com>
2026-02-11 11:53:37 -0800

    Updated constructionSetMap.lua; added draw method to draw sonic 1 blocks (GHZ only so far)

--- tools/constructionSet/constructionSetMap.lua
+++ tools/constructionSet/constructionSetMap.lua
@@ -3,6 +3,21 @@ return {
 		return {
 			graphics = params.graphics,
 
+			draw = function(self)
+				self:drawSonic1Blocks()
+			end,
+
+			drawSonic1Blocks = function(self)
+				-- Green Hill Zone
+				self.graphics:setColor(1, 0.5, 0.5)
+				self.graphics:rectangle("fill", 0, 0, (40 * 256), (5 * 256))
+
+				self.graphics:rectangle("fill", (41 * 256), 0, (33 * 256), (6 * 256))
+
+				self.graphics:rectangle("fill", (75 * 256), 0, (45 * 256), (6 * 256))
+			end,
+
+
 			---------------------- Graphics Object Methods ------------------------
 
 		    moveImage = function(self, deltaX, deltaY)
