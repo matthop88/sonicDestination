63c4f56db43db04b494d9ae3d231dfc8f42a5447
Matthew Hopkins <matthop88@gmail.com>
2026-02-12 12:20:15 -0800

    Updated constructionSetMap.lua; all Sonic & Knuckles zones now fit as well

--- tools/constructionSet/constructionSetMap.lua
+++ tools/constructionSet/constructionSetMap.lua
@@ -10,12 +10,13 @@ return {
 				self:drawSonic2Blocks()
 				self:drawSonicCDBlocks()
 				self:drawSonic3Blocks()
+				self:drawSonicAndKnucklesBlocks()
 			end,
 
 			handleKeypressed = function(self, key)
 				if key == "S" then
 					self.destination = self.destination + 1000
-					if self.destination > 9000 then
+					if self.destination > 10000 then
 						self.destination = 0
 					end
 					self.transparency:setDestination(self.destination)
@@ -50,13 +51,13 @@ return {
 				local x = self:drawZone(0, 8192, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 44, h = 4 }, { w = 44.5, h = 5 } })
 				self:drawZone(x + 256, 8192, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 41, h = 8 }, { w = 45.5, h = 8 } })
 
-				local x = self:drawZone(0, 10496, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 44, h = 8 }, { w = 46.5, h = 8 }})
+				x = self:drawZone(0, 10496, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 44, h = 8 }, { w = 46.5, h = 8 }})
 				self:drawZone(x + 256, 10496, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 42.5, h = 8 }, { w = 56.0, h = 8 }})
 			
-				local x = self:drawZone(0, 12800, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 41.5, h = 8 }, { w = 52.0, h = 8 }})
+				x = self:drawZone(0, 12800, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 41.5, h = 8 }, { w = 52.0, h = 8 }})
 				self:drawZone(x + 256, 12800, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 37.0, h = 8 }, { w = 38.0, h = 8 }})
 
-				local x = self:drawZone(0, 15104, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 49, h = 8 }, { w = 46.5, h = 8 }})
+				x = self:drawZone(0, 15104, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 49, h = 8 }, { w = 46.5, h = 8 }})
 				self:drawZone(x + 256, 15104, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 36, h = 8 }, { w = 32, h = 8 }, { w = 49, h = 8 }})
 
 				self:drawZone(0, 17408, self:getColor(0.5, 0.5, 0.5, -6000), { { w = 21.5, h = 6 }, { w = 50.5, h = 7 }, { w = 15, h = 2.5 }})
@@ -67,49 +68,66 @@ return {
 				self:drawZone(x + 256, 20480, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 15, h = 4 }, { w = 15, h = 4 }})
 				self:drawZone(11008, 22016, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 42, h = 4 }, { w = 42, h = 4 }, { w = 42, h = 4 }, { w = 42, h = 4 }})
 				
-				local x = self:drawZone(0, 23296, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }})
+				x = self:drawZone(0, 23296, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }})
 				self:drawZone(x + 256, 23296, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 6, h = 6 }, { w = 6, h = 6 }})
 				self:drawZone(0, 25088, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 40, h = 6 }, { w = 40, h = 6 }, { w = 40, h = 6 }, { w = 40, h = 6 }})
 			
-				local x = self:drawZone(0, 26880, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 30, h = 6 }, { w = 30, h = 6 }, { w = 30, h = 6 }, { w = 30, h = 6 }})
+				x = self:drawZone(0, 26880, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 30, h = 6 }, { w = 30, h = 6 }, { w = 30, h = 6 }, { w = 30, h = 6 }})
 				self:drawZone(54528, 20480, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 15, h = 8 }, { w = 15, h = 8 }})
 				self:drawZone(x + 256, 26880, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 30, h = 8 }, { w = 30, h = 8 }, { w = 30, h = 8 }, { w = 30, h = 8 }})
 			
-				local x = self:drawZone(0, 28672, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }})
+				x = self:drawZone(0, 28672, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }})
 				self:drawZone(x + 256, 28672, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 16, h = 4 }, { w = 16, h = 4 }})
 				self:drawZone(0, 29952, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }, { w = 44, h = 4 }})
 			
-				local x = self:drawZone(0, 31232, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 36, h = 8 }, { w = 36, h = 8 }, { w = 36, h = 8 }, { w = 36, h = 8 }})
+				x = self:drawZone(0, 31232, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 36, h = 8 }, { w = 36, h = 8 }, { w = 36, h = 8 }, { w = 36, h = 8 }})
 				x = self:drawZone(x + 256, 31232, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 15, h = 8 }, { w = 15, h = 8 }})
 				self:drawZone(x + 256, 31232, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 64, h = 3 } })
 				self:drawZone(x + 256, 32256, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 64, h = 3 } })
 				self:drawZone(0, 33536, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 37, h = 8 }, { w = 37, h = 8 }, { w = 37, h = 8 }, { w = 37, h = 8 }})
 			
-				local x = self:drawZone(0, 35840, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }})
+				x = self:drawZone(0, 35840, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }, { w = 48, h = 6 }})
 				self:drawZone(0, 37632, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 48, h = 8 }, { w = 48, h = 8 }, { w = 48, h = 8 }, { w = 48, h = 8 }})
 			
-				local x = self:drawZone(0, 39936, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }})
+				x = self:drawZone(0, 39936, self:getColor(0.3, 0.3, 0.3, -7000), { { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }})
 				self:drawZone(x + 256, 39936, self:getColor(0.7, 0.7, 0.7, -7000), { { w = 16, h = 4 }, { w = 16, h = 4 }})
 				self:drawZone(0, 42240, self:getColor(0.2, 0.2, 0.2, -7000), { { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }, { w = 32, h = 8 }})
 			
 			end,
 
 			drawSonic3Blocks = function(self)
-				local x = self:drawZone(0, 45056, self:getColor(0.3, 1, 0.3, -8000), { { w = 46.125, h = 6.5 }, { w = 60, h = 10 }})
-				self:drawZone(x + 256, 45056, self:getColor(0.3, 0.3, 1, -8000), { { w = 56, h = 12 }, { w = 72, h = 12 }})
+				local x = self:drawZone(0, 44544, self:getColor(0.3, 1, 0.3, -8000), { { w = 46.125, h = 6.5, m = 48 }, { w = 60, h = 10, m = 64 }})
+				self:drawZone(x + 256, 44544, self:getColor(0.3, 0.3, 1, -8000), { { w = 56, h = 12 }, { w = 72, h = 12 }})
 
-				local x = self:drawZone(0, 48384, self:getColor(1, 0.3, 0.3, -8000), { { w = 48, h = 16 }, { w = 64, h = 12.5 }})
-				self:drawZone(x + 256, 48384, self:getColor(1, 0.3, 1, -8000), { { w = 56, h = 12.5 }, { w = 76, h = 12.5 }})
+				x = self:drawZone(0, 47360, self:getColor(1, 0.3, 0.3, -8000), { { w = 48, h = 16, y = -896 }, { w = 64, h = 12.5 }})
+				self:drawZone(x + 256, 47360, self:getColor(1, 0.3, 1, -8000), { { w = 56, h = 12.5, y = 512 }, { w = 76, h = 12.5, y = 512 }})
 			
-				self:drawZone(0, 52736, self:getColor(0.3, 1, 1, -8000), { { w = 112.6875, h = 8 }, { w = 64, h = 12 }})
-				self:drawZone(0, 56064, self:getColor(1, 1, 0.3, -8000), { { w = 64, h = 12 }, { w = 68.5, h = 12 }})
+				x = self:drawZone(0, 50688, self:getColor(0.3, 1, 1, -8000), { { w = 112.6875, h = 8 } })
+				self:drawZone(x + 256, 51200, self:getColor(0.3, 1, 1, -8000), { { w = 64, h = 12 }})
+				self:drawZone(0, 54528, self:getColor(1, 1, 0.3, -8000), { { w = 64, h = 12, y = -1536, m = 112.6875 }, { w = 68.5, h = 12 }})
+			end,
+
+			drawSonicAndKnucklesBlocks = function(self)
+				self:drawZone(16640, 54528, self:getColor(0.6, 0.3, 1, -9000), { { w = 48, h = 12, y = -1536, m = 117 }, { w = 56.5, h = 12 }})
+				self:drawZone(46000, 51200, self:getColor(0.3, 1, 0.6, -9000), { { w = 69, h = 12.5 }})
+				local x = self:drawZone(0, 56192, self:getColor(0.3, 1, 0.6, -9000), { { w = 85.5, h = 8 }})
+				self:drawZone(x + 256, 56192, self:getColor(0.6, 1, 0.3, -9000), { { w = 16, h = 7 }})
+				x = self:drawZone(0, 58368, self:getColor(1, 0.6, 0.3, -9000), { { w = 70, h = 11.5 }, { w = 72, h = 10.5 }})
+				self:drawZone(x + 256, 57856, self:getColor(0.6, 1, 0.3, -9000), { { w = 46, h = 12 }, { w = 64, h = 12 }})
+				x = self:drawZone(0, 61440, self:getColor(1, 1, 0.3, -9000), { { w = 26, h = 12 }})
+				x = self:drawZone(x + 256, 61440, self:getColor(1, 0.3, 1, -9000), { { w = 27, h = 16 }, { w = 3.5, h = 1.5 }})
+				self:drawZone(x + 256, 61440, self:getColor(0.6, 0, 0, -9000), { { w = 56.5, h = 12.5 }, { w = 56, h = 14, y = -256 }})
+				self:drawZone(x + 256, 64895, self:getColor(1, 0, 0, -9000), { { w = 116, h = 2 }})
 			end,
 
 			drawZone = function(self, x, y, c, zoneInfo)
 				self.graphics:setColor(c)
 				for _, zone in ipairs(zoneInfo) do
-					self.graphics:rectangle("fill", x, y, zone.w * 256, zone.h * 256)
-					x = x + (zone.w * 256) + 256
+					local zoneY = y
+					if zone.y then zoneY = y + zone.y end
+					self.graphics:rectangle("fill", x, zoneY, zone.w * 256, zone.h * 256)
+					if zone.m then x = x + (zone.m * 256) + 256
+					else           x = x + (zone.w * 256) + 256 end
 				end
 				return x
 			end,
