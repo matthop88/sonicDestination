8a3825944d35231fd87df9945477e02bf1587963
Matthew Hopkins <matthop88@gmail.com>
2026-02-16 14:41:31 -0800

    Removed all coordinate logic from construction set map

--- tools/constructionSet/constructionSetMap.lua
+++ tools/constructionSet/constructionSetMap.lua
@@ -3,9 +3,9 @@ local CHUNKS, SOLIDS
 return {
 	create = function(self, params)
 		return {
-			graphics = params.graphics,
-			hex = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" },
-
+			graphics         = params.graphics,
+			coordinateMaster = require("tools/constructionSet/coordinateMaster"),
+			
 			chunks = {
 				place = function(self, chunkID, x, y, xFlipped)
 					for n, chunk in ipairs(self) do
@@ -63,16 +63,7 @@ return {
 			end,
 
 			drawCoordinates = function(self)
-				local scale = self.graphics:getScale()
-				if scale < 0.15 then
-					self:drawSuperChunkCoordinates()
-				end
-				if scale > 0.05 and scale < 2 then
-					self:drawChunkCoordinates()
-				end 
-				if scale > 1 and scale <= 40 then
-					self:drawTileCoordinates()
-				end
+				self.coordinateMaster:drawCoordinates(self.graphics)
 			end,
 
 			handleKeypressed = function(self, key)
@@ -91,115 +82,6 @@ return {
 
 			update = function(self, dt)
 				self.objects:update(dt)
-			end,
-
-			drawSuperChunkCoordinates = function(self)
-				local leftMostX,  topMostY    = self.graphics:screenToImageCoordinates(0, 0)
-				local rightMostX, bottomMostY = self.graphics:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
-
-				local alpha = 1 - ((self.graphics:getScale() - 0.05) * 10)
-				
-				self.graphics:setColor(1, 1, 1, alpha)
-				
-				local fontSize = math.min(1536, math.max(768, 768 - (topMostY / 3)))
-				self.graphics:setFontSize(fontSize)
-				
-				for x = 0, 15 do
-					self.graphics:printf("" .. self.hex[x + 1], x * 256 * 16, math.max(-2400, topMostY), 256 * 16, "center")
-				end
-
-				local fontSize = math.min(1536, math.max(768, 768 - (leftMostX / 5)))
-				self.graphics:setFontSize(fontSize)
-				
-				for y = 0, 15 do
-					self.graphics:printf("" .. self.hex[y + 1], math.max(-3000, leftMostX), (y * 256 * 16 + (2048 - (fontSize * 0.6))), fontSize * 0.8, "right")
-				end
-
-			end,
-
-			drawChunkCoordinates = function(self)
-				local leftMostX,  topMostY    = self.graphics:screenToImageCoordinates(0, 0)
-				local rightMostX, bottomMostY = self.graphics:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
-
-				leftChunk   = math.floor(leftMostX / 256)
-				rightChunk  = math.floor(rightMostX / 256)
-				topChunk    = math.floor(topMostY / 256)
-				bottomChunk = math.floor(bottomMostY / 256)
-
-				local alpha = (self.graphics:getScale() - 0.05) * 10
-				
-				if self.graphics:getScale() > 1 then
-					alpha = 1 - (self.graphics:getScale() - 1)
-				end
-				self.graphics:setColor(1, 1, 1, alpha)
-
-				local fontSize = math.min(96, math.max(24 / self.graphics:getScale(), 24 - (leftMostX / 3)))
-				self.graphics:setFontSize(fontSize)
-
-				for chunkY = math.max(0, topChunk), math.min(255, bottomChunk) do
-					local y = chunkY % 16
-					local scy = math.floor(chunkY / 16)
-					
-					self.graphics:printf(self.hex[scy + 1] .. self.hex[y + 1], math.max(-200, leftMostX), chunkY * 256 + (128 - (fontSize * 0.7)), 1.5 * fontSize, "right")
-				end
-				
-				fontSize = math.min(96, math.max(24 / self.graphics:getScale(), 24 - (topMostY / 2)))
-				self.graphics:setFontSize(fontSize)
-			
-				self.graphics:setColor(0, 0, 0, alpha)
-				self.graphics:rectangle("fill", leftMostX, topMostY, rightMostX - leftMostX, self.graphics:getFontHeight())
-				self.graphics:setColor(1, 1, 1, alpha)
-
-				for chunkX = math.max(0, leftChunk), math.min(255, rightChunk) do
-					local x = chunkX % 16
-					local scx = math.floor(chunkX / 16)
-
-					self.graphics:printf(self.hex[scx + 1] .. self.hex[x + 1], chunkX * 256, math.max(-150, topMostY), 256, "center")
-				end
-			end,
-
-			drawTileCoordinates = function(self)
-				local fontWidth = self.graphics:getFontWidth("FFF")
-				local leftMostX,  topMostY    = self.graphics:screenToImageCoordinates(0, 0)
-				local rightMostX, bottomMostY = self.graphics:screenToImageCoordinates(love.graphics:getWidth(), love.graphics:getHeight())
-
-				leftTile   = math.floor(leftMostX / 16)
-				rightTile  = math.floor(rightMostX / 16)
-				topTile    = math.floor(topMostY / 16)
-				bottomTile = math.floor(bottomMostY / 16)
-				
-				local alpha = self.graphics:getScale() - 1
-				if self.graphics:getScale() > 20 then
-					alpha = 1 - ((self.graphics:getScale() - 20) / 20)
-				end
-				local s = 12 / self.graphics:getScale()
-				
-				local fontSize = math.max(2, math.min(6, math.max(s, s - (leftMostX / 6))))
-				self.graphics:setFontSize(fontSize)
-				self.graphics:setColor(1, 1, 1, alpha)
-				
-				for tileY = math.max(0, topTile), math.min(4095, bottomTile) do
-					local y = tileY % 16
-					local cy = math.floor(tileY / 16) % 16
-					local scy = math.floor(tileY / 256)
-					self.graphics:printf(self.hex[scy + 1] .. self.hex[cy + 1] .. self.hex[y + 1], math.max(-16, leftMostX), tileY * 16 + (8 - fontSize / 2), 3 * fontSize, "left")
-				end
-
-				local fontSize = math.max(2, math.min(6, math.max(s, s - (topMostY / 3))))
-				self.graphics:setFontSize(fontSize)
-
-				self.graphics:setColor(0, 0, 0, alpha)
-				self.graphics:rectangle("fill", leftMostX, topMostY, rightMostX - leftMostX, self.graphics:getFontHeight() + 0.5)
-				self.graphics:setColor(1, 1, 1, alpha)
-				for tileX = math.max(0, leftTile), math.min(4095, rightTile) do
-					local x = tileX % 16
-					local cx = math.floor(tileX / 16) % 16
-					local scx = math.floor(tileX / 256)
-					
-					self.graphics:printf(self.hex[scx + 1] .. self.hex[cx + 1] .. self.hex[x + 1], tileX * 16, math.max(-9, topMostY), 16, "center")
-				end
-
-				
 			end,
 	
 			---------------------- Graphics Object Methods ------------------------
