40f8bd03ed943a75c15dec47a2013cbb45feef7d
Matthew Hopkins <matthop88@gmail.com>
2026-02-20 16:49:30 -0800

    Create sampleData.lua for map serialization

---
+++ tools/constructionSet/data/sampleData.lua
@@ -0,0 +1,8 @@
+local A = "ghzChunks_2"
+local B = "scdPtpChunks"
+
+return {
+  { row = 1, data = {} },
+  { row = 2, data = { {A, 1}, {A, 2}, {A, 2}, {A, 3}, {A, 4}, {}, {B, 1}, {B, 2}, {B, 3}, {B, 4}, } },
+  { row = 3, data = { {A, 5}, {A, 6}, {A, 6}, {A, 6}, {A, 7}, {}, {B, 5}, {B, 5}, {B, 5}, {B, 5}, } },
+}
