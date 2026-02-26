132439a0bbc5fd24f0b8c4fc7dd65707e510e83f
Matthew Hopkins <matthop88@gmail.com>
2026-02-25 17:15:21 -0800

    Updated itemTemplate.lua; extracted item

--- tools/constructionSet/templates/itemTemplate.lua
+++ tools/constructionSet/templates/itemTemplate.lua
@@ -1,50 +1,6 @@
 local COLOR = require("tools/lib/colors")
 
-local ITEM = {
-    create = function(self, name, spritePath, containerWidth, containerHeight)
-        return ({
-            init = function(self, name, spritePath, containerWidth, containerHeight)
-                self.name   = name
-                self.sprite = require("tools/lib/sprites/sprite"):create(spritePath, 0, 0)
-                self.scale  = 1
-                if containerWidth and containerHeight then
-                    self.scale = 8 / math.sqrt(math.max(self.sprite:getW(), self.sprite:getH()))
-                end
-
-                return self
-            end,
-            
-            getName = function(self) return self.name          end,
-            getW    = function(self) return self.sprite:getW() end,
-            getH    = function(self) return self.sprite:getH() end,
-
-            draw    = function(self, graphics, x, y, w, h)
-                self.sprite:drawAt(graphics, x, y, self.scale, self.scale)
-            end,
-
-            drawThumbnail = function(self, graphics, x, y, w, h)
-                self.sprite:drawThumbnail(graphics, x, y, self.scale, self.scale)
-            end,  
-
-            update = function(self, dt)
-                self.sprite:update(dt)
-            end,
-
-            flipX  = function(self)     
-                -- do nothing
-            end,
-
-            place        = function(self, map, x, y)
-                self:release()
-                return map:placeObject(self, x, y)
-            end, 
-
-            hold         = function(self) self.isHeld = true    end,
-            release      = function(self) self.isHeld = false   end,
-            
-        }):init(name, spritePath, containerWidth, containerHeight)
-    end,
-}
+local ITEM = require("tools/constructionSet/templates/item")
 
 return {
     create = function(self, name, spritePath, containerWidth, containerHeight)
