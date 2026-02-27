5be68c32b651f23131bf12433ad192f7ca5e6612
Matthew Hopkins <matthop88@gmail.com>
2026-02-26 21:16:44 -0800

    Create cmdSchema.lua

---
+++ tools/constructionSet/cmdSchema.lua
@@ -0,0 +1,16 @@
+return {
+	VERSION = 0.1,
+
+	COMMANDS = {
+		dataIn = {
+			description = "name of data file to read for both map and objects",
+			shortcut = "i",
+			required = false,
+		},
+		dataOut = {
+			description = "name of data file to write for both map and objects",
+			shortcut = "o",
+			required = false,
+		},
+	},
+}
