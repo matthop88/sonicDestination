2b409b194a98141352dc790ed3e365b74b78988a
Matthew Hopkins <matthop88@gmail.com>
2026-02-27 16:23:33 -0800

    Updated mapReader.lua to support old map style

--- tools/constructionSet/mapReader.lua
+++ tools/constructionSet/mapReader.lua
@@ -7,18 +7,28 @@ return {
 	readMapIntoChunksList = function(self, map, chunksList)
 		local chunkFactory = require("tools/constructionSet/chunkFactory")
 
+		local CHUNK_DATA_NAME = map.chunksDataName
+
 		for _, row in ipairs(map) do
 			local y = row.row
 			for x, element in ipairs(row.data) do
-				if element[CHUNKS_IMG_NAME] and element[CHUNK_ID] then
-					local CHUNKS = require("tools/lib/dataStructures/lazyVal"):create()
-        			local SOLIDS = require("tools/lib/dataStructures/lazyVal"):create()
-					
-					CHUNKS:set(chunkFactory:getChunks(element[CHUNKS_IMG_NAME]))
-					SOLIDS:set(chunkFactory:getSolids(element[CHUNKS_IMG_NAME]))
+				local CHUNKS = require("tools/lib/dataStructures/lazyVal"):create()
+	        	local SOLIDS = require("tools/lib/dataStructures/lazyVal"):create()
+				
+				if type(element) == "table" then
+					if element[CHUNKS_IMG_NAME] and element[CHUNK_ID] then
+						CHUNKS:set(chunkFactory:getChunks(element[CHUNKS_IMG_NAME]))
+						SOLIDS:set(chunkFactory:getSolids(element[CHUNKS_IMG_NAME]))
 
-					local chunk = CHUNK:create(element[CHUNK_ID], CHUNKS, SOLIDS)
-					if element.xFlip then chunk:flipX() end
+						local chunk = CHUNK:create(element[CHUNK_ID], CHUNKS, SOLIDS)
+						if element.xFlip then chunk:flipX() end
+						chunksList:add(chunk, x - 1, y - 1)
+					end
+				else
+					CHUNKS:set(chunkFactory:getChunks(CHUNK_DATA_NAME))
+					SOLIDS:set(chunkFactory:getSolids(CHUNK_DATA_NAME))
+
+					local chunk = CHUNK:create(element, CHUNKS, SOLIDS)
 					chunksList:add(chunk, x - 1, y - 1)
 				end
 			end
