cc2d108bd84447572424664da19dc8a0f2ed8244
Matthew Hopkins <matthop88@gmail.com>
2026-02-27 16:25:45 -0800

    Updated scdPtp1Map.lua to support row/data paradigm

--- game/resources/zones/maps/scdPtp1Map.lua
+++ game/resources/zones/maps/scdPtp1Map.lua
@@ -3,6 +3,6 @@ return {
   chunksImageName = "scdPtpChunks",
   objectsDataName = "scdPtp1Objects",
   -----------------------------------
-  {   1,   2,   3,   4, },
-  {   5,   5,   5,   5, },
+  {   row = 1, data = { 1,   2,   3,   4, } },
+  {   row = 2, data = { 5,   5,   5,   5, } },
 }
