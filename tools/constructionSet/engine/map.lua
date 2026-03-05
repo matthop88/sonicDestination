167ae9c3b1df86ec072c1311c59d42cecd14a45d
Matthew Hopkins <matthop88@gmail.com>
2026-03-05 11:48:18 -0800

    Updated map.lua; simplified clear() function

--- tools/constructionSet/engine/map.lua
+++ tools/constructionSet/engine/map.lua
@@ -100,22 +100,9 @@ return {
 			end,
 
 			clear = function(self)
-				-- Clear chunks
-				for i = 1, #self.chunks do
-					self.chunks[i] = {}
-				end
-				
-				-- Clear objects
-				self.objects.objList = require("game/util/dataStructures/linkedList"):create()
-				self.objects.selected = nil
-				self.objects.held = nil
-				
-				-- Clear player
-				self.player.obj = nil
-				self.player.x = nil
-				self.player.y = nil
-				self.player.selected = false
-				self.player.held = nil
+				self.chunks:clear()
+				self.objects:clear()
+				self.player:clear()
 			end,
 
 			saveMap = function(self, filename)
