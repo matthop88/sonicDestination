617c02262117e770c91dd14b0a8154f0a96513e0
Matthew Hopkins <matthop88@gmail.com>
2026-03-10 15:32:22 -0700

    Refactored list.lua to be more readable

--- tools/soundGraph/list.lua
+++ tools/soundGraph/list.lua
@@ -9,57 +9,81 @@ return {
 			selectedIndex = nil,
 
 			draw = function(self)
-				-- Calculate total height
-				local totalHeight = #self.items * self.itemHeight
+				self:drawBackground()
+				self:drawItems()
+			end,
 
+			drawBackground = function(self)
+				local totalHeight = #self.items * self.itemHeight
+				
 				-- Draw background rectangle (black with white outline)
 				love.graphics.setColor(0, 0, 0)
 				love.graphics.rectangle("fill", self.x, self.y, self.width, totalHeight)
 				love.graphics.setColor(1, 1, 1)
 				love.graphics.rectangle("line", self.x, self.y, self.width, totalHeight)
+			end,
 
-				-- Get mouse position
+			drawItems = function(self)
 				local mx, my = love.mouse.getPosition()
 
-				-- Draw each item
 				for i, item in ipairs(self.items) do
 					local itemY = self.y + (i - 1) * self.itemHeight
-					local isHovered = mx >= self.x and mx <= self.x + self.width and
-					                  my >= itemY and my <= itemY + self.itemHeight
+					local isHovered = self:itemContainsPt(i, mx, my)
 
 					if isHovered then
-						-- Draw white background for hovered item
-						love.graphics.setColor(1, 1, 1)
-						love.graphics.rectangle("fill", self.x, itemY, self.width, self.itemHeight)
-						-- Draw black text
-						love.graphics.setColor(0, 0, 0)
+						self:drawReversedText(item, itemY)
 					else
-						-- Draw white text
-						love.graphics.setColor(1, 1, 1)
+						self:drawWhiteText(item, itemY)
 					end
-
-					-- Draw item text
-					local font = love.graphics.getFont()
-					local textY = itemY + (self.itemHeight - font:getHeight()) / 2
-					love.graphics.print(item, self.x + 5, textY)
 				end
 			end,
 
-			handleClick = function(self, mx, my)
-				local totalHeight = #self.items * self.itemHeight
+			itemContainsPt = function(self, i, px, py)
+				local itemY = self.y + (i - 1) * self.itemHeight
+				return px >= self.x and px <= self.x + self.width and
+				       py >= itemY and py <= itemY + self.itemHeight
+			end,
 
+			drawReversedText = function(self, item, itemY)
+				-- Draw white background for hovered item
+				love.graphics.setColor(1, 1, 1)
+				love.graphics.rectangle("fill", self.x, itemY, self.width, self.itemHeight)
+				
+				-- Draw black text
+				love.graphics.setColor(0, 0, 0)
+				self:drawText(item, itemY)
+			end,
+
+			drawWhiteText = function(self, item, itemY)
+				-- Draw white text
+				love.graphics.setColor(1, 1, 1)
+				self:drawText(item, itemY)
+			end,
+
+			drawText = function(self, item, itemY)
+				local font = love.graphics.getFont()
+				local textY = itemY + (self.itemHeight - font:getHeight()) / 2
+				love.graphics.print(item, self.x + 5, textY)
+			end,
+
+			handleClick = function(self, mx, my)
 				-- Check if click is within list bounds
-				if mx >= self.x and mx <= self.x + self.width and
-				   my >= self.y and my <= self.y + totalHeight then
+				if self:listBoxContainsPt(mx, my) then
 					-- Determine which item was clicked
 					local relativeY = my - self.y
 					local clickedIndex = math.floor(relativeY / self.itemHeight) + 1
 					if clickedIndex >= 1 and clickedIndex <= #self.items then
 						self.selectedIndex = clickedIndex
-						return self.items[clickedIndex], clickedIndex
+						return self:getSelectedItem()
 					end
 				end
 				return nil, nil
+			end,
+
+			listBoxContainsPt = function(self, px, py)
+				local totalHeight = #self.items * self.itemHeight
+				return px >= self.x and px <= self.x + self.width and
+				       py >= self.y and py <= self.y + totalHeight
 			end,
 
 			getSelectedItem = function(self)
