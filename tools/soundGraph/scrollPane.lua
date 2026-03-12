5aefce271eb6ac62257e220f47d59a6f37fdde02
Matthew Hopkins <matthop88@gmail.com>
2026-03-12 16:19:20 -0700

    Created scrollPane.lua

---
+++ tools/soundGraph/scrollPane.lua
@@ -0,0 +1,61 @@
+return {
+	create = function(self, params)
+		local graphics = require("tools/lib/bufferedGraphics"):create(
+			nil,
+			params.width or 200, 
+			params.height or 400
+		)
+		
+		return ({
+			x = params.x or 0,
+			y = params.y or 0,
+			width = params.width or 200,
+			height = params.height or 400,
+			graphics = graphics,
+			list = params.list,
+			scrollY = 0,
+
+			init = function(self)
+				self.list.x = 0
+				self.list.y = 0
+				self.graphics:setY(0)
+				return self
+			end,
+
+			draw = function(self)
+				self.graphics:clear(0, 0, 0, 0)
+				local mx, my = love.mouse.getPosition()
+				local bufferMx, bufferMy = self:translateMouseCoordinates(mx, my)
+				self.list:draw(self.graphics, bufferMx, bufferMy)
+				self.graphics:blitToScreen(self.x, self.y)
+			end,
+
+			update = function(self, dt)
+				self.list:update(dt)
+			end,
+
+			translateMouseCoordinates = function(self, mx, my)
+				return mx - self.x, my - self.y
+			end,
+
+			scrollBy = function(self, deltaY)
+				self.scrollY = self.scrollY + deltaY
+				self.graphics:setY(self.scrollY)
+			end,
+
+			scrollTo = function(self, y)
+				self.scrollY = y
+				self.graphics:setY(self.scrollY)
+			end,
+
+			getScrollY = function(self)
+				return self.scrollY
+			end,
+
+			handleClick = function(self, mx, my)
+				local bufferMx, bufferMy = self:translateMouseCoordinates(mx, my)
+				return self.list:handleClick(bufferMx, bufferMy)
+			end,
+		}):init()
+	end,
+}
