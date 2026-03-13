d6177342a617a9b41ae9cdb75efa5197fe773331
Matthew Hopkins <matthop88@gmail.com>
2026-03-13 12:45:25 -0700

    Create soundData.lua

---
+++ tools/soundGraph/soundData.lua
@@ -0,0 +1,14 @@
+return {
+	sonicBraking   = { label = "Sonic Braking",    filename = "brake.ogg",             volume = 0.2, },
+	sonicJumping   = { label = "Sonic Jumping",    filename = "jump.ogg",              volume = 0.2, },
+	sonicCDJumping = { label = "Sonic CD Jumping", filename = "sonicCDJump.mp3",       volume = 0.9,  startPoint = 19520, },
+	ringCollectL   = { label = "Ring Collect L",   filename = "ring-collect-L.mp3",    volume = 0.4, },
+	ringCollectR   = { label = "Ring Collect R",   filename = "ring-collect-R.mp3",    volume = 0.4, },
+	giantRing      = { label = "Giant Ring",       filename = "giantRing.mp3",         volume = 0.5,  startPoint =  4352, },
+	vanish         = { label = "Vanish",           filename = "vanish.mp3",            volume = 0.5,  startPoint =  3968, },
+	badnikDeath    = { label = "Badnik Death",     filename = "badnik-death.ogg",      volume = 0.5, },
+	sonicHit       = { label = "Sonic Hit",        filename = "sonicHit.ogg",          volume = 0.8, },
+	iceExplode     = { label = "Ice Explode",      filename = "extra/IceExplode1.wav", volume = 0.8, },
+	klank          = { label = "Klank",            filename = "extra/AnvilDing.wav",   volume = 0.8, },
+	ouch           = { label = "Ouch",             filename = "extra/Ooouuucchh.wav",  volume = 0.8, },
+}
