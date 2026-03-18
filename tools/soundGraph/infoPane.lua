0e0b12e04efe4d75dbbf7b231f9e808a2b719d04
Matthew Hopkins <matthop88@gmail.com>
2026-03-18 13:43:28 -0700

    Added timeline and scroll thumb to the info pane

--- tools/soundGraph/infoPane.lua
+++ tools/soundGraph/infoPane.lua
@@ -9,6 +9,13 @@ return {
 			soundModel = nil,
 			font = love.graphics.newFont(16),
 			
+			-- Timeline scrubber
+			timelineY = (params.y or 512) + 170,
+			timelineMargin = 40,
+			thumbWidth = 12,
+			thumbHeight = 20,
+			isDraggingThumb = false,
+			
 			setSoundObject = function(self, soundObject)
 				self.soundObject = soundObject
 			end,
@@ -22,6 +29,7 @@ return {
 				self:drawBackground()
 				self:drawBorder()
 				self:drawInfoText()
+				self:drawTimeline()
 			end,
 			
 			drawBackground = function(self)
@@ -102,6 +110,88 @@ return {
 					-- Regular format for 10 seconds and above
 					return string.format("%d:%05.2f", minutes, seconds)
 				end
+			end,
+			
+			drawTimeline = function(self)
+				if not self.soundObject or not self.soundModel then return end
+				
+				-- Draw green timeline line
+				local lineStartX = self.x + self.timelineMargin
+				local lineEndX = self.x + self.width - self.timelineMargin
+				love.graphics.setColor(0, 1, 0)
+				love.graphics.setLineWidth(2)
+				love.graphics.line(lineStartX, self.timelineY, lineEndX, self.timelineY)
+				love.graphics.setLineWidth(1)
+				
+				-- Calculate thumb position based on current playback position
+				local duration = self.soundObject:getSampleCount() / self.soundObject:getSampleRate()
+				local currentTime = self.soundObject.audioSource:tell("seconds")
+				local progress = duration > 0 and (currentTime / duration) or 0
+				local lineWidth = lineEndX - lineStartX
+				local thumbX = lineStartX + (progress * lineWidth)
+				
+				-- Draw white thumb
+				love.graphics.setColor(1, 1, 1)
+				love.graphics.rectangle("fill", 
+					thumbX - self.thumbWidth / 2, 
+					self.timelineY - self.thumbHeight / 2,
+					self.thumbWidth,
+					self.thumbHeight)
+			end,
+			
+			getThumbPosition = function(self)
+				if not self.soundObject or not self.soundModel then return 0 end
+				
+				local lineStartX = self.x + self.timelineMargin
+				local lineEndX = self.x + self.width - self.timelineMargin
+				local duration = self.soundObject:getSampleCount() / self.soundObject:getSampleRate()
+				local currentTime = self.soundObject.audioSource:tell("seconds")
+				local progress = duration > 0 and (currentTime / duration) or 0
+				local lineWidth = lineEndX - lineStartX
+				
+				return lineStartX + (progress * lineWidth)
+			end,
+			
+			isMouseOverThumb = function(self, mx, my)
+				if not self.soundObject or not self.soundModel then return false end
+				
+				local thumbX = self:getThumbPosition()
+				local thumbLeft = thumbX - self.thumbWidth / 2
+				local thumbRight = thumbX + self.thumbWidth / 2
+				local thumbTop = self.timelineY - self.thumbHeight / 2
+				local thumbBottom = self.timelineY + self.thumbHeight / 2
+				
+				return mx >= thumbLeft and mx <= thumbRight and my >= thumbTop and my <= thumbBottom
+			end,
+			
+			handleMousePressed = function(self, mx, my)
+				if self:isMouseOverThumb(mx, my) then
+					self.isDraggingThumb = true
+					return true
+				end
+				return false
+			end,
+			
+			handleMouseReleased = function(self)
+				self.isDraggingThumb = false
+			end,
+			
+			handleMouseDragged = function(self, mx, my)
+				if not self.isDraggingThumb or not self.soundObject or not self.soundModel then return end
+				
+				-- Calculate new position based on mouse X
+				local lineStartX = self.x + self.timelineMargin
+				local lineEndX = self.x + self.width - self.timelineMargin
+				local lineWidth = lineEndX - lineStartX
+				
+				-- Clamp mouse position to timeline bounds
+				local clampedX = math.max(lineStartX, math.min(mx, lineEndX))
+				local progress = (clampedX - lineStartX) / lineWidth
+				
+				-- Set audio position
+				local duration = self.soundObject:getSampleCount() / self.soundObject:getSampleRate()
+				local newTime = progress * duration
+				self.soundObject.audioSource:seek(newTime, "seconds")
 			end,
 			
 			update = function(self, dt)
