ca43e9939ac18a48b545ae137cac20f808e8da92
Matthew Hopkins <matthop88@gmail.com>
2026-03-23 16:34:21 -0700

    Updated soundGraphApp.lua to make use of marker pane

--- tools/soundGraph/soundGraphApp.lua
+++ tools/soundGraph/soundGraphApp.lua
@@ -2,8 +2,9 @@
 --                      Local Variables                     --
 --------------------------------------------------------------
 
-local WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 712
+local WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 776
 local WAVEFORM_HEIGHT = 512
+local MARKER_PANE_HEIGHT = 64
 local INFO_PANE_HEIGHT = 200
 
 local SOUND_OBJECT = nil
@@ -13,9 +14,21 @@ local SOUND_VIEW = require("tools/soundGraph/soundView"):create {
 	marginLeft = 100,
 }
 
-local INFO_PANE = require("tools/soundGraph/infoPane"):create {
+local MARKER_PANE = require("tools/soundGraph/markerPane"):create {
 	x = 0,
 	y = WAVEFORM_HEIGHT,
+	width = WINDOW_WIDTH,
+	height = MARKER_PANE_HEIGHT,
+	soundView = SOUND_VIEW,
+	onMarkerChanged = function()
+		-- Future: handle marker position changes
+		print("Start marker moved")
+	end,
+}
+
+local INFO_PANE = require("tools/soundGraph/infoPane"):create {
+	x = 0,
+	y = WAVEFORM_HEIGHT + MARKER_PANE_HEIGHT,
 	width = WINDOW_WIDTH,
 	height = INFO_PANE_HEIGHT,
 	onPositionChanged = function()
@@ -40,6 +53,7 @@ local SOUND_LIST = require("tools/soundGraph/soundList"):create {
 		print("Analysis coroutine created")
 		SOUND_VIEW:refresh(SOUND_OBJECT, 64, 100)
 		INFO_PANE:setSoundObject(SOUND_OBJECT)
+		MARKER_PANE:setSoundObject(SOUND_OBJECT)
 	end,
 }
 
@@ -51,12 +65,14 @@ function love.draw()
     if SOUND_VIEW then
         SOUND_VIEW:draw()
     end
+    MARKER_PANE:draw()
     INFO_PANE:draw()
     SOUND_LIST:draw()
 end
 
 function love.update(dt)
     SOUND_LIST:update(dt)
+    MARKER_PANE:update(dt)
     INFO_PANE:update(dt)
     if SOUND_VIEW then
         SOUND_VIEW:update(dt)
@@ -64,9 +80,10 @@ function love.update(dt)
             local progress = SOUND_VIEW:getProgress()
             setProgressBarText(string.format("Loading Sound Data... %.0f%%", progress * 100))
         else
-            -- Update info pane with sound model once analysis is complete
+            -- Update panes with sound model once analysis is complete
             if SOUND_VIEW.soundModel and not INFO_PANE.soundModel then
                 INFO_PANE:setSoundModel(SOUND_VIEW.soundModel)
+                MARKER_PANE:setSoundModel(SOUND_VIEW.soundModel)
             end
         end
     end
@@ -76,12 +93,21 @@ function love.update(dt)
         local mx, my = love.mouse.getPosition()
         INFO_PANE:handleMouseDragged(mx, my)
     end
+    
+    -- Handle marker dragging
+    if MARKER_PANE.isDraggingStart then
+        local mx, my = love.mouse.getPosition()
+        MARKER_PANE:handleMouseDragged(mx, my)
+    end
 end
 
 function love.mousepressed(mx, my)
     local handled = SOUND_LIST:handleMousePressed(mx, my)
     if not handled then
         handled = INFO_PANE:handleMousePressed(mx, my)
+    end
+    if not handled then
+        handled = MARKER_PANE:handleMousePressed(mx, my)
     end
     if not handled and SOUND_VIEW and SOUND_OBJECT then
         print(SOUND_VIEW:getSampleXFromMouseX())
@@ -91,6 +117,7 @@ end
 function love.mousereleased()
     SOUND_LIST:handleMouseReleased()
     INFO_PANE:handleMouseReleased()
+    MARKER_PANE:handleMouseReleased()
 end
 
 function love.keypressed(key)
