2f008029e75d92edc7495dcc647fcd077217b0e6
Matthew Hopkins <matthop88@gmail.com>
2026-03-25 12:30:47 -0700

    Added endpoint to music data file

--- tools/soundGraph/data/musicData.lua
+++ tools/soundGraph/data/musicData.lua
@@ -1,3 +1,3 @@
 return {
-	constructionSet = { label = "Construction Set", filename = "construction.ogg", volume = 0.5 },
+	constructionSet = { label = "Construction Set", filename = "construction.ogg", volume = 0.5, endPoint = 15168694 },
 }
