f8d33f72880e8933a38cafb8b847ec1dd84feb3b
Matthew Hopkins <matthop88@gmail.com>
2026-03-26 12:16:50 -0700

    Updated markerPane.lua; simplified somewhat

--- tools/soundGraph/markerPane.lua
+++ tools/soundGraph/markerPane.lua
@@ -16,6 +16,8 @@ return {
 			markerSize = 12,
 			startMarker = nil,
 			endMarker = nil,
+			loopStartMarker = nil,
+			loopEndMarker = nil,
 			onMarkerChanged = params.onMarkerChanged,
 			
 			init = function(self)
@@ -25,25 +27,31 @@ return {
 				-- Create markers using factory
 				self.startMarker = markerFactory.createStartMarker(self)
 				self.endMarker = markerFactory.createEndMarker(self)
+				self.loopStartMarker = markerFactory.createLoopStartMarker(self)
+				self.loopEndMarker = markerFactory.createLoopEndMarker(self)
 				
 				return self
 			end,
 			
 			setSoundObject = function(self, soundObject)
 				self.soundObject = soundObject
-				-- Initialize both markers from soundObject
+				-- Initialize all markers from soundObject
 				if soundObject and self.soundView then
 					self.startMarker:initializeFromSoundObject()
 					self.endMarker:initializeFromSoundObject()
+					self.loopStartMarker:initializeFromSoundObject()
+					self.loopEndMarker:initializeFromSoundObject()
 				end
 			end,
 			
 			setSoundModel = function(self, soundModel)
 				self.soundModel = soundModel
-				-- Initialize both markers from soundObject
+				-- Initialize all markers from soundObject
 				if self.soundObject and self.soundView then
 					self.startMarker:initializeFromSoundObject()
 					self.endMarker:initializeFromSoundObject()
+					self.loopStartMarker:initializeFromSoundObject()
+					self.loopEndMarker:initializeFromSoundObject()
 				end
 			end,
 			
@@ -51,46 +59,13 @@ return {
 				self:drawBackground()
 				self.startMarker:draw()
 				self.endMarker:draw()
+				self.loopStartMarker:draw()
+				self.loopEndMarker:draw()
 			end,
 			
 			drawBackground = function(self)
 				love.graphics.setColor(0.1, 0.1, 0.1)
 				love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-			end,
-			
-			getStartMarkerScreenX = function(self)
-				return self.startMarker:getScreenX()
-			end,
-			
-			getStartMarkerSample = function(self)
-				local totalSample = self.startMarker:getSample()
-				
-				-- Debug output
-				if self.soundView and self.soundModel then
-					local marginLeft = self.soundView.marginLeft or 100
-					local sampleOffset = self.startMarker.imageX - marginLeft
-					print(string.format("Marker: imageX=%.2f, marginLeft=%.2f, sampleOffset=%d, channelCount=%d, totalSample=%d",
-						self.startMarker.imageX, marginLeft, sampleOffset, 
-						self.soundModel:getChannelCount(), totalSample))
-				end
-				
-				return totalSample
-			end,
-			
-			getStartMarkerProgress = function(self)
-				return self.startMarker:getProgress()
-			end,
-			
-			getEndMarkerSample = function(self)
-				return self.endMarker:getSample()
-			end,
-			
-			getEndMarkerProgress = function(self)
-				return self.endMarker:getProgress()
-			end,
-			
-			getEndMarkerScreenX = function(self)
-				return self.endMarker:getScreenX()
 			end,
 			
 			handleMousePressed = function(self, mx, my)
@@ -100,22 +75,33 @@ return {
 				if self.endMarker:handlePressed(mx, my) then
 					return true
 				end
+				if self.loopStartMarker:handlePressed(mx, my) then
+					return true
+				end
+				if self.loopEndMarker:handlePressed(mx, my) then
+					return true
+				end
 				return false
 			end,
 			
 			handleMouseReleased = function(self)
 				self.startMarker:handleReleased()
 				self.endMarker:handleReleased()
+				self.loopStartMarker:handleReleased()
+				self.loopEndMarker:handleReleased()
 			end,
 			
 			handleMouseDragged = function(self, mx, my)
 				self.startMarker:handleDragged(mx, my)
 				self.endMarker:handleDragged(mx, my)
+				self.loopStartMarker:handleDragged(mx, my)
+				self.loopEndMarker:handleDragged(mx, my)
 			end,
 			
 			update = function(self, dt)
 				-- Handle marker dragging
-				if self.startMarker.isDragging or self.endMarker.isDragging then
+				if self.startMarker.isDragging or self.endMarker.isDragging 
+					or self.loopStartMarker.isDragging or self.loopEndMarker.isDragging then
 					local mx, my = love.mouse.getPosition()
 					self:handleMouseDragged(mx, my)
 				end
