c8549e0611d2b623b31ad99fc23e08a8da41c4b3
Matthew Hopkins <matthop88@gmail.com>
2026-03-30 13:51:45 -0700

    Create musicData.lua

---
+++ tools/constructionSet/data/music/musicData.lua
@@ -0,0 +1,3 @@
+return {
+	greenHillZone   = { label = "Green Hill Zone",  filename = "ghz.mp3", volume = 0.5, startPoint = 14416, endPoint =  4707071, loopStartPoint = 1283220, loopEndPoint = 4669064 },
+}
