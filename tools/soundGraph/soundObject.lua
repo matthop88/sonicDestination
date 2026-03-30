3671ea6535f27533e81fe8dcbf8e75ff525b7073
Matthew Hopkins <matthop88@gmail.com>
2026-03-30 13:50:28 -0700

    Fixed indentation in soundObject.lua

--- tools/soundGraph/soundObject.lua
+++ tools/soundGraph/soundObject.lua
@@ -105,9 +105,9 @@ return {
 				return false
 			end,
 			
-		isLoopingEnabled = function(self)
-			return self.soundInfo and self.soundInfo.isMusic and self.loopMarkersEnabled
-		end,
+			isLoopingEnabled = function(self)
+				return self.soundInfo and self.soundInfo.isMusic and self.loopMarkersEnabled
+			end,
 			
 			checkLoopEndReached = function(self)
 				if self:isPlaying() and self:isLoopingEnabled() then
@@ -176,17 +176,17 @@ return {
 				self.loopEndPoint = value
 			end,
 			
-		setVolume = function(self, volume)
-			self.volume = volume
-			self.audioSource:setVolume(volume)
+			setVolume = function(self, volume)
+				self.volume = volume
+				self.audioSource:setVolume(volume)
+				
+				local cacheKey = self.soundInfo.key or self.soundInfo.filename
+				volumeCache[cacheKey] = volume
+			end,
 			
-			local cacheKey = self.soundInfo.key or self.soundInfo.filename
-			volumeCache[cacheKey] = volume
-		end,
-		
-		setLoopMarkersEnabled = function(self, enabled)
-			self.loopMarkersEnabled = enabled
-		end,
+			setLoopMarkersEnabled = function(self, enabled)
+				self.loopMarkersEnabled = enabled
+			end,
 			
 			enforceConstraints = function(self)
 				self.startPoint     = math.min(self.startPoint, self.endPoint)
