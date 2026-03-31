5a2feddbab7024849ad6a71f79b8a5c2706e5914
Matthew Hopkins <matthop88@gmail.com>
2026-03-31 10:56:47 -0700

    Updated miscellaneousPanel.lua to have better dimensions

--- tools/constructionSet/panels/miscellaneousPanel.lua
+++ tools/constructionSet/panels/miscellaneousPanel.lua
@@ -2,17 +2,21 @@ return {
     create = function(self)
         local buttonList = {}
         local WIDTH, HEIGHT = 256, 256
+        local WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800
 
-        table.insert(buttonList, require("tools/constructionSet/buttons/musicButton"):create {})
+        table.insert(buttonList, require("tools/constructionSet/buttons/musicButton"):create {
+            windowWidth = WINDOW_WIDTH,
+            windowHeight = WINDOW_HEIGHT,
+        })
 
         local palette = require("tools/constructionSet/palette"):create { objects = buttonList, CONTAINER_WIDTH = WIDTH, CONTAINER_HEIGHT = HEIGHT }
         
         return {
-            draw               = function(self, graphics)   palette:draw(graphics)             end,
-            update             = function(self, dt, mx, my) palette:update(dt, mx, my)         end,
-            handleMousepressed = function(self, mx, my)     palette:handleMousepressed(mx, my) end,
-            handleMousereleased = function(self, mx, my)    palette:handleMousereleased(mx, my) end,
-            handleKeypressed   = function(self, key)                                           end,
+            draw                = function(self, graphics)   palette:draw(graphics)             end,
+            update              = function(self, dt, mx, my) palette:update(dt, mx, my)         end,
+            handleMousepressed  = function(self, mx, my)     palette:handleMousepressed(mx, my) end,
+            handleMousereleased = function(self, mx, my)     palette:handleMousereleased(mx, my) end,
+            handleKeypressed    = function(self, key)                                           end,
         }
     end,
 }
