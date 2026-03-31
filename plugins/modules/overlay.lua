794a7f080f1e19b0dad5674bbe09460f9449b69a
Matthew Hopkins <matthop88@gmail.com>
2026-03-31 10:54:00 -0700

    Create overlay.lua plugin

---
+++ plugins/modules/overlay.lua
@@ -0,0 +1,47 @@
+return {
+	init = function(self, params)
+		self.components = {}
+		return self
+	end,
+	
+	add = function(self, component)
+		table.insert(self.components, component)
+		return self
+	end,
+	
+	draw = function(self, ...)
+		for _, component in ipairs(self.components) do
+			if component.draw then
+				component:draw(...)
+			end
+		end
+	end,
+	
+	update = function(self, dt, ...)
+		for _, component in ipairs(self.components) do
+			if component.update then
+				component:update(dt, ...)
+			end
+		end
+	end,
+	
+	handleMousepressed = function(self, mx, my)
+		for _, component in ipairs(self.components) do
+			if component.handleMousePressed then
+				local handled = component:handleMousePressed(mx, my)
+				if handled then
+					return true
+				end
+			end
+		end
+		return false
+	end,
+	
+	handleMousereleased = function(self, mx, my)
+		for _, component in ipairs(self.components) do
+			if component.handleMouseReleased then
+				component:handleMouseReleased(mx, my)
+			end
+		end
+	end,
+}
