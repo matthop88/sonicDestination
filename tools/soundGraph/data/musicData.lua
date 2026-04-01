4202cf2a6daa8105a165ae1c89e443b0f94a5c7d
Matthew Hopkins <matthop88@gmail.com>
2026-04-01 10:40:25 -0700

    Delete tools/soundGraph/data/musicData.lua

--- tools/soundGraph/data/musicData.lua
+++
@@ -1,4 +0,0 @@
-return {
-	constructionSet = { label = "Construction Set", filename = "construction.ogg", volume = 0.5, startPoint =     0, endPoint = 15168694, loopStartPoint = 1262066, loopEndPoint = 1578532 },
-	greenHillZone   = { label = "Green Hill Zone",  filename = "ghz.mp3",          volume = 0.5, startPoint = 14416, endPoint =  4707071, loopStartPoint = 1283220, loopEndPoint = 4669064 },
-}
