f58983a3c29b06ebc6a5a3916867e1ecb5f7dd1a
Matthew Hopkins <matthop88@gmail.com>
2026-04-03 08:37:01 -0700

    Changed label of Palm Tree Panic US Present to be more readable

--- game/music/musicData.lua
+++ game/music/musicData.lua
@@ -1,5 +1,5 @@
 return {
-	constructionSet     = { label = nil,                          filename = "construction.ogg", volume = 0.5, loopStartPoint = 0, loopEndPoint = 15168694 },
-	greenHillZone       = { label = "Green Hill Zone",            filename = "ghz.mp3",       volume = 0.5, startPoint = 14416, endPoint =  4707071, loopStartPoint = 1283220, loopEndPoint = 4669064 },
-	palmTreePanicPresUS = { label = "Palm Tree Panic Pres (US)",  filename = "ptpPresUS.mp3", volume = 0.5, startPoint = 0,     endPoint =  9600716, loopStartPoint = 1567904, loopEndPoint = 7078184 },
+	constructionSet     = { label = nil,                             filename = "construction.ogg", volume = 0.5, loopStartPoint = 0, loopEndPoint = 15168694 },
+	greenHillZone       = { label = "Green Hill Zone",               filename = "ghz.mp3",       volume = 0.5, startPoint = 14416, endPoint =  4707071, loopStartPoint = 1283220, loopEndPoint = 4669064 },
+	palmTreePanicPresUS = { label = "Palm Tree Panic Present (US)",  filename = "ptpPresUS.mp3", volume = 0.5, startPoint = 0,     endPoint =  9600716, loopStartPoint = 1567904, loopEndPoint = 7078184 },
 }
