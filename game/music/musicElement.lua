fea427f6f0e077a8f59d2c754b6ea5782d9cd82a
Matthew Hopkins <matthop88@gmail.com>
2026-04-03 14:42:53 -0700

    Update musicElement.lua; removed debug printlns

--- game/music/musicElement.lua
+++ game/music/musicElement.lua
@@ -74,11 +74,8 @@ return {
 				self:jumpToSample(self.endPoint)
 				self:pause()
 			end,
-		
+			
 			play = function(self)
-				local trackType = self.echoSource and "echo" or "main"
-				print(string.format("[musicElement] play() called: type=%s, musicPath=%s", trackType, self.musicPath))
-				
 				if not self.echoSource then
 					self:refreshVolume()
 					self.audioSource:play()
@@ -116,7 +113,7 @@ return {
 				local targetSample = self.loopStartPoint + delta
 				self:jumpToSample(targetSample)
 			end,
-				
+					
 			update = function(self, dt)
 				if self.echoSource and not self:isPlaying() then
 					local sourceSample = self.echoSource:getCurrentSample()
@@ -124,8 +121,6 @@ return {
 						self:refreshVolume()
 						local targetSample = sourceSample - self.echoDelay
 						self:jumpToSample(targetSample)
-						print(string.format("[musicElement] Echo starting at sample %d (source at %d, delay %d samples)", 
-							targetSample, sourceSample, self.echoDelay))
 						self.audioSource:play()
 					end
 				end
@@ -145,10 +140,8 @@ return {
 				if self.volumeFn then volume = self.volumeFn() end
 				return volume
 			end,
-	
+			
 			setVolume = function(self, volume)
-				print(string.format("[musicElement] setVolume called: volume=%.2f, has volumeFn=%s, musicPath=%s", 
-					volume, tostring(self.volumeFn ~= nil), self.musicPath))
 				self.volume = volume
 				self:refreshVolume()
 			end,
@@ -157,28 +150,21 @@ return {
 				self.volumeFn = volumeFn
 				self:refreshVolume()
 			end,
-			
+					
 			setEchoSource = function(self, sourceTrack, delay, strength)
 				self.echoSource = sourceTrack
 				self.echoDelay = delay * 44100
 				self.echoStrength = strength
-				print(string.format("[musicElement] setEchoSource: delay=%.2f sec (%d samples), strength=%.2f", 
-					delay, self.echoDelay, strength))
 			end,
-	
+			
 			setVolumeScalar = function(self, volumeScalar)
-				print(string.format("[musicElement] setVolumeScalar called: scalar=%.2f, has volumeFn=%s, musicPath=%s", 
-					volumeScalar, tostring(self.volumeFn ~= nil), self.musicPath))
 				self.volumeScalar = volumeScalar
 				self:refreshVolume()
 			end,
-		
+				
 			refreshVolume = function(self)
 				local calculatedVolume = self:getVolume()
 				local finalVolume = calculatedVolume * self.volumeScalar
-				local echoInfo = self.echoSource and string.format("echo(sampleDelay=%d)", self.echoDelay) or "main"
-				print(string.format("[musicElement] refreshVolume: %s, baseVolume=%.2f, calculatedVolume=%.2f, scalar=%.2f, finalVolume=%.2f", 
-					echoInfo, self.volume, calculatedVolume, self.volumeScalar, finalVolume))
 				self.audioSource:setVolume(finalVolume)
 			end,
 			
