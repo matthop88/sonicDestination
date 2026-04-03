3de8be837dd3b84cfbd2e131e4c7cb44f37d5a69
Matthew Hopkins <matthop88@gmail.com>
2026-04-03 14:24:35 -0700

    Updated musicManager.lua to have enhanced echo algorithm

--- game/music/musicManager.lua
+++ game/music/musicManager.lua
@@ -5,12 +5,14 @@ return {
 		return {
 			tracks       = {},
 			volumeScalar = 1,
-			
-			newTrack = function(self, trackName, effectName)
+					
+			newTrack = function(self, trackName, effectName, delay, strength, echoCount)
+				print(string.format("[musicManager] newTrack called: trackName=%s, effect=%s, delay=%.2f, strength=%.2f, echoCount=%d, current track count=%d", 
+					trackName, tostring(effectName), delay or 0, strength or 0, echoCount or 6, #self.tracks))
 				if effectName == "Reverb" then
-					self:newReverbTrack(trackName)
+					self:newReverbTrack(trackName, delay, strength)
 				elseif effectName == "Echo" then
-					self:newEchoTrack(trackName)
+					self:newEchoTrack(trackName, delay, strength, echoCount)
 				else
 					local musicElement = require("game/music/musicElement"):create(MUSIC_DATA, trackName)
 					table.insert(self.tracks, musicElement)
@@ -19,32 +21,39 @@ return {
 				end
 			end,
 
-			newReverbTrack = function(self, trackName)
+			newReverbTrack = function(self, trackName, delay, strength)
 				local musicElement = require("game/music/musicElement"):create(MUSIC_DATA, trackName)
 				musicElement:setVolumeScalar(self.volumeScalar)
 				table.insert(self.tracks, musicElement)
 				local musicElementEcho = require("game/music/musicElement"):create(MUSIC_DATA, trackName)
-				musicElementEcho:setDelay(0.3)
-				musicElementEcho:setVolumeFn(function() return musicElement:getVolume() * 0.5 end)
+				musicElementEcho:setDelay(delay / 4)
+				musicElementEcho:setVolumeFn(function() return musicElement:getVolume() * strength end)
 				musicElementEcho:setVolumeScalar(self.volumeScalar)
 				table.insert(self.tracks, musicElementEcho)
 			end,
-
-			newEchoTrack = function(self, trackName)
+		
+			newEchoTrack = function(self, trackName, delay, strength, echoCount)
+				echoCount = echoCount or 6
+				print(string.format("[musicManager] newEchoTrack called: trackName=%s, delay=%.2f, strength=%.2f, echoCount=%d", trackName, delay, strength, echoCount))
 				local musicElement = require("game/music/musicElement"):create(MUSIC_DATA, trackName)
 				musicElement:setVolumeScalar(self.volumeScalar)
 				table.insert(self.tracks, musicElement)
-				local decayRate = 0.5
-				for i = 1, 4 do
+				print(string.format("[musicManager] Created main track, now have %d tracks", #self.tracks))
+				
+				local previousTrack = musicElement
+				for i = 1, echoCount do
 					local musicElementEcho = require("game/music/musicElement"):create(MUSIC_DATA, trackName)
-					musicElementEcho:setDelay(1 * i)
-					musicElementEcho:setVolumeFn(function() return musicElement:getVolume() * (decayRate ^ i) end)
+					musicElementEcho:setEchoSource(previousTrack, delay, strength)
 					musicElementEcho:setVolumeScalar(self.volumeScalar)
 					table.insert(self.tracks, musicElementEcho)
+					print(string.format("[musicManager] Created echo track %d (delay=%.2f sec, %d samples), now have %d tracks", 
+						i, delay, delay * 44100, #self.tracks))
+					previousTrack = musicElementEcho
 				end
 			end,
-			
+					
 			play = function(self)
+				print(string.format("[musicManager] play() called: playing %d tracks", #self.tracks))
 				for _, track in ipairs(self.tracks) do
 					track:play()
 				end
@@ -67,22 +76,29 @@ return {
 					track:update(dt)
 				end
 			end,
-			
+					
 			clear = function(self)
+				print(string.format("[musicManager] clear() called: clearing %d tracks", #self.tracks))
 				for _, track in ipairs(self.tracks) do
 					track:stop()
 				end
 				self.tracks = {}
 			end,
-				
+						
 			setVolume = function(self, volume)
+				print(string.format("[musicManager] setVolume(%.2f) called: %d tracks", volume, #self.tracks))
 				self.volumeScalar = volume
 				for _, track in ipairs(self.tracks) do
-					track:setVolume(volume)
+					if not track.echoSource then
+						track:setVolume(volume)
+					else
+						track:refreshVolume()
+					end
 				end
 			end,
 			
 			setPitch = function(self, pitch)
+				print(string.format("[musicManager] setPitch(%.2f) called: %d tracks", pitch, #self.tracks))
 				for _, track in ipairs(self.tracks) do
 					track:setPitch(pitch)
 				end
