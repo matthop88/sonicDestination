e8c2dac7a54554947a7ecf2dd9b3f358e552dac7
Matthew Hopkins <matthop88@gmail.com>
2026-04-10 15:08:09 -0700

    Updated bigBall.lua to move when contacting Sonic. Moves wrong way.

--- game/world/gameObjects/objects/bigBall.lua
+++ game/world/gameObjects/objects/bigBall.lua
@@ -3,7 +3,10 @@ local SOUND_MANAGER = requireRelative("sound/soundManager")
 return {
 	create = function(self)
 		return {
-			
+			onCollisionWithPlayer = function(self, player)
+				self:setXSpeed(3)
+			end,
+
 		}
 	end,            
 }
