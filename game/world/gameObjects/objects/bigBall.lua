f4d711d0a66e46ad2a32a3a2b25a660f46786061
Matthew Hopkins <matthop88@gmail.com>
2026-04-10 16:52:22 -0700

    Updated bigBall.lua

--- game/world/gameObjects/objects/bigBall.lua
+++ game/world/gameObjects/objects/bigBall.lua
@@ -3,9 +3,33 @@ local SOUND_MANAGER = requireRelative("sound/soundManager")
 return {
 	create = function(self)
 		return {
+			rotation = 0,
+
 			onCollisionWithPlayer = function(self, player)
-				self:setXSpeed(-30)
+				self:setXSpeed(-16 * math.pi)
 			end,
+
+			draw = function(self) 
+                if self.active then
+                    self.sprite:drawRotated(self.x, self.y, self.rotation) 
+                end
+                if self.selectedInVisualizer then
+                    self.sprite:drawBorder(self.x, self.y)
+                end
+            end,
+
+            update = function(self, dt)
+                if self.active then
+                    self.sprite:update(dt)
+                    self:updateHitBox(dt)
+                    self.deleted = self.sprite.deleted
+                    local deltaX = self:getXVelocity() * dt
+                    self:setX(self:getX() + deltaX)
+                    self.rotation = self.rotation + (deltaX / 24)
+                    self:setY(self:getY() + (self:getYSpeed()    * dt))
+                    
+                end
+            end,
 
 		}
 	end,            
