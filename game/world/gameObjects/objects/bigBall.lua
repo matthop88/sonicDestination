c172734ae925dacc2b2912cc412c41f618dea79a
Matthew Hopkins <matthop88@gmail.com>
2026-04-10 17:19:11 -0700

    Updated bigBall.lua to refactor to calculateSpeed()

--- game/world/gameObjects/objects/bigBall.lua
+++ game/world/gameObjects/objects/bigBall.lua
@@ -1,15 +1,22 @@
 local SOUND_MANAGER = requireRelative("sound/soundManager")
 
+local ACCELERATION = 100
+local MAX_SPEED    = 180
+
 return {
 	create = function(self)
 		return {
 			rotation = 0,
-			acceleration = -150,
-			maxSpeed     = -180,
+			vector   = 0,
 			colliding = false,
 
 			onCollisionWithPlayer = function(self, player)
 				self.colliding = true
+				if player:getX() < self:getX() then
+					self.vector = -1
+				else
+					self.vector = 1
+				end
 			end,
 
 			draw = function(self) 
@@ -23,12 +30,7 @@ return {
 
             update = function(self, dt)
                 if self.active then
-                	if self.colliding then
-                		self.xSpeed = math.max(self.maxSpeed, self.xSpeed + (self.acceleration * dt))
-                	else
-                		self.xSpeed = math.min(0, self.xSpeed - (self.acceleration * dt))
-                	end
-                	self.colliding = false
+                	self:calculateSpeed(dt)
                     self.sprite:update(dt)
                     self:updateHitBox(dt)
                     self.deleted = self.sprite.deleted
@@ -38,6 +40,23 @@ return {
                     self:setY(self:getY() + (self:getYSpeed()    * dt))
                     
                 end
+            end,
+
+            calculateSpeed = function(self, dt)
+            	if self.colliding then
+            		if self.vector == -1 then
+            			self.xSpeed = math.max(-MAX_SPEED, self.xSpeed - (ACCELERATION * dt))
+            		else
+            			self.xSpeed = math.min(MAX_SPEED, self.xSpeed + (ACCELERATION * dt))
+            		end
+            	else
+            		if self.vector == -1 then
+            			self.xSpeed = math.min(0, self.xSpeed + (ACCELERATION * dt))
+            		else
+            			self.xSpeed = math.max(0, self.xSpeed - (ACCELERATION * dt))
+            		end
+            	end
+            	self.colliding = false
             end,
 
 		}
