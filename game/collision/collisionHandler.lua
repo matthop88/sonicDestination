09c5a8aefeb63ca6c6d1cbdb7cda98a28247377d
Matthew Hopkins <matthop88@gmail.com>
2026-04-14 15:04:23 -0700

    Updated collisionHandler.lua to have collision with objects harmful to NPCs

--- game/collision/collisionHandler.lua
+++ game/collision/collisionHandler.lua
@@ -2,4 +2,10 @@ return {
 	handleCollisionWithPlayer = function(self, object, player)
 		object:onCollisionWithPlayer(player)
 	end,
+
+	handleCollisionWithDangerousToNPCs = function(self, object, objectDangerousToNPCs)
+		if object.onCollisionWithDangerousToNPCs then
+			object:onCollisionWithDangerousToNPCs(objectDangerousToNPCs)
+		end
+	end,
 }
