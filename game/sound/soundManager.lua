0a69638101bce23252c7bd8db468c2d850a29b28
Matthew Hopkins <matthop88@gmail.com>
2026-04-14 17:38:30 -0700

    Added sound overrides to sound manager

--- game/sound/soundManager.lua
+++ game/sound/soundManager.lua
@@ -45,6 +45,10 @@ return ({
         if propData.volume and propData.volume.sounds then
             self.volumeScalar = propData.volume.sounds
         end
+        if propData.jumpSound        then self:setOverride("sonicJumping", propData.jumpSound)        end
+        if propData.sonicHitSound    then self:setOverride("sonicHit",     propData.sonicHitSound)    end
+        if propData.badnikDeathSound then self:setOverride("badnikDeath",  propData.badnikDeathSound) end
+    
     end,
         
 }):init()
