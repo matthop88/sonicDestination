86afb2e4834104f8a87501f01b3086a7657bdc37
Matthew Hopkins <matthop88@gmail.com>
2026-04-16 11:24:18 -0700

    Extracted ok button creation code from soundsPanel.lua

--- tools/constructionSet/sounds/soundsPanel.lua
+++ tools/constructionSet/sounds/soundsPanel.lua
@@ -2,44 +2,6 @@ local COLOR = require("tools/lib/colors")
 local verticalSliderPane = require("tools/lib/components/verticalSliderPane")
 local horizontalSlider = require("tools/lib/components/horizontalSlider")
 
-local createOkButton = function(params)
-	local buttonFontSize = params.fontSize or 16
-	local buttonFont     = love.graphics.newFont(buttonFontSize)
-
-	return {
-		x = params.x,
-		y = params.y,
-		width = params.width or 100,
-		height = params.height or 40,
-		hovered = false,
-		cornerRadius = 10,
-		
-		draw = function(self)
-			if self.hovered then
-				love.graphics.setColor(COLOR.MEDIUM_GREY)
-			else
-				love.graphics.setColor(COLOR.MEDIUM_DARK_GREY)
-			end
-			love.graphics.rectangle("fill", self.x, self.y, self.width, self.height, self.cornerRadius, self.cornerRadius)
-			love.graphics.setColor(COLOR.PURE_WHITE)
-			love.graphics.rectangle("line", self.x, self.y, self.width, self.height, self.cornerRadius, self.cornerRadius)
-			
-			love.graphics.setFont(buttonFont)
-			love.graphics.printf("OK", self.x, self.y + 10, self.width, "center")
-		end,
-		
-		update = function(self, mx, my)
-			self.hovered = self:containsPoint(mx, my)
-		end,
-		
-		containsPoint = function(self, mx, my)
-			return mx >= self.x and mx <= self.x + self.width and
-			       my >= self.y and my <= self.y + self.height
-		end,
-	}
-end
-
-				
 return {
 	create = function(self, params)
 		local okButton = nil
@@ -52,7 +14,7 @@ return {
 			visible = false,
 				
 			init = function(self)
-				okButton = createOkButton {
+				okButton = require("tools/lib/components/okButton"):create {
 					x = self.x + self.width - 120,
 					y = self.y + self.height - 60,
 					width = 100,
