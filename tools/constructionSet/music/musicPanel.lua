a50bf11278f20e0a55e4887a3c15162e1949f2a2
Matthew Hopkins <matthop88@gmail.com>
2026-04-16 11:24:54 -0700

    Extracted ok button creation code from musicPanel

--- tools/constructionSet/music/musicPanel.lua
+++ tools/constructionSet/music/musicPanel.lua
@@ -66,39 +66,6 @@ local createMusicNameField = function(params)
 	return field
 end
 
-local createOkButton = function(params)
-	return {
-		x = params.x,
-		y = params.y,
-		width = params.width or 100,
-		height = params.height or 40,
-		hovered = false,
-		cornerRadius = 10,
-		
-		draw = function(self)
-			if self.hovered then
-				love.graphics.setColor(COLOR.MEDIUM_GREY)
-			else
-				love.graphics.setColor(COLOR.MEDIUM_DARK_GREY)
-			end
-			love.graphics.rectangle("fill", self.x, self.y, self.width, self.height, self.cornerRadius, self.cornerRadius)
-			love.graphics.setColor(COLOR.PURE_WHITE)
-			love.graphics.rectangle("line", self.x, self.y, self.width, self.height, self.cornerRadius, self.cornerRadius)
-			
-			love.graphics.printf("OK", self.x, self.y + 10, self.width, "center")
-		end,
-		
-		update = function(self, mx, my)
-			self.hovered = self:containsPoint(mx, my)
-		end,
-		
-		containsPoint = function(self, mx, my)
-			return mx >= self.x and mx <= self.x + self.width and
-			       my >= self.y and my <= self.y + self.height
-		end,
-	}
-end
-
 local changeMusicTrack = function(trackName, effectName, echoCount)
 	MUSIC_MANAGER:clear()
 	if trackName ~= "None" then
@@ -253,7 +220,7 @@ return {
 					end,
 				}
 				
-				okButton = createOkButton {
+				okButton = require("tools/lib/components/okButton"):create {
 					x = self.x + self.width - 120,
 					y = self.y + self.height - 60,
 					width = 100,
