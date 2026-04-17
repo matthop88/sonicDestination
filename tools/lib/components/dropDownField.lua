7e2ec45288735827f30352fcc752ce0fb6b376b7
Matthew Hopkins <matthop88@gmail.com>
2026-04-17 11:00:48 -0700

    Created dropDownField.lua component

---
+++ tools/lib/components/dropDownField.lua
@@ -0,0 +1,91 @@
+local COLOR = require("tools/lib/colors")
+
+return {
+	create = function(self, params)
+		local fieldFont = love.graphics.newFont(20)
+		return {
+			x = params.x,
+			y = params.y,
+			width = params.width,
+			height = params.height or 50,
+			label = params.label or "",
+			selectedValue = params.selectedValue or "None",
+			hovered = false,
+			list = nil,
+			listItems = params.list or {},
+
+			draw = function(self)
+				-- Draw field background
+				if self.hovered then
+					love.graphics.setColor(COLOR.LIGHTER_GREY)
+				else
+					love.graphics.setColor(COLOR.MEDIUM_DARK_GREY)
+				end
+				love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+				love.graphics.setColor(COLOR.PURE_WHITE)
+				love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+				
+				-- Draw label and selected value
+				love.graphics.setColor(COLOR.PURE_WHITE)
+				love.graphics.setFont(fieldFont)
+				
+				if self.label and self.label ~= "" then
+					love.graphics.print(self.label .. ":", self.x + 10, self.y + 15)
+					local labelWidth = fieldFont:getWidth(self.label .. ": ")
+					love.graphics.print(self.selectedValue, self.x + 10 + labelWidth, self.y + 15)
+				else
+					love.graphics.printf(self.selectedValue, self.x + 10, self.y + 15, self.width - 20, "left")
+				end
+			end,
+			
+			update = function(self, dt, mx, my)
+				self.hovered = self:containsPoint(mx, my)
+				if self.list then
+					self.list:update(dt, mx, my)
+				end
+			end,
+
+			handleMousepressed = function(self, mx, my)
+				if self:containsPoint(mx, my) then
+					self:showList()
+					return true
+				end
+			end,
+			
+			containsPoint = function(self, mx, my)
+				return mx >= self.x and mx <= self.x + self.width and
+				       my >= self.y and my <= self.y + self.height
+			end,
+			
+			setSelectedValue = function(self, value)
+				self.selectedValue = value
+			end,
+
+			showList = function(self)
+				if not self.list then
+					self:initializeList()
+				end
+				self.list:setVisible(true)
+			end,
+
+			initializeList = function(self)
+				self.list = require("tools/lib/guiList/list"):create {
+					x      = self.x,
+					y      = self.y + self.height,
+					width  = self.width,
+					height = 400,
+					fontSize = 24,
+					scrollSpeed = 1200,
+					items = self.listItems,
+					onItemSelected = function(list, item, index)
+						list:setVisible(false)
+					end,
+				}
+			
+				if _G.getModals then
+					getModals():add(self.list)
+				end
+			end,
+		}
+	end,
+}
