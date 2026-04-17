c074ab046205b79de4191645b754092a475c6612
Matthew Hopkins <matthop88@gmail.com>
2026-04-17 10:59:22 -0700

    Updated game.lua properties to include bowling strike sound effect

--- game/properties/game.lua
+++ game/properties/game.lua
@@ -10,8 +10,8 @@ return {
     -- For sonicHitSound, default is "sonicHit", and alternates are "iceExplode" and "klankOuch".
     sonicHitSound = "sonicHit",
 
-    -- For badnik death sound, default is "badnikDeath"; alternate is "smoosh".
-    badnikDeathSound = "smoosh",
+    -- For badnik death sound, default is "badnikDeath"; alternates are "bowlingStrike" and "smoosh".
+    badnikDeathSound = "bowlingStrike",
 
-    volume = { music = 0.3, sounds = 0.9 },
+    volume = { music = 0.5, sounds = 0.9 },
 }
