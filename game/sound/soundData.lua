5890b9e22e4ea953748ad880e27e526cd9cf989c
Matthew Hopkins <matthop88@gmail.com>
2026-04-21 14:29:13 -0700

    Updated soundData.lua; hard coded reverb data into rings and bowling strike

--- game/sound/soundData.lua
+++ game/sound/soundData.lua
@@ -2,8 +2,8 @@ return {
 	sonicBraking   = { label = "Sonic Braking",    filename = "brake.ogg",             volume = 0.2,  startPoint =  1982, endPoint =  44098, },
 	sonicJumping   = { label = "Sonic Jumping",    filename = "jump.ogg",              volume = 0.2,                      endPoint =  38712, },
 	sonicCDJumping = { label = "Sonic CD Jumping", filename = "sonicCDJump.mp3",       volume = 0.9,  startPoint = 19520, endPoint =  38519, },
-	ringCollectL   = { label = "Ring Collect L",   filename = "ring-collect-L.mp3",    volume = 0.4,  startPoint =  1636, endPoint =  55260, },
-	ringCollectR   = { label = "Ring Collect R",   filename = "ring-collect-R.mp3",    volume = 0.4,  startPoint =  2984, endPoint =  58018, },
+	ringCollectL   = { label = "Ring Collect L",   filename = "ring-collect-L.mp3",    volume = 0.4,  startPoint =  1636, endPoint =  55260, effect = { type = "Reverb", delay = 0.4, strength = 0.8 } },
+	ringCollectR   = { label = "Ring Collect R",   filename = "ring-collect-R.mp3",    volume = 0.4,  startPoint =  2984, endPoint =  58018, effect = { type = "Reverb", delay = 0.4, strength = 0.8 } },
 	giantRing      = { label = "Giant Ring",       filename = "giantRing.mp3",         volume = 0.5,  startPoint =  4352, endPoint =  95882, },
 	vanish         = { label = "Vanish",           filename = "vanish.mp3",            volume = 0.5,  startPoint =  3968, endPoint = 162026, },
 	badnikDeath    = { label = "Badnik Death",     filename = "badnik-death.ogg",      volume = 0.5,  startPoint =  5328, endPoint =  45192, },
@@ -18,7 +18,7 @@ return {
         { filename = "extra/Ooouuucchh.wav",  volume = 0.8, startPoint =  8182, endPoint =  38054, delay = 0.25 },
     },
     bowlingStrike  = { 
-    	label = "Bowling Strike",   filename = "extra/emycutiepantsBowlingStrike.mp3",
+    	label = "Bowling Strike",   filename = "extra/emycutiepantsBowlingStrike.mp3", effect = { type = "Reverb", delay = 0.4, strength = 0.8 },
     	-- Credit to Emycutiepants https://pixabay.com/users/_emycutiepants_-50116316 
     },
 }
