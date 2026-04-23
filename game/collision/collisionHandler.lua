1c7f31de1c5eea79f17500f9f297f6e16011b08e
Matthew Hopkins <matthop88@gmail.com>
2026-04-23 16:30:49 -0700

    Added onCollisionWithSolid() to collisionHandler

--- game/collision/collisionHandler.lua
+++ game/collision/collisionHandler.lua
@@ -8,4 +8,10 @@ return {
 			object:onCollisionWithDangerousToNPCs(objectDangerousToNPCs)
 		end
 	end,
+
+	handleCollisionWithSolid = function(self, object, solidObject)
+		if object.onCollisionWithSolid then
+			object:onCollisionWithSolid(solidObject)
+		end
+	end,
 }
