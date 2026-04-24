a20e6317e9e78e601b56402ed3516c34b86e12b6
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 12:50:55 -0700

    Added animation facility to moveTowardsEdge

--- game/world/badniks/scripts/commands/moveTowardsEdge.lua
+++ game/world/badniks/scripts/commands/moveTowardsEdge.lua
@@ -1,12 +1,14 @@
 return function(params)
     return {
-        xSpeed   =  params.xSpeed     or 100,
+        xSpeed    = params.xSpeed     or 100,
+        animation = params.animation,
         
         reset = function(self)
             self.elapsed = 0
         end,
 
         execute = function(self, dt, actor)
+            if self.animation then actor:setAnimationByLabel(self.animation) end
             if (actor.scanGround and not actor:scanGround()) or actor.hitSolid then
                 self:reset()
                 return true
