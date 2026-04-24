526a0f73631a988576324b170cb7b95ead0838ad
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 12:52:03 -0700

    Update wait.lua

--- game/world/badniks/scripts/commands/wait.lua
+++ game/world/badniks/scripts/commands/wait.lua
@@ -1,13 +1,15 @@
 return function(params)
     return {
-        duration =  params.numSeconds or 1,
-        elapsed  =  0,
+        duration  = params.numSeconds or 1,
+        elapsed   = 0,
+        animation = params.animation,
 
         reset = function(self)
             self.elapsed = 0
         end,
 
         execute = function(self, dt, actor)
+            if self.animation then actor:setAnimationByLabel(self.animation) end
             actor:setXSpeed(0)
             actor:setYSpeed(0)
             self.elapsed = self.elapsed + dt
