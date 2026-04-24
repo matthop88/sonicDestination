45bd6c32283f574e381d055acb2e309e22fa4d37
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 15:22:09 -0700

    Updated branchIfPlayerAt.lua to allow detection within your threshold, directional facing logic

--- game/world/badniks/scripts/commands/branchIfPlayerAt.lua
+++ game/world/badniks/scripts/commands/branchIfPlayerAt.lua
@@ -1,8 +1,13 @@
 local createIsPlayerWithinThresholdFunction = function(deltaX, program)
-    return function(badnikX)
+    return function(badnikX, badnikY, xFlip)
         local playerX = GLOBALS:getPlayer():getX() 
-        if math.abs(playerX - badnikX) < deltaX then
-            return program
+        local playerY = GLOBALS:getPlayer():getY()
+        if math.abs(playerY - badnikY) < 50 then
+            if badnikX - playerX > 0 and badnikX - playerX < deltaX and not xFlip then
+                return program
+            elseif playerX - badnikX > 0 and playerX - badnikX < deltaX and xFlip then
+                return program
+            end
         end
     end
 end
