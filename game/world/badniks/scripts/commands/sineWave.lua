b1bf62de122c86bfd6639c525d97b92b412c4faf
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 12:51:41 -0700

    Updated sineWave.lua to be animation savvy

--- game/world/badniks/scripts/commands/sineWave.lua
+++ game/world/badniks/scripts/commands/sineWave.lua
@@ -6,12 +6,14 @@ return function(params)
         amplitude  = params.amplitude  or 100,
         elapsed    = 0,
         radians    = 0,
+        animation  = params.animation,
 
         reset = function(self)
             self.elapsed = 0
         end,
 
         execute = function(self, dt, actor)
+            if self.animation then actor:setAnimationByLabel(self.animation) end
             self.radians = self.radians + (self.speed * dt)
             actor:setXSpeed(self.wavelength * self.speed)
             actor:setYSpeed(math.sin(self.radians) * self.amplitude * self.speed)
