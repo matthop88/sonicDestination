a4e37286d2a9e2776993548baa7ba3b473b0f7db
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 15:23:00 -0700

    Added directional and y threshold capabilities to scriptEngine

--- game/world/badniks/scripts/lib/scriptEngine.lua
+++ game/world/badniks/scripts/lib/scriptEngine.lua
@@ -2,7 +2,7 @@ return {
     execute = function(self, dt, programs, actor)
         local program = programs:getCurrent()
         if program.playerThresholdBranch then
-            local targetProg = program.playerThresholdBranch(actor:getX())
+            local targetProg = program.playerThresholdBranch(actor:getX(), actor:getY(), actor.xFlip)
             if targetProg then
                 programs:setCurrent(targetProg)
                 program = programs:getCurrent()
