7f9610b1c802571cdab2b5a1403280866c1428c1
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 15:11:30 -0700

    Updated scriptEngine.lua to check player x coordinates

--- game/world/badniks/scripts/lib/scriptEngine.lua
+++ game/world/badniks/scripts/lib/scriptEngine.lua
@@ -1,7 +1,15 @@
 return {
-    execute = function(self, dt, program, actor)
+    execute = function(self, dt, programs, actor)
+        local program = programs:getCurrent()
+        if program.playerThresholdBranch then
+            local targetProg = program.playerThresholdBranch(actor:getX())
+            if targetProg then
+                programs:setCurrent(targetProg)
+                program = programs:getCurrent()
+            end
+        end
         local instruction = program:get()
-        if instruction:execute(dt, actor) then
+        if instruction:execute(dt, actor, programs) then
             program:next()
         end
     end,
