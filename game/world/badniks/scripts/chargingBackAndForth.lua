4aa96edb5e869d406798fc3c8327a4965794d286
Matthew Hopkins <matthop88@gmail.com>
2026-04-24 14:40:17 -0700

    Multiplexing programs, adding two new commands in chargingBackAndForth.lua

--- game/world/badniks/scripts/chargingBackAndForth.lua
+++ game/world/badniks/scripts/chargingBackAndForth.lua
@@ -1,14 +1,23 @@
-local MOVE_TOWARDS_EDGE  = requireRelative("world/badniks/scripts/commands/moveTowardsEdge")
-local WAIT               = requireRelative("world/badniks/scripts/commands/wait")
-local FLIPX              = requireRelative("world/badniks/scripts/commands/flipX")
+local MOVE_TOWARDS_EDGE   = requireRelative("world/badniks/scripts/commands/moveTowardsEdge")
+local WAIT                = requireRelative("world/badniks/scripts/commands/wait")
+local FLIPX               = requireRelative("world/badniks/scripts/commands/flipX")
+local BRANCH_IF_PLAYER_AT = requireRelative("world/badniks/scripts/commands/branchIfPlayerAt")
+local GO_TO_PROGRAM       = requireRelative("world/badniks/scripts/commands/goToProgram")
 
 return {
-	name    = "chargingBackAndForth",
-	title   = "Charging Back and Forth",
-	program = {
-		MOVE_TOWARDS_EDGE { xSpeed     = 200, animation = "charging", },
-		WAIT              { numSeconds =   1, animation = "moving",   },
-		FLIPX(),
-		WAIT              { numSeconds =   1, },
+	name     = "chargingBackAndForth",
+	title    = "Charging Back and Forth",
+	programs = {
+		default = {
+			BRANCH_IF_PLAYER_AT { deltaX   = 100, program = "charging", },
+			MOVE_TOWARDS_EDGE { xSpeed     =  50, animation = "moving", },
+			WAIT              { numSeconds =   1, },
+			FLIPX(),
+			WAIT              { numSeconds =   1, },
+		},
+		charging = {
+			MOVE_TOWARDS_EDGE { xSpeed     = 200, animation = "charging", },
+			GO_TO_PROGRAM     { program = "default", },
+		}
 	},
 }
