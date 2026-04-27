f90fa9c7ddd726d54ac35a9b2578f0f5826c9d83
Matthew Hopkins <matthop88@gmail.com>
2026-04-27 16:51:00 -0700

    Updated patabata.lua to notify badnik hit

--- game/world/gameObjects/objects/patabata.lua
+++ game/world/gameObjects/objects/patabata.lua
@@ -11,15 +11,18 @@ return {
 				if player:isSpinning() then
 					self:setAnimation("dying")
 					self:setDead()
-					SOUND_MANAGER:play("badnikDeath")
+					SOUND_MANAGER:playAction("badnikHit")
         			player:reboundIfPossible(self.y, 90)
 				end
 			end,
 
 			onCollisionWithDangerousToNPCs = function(self, dangerousObject)
 				self:setAnimation("dying")
+				if dangerousObject.notifyBadnikDeath then
+					dangerousObject:notifyBadnikDeath(self)
+				end 
 				self:setDead()
-				SOUND_MANAGER:play("badnikDeath")
+				SOUND_MANAGER:playAction("badnikHit")
 			end,
 		}
 	end,
