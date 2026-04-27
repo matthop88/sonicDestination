feef03cace6372cb372dc47059ba31d703fe6e8f
Matthew Hopkins <matthop88@gmail.com>
2026-04-27 15:27:18 -0700

    Updated tamabboh.lua to play "YAAAH!" when falling to death

--- game/world/gameObjects/objects/tamabboh.lua
+++ game/world/gameObjects/objects/tamabboh.lua
@@ -1,5 +1,6 @@
 local SOUND_MANAGER  = requireRelative("sound/soundManager")
 local SCRIPT_REPO    = requireRelative("world/badniks/scripts/lib/scriptRepo")
+local SCRIPT_ENGINE  = requireRelative("world/badniks/scripts/lib/scriptEngine")
 
 return {
 	create = function(self)
@@ -7,7 +8,9 @@ return {
 		local SENSOR_DY = 21
 
 		return {
-			script   = SCRIPT_REPO:get("chargingBackAndForth"),
+			script         = SCRIPT_REPO:get("chargingBackAndForth"),
+			dangerousSpeed = 175,
+			fallingToDeath = false,
 
 			onCollisionWithPlayer = function(self, player)
 				if player:isSpinning() then
@@ -42,6 +45,47 @@ return {
 
 				return self.groundScanner:scan()
 			end,
+
+			update = function(self, dt)
+                if self.active then
+                	self:initGravityScanner()
+                	local nearestGroundLevel = nil
+                	if self.ySpeed >= 0 and not self.fallingToDeath then
+                		nearestGroundLevel = self.gravityScanner:findNearestGroundWithin(self.ySpeed * dt)
+                		if nearestGroundLevel == nil and self.ySpeed == 0 then
+							if not self.gravityScanner:findNearestGroundWithin(8192) then
+								self.fallingToDeath = true
+								SOUND_MANAGER:play("yaaaaah")
+							end
+						end
+                	end
+                	if nearestGroundLevel == nil then
+                		self.ySpeed = self.ySpeed + (787.0 * dt)
+                	end
+                    if self.script and self:isAlive() then SCRIPT_ENGINE:execute(dt, self.script, self) end
+                    self.sprite:update(dt)
+                    self:updateHitBox(dt)
+                    self.deleted = self.sprite.deleted
+                    local deltaY = self.ySpeed * dt
+                    if nearestGroundLevel and nearestGroundLevel < deltaY then 
+						if self.ySpeed > 0 then
+							self:setY(self:getY() + nearestGroundLevel)
+							self.ySpeed = 0
+						end
+					else
+						self:setY(self:getY() + deltaY)
+                    end
+                    self:setX(self:getX() + (self:getXVelocity() * dt))
+                    self.hitSolid = false
+                    if self.y >= 65536 then self.deleted = true end
+                end
+            end,
+
+			initGravityScanner = function(self)
+            	if not self.gravityScanner then
+            		self.gravityScanner = requireRelative("collision/sensors/badniks/groundScanner"):create { OWNER = self, GRAPHICS = self.graphics, WORLD = self.world, dx = -6, dy = 21 }
+            	end
+            end,
 
 		}
 	end,
