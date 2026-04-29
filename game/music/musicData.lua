08ec16e32b8a06a719f642a93daef0b122d97cb6
Matthew Hopkins <matthop88@gmail.com>
2026-04-29 15:14:50 -0700

    Updated musicData.lua to include Ice Cap Zone Prototype 1

--- game/music/musicData.lua
+++ game/music/musicData.lua
@@ -2,4 +2,5 @@ return {
 	constructionSet     = { label = nil,                             filename = "construction.ogg", volume = 0.5, loopStartPoint = 0, loopEndPoint = 15168694 },
 	greenHillZone       = { label = "Green Hill Zone",               filename = "ghz.mp3",       volume = 0.5, startPoint = 14416, endPoint =  4707071, loopStartPoint = 1283220, loopEndPoint = 4669064 },
 	palmTreePanicPresUS = { label = "Palm Tree Panic Present (US)",  filename = "ptpPresUS.mp3", volume = 0.5, startPoint = 0,     endPoint =  9600716, loopStartPoint = 1567904, loopEndPoint = 7078184 },
+	iceCapZoneProto1    = { label = "Ice Cap Zone Prototype 1",      filename = "iczProto1.mp3", volume = 0.5, startPoint = 0,     endPoint = 10103039, loopStartPoint =  322534, loopEndPoint = 5328190 },
 }
