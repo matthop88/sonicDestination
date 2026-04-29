0c1bda1a1b86ee4e86ff44b02ce21140e1d26468
Matthew Hopkins <matthop88@gmail.com>
2026-04-28 17:03:54 -0700

    Create backgroundEngine.lua

---
+++ game/world/background/backgroundEngine.lua
@@ -0,0 +1,40 @@
+return {
+	createFromFile = function(self, filename)
+		local bgData = requireRelative("resources/zones/backgrounds/" .. filename)
+		local bgImgPath = relativePath("resources/images/backgrounds/" .. bgData.bgImageName .. ".png")
+
+		local bgImg = love.graphics.newImage(bgImgPath)
+		bgImg:setFilter("nearest", "nearest")
+
+		return ({
+			bgImg   = bgImg,
+			bgData  = bgData,
+			slices  = {},
+
+			init = function(self)
+				self.slices = {}
+				local y = 0
+				for _, slice in ipairs(self.bgData.slices) do
+					table.insert(self.slices, {
+						y = y,
+						quad = love.graphics.newQuad(slice.x, slice.y, slice.w, slice.h, self.bgImg:getWidth(), self.bgImg:getHeight())
+					})
+					y = y + slice.h
+				end
+
+				return self
+			end,
+
+			draw = function(self, graphics)
+				local x0, y0 = graphics:screenToImageCoordinates(0, 0)
+				graphics:setColor(0, 0.57, 1.0)
+				graphics:rectangle("fill", graphics:calculateViewport())
+				graphics:setColor(1, 1, 1)
+				for _, slice in ipairs(self.slices) do
+					graphics:draw(self.bgImg, slice.quad, x0, y0 + slice.y, 0, 1, 1)
+				end
+			end,
+			    
+		}):init()
+	end,
+}
