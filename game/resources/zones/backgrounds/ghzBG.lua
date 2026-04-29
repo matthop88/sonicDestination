85d2025aacf281553bca4e59ac1fbeb6324db3cc
Matthew Hopkins <matthop88@gmail.com>
2026-04-29 10:35:33 -0700

    Added names to ghzBg slices

--- game/resources/zones/backgrounds/ghzBG.lua
+++ game/resources/zones/backgrounds/ghzBG.lua
@@ -1,11 +1,11 @@
 return {
 	bgImageName = "ghzBG",
 	slices = {
-		{ x = 24, y = 181, w = 3840, h = 32 },
-		{ x = 24, y = 221, w = 3840, h = 16 },
-		{ x = 24, y = 245, w = 3840, h = 16 },
-		{ x = 24, y = 269, w = 8192, h = 48 },
-		{ x = 24, y = 325, w = 8192, h = 40 },
-		{ x = 24, y = 373, w = 3840, h = 104 },
+		{ name = "clouds1",       x = 24, y = 181, w = 3840, h = 32 },
+		{ name = "clouds2",       x = 24, y = 221, w = 3840, h = 16 },
+		{ name = "clouds3",       x = 24, y = 245, w = 3840, h = 16 },
+		{ name = "mountainPeaks", x = 24, y = 269, w = 8192, h = 48 },
+		{ name = "mountains",     x = 24, y = 325, w = 8192, h = 40 },
+		{ name = "ocean",         x = 24, y = 373, w = 3840, h = 104 },
 	}
 }
