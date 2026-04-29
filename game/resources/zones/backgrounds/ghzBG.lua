f3002717ff90787c155eae353cbd5af6c06a8b7d
Matthew Hopkins <matthop88@gmail.com>
2026-04-29 11:21:45 -0700

    Update ghzBG.lua to have xScalars

--- game/resources/zones/backgrounds/ghzBG.lua
+++ game/resources/zones/backgrounds/ghzBG.lua
@@ -1,11 +1,11 @@
 return {
 	bgImageName = "ghzBG",
 	slices = {
-		{ name = "clouds1",       x = 24, y = 181, w = 3840, h = 32, xSpeed = -120, },
-		{ name = "clouds2",       x = 24, y = 221, w = 3840, h = 16, xSpeed = -60, },
-		{ name = "clouds3",       x = 24, y = 245, w = 3840, h = 16, xSpeed = -30, },
-		{ name = "mountainPeaks", x = 24, y = 269, w = 8192, h = 48 },
-		{ name = "mountains",     x = 24, y = 325, w = 8192, h = 40 },
-		{ name = "ocean",         x = 24, y = 373, w = 3840, h = 104 },
+		{ name = "clouds1",       x = 24, y = 181, w = 3840, h = 32,  xSpeed = -120, },
+		{ name = "clouds2",       x = 24, y = 221, w = 3840, h = 16,  xSpeed = -60,  },
+		{ name = "clouds3",       x = 24, y = 245, w = 3840, h = 16,  xSpeed = -30,  },
+		{ name = "mountainPeaks", x = 24, y = 269, w = 8192, h = 48,  xScalar = 16,  },
+		{ name = "mountains",     x = 24, y = 325, w = 8192, h = 40,  xScalar =  8,  },
+		{ name = "ocean",         x = 24, y = 373, w = 3840, h = 104, xScalar =  7,  },
 	}
 }
