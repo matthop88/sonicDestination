9e34ec970ca79b776b91e071c336532ca2638663
Matthew Hopkins <matthop88@gmail.com>
2026-04-29 11:22:23 -0700

    Updated world.lua to pass in graphics to update

--- game/world/world.lua
+++ game/world/world.lua
@@ -138,7 +138,7 @@ return {
     drawSolidAt = function(self, x, y, color) TERRAIN:drawSolidAt(x, y, color) end,
 
     update = function(self, dt)
-        BACKGROUND:update(dt)
+        BACKGROUND:update(dt, GRAPHICS)
         TERRAIN:update(dt)
         self.objects:head()
         while not self.objects:isEnd() do
