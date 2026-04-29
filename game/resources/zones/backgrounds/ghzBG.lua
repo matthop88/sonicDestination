f284a7dc64501049100bef5005f5af048498ab42
Matthew Hopkins <matthop88@gmail.com>
2026-04-29 10:42:44 -0700

    Updated ghzBG.lua to add xSpeed to clouds

--- game/resources/zones/backgrounds/ghzBG.lua
+++ game/resources/zones/backgrounds/ghzBG.lua
@@ -1,9 +1,9 @@
 return {
 	bgImageName = "ghzBG",
 	slices = {
-		{ name = "clouds1",       x = 24, y = 181, w = 3840, h = 32 },
-		{ name = "clouds2",       x = 24, y = 221, w = 3840, h = 16 },
-		{ name = "clouds3",       x = 24, y = 245, w = 3840, h = 16 },
+		{ name = "clouds1",       x = 24, y = 181, w = 3840, h = 32, xSpeed = -3, },
+		{ name = "clouds2",       x = 24, y = 221, w = 3840, h = 16, xSpeed = -2, },
+		{ name = "clouds3",       x = 24, y = 245, w = 3840, h = 16, xSpeed = -1, },
 		{ name = "mountainPeaks", x = 24, y = 269, w = 8192, h = 48 },
 		{ name = "mountains",     x = 24, y = 325, w = 8192, h = 40 },
 		{ name = "ocean",         x = 24, y = 373, w = 3840, h = 104 },
