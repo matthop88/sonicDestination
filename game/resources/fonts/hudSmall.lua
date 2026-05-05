ec90dc3993ac4f4b5b7a1867ecb01e11c18cfabd
Matthew Hopkins <matthop88@gmail.com>
2026-05-05 15:35:33 -0700

    Create hudSmall.lua

---
+++ game/resources/fonts/hudSmall.lua
@@ -0,0 +1,26 @@
+return {
+	image = "hud.png",
+	spaceWidth = 8,
+	data  = {
+		-- $ is mini Sonic icon
+		{ key = "$", value = { quad = { x =  40, y = 401, w = 16, h =  14 }, } },
+		{ key = ":", value = { quad = { x =  90, y = 225, w =  4, h =  10 }, } },
+		{ key = "s", value = { quad = { x =  57, y = 401, w =  5, h =   6 }, } },
+		{ key = "o", value = { quad = { x =  63, y = 401, w =  5, h =   6 }, } },
+		{ key = "n", value = { quad = { x =  70, y = 401, w =  6, h =   6 }, } },
+		{ key = "i", value = { quad = { x =  78, y = 401, w =  2, h =   6 }, } },
+		{ key = "c", value = { quad = { x =  82, y = 401, w =  5, h =   6 }, } },
+		{ key = "x", value = { quad = { x =  62, y = 410, w =  5, h =   5 }, } },
+		
+		{ key = "0", value = { quad = { x = 121, y = 472, w = 7, h = 7 }, } },
+		{ key = "1", value = { quad = { x = 138, y = 472, w = 4, h = 7 }, } },
+		{ key = "2", value = { quad = { x = 158, y = 472, w = 7, h = 7 }, } },
+		{ key = "3", value = { quad = { x = 169, y = 472, w = 7, h = 7 }, } },
+	  { key = "4", value = { quad = { x = 185, y = 472, w = 7, h = 7 }, } },
+		{ key = "5", value = { quad = { x = 121, y = 488, w = 7, h = 7 }, } },
+		{ key = "6", value = { quad = { x = 137, y = 488, w = 7, h = 7 }, } },
+		{ key = "7", value = { quad = { x = 153, y = 488, w = 7, h = 7 }, } },
+		{ key = "8", value = { quad = { x = 169, y = 488, w = 7, h = 7 }, } },
+		{ key = "9", value = { quad = { x = 185, y = 488, w = 7, h = 7 }, } },
+	},
+}
