7ad126e1c0b0a332fa4728ff705f6209fc976024
Matthew Hopkins <matthop88@gmail.com>
2026-05-06 13:57:31 -0700

    Added capability of font blocks to calculate width and height, highlight

--- tools/fontBlaster/fontBlock.lua
+++ tools/fontBlaster/fontBlock.lua
@@ -3,8 +3,10 @@ local createGlyph = function(font, key)
 	local f = font.data[key]
 	if f == nil then
 		glyph.w = font.spaceWidth or 16
+		glyph.h = 0
 	else
 		glyph.w = f.w + 1
+		glyph.h = f.h
 		glyph.quad = f.quad
 	end
 	return glyph
@@ -28,17 +30,50 @@ local drawFontObject = function(self, graphics)
 		end
 		myX = myX + glyph.w
 	end
+	if self.highlighted then
+		graphics:setColor(1, 1, 0)
+		graphics:rectangle("line", self.x - 1, self.y - 1, self.w + 1, self.h + 2)
+	end
+end
+
+local calculateWidth = function(self)
+	local width = 0
+	for _, glyph in ipairs(self.obj.glyphs) do
+		width = width + glyph.w
+	end
+	return width
+end
+
+local calculateHeight = function(self)
+	local height = 0
+	for _, glyph in ipairs(self.obj.glyphs) do
+		height = math.max(height, glyph.h)
+	end
+	return height
 end
 
 return {
 	create = function(self, params)
-		return {
-			obj = createFontObject(params.font, params.fontData),
-			x   = params.x,
-			y   = params.y,
+		return ({
+			obj         = createFontObject(params.font, params.fontData),
+			x           = params.x,
+			y           = params.y,
+			highlighted = false,
 
 			draw = drawFontObject,
 
-		}
+			init = function(self)
+				self.w = calculateWidth(self)
+				self.h = calculateHeight(self)
+
+				return self
+			end,
+
+			update = function(self, dt, graphics)
+				local mx, my = graphics:screenToImageCoordinates(love.mouse.getPosition())
+				self.highlighted = (mx >= self.x and my >= self.y and mx < self.x + self.w and my < self.y + self.h)
+			end,
+
+		}):init()
 	end,
 }
