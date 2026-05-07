ba72388897ca7efe66efe4e45e1a8ce5a7b08c83
Matthew Hopkins <matthop88@gmail.com>
2026-05-07 15:57:41 -0700

    Update hudEngine.lua to use digits

--- game/world/hud/hudEngine.lua
+++ game/world/hud/hudEngine.lua
@@ -40,16 +40,23 @@ return {
     	return ({
     		fontEngine = require(relativePath("fonts/fontEngine")):create(),
     
+    		timer           = 600,
+			timeMinutes     = 9,
+    		timeSecondsTens = 5,
+    		timeSecondsOnes = 9,
+    		
     		init = function(self)
-    			local time  = self.fontEngine:newFontObject(TIME)
-    			local timeDigits = self.fontEngine:newFontObject(TIME_DIGITS)
+    			self.time  = self.fontEngine:newFontObject(TIME)
+    			self.timeDigits = self.fontEngine:newFontObject(TIME_DIGITS)
     			self.timeHud = require(relativePath("fonts/fontGroup")):create()
-    								:add(time, { 0.99, 0.99, 0.0 })
-    								:add(timeDigits)
+    								:add(self.time, { 0.99, 0.99, 0.0 })
+    								:add(self.timeDigits)
     								
     			self.scoreHud = self.fontEngine:newFontObject(SCORE)
     			self.ringsHud = self.fontEngine:newFontObject(RINGS)
     			self.lifeHud  = self.fontEngine:newFontObject(LIFE)
+
+    			self.DIGITS  = require(relativePath("world/hud/digits")):create("hud")
     			return self
     		end,
 
@@ -67,6 +74,25 @@ return {
             	self.lifeHud:draw(graphics, lifeX, lifeY, DISABLED_COLOR)
 
             	graphics:setScale(oldScale)
+            end,
+
+            update = function(self, dt)
+            	local oldTimer = math.floor(self.timer)
+            	self.timer = self.timer - dt
+            	if math.floor(self.timer) ~= oldTimer then
+            		if self.timeMinutes > 0 then
+	            		self.timeSecondsOnes = self.timeSecondsOnes - 1
+	            		if self.timeSecondsOnes < 0 then
+	            			self.timeSecondsOnes = 9
+	            			self.timeSecondsTens = self.timeSecondsTens - 1
+	            			if self.timeSecondsTens < 0 then
+	            				self.timeSecondsTens = 5
+	            				self.timeMinutes = self.timeMinutes - 1
+	            			end
+	            		end
+	            		self.DIGITS:replaceDigits(self.timeDigits, { self.timeMinutes, ":", self.timeSecondsTens, self.timeSecondsOnes })
+	            	end
+            	end
             end,
     	}):init()
 	
