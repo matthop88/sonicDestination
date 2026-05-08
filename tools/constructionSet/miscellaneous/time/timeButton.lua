f3174d476438558e06cfbede453aea0dd8ec448c
Matthew Hopkins <matthop88@gmail.com>
2026-05-08 12:45:39 -0700

    Create timeButton.lua

---
+++ tools/constructionSet/miscellaneous/time/timeButton.lua
@@ -0,0 +1,65 @@
+local COLOR = require("tools/lib/colors")
+
+return {
+	create = function(self, params)
+		local title     = nil
+		local timePanel = nil
+
+		return {
+			label = params.label or "Time",
+			hasFocus = false,
+			isSelected = false,
+			isPressed = false,
+			
+			drawInContainer = function(self, graphics, x, y, w, h)
+				if self.isPressed then
+					graphics:setColor(COLOR.YELLOW)
+					graphics:rectangle("fill", x - w/2, y - h/2, w, h)
+					graphics:setColor(COLOR.JET_BLACK)
+				elseif self.isSelected then
+					graphics:setColor(COLOR.YELLOW)
+				else
+					graphics:setColor(COLOR.PURE_WHITE)
+				end
+				graphics:setFontSize(48)
+				graphics:printf(self.label, x - w/2, y - 24, w, "center")
+				graphics:setFontSize(24)
+			end,
+
+			updateInContainer = function(self, dt)
+			end,
+			
+			gainFocus = function(self)
+				self.hasFocus = true
+			end,
+			
+			loseFocus = function(self)
+				self.hasFocus = false
+			end,
+			
+			select = function(self)
+				self.isSelected = true
+				self.isPressed = true
+			end,
+			
+			deselect = function(self)
+				self.isSelected = false
+			end,
+			
+			handleMousereleased = function(self, mx, my)
+				self.isPressed = false
+				if self.isSelected then
+					self:showTimePanel()
+				end
+			end,
+			
+			showTimePanel = function(self)
+				-- Do nothing
+			end,
+			
+			newObject = function(self)
+				return nil
+			end,
+		}
+	end,
+}
