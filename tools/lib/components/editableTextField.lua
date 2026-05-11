6e74dc3f1e077bafdb67bf5b5bbc57def1a78b1a
Matthew Hopkins <matthop88@gmail.com>
2026-05-11 12:37:16 -0700

    Added display transformation and input validation capabilities to editable text field

--- tools/lib/components/editableTextField.lua
+++ tools/lib/components/editableTextField.lua
@@ -15,10 +15,11 @@ return {
 			hovered      = false,
 			editing      = false,
 			inputLayerFn = params.inputLayerFn,
+			validKeys    = params.validKeys,
+			transformer  = params.transformer,
 
 			init = function(self)
-				-- Do something?
-
+				self:transformText()
 				return self
 			end,
 
@@ -38,7 +39,7 @@ return {
 				love.graphics.setColor(COLOR.PURE_WHITE)
 				love.graphics.setFont(fieldFont)
 				
-				love.graphics.printf(self.text, self.x + 10, self.y + 15, self.w - 20, "left")
+				love.graphics.printf(self.displayedText, self.x + 10, self.y + 15, self.w - 20, "left")
 			end,
 			
 			update = function(self, dt, mx, my)
@@ -64,13 +65,36 @@ return {
 				if not self.visible then return end
 				
 				if self.editing then
-					if key == "space" or key == "shiftspace" then key = " " end
-					if key == "backspace" then 
+					if key == "space" or key == "shiftspace" then 
+						key = " "
+					elseif key == "backspace" then 
 						self.text = string.sub(self.text, 1, string.len(self.text) - 1)
-					elseif string.len(key) == 1 then
-						self.text = self.text .. key
+						self:transformText()
+						return true
+					elseif string.len(key) ~= 1 or not self:isValid(key) then
+						return true
 					end
+					self.text = self.text .. key
+					self:transformText()
 					return true
+				end
+			end,
+
+			isValid = function(self, key)
+				if not self.validKeys then 
+					return true
+				else
+					for _, k in ipairs(self.validKeys) do
+						if key == k then return true end
+					end
+				end
+			end,
+
+			transformText = function(self)
+				if self.transformer then
+					self.displayedText = self.transformer(self.text)
+				else
+					self.displayedText = self.text
 				end
 			end,
 			
