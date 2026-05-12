9dba09ea020bc14e30dea00907c6419bb631372b
Matthew Hopkins <matthop88@gmail.com>
2026-05-12 11:26:34 -0700

    Create lampPostHead.lua

---
+++ game/sprites/data/objects/parts/lampPostHead.lua
@@ -0,0 +1,12 @@
+return {
+    imageName = "commonObj",
+    animations  = {
+        standingBlue = { fps = 1, isDefault = true, offset = { x = 8, y = 32 }, w = 16, h = 16,
+            ----------------------------------------------------------------------------------
+            { x = 132, y = 486, w = 16, h = 16, offset = { x = 8, y = 32 }, },
+        },
+        standingRed  = { fps = 1, offset = { x = 8, y = 32 }, w = 16, h = 16,
+            { x = 132, y = 510, w = 16, h = 16, offset = { x = 8, y = 32 }, },
+        },
+    },
+}
