faca9c7148a0027470f642e2143201cf7d078713
Matthew Hopkins <matthop88@gmail.com>
2026-05-12 11:26:57 -0700

    Create lampPostBody.lua

---
+++ game/sprites/data/objects/parts/lampPostBody.lua
@@ -0,0 +1,9 @@
+return {
+    imageName = "commonObj",
+    animations  = {
+        standing = { fps = 1, isDefault = true, offset = { x = 8, y = 16 }, w = 16, h = 48,
+            ----------------------------------------------------------------------------------
+            { x = 132, y = 534, w = 16, h = 48, offset = { x = 8, y = 16 }, },
+        },
+    },
+}
