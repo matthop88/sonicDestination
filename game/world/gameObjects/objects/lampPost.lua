4d53fb894e28a19ac34dbe4790053539f2c7ffd8
Matthew Hopkins <matthop88@gmail.com>
2026-05-13 11:40:48 -0700

    Added manual delay before postLamp sound

--- game/world/gameObjects/objects/lampPost.lua
+++ game/world/gameObjects/objects/lampPost.lua
@@ -7,9 +7,25 @@ return {
 				if self.active and not self.triggered then
 					self:setAnimation("animatingRed")
 					SOUND_MANAGER:playAction("lampPost")
+					self.postLampTimer = 15
 					self.triggered = true
 				end
 			end,
+
+			update = function(self, dt)
+                if self.active then
+                    self.sprite:update(dt)
+                    self.deleted = self.sprite.deleted
+                    self:updateHitBox(dt)
+                    if self.postLampTimer then
+                    	self.postLampTimer = self.postLampTimer - (60 * dt)
+                    	if self.postLampTimer < 0 then
+                    		SOUND_MANAGER:playAction("postLamp")
+							self.postLampTimer = nil
+						end
+					end
+                end
+            end,
 		}
 	end,
 }
