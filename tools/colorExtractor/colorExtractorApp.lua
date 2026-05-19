9b4227aa6e54bdd98b25f2aca66690e1d2300c6c
Matthew Hopkins <matthop88@gmail.com>
2026-05-19 15:04:41 -0700

    Updated colorExtractorApp.lua to extract colors and draw as white

--- tools/colorExtractor/colorExtractorApp.lua
+++ tools/colorExtractor/colorExtractorApp.lua
@@ -5,9 +5,11 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1024, 600
 
-local destRect = { x = 512, y = 0 }
+local destRect = { x = 256, y = 16 }
 local rects = {}
 local selectedColor = nil
+local selectedTileCoordinates = { x = 0, y = 0 }
+local rectData = {}
 
 --------------------------------------------------------------
 --              Static code - is executed first             --
@@ -22,9 +24,11 @@ local imgPath = "game/resources/images/backgrounds/ghzBGTiles.png"
 --                     LOVE2D Functions                     --
 --------------------------------------------------------------
 
-function love.mousepressed(mx, my)
-    local x, y = calculateTileCoordinates(mx, my)
+function love.mousereleased(mx, my)
+    local x, y = getImageViewer():screenToImageCoordinates(mx, my)
+    x, y = math.floor(math.floor(x) / 16) * 16, math.floor(math.floor(y) / 16) * 16
     printToReadout("Tile Coordinates: { x = " .. x .. ", y = " .. y .. " }")
+    selectedTileCoordinates = { x = x, y = y }
 end
 
 function love.keypressed(key)
@@ -53,8 +57,8 @@ end
 function calculateTileCoordinates(mx, my)
     local imageViewer = getImageViewer()
     local px, py = imageViewer:screenToImageCoordinates(mx, my)
-
-    return imageViewer:imageToScreenCoordinates(math.floor(px / 16) * 16, math.floor(py / 16) * 16)
+    local tx, ty = imageViewer:imageToScreenCoordinates(math.floor(px / 16) * 16, math.floor(py / 16) * 16)
+    return math.floor(tx), math.floor(ty)
 end
     
 
@@ -65,12 +69,42 @@ function onColorSelected(color)
     printToReadout(string.format("R = %s, G = %s, B = %s", love.math.colorToBytes(r, g, b)))
 end 
 
+extractColor = function(x, y, r, g, b, a)
+    if x >= selectedTileCoordinates.x and y >= selectedTileCoordinates.y
+        and x <  selectedTileCoordinates.x + 16 and y < selectedTileCoordinates.y + 16 then
+            if r == selectedColor[1] and g == selectedColor[2] and b == selectedColor[3] then
+                table.insert(rectData, { x = destRect.x + x - selectedTileCoordinates.x, y = destRect.y + y - selectedTileCoordinates.y })
+                return 0, 0, 0, 0
+            end
+    end
+    return r, g, b, a
+end
+
+addExtractedColor = function(x, y, r, g, b, a)
+    if x >= destRect.x and y >= destRect.y and x < destRect.x + 16 and y < destRect.y + 16 then
+        for _, d in ipairs(rectData) do
+            --print("RectData.x, y = ", d.x, d.y)
+            if x == d.x and y == d.y then
+                return 1, 1, 1, 1
+            end
+        end
+    end
+    return r, g, b, a
+end
+    
+
 function addRect()
     if selectedColor then
-        table.insert(rects, { x = destRect.x, y = destRect.y, c = selectedColor })  
+        print("selectedTileCoordinates: x = " .. selectedTileCoordinates.x .. ", y = " .. selectedTileCoordinates.y)
+        --table.insert(rects, { x = destRect.x, y = destRect.y, c = selectedColor })  
+        rectData = {}
+        getImageViewer():editPixels(extractColor)
+        getImageViewer():editPixels(addExtractedColor)
         destRect.x = destRect.x + 16
     end
 end
+
+
 
 
 --------------------------------------------------------------
