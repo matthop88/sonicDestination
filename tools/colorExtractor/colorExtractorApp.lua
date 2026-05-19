b34523bf084eed275b52129b680c0a40b2763f0a
Matthew Hopkins <matthop88@gmail.com>
2026-05-19 14:24:01 -0700

    Update colorExtractorApp.lua

--- tools/colorExtractor/colorExtractorApp.lua
+++ tools/colorExtractor/colorExtractorApp.lua
@@ -5,6 +5,10 @@
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = 1024, 600
 
+local destRect = { x = 512, y = 0 }
+local rects = {}
+local selectedColor = nil
+
 --------------------------------------------------------------
 --              Static code - is executed first             --
 --------------------------------------------------------------
@@ -23,7 +27,9 @@ function love.mousepressed(mx, my)
     printToReadout("Tile Coordinates: { x = " .. x .. ", y = " .. y .. " }")
 end
 
--- ...
+function love.keypressed(key)
+    if key == "space" then addRect() end
+end
 -- ...
 
 --------------------------------------------------------------
@@ -35,7 +41,13 @@ function drawOverlays()
     local x, y = calculateTileCoordinates(love.mouse.getPosition())
     love.graphics.setLineWidth(scale)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("line", x - 1, y - 1, 18 * scale, 18 * scale)
+    love.graphics.rectangle("line", x - scale, y - scale, 18 * scale, 18 * scale)
+
+    for _, rect in ipairs(rects) do
+        love.graphics.setColor(rect.c)
+        local rx, ry = getImageViewer():imageToScreenCoordinates(rect.x, rect.y)
+        love.graphics.rectangle("fill", rx, ry, scale * 16, scale * 16)
+    end
 end
 
 function calculateTileCoordinates(mx, my)
@@ -46,8 +58,20 @@ function calculateTileCoordinates(mx, my)
 end
     
 
--- ...    
--- ...
+function onColorSelected(color)
+    selectedColor = color
+    local r, g, b = unpack(color)
+    print(string.format("{ r = %.2f, g = %.2f, b = %.2f }", r, g, b))
+    printToReadout(string.format("R = %s, G = %s, B = %s", love.math.colorToBytes(r, g, b)))
+end 
+
+function addRect()
+    if selectedColor then
+        table.insert(rects, { x = destRect.x, y = destRect.y, c = selectedColor })  
+        destRect.x = destRect.x + 16
+    end
+end
+
 
 --------------------------------------------------------------
 --                          Plugins                         --
@@ -58,9 +82,16 @@ PLUGINS = require("plugins/engine")
     :add("imageViewer", 
     { 
         imagePath         = imgPath,
+        pixelated         = true,
         accessorFnName    = "getImageViewer"
     })
     :add("drawingLayer", { drawingFn      = drawOverlays     })
+    :add("selectColor", 
+    {
+        imageViewer       = getImageViewer(),
+        onColorSelected   = onColorSelected,
+        accessorFnName    = "getColorSelector"
+    })
     :add("readout",      { printFnName    = "printToReadout" })
     :add("zooming",      { imageViewer    = getImageViewer() })
     :add("scrolling",    { imageViewer    = getImageViewer() })
