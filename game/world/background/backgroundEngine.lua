ad40a3ce2089b00f5a11cdaf50869ba6f9345c90
Matthew Hopkins <matthop88@gmail.com>
2026-06-03 14:16:23 -0700

    Updated background engine to have smoothly animating colors

--- game/world/background/backgroundEngine.lua
+++ game/world/background/backgroundEngine.lua
@@ -1,4 +1,5 @@
 return {
+	
 	createFromFile = function(self, filename)
 		local bgData = requireRelative("resources/zones/backgrounds/" .. filename)
 		local background = requireRelative("world/background/backgroundBuilder"):create(bgData)
@@ -11,23 +12,23 @@ return {
 			prevX      = 0,
 
 			COLORS     = {
-				{ 0.71, 0.85, 0.99 },
-    			{ 0.56, 0.71, 0.99 },
-    			{ 0.42, 0.56, 0.99 },
-    			{ 0.42, 0.56, 0.71 },
-			    index = 1,
+			    { base = { 0.71, 0.85, 0.99 }, delta = { -0.15, -0.14,  0.0  } },
+			    { base = { 0.56, 0.71, 0.99 }, delta = { -0.14, -0.15,  0.0  } },
+			    { base = { 0.42, 0.56, 0.99 }, delta = {  0.0,   0.0,  -0.28 } },
+			    { base = { 0.42, 0.56, 0.71 }, delta = {  0.29,  0.29,  0.28 } },
+			    offset = 1,
+			    dx     = 0,
 			    update = function(self, dt)
-			    	self.index = self.index + (10 * dt)
-			    	if self.index >= #self + 1 then
-			    		self.index = self.index - #self
-			    	end
+			        self.offset = self.offset + (9 * dt)
+			        self.dx = self.offset - math.floor(self.offset)
+			        if self.offset >= 5 then self.offset = self.offset - 4 end
 			    end,
-
-			    get = function(self, ndx)
-			    	ndx = math.floor(self.index) + ndx
-			    	if ndx > #self then ndx = ndx - #self end
-			    	return self[ndx]
-			    end,
+			    get = function(self, n)
+			        local index = n + math.floor(self.offset)
+			        if index > 4 then index = index - 4 end
+			        local c = self[index]
+			        return { c.base[1] + (c.delta[1] * self.dx), c.base[2] + (c.delta[2] * self.dx), c.base[3] + (c.delta[3] * self.dx) }
+    			end,
 			},
 
 			init = function(self)
