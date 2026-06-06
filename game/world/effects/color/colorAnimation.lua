8adc819f4caf133864be351be6a3848de96db026
Matthew Hopkins <matthop88@gmail.com>
2026-06-05 18:17:55 -0700

    Create colorAnimation.lua

---
+++ game/world/effects/color/colorAnimation.lua
@@ -0,0 +1,37 @@
+return {
+	create = function(self, colors)
+		local entries = {}
+		for n, color in ipairs(colors) do
+			table.insert(entries, { base = { color[1], color[2], color[3] } })
+			if n > 1 then
+				local prevColor = entries[n - 1].base
+				entries[n - 1].delta = { color[1] - prevColor[1], color[2] - prevColor[2], color[3] - prevColor[3] }
+			end
+		end
+		local firstColor = entries[1].base
+		local lastColor  = entries[#colors].base
+		entries[#colors].delta = { firstColor[1] - lastColor[1], firstColor[2] - lastColor[2], firstColor[3] - lastColor[3] }
+
+		local colorAnimator = {
+			offset = 1,
+			dx     = 0,
+			update = function(self, dt)
+				self.offset = self.offset + (9 * dt)
+			        self.dx = self.offset - math.floor(self.offset)
+			        if self.offset >= 5 then self.offset = self.offset - 4 end
+			    end,
+		    get = function(self, n)
+		        local index = n + math.floor(self.offset)
+		        if index > 4 then index = index - 4 end
+		        local c = self[index]
+		        return { c.base[1] + (c.delta[1] * self.dx), c.base[2] + (c.delta[2] * self.dx), c.base[3] + (c.delta[3] * self.dx) }
+			end,
+		}
+
+		for n, entry in ipairs(entries) do
+			table.insert(colorAnimator, entry)
+		end
+
+		return colorAnimator
+	end
+}
