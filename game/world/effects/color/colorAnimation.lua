0e9e0019672c8b87e436a46c74a5458dae3b298d
Matthew Hopkins <matthop88@gmail.com>
2026-06-12 13:46:38 -0700

    Updated colorAnimation.lua so there is a smooth/not-smooth option, executes a bit faster

--- game/world/effects/color/colorAnimation.lua
+++ game/world/effects/color/colorAnimation.lua
@@ -1,5 +1,5 @@
 return {
-	create = function(self, colors)
+	create = function(self, colors, isSmooth)
 		local entries = {}
 		for n, color in ipairs(colors) do
 			table.insert(entries, { base = { color[1], color[2], color[3] } })
@@ -13,10 +13,11 @@ return {
 		entries[#colors].delta = { firstColor[1] - lastColor[1], firstColor[2] - lastColor[2], firstColor[3] - lastColor[3] }
 
 		local colorAnimator = {
-			offset = 1,
-			dx     = 0,
+			offset   = 1,
+			dx       = 0,
+			isSmooth = isSmooth,
 			update = function(self, dt)
-				self.offset = self.offset + (9 * dt)
+				self.offset = self.offset + (12 * dt)
 			        self.dx = self.offset - math.floor(self.offset)
 			        if self.offset >= 5 then self.offset = self.offset - 4 end
 			    end,
@@ -24,7 +25,11 @@ return {
 		        local index = n + math.floor(self.offset)
 		        if index > 4 then index = index - 4 end
 		        local c = self[index]
-		        return { c.base[1] + (c.delta[1] * self.dx), c.base[2] + (c.delta[2] * self.dx), c.base[3] + (c.delta[3] * self.dx) }
+		        if self.isSmooth then
+		        	return { c.base[1] + (c.delta[1] * self.dx), c.base[2] + (c.delta[2] * self.dx), c.base[3] + (c.delta[3] * self.dx) }
+		        else
+		        	return { c.base[1], c.base[2], c.base[3] }
+		        end
 			end,
 		}
 
