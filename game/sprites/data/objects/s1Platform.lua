140d96c1c46a03cf39851a03c00440152542da29
Matthew Hopkins <matthop88@gmail.com>
2026-06-22 15:15:00 -0700

    Create s1Platform.lua sprite. Shows up in sandbox

---
+++ game/sprites/data/objects/s1Platform.lua
@@ -0,0 +1,10 @@
+return {
+    imageName  = "ghzObjects",
+    animations  = {
+        platform = { fps = 1, offset = { x = 32, y = 15 }, w = 64, h = 30, isDefault = true, 
+            hitBox = { rX = 32, rY = 15 },
+            ----------------------------------------------------------------------------------------------------------
+            { x = 300, y = 214, w = 64, h = 30, offset = { x = 32, y = 15 }, },
+        },
+    },
+}
