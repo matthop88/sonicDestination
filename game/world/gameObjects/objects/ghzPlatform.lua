118b980b65ede6953ac83b8404fc48d284033e98
Matthew Hopkins <matthop88@gmail.com>
2026-06-23 15:45:48 -0700

    Updated ghzPlatform.lua to include getLeftEdge() and getRightEdge()

--- game/world/gameObjects/objects/ghzPlatform.lua
+++ game/world/gameObjects/objects/ghzPlatform.lua
@@ -4,6 +4,14 @@ return {
 			onCollisionWithPlayer = function(self, player)
 				if player.isStandingOn ~= self then player:landOn(self) end
 			end,
+
+			getLeftEdge = function(self)
+				return self.x - self:getW() / 2
+			end,
+
+			getRightEdge = function(self)
+				return self.x + self:getW() / 2
+			end,
 		}
 	end,
 }
