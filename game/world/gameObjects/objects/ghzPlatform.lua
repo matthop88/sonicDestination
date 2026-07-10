85baf43c031e89197d3070f88e4791d1c3b9a915
Matthew Hopkins <matthop88@gmail.com>
2026-07-10 16:09:06 +0000

    Updated ghzPlatform.lua to dip when Sonic is on it

--- game/world/gameObjects/objects/ghzPlatform.lua
+++ game/world/gameObjects/objects/ghzPlatform.lua
@@ -1,9 +1,18 @@
 return {
 	create = function(self)
 		return {
+			player = nil,
+			dipLevel = 0,
+			origY = 0,
+
+			onCreation = function(self)
+				self.origY = self.y
+			end,
+
 			onCollisionWithPlayer = function(self, player)
-				if player.isStandingOn ~= self and player.velocity.y > 0 and player.position.y < self.y then 
+				if player.standingOn ~= self and player.velocity.y > 0 and player.position.y < self.y then 
 					player:landOn(self) 
+					self.player = player
 				end
 			end,
 
@@ -14,6 +23,25 @@ return {
 			getRightEdge = function(self)
 				return self.x + self:getW() / 2
 			end,
+
+			update = function(self, dt)
+            	if self.player then
+            		if self.player.standingOn == self then
+            			if self.dipLevel < 15 then
+	                		self.dipLevel = math.min(15, self.dipLevel + (50 * dt))
+	                		self.player:landOn(self)
+	                	end
+	                else
+	                	if self.dipLevel > 0 then
+	                		self.dipLevel = math.max(0, self.dipLevel - (50 * dt))
+	                	end
+	                end
+                end
+                self.y = self.origY + math.max(0, self.dipLevel - 5)
+                self.sprite:update(dt)
+                self:updateHitBox(dt)
+                self.deleted = self.sprite.deleted
+            end,
 		}
 	end,
 }
