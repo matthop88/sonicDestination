cfedb9f197eb863553d7bff6cd91ed5e3cb8004e
Matthew Hopkins <matthop88@gmail.com>
2026-07-13 16:09:10 -0700

    Create monitorScreen.lua

---
+++ game/sprites/data/objects/parts/monitorScreen.lua
@@ -0,0 +1,14 @@
+return {
+    imageName = "commonObj",
+    animations  = {
+        flashing = { fps = 16, isDefault = true, offset = { x = 8, y = 11 }, w = 16, h = 16,
+            ------------------------------------------------------------------------------
+            { x = 24, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 48, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+        },
+    },
+}
