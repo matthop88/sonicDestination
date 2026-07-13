108f06984bc885e98cbdc60027b39a87bd478958
Matthew Hopkins <matthop88@gmail.com>
2026-07-13 16:08:32 -0700

    Create monitorHull.lua

---
+++ game/sprites/data/objects/parts/monitorHull.lua
@@ -0,0 +1,9 @@
+return {
+    imageName = "commonObj",
+    animations  = {
+        standing = { fps = 1, isDefault = true, offset = { x = 15, y = 15 }, w = 30, h = 30,
+            ------------------------------------------------------------------------------
+            { x = 25, y = 487, w = 30, h = 30, offset = { x = 15, y  = 15 }, },
+        },
+    },
+}
