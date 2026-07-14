eab91ac1bc72e0aedb0f3e41e0a4a429cff740ce
Matthew Hopkins <matthop88@gmail.com>
2026-07-14 12:54:43 -0700

    Updated monitor.lua to allow Sonic to land on top. Jittery at edges.

--- game/world/gameObjects/objects/monitor.lua
+++ game/world/gameObjects/objects/monitor.lua
@@ -1,6 +1,26 @@
 return {
 	create = function(self)
 		return {
+			player = nil,
+			
+			onCollisionWithPlayer = function(self, player)
+				if player.standingOn ~= self and player.velocity.y > 0 and player.position.y < self.y then 
+					player:landOn(self) 
+					self.player = player
+				end
+			end,
+
+			getLeftEdge = function(self)
+				return self.x - self:getW() / 2
+			end,
+
+			getRightEdge = function(self)
+				return self.x + self:getW() / 2
+			end,
+
+			getTop = function(self)
+				return (self.y - self:getH() / 2) - 3
+			end,
 		}
 	end,
 }
