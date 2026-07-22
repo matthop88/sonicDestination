c4f4fe9ae3c60e7a427249785d86be55ca9575cf
Matthew Hopkins <matthop88@gmail.com>
2026-07-21 17:02:06 -0700

    Added pushing to monitor.lua

--- game/world/gameObjects/objects/monitor.lua
+++ game/world/gameObjects/objects/monitor.lua
@@ -4,10 +4,25 @@ return {
 			player = nil,
 
 			onCollisionWithPlayer = function(self, player)
+				self.player = player
 				if player.velocity.y == 0 then
 					player:move(self:getHitBox():calculatePushOnOther(player:getHitBox()), 0)
+					if (player.velocity.x > 0 and player.position.x < self.x) or (player.velocity.x < 0 and player.position.x > self.x) then
+						player:setPushing(self)
+					end
 				end
 			end,
+
+			update = function(self, dt)
+                if self.active then
+                    self.sprite:update(dt)
+                    self:updateHitBox(dt)
+                    self.deleted = self.sprite.deleted
+                    self:setX(self:getX() + (self:getXVelocity() * dt))
+                    self:setY(self:getY() + (self:getYSpeed()    * dt))
+                    self.hitSolid = false
+                end
+            end,
 
 			getLeftEdge = function(self)
 				return self.x - self:getW() / 2
