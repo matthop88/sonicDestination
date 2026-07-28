7500c043254caa5d7e47fefd11cec365f0476fdc
Matthew Hopkins <matthop88@gmail.com>
2026-07-28 16:35:33 -0700

    Updated monitorScreen sprite so that monitor face rises

--- game/sprites/data/objects/parts/monitorScreen.lua
+++ game/sprites/data/objects/parts/monitorScreen.lua
@@ -1,3 +1,6 @@
+local FACE_GRAVITY = 120
+local RISE_SPEED   = 120
+            
 return {
     imageName = "commonObj",
     animations  = {
@@ -12,11 +15,17 @@ return {
         },
         exploding = { fps = 5, offset = { x = 8, y = 11 }, w = 16, h = 16,
             reps = 1, terminal = true, endingFrame = 6,
+            calculateOffsets = function(frameNumber)
+                local dt           = (frameNumber - 1) / 5
+                local gravModifier = ((FACE_GRAVITY    / 2) * dt * dt)
+                local yOffset      = (RISE_SPEED * dt) - gravModifier
+                return 8, 11 + yOffset
+            end,
             { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
             { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
             { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
-            {},
-            {},
+            { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 603, w = 16, h = 16, offset = { x = 8, y = 11 }, },
             {},
         },  
     },
