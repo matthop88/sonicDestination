02c11a3c657f5d4c3a233d0ce649c74434fc320c
Matthew Hopkins <matthop88@gmail.com>
2026-08-14 14:43:59 -0700

    Made ring HUD have yellow color, extracted Time HUD (refactoring)

--- game/world/hud/hudEngine.lua
+++ game/world/hud/hudEngine.lua
@@ -43,10 +43,13 @@ return {
 	    }
 
 	    local DISABLED_COLOR = { 1, 1, 1, 0.5 }
+
+	    local TIME_HUD = {
+	    	fontEngine = require(relativePath("fonts/fontEngine")):create(),
     
-    	return ({
-    		fontEngine = require(relativePath("fonts/fontEngine")):create(),
-    
+    		TIME_X          = 48,
+    		TIME_Y          = 96,
+    		
     		timer           = 3600,
     		timeMinutesTens = 5,
 			timeMinutes     = 9,
@@ -56,49 +59,21 @@ return {
     		redColor        = false,
     		
     		init = function(self)
-    			self:initTimeHud()
-    			self:initRingsHud()
-    			self.scoreHud = self.fontEngine:newFontObject(SCORE)
-    			self.lifeHud  = self.fontEngine:newFontObject(LIFE)
-
     			self.DIGITS  = require(relativePath("world/hud/digits")):create("hud")
-    			return self
-    		end,
-
-    		initTimeHud = function(self)
+    			
     			self.time  = self.fontEngine:newFontObject(TIME)
     			self.timeDigits = self.fontEngine:newFontObject(TIME_DIGITS)
-    			self.timeHud = require(relativePath("fonts/fontGroup")):create()
+    			self.timeHudObj = require(relativePath("fonts/fontGroup")):create()
     								:add(self.time, { 0.99, 0.99, 0.0 })
     								:add(self.timeDigits)
-    								
     		end,
 
-    		initRingsHud = function(self)
-    			self.rings      = self.fontEngine:newFontObject(RINGS)
-    			self.ringDigits = self.fontEngine:newFontObject(RING_DIGITS)
-    			self.ringsHud   = require(relativePath("fonts/fontGroup")):create()
-    								:add(self.rings, { 0.99, 0.99, 0.0 })
-    								:add(self.ringDigits)
-    		end,
+    		draw = function(self, graphics)
+    			local timeX, timeY = graphics:screenToImageCoordinates(self.TIME_X, self.TIME_Y)
+				self.timeHudObj:draw(graphics, timeX, timeY)
+			end,
 
-            draw = function(self, graphics)
-            	local oldScale = graphics:getScale()
-				graphics:setScale(3)
-				local scoreX, scoreY = graphics:screenToImageCoordinates(48, 48)
-				local timeX,  timeY  = graphics:screenToImageCoordinates(48, 96)
-				local ringsX, ringsY = graphics:screenToImageCoordinates(48, 144)
-				local lifeX,  lifeY  = graphics:screenToImageCoordinates(48, 700)
-				
-				self.scoreHud:draw(graphics, scoreX, scoreY, DISABLED_COLOR)
-            	self.timeHud:draw( graphics, timeX,  timeY)
-            	self.ringsHud:draw(graphics, ringsX, ringsY)
-            	self.lifeHud:draw(graphics, lifeX, lifeY, DISABLED_COLOR)
-
-            	graphics:setScale(oldScale)
-            end,
-
-            update = function(self, dt)
+			update = function(self, dt)
             	local oldTimer = math.floor(self.timer)
             	self.timer = self.timer - dt
             	if self.timer < 30 then
@@ -156,8 +131,63 @@ return {
     			elseif timeProps.timeAtStart then
     				self.timer = timeProps.timeAtStart
     			end
-    			self:initTimeHud()
+    			self:init()
     			if self.timer >= 0 then self:updateDigits() end
+            end,
+
+	    }
+    
+    	return ({
+    		fontEngine = require(relativePath("fonts/fontEngine")):create(),
+    
+    		init = function(self)
+    			self:initTimeHud()
+    			self:initRingsHud()
+    			self.scoreHud = self.fontEngine:newFontObject(SCORE)
+    			self.lifeHud  = self.fontEngine:newFontObject(LIFE)
+
+    			self.DIGITS  = require(relativePath("world/hud/digits")):create("hud")
+    			return self
+    		end,
+
+    		initTimeHud = function(self)
+    			self.timeHud = TIME_HUD
+    			self.timeHud:init()					
+    		end,
+
+    		initRingsHud = function(self)
+    			self.rings      = self.fontEngine:newFontObject(RINGS)
+    			self.ringDigits = self.fontEngine:newFontObject(RING_DIGITS)
+    			self.ringsHud   = require(relativePath("fonts/fontGroup")):create()
+    								:add(self.rings, { 0.99, 0.99, 0.0 })
+    								:add(self.ringDigits)
+    		end,
+
+            draw = function(self, graphics)
+            	local oldScale = graphics:getScale()
+				graphics:setScale(3)
+				local scoreX, scoreY = graphics:screenToImageCoordinates(48, 48)
+				local ringsX, ringsY = graphics:screenToImageCoordinates(48, 144)
+				local lifeX,  lifeY  = graphics:screenToImageCoordinates(48, 700)
+				
+				self.scoreHud:draw(graphics, scoreX, scoreY, DISABLED_COLOR)
+            	self.timeHud:draw(graphics)
+            	self.ringsHud:draw(graphics, ringsX, ringsY)
+            	self.lifeHud:draw(graphics, lifeX, lifeY, DISABLED_COLOR)
+
+            	graphics:setScale(oldScale)
+            end,
+
+            update = function(self, dt)
+            	self.timeHud:update(dt)
+            end,
+
+            getTimer = function(self)
+            	return self.timeHud:getTimer()
+            end,
+
+            refreshFromTimeProps = function(self, timeProps, timeOverride)
+            	self.timeHud:refreshFromTimeProps(timeProps, timeOverride)
             end,
     	}):init()
 	
