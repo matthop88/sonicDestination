fe6c8ecd007f67d897d84825a490186615cdb75c
Matthew Hopkins <matthop88@gmail.com>
2026-08-20 10:30:56 -0700

    Finalized genericness of genericPanel

--- tools/constructionSet/miscellaneous/genericPanel.lua
+++ tools/constructionSet/miscellaneous/genericPanel.lua
@@ -1,93 +1,99 @@
 local COLOR = require("tools/lib/colors")
 
+local createLabel    = function(name, x, y, w, h)
+	return require("tools/lib/components/label"):create { x = x, y = y, w = w, h = h, label = name }
+end
+
+local createDropDown = function(params, x, y, w, h)
+	return require("tools/lib/components/dropDownField"):create {
+		x             = x,
+		y             = y,
+		width         = w,
+		height        = h,
+		label         = "",
+		list          = params.list,
+		selectedValue = params.selectedValue,
+		comparisonFn  = function(listItem, value) return listItem.value == value end,
+		onChanged     = params.onChanged,
+	}
+end
+
+local createEditableTextField = function(params, x, y, w, h)
+	return require("tools/lib/components/editableTextField"):create {
+		x            = x,
+		y            = y,
+		w            = w,
+		h            = h,
+		text         = params.text,
+		onChanged    = params.onChanged,
+		inputLayerFn = getInputLayer,
+		validKeys    = params.validKeys,
+		transformer  = function(text) return string.upper(text) end,
+	}
+end
+
 return {
 	create = function(self, params)
 		return ({
-			title     = params.title or "Untitled Panel",
-			titleFont = love.graphics.newFont(24),
-			visible   = false,
+			title      = params.title or "Untitled Panel",
+			titleFont  = love.graphics.newFont(24),
+			visible    = false,
 			
-			x         = params.x or 300,
-			y         = params.y or 250,
-			w         = params.w or 830,
-			h         = params.h or 400,
-				
-			init = function(self)
-				if _G.getModals then
-					getModals():add(self)
-				end
+			x          = params.x                 or 300,
+			y          = params.y                 or 250,
+			w          = params.w                 or 830,
+			h          = params.h                 or 400,
+			topX       = params.components.topX   or   0,
+			topY       = params.components.topY   or   0,
+			labelW     = params.components.labelW or 150,
+			componentW = params.components.w      or 300,
+			componentH = params.components.h      or  50,
+			gapX       = params.components.gapX   or   0,
+			gapY       = params.components.gapY   or   0,
 
-				self.timeAtStartLabel = require("tools/lib/components/label"):create { x = self.x + 20, y = self.y +  80, w = 150, h = 50, label = "Time at Start", }
-				self.timeTextLabel    = require("tools/lib/components/label"):create { x = self.x + 20, y = self.y + 140, w = 150, h = 50, label = "Text", }
-				self.timeMonDurLabel  = require("tools/lib/components/label"):create { x = self.x + 20, y = self.y + 200, w = 150, h = 50, label = "Monitor Duration", }
-				self.okButton         = require("tools/lib/components/okButton"):create {
+			components = {},
+			dropDowns  = {},
+				
+			init = function(self, params)
+				if _G.getModals then getModals():add(self) end
+
+				self:addComponents(params.components)
+				self:addOkButton()
+
+				return self
+			end,
+
+			addComponents = function(self, components)
+				local x = self.x + self.topX
+				local y = self.y + self.topY
+
+				for _, component in ipairs(components) do
+					self:addLabeledComponent(component, x, y)
+					y = y + self.componentH + self.gapY
+				end
+			end,
+
+			addLabeledComponent = function(self, component, x, y)
+				table.insert(self.components, createLabel(component.label, x, y, self.labelW, self.componentH))
+				x = x + self.labelW + self.gapX
+				local c = self:createComponent(component, x, y)
+				table.insert(self.components, c)
+				if component.type == "dropDown" then table.insert(self.dropDowns, c) end
+			end,
+
+			createComponent = function(self, component, x, y)
+				if     component.type == "dropDown"          then return createDropDown(component, x, y, self.componentW, self.componentH)
+				elseif component.type == "editableTextField" then return createEditableTextField(component, x, y, self.componentW, self.componentH)
+				end
+			end,
+
+			addOkButton = function(self)
+				self.okButton = require("tools/lib/components/okButton"):create {
 					x = self.x + self.w - 120,
 					y = self.y + self.h - 60,
 					width = 100,
 					height = 40,
 				}
-
-				local timeProperties = getProperties():getTime()
-				
-				self.timesAtStartDropdown = require("tools/lib/components/dropDownField"):create {
-					x = self.x + 200,
-					y = self.y + 80,
-					width = 300,
-					height = 50,
-					label = "",
-					list = require("tools/constructionSet/miscellaneous/time/timesAtStart"),
-					selectedValue = timeProperties.timeAtStart,
-					comparisonFn = function(listItem, value)
-						return listItem.value == value
-					end,
-					onChanged = function(item, index)
-						timeProperties.timeAtStart = item.value
-					end,
-				}
-
-				self.timeTextEditableField = require("tools/lib/components/editableTextField"):create {
-					x = self.x + 200,
-					y = self.y + 140,
-					w = 300,
-					height = 50,
-					text = getProperties().time.timeLabel or "time",
-					onChanged = function(text) getProperties():getTime().timeLabel = text end,
-					inputLayerFn = getInputLayer,
-					validKeys = { "b", "c", "e", "g", "i", "m", "n", "o", "r", "s", "t", "u", },
-					transformer = function(text) return string.upper(text) end,
-				}
-
-				self.timeMonitorDurationsDropdown = require("tools/lib/components/dropDownField"):create {
-					x = self.x + 200,
-					y = self.y + 200,
-					width = 300,
-					height = 50,
-					label = "",
-					list = require("tools/constructionSet/miscellaneous/time/timeMonitorDurations"),
-					selectedValue = timeProperties.timeMonitorDurations,
-					comparisonFn = function(listItem, value)
-						return listItem.value == value
-					end,
-					onChanged = function(item, index)
-						timeProperties.timeMonitorDurations = item.value
-					end,
-				}
-
-				self.components = {
-					self.timeAtStartLabel,
-					self.timeTextLabel,
-					self.timeMonDurLabel,
-					self.timesAtStartDropdown,
-					self.timeTextEditableField,
-					self.timeMonitorDurationsDropdown,
-				}
-
-				self.dropdowns = {
-					self.timesAtStartDropdown,
-					self.timeMonitorDurationsDropdown,
-				}
-
-				return self
 			end,
 					
 			draw = function(self)
@@ -174,23 +180,23 @@ return {
 			end,
 
 			anyListIsVisible = function(self)
-				for _, dropdown in ipairs(self.dropdowns) do
-					if dropdown:isListVisible() then return true end
+				for _, dropDown in ipairs(self.dropDowns) do
+					if dropDown:isListVisible() then return true end
 				end
 			end,
 
 			hideLists = function(self)
-				for _, dropdown in ipairs(self.dropdowns) do
-					dropdown:hideList()
+				for _, dropDown in ipairs(self.dropDowns) do
+					dropDown:hideList()
 				end
 			end,
 
 			anyListContainsPoint = function(self, mx, my)
-				for _, dropdown in ipairs(self.dropdowns) do
-					if dropdown:listContainsPoint(mx, my) then return true end
+				for _, dropDown in ipairs(self.dropDowns) do
+					if dropDown:listContainsPoint(mx, my) then return true end
 				end
 			end,
-		}):init()
+		}):init(params)
 	end,
 }
 
