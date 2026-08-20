d8fb6eac5c9e1dac6ecf3da71474a035f80d923f
Matthew Hopkins <matthop88@gmail.com>
2026-08-20 13:22:34 -0700

    Updated ringsButton.lua to bring up ringsPanel

--- tools/constructionSet/miscellaneous/rings/ringsButton.lua
+++ tools/constructionSet/miscellaneous/rings/ringsButton.lua
@@ -2,7 +2,14 @@ return {
 	create = function(self, params)
 		return require("tools/constructionSet/miscellaneous/panelButton"):create {
 			label       = "Rings",
-			createPanel = nil,
+			createPanel = function(self)
+				return require("tools/constructionSet/miscellaneous/rings/ringsPanel"):create {
+					x     = 300,
+					y     = 150,
+					w     = 830,
+					h     = 400,
+				}
+			end,
 		}
 	end,
 }
