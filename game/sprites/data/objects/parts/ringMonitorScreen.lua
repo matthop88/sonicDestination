ca7e53925eb2b207b09e318b5e89cf80b9604e82
Matthew Hopkins <matthop88@gmail.com>
2026-08-21 11:48:00 -0700

    Added ringMonitorScreen.lua

---
+++ game/sprites/data/objects/parts/ringMonitorScreen.lua
@@ -0,0 +1,41 @@
+local FACE_GRAVITY = 120
+local RISE_SPEED   = 120
+            
+return {
+    imageName = "commonObj",
+    animations  = {
+        flashing = { fps = 48, isDefault = true, offset = { x = 8, y = 11 }, w = 16, h = 16,
+            ------------------------------------------------------------------------------
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 24, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 24, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 48, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 48, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 72, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+        },
+        exploding = { fps = 5, offset = { x = 8, y = 11 }, w = 16, h = 16,
+            reps = 1, terminal = true, endingFrame = 6,
+            calculateOffsets = function(frameNumber)
+                local dt           = (frameNumber - 1) / 5
+                local gravModifier = ((FACE_GRAVITY    / 2) * dt * dt)
+                local yOffset      = (RISE_SPEED * dt) - gravModifier
+                return 8, 11 + yOffset
+            end,
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            { x = 96, y = 540, w = 16, h = 16, offset = { x = 8, y = 11 }, },
+            {},
+        },  
+    },
+}
