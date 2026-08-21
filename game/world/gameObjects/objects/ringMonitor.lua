2f23d4b618243f1c4ac896da33d06c6e319c1935
Matthew Hopkins <matthop88@gmail.com>
2026-08-21 15:32:49 -0700

    Utilizing double ring sound in ringMonitor

--- game/world/gameObjects/objects/ringMonitor.lua
+++ game/world/gameObjects/objects/ringMonitor.lua
@@ -1,9 +1,8 @@
 local SOUND_MANAGER  = requireRelative("sound/soundManager")
 
 local onDestroy = function(self)
-	SOUND_MANAGER:play("ringCollectR")
-    SOUND_MANAGER:play("ringCollectL")
-	self.player:incrementRingCount(10)
+	SOUND_MANAGER:play("doubleRing")
+    self.player:incrementRingCount(10)
 end
 
 return {
