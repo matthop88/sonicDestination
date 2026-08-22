427bcdcee987be9214e8f63b1aa6d5ed284ec796
Matthew Hopkins <matthop88@gmail.com>
2026-08-21 17:41:37 -0700

    Updated constructionSetApp.lua to include 5 new CC Past chunks

--- tools/constructionSet/constructionSetApp.lua
+++ tools/constructionSet/constructionSetApp.lua
@@ -21,7 +21,7 @@ local STICKY_MOUSE    = require("tools/constructionSet/stickyMouse"):create(MAP)
 local CHUNKS_PANEL    = require("tools/constructionSet/panels/chunksPanel"):create(STICKY_MOUSE, { 10, 19, 20, 4, 13, 23, 25, 24, 39, 35, 34, 37, 7, 27, 28, 17 })
 local CHUNKS_2_PANEL  = require("tools/constructionSet/panels/chunksPanel"):create(STICKY_MOUSE, { 98, 99, 30, 31, 100, 63, 101, 102, 103, 104 })
 local CHUNKS_3_PANEL  = require("tools/constructionSet/panels/chunksPanel"):create(STICKY_MOUSE, { 74, 75, 72, 7, 80, 47, 43 })
-local CHUNKS_4_PANEL  = require("tools/constructionSet/panels/chunksPanel"):create(STICKY_MOUSE, { 4, 5 })
+local CHUNKS_4_PANEL  = require("tools/constructionSet/panels/chunksPanel"):create(STICKY_MOUSE, { 9, 22, 40, 56, 57, 4, 5 })
 local BADNIKS_PANEL   = require("tools/constructionSet/panels/badniksPanel"):create( { "motobug" },              STICKY_MOUSE)
 local BADNIKS_2_PANEL = require("tools/constructionSet/panels/badniksPanel"):create( { "patabata", "tamabboh" }, STICKY_MOUSE)
 
