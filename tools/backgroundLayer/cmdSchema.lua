8623288e0101d072c48b593cd32611a7dfbbc9cb
Matthew Hopkins <matthop88@gmail.com>
2026-08-25 10:49:53 -0700

    Added cmdSchema.lua to backgroundLayer

---
+++ tools/backgroundLayer/cmdSchema.lua
@@ -0,0 +1,17 @@
+return {
+	VERSION = 0.1,
+
+	COMMANDS = {
+		backgroundImage = {
+			description = "background image to slice",
+			shortcut = "i",
+			required = false,
+		},
+
+		backgroundTestData = {
+			description = "background data to test",
+			shortcut = "t",
+			required = false,
+		},
+	},
+}
